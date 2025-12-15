d486177580676e1aad9c941c639a09f4004f57e5
coolbeatz71 <sigmacool@gmail.com>
2025-12-15 03:54:20 +0200

    bug: remove NewPasswordMustBeDifferent
    this is done at the handler level

--- src/Modules/Auth/Auth/Application/Admin/UseCases/Commands/ChangePassword/AdminChangePasswordValidator.cs
+++ src/Modules/Auth/Auth/Application/Admin/UseCases/Commands/ChangePassword/AdminChangePasswordValidator.cs
@@ -24,9 +24,5 @@ public partial class AdminChangePasswordValidator : AbstractValidator<AdminChang
 
         // New password validation - strong password requirements
         RuleFor(x => x.NewPassword).PasswordValidation(fieldName: "New password");
-
-        // Cross-field validation - new password must be different from old password
-        RuleFor(x => x).NewPasswordMustBeDifferent();
     }
-
 }
