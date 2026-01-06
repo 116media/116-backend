a0ccfa2bc1b52ccc17e1bd94c5e09b205e3c4344
coolbeatz71 <sigmacool@gmail.com>
2026-01-06 11:20:46 +0200

    refactor: update AdminGetSessionMetricsEndpointV1 response model

--- src/Modules/Identity/Identity/Application/Session/UseCases/Admin/Queries/GetSessionMetrics/V1/AdminGetSessionMetricsEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/Session/UseCases/Admin/Queries/GetSessionMetrics/V1/AdminGetSessionMetricsEndpointV1.cs
@@ -3,9 +3,7 @@ using _116.Identity.Application.Shared.Authorizations.Policies;
 using _116.Identity.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using _116.Shared.Contracts.Application.CQRS;
-
 using Carter;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -15,13 +13,17 @@ namespace _116.Identity.Application.Session.UseCases.Admin.Queries.GetSessionMet
 /// <summary>
 /// Response model for session metrics.
 /// </summary>
-/// <param name="ClientPlatforms">Session counts grouped by client platform.</param>
-/// <param name="DeviceTypes">Session counts grouped by device type.</param>
+/// <param name="Browsers">Session counts grouped by browser type.</param>
+/// <param name="Devices">Session counts grouped by device type.</param>
+/// <param name="Platforms">Session counts grouped by platform/OS.</param>
+/// <param name="Clients">Session counts grouped by client application type.</param>
 /// <param name="TotalActiveSessions">Total number of active sessions.</param>
 /// <param name="TotalActiveUsers">Total number of unique active users.</param>
 public record AdminGetSessionMetricsResponse(
-    ClientPlatformMetrics ClientPlatforms,
-    DeviceTypeMetrics DeviceTypes,
+    BrowserMetrics Browsers,
+    DeviceMetrics Devices,
+    PlatformMetrics Platforms,
+    ClientMetrics Clients,
     int TotalActiveSessions,
     int TotalActiveUsers
 );
@@ -39,24 +41,29 @@ public class AdminGetSessionMetricsEndpointV1 : ICarterModule
     /// <param name="app">The route builder used to register API endpoints.</param>
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        RouteGroupBuilder group = app
-            .MapApiVersionGroup(1)
+        RouteGroupBuilder group = app.MapApiVersionGroup(1)
             .MapGroup($"{IdentityConstants.Admin}/{SessionRouteConstants.Endpoint}")
             .WithTags($"{IdentityConstants.Admin}::{SessionRouteConstants.Endpoint}");
 
-        group.MapGet(SessionRouteConstants.Metrics, async (IDispatcher dispatcher) =>
-            {
-                var query = new AdminGetSessionMetricsQuery();
-                AdminGetSessionMetricsResult result = await dispatcher.Send(request: query);
+        group
+            .MapGet(
+                SessionRouteConstants.Metrics,
+                async (IDispatcher dispatcher) =>
+                {
+                    var query = new AdminGetSessionMetricsQuery();
+                    AdminGetSessionMetricsResult result = await dispatcher.Send(request: query);
 
-                var response = new AdminGetSessionMetricsResponse(
-                    DeviceTypes: result.DeviceTypes,
-                    ClientPlatforms: result.ClientPlatforms,
-                    TotalActiveUsers: result.TotalActiveUsers,
-                    TotalActiveSessions: result.TotalActiveSessions
-                );
-                return Results.Ok(value: response);
-            })
+                    var response = new AdminGetSessionMetricsResponse(
+                        Browsers: result.Browsers,
+                        Devices: result.Devices,
+                        Platforms: result.Platforms,
+                        Clients: result.Clients,
+                        TotalActiveUsers: result.TotalActiveUsers,
+                        TotalActiveSessions: result.TotalActiveSessions
+                    );
+                    return Results.Ok(value: response);
+                }
+            )
             .WithName(endpointName: AdminGetSessionMetricsMetaField.AdminGetSessionMetrics.Name)
             .WithSummary(summary: AdminGetSessionMetricsMetaField.AdminGetSessionMetrics.Summary)
             .WithDescription(description: AdminGetSessionMetricsMetaField.AdminGetSessionMetrics.Description)
