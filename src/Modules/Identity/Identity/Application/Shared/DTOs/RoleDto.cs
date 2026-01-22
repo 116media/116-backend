37aea01eff3c31421a17d811b6cecd1f38222b70
coolbeatz71 <sigmacool@gmail.com>
2026-01-22 10:26:15 +0200

    feat: update RoleDto with status fields

--- src/Modules/Identity/Identity/Application/Shared/DTOs/RoleDto.cs
+++ src/Modules/Identity/Identity/Application/Shared/DTOs/RoleDto.cs
@@ -6,4 +6,7 @@ namespace _116.Identity.Application.Shared.DTOs;
 /// <param name="Id">The unique identifier of the role</param>
 /// <param name="Name">Name of the role (e.g., "Admin", "Editor")</param>
 /// <param name="Description">Human-readable description of the role's purpose</param>
-public record RoleDto(Guid Id, string Name, string Description);
+/// <param name="IsActive">Indicates whether the role is active and can be assigned</param>
+/// <param name="IsDeleted">Indicates whether the role has been soft-deleted</param>
+/// <param name="DeletedAt">Date and time when the role was soft-deleted</param>
+public record RoleDto(Guid Id, string Name, string Description, bool IsActive, bool IsDeleted, DateTime? DeletedAt);
