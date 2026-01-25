ca7c5df9c3d0731c47d312089bd84a47676618ad
coolbeatz71 <sigmacool@gmail.com>
2026-01-25 10:30:00 +0200

    feat: create AdminHardDeletePermissionCommand

---
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/HardDeletePermission/AdminHardDeletePermissionCommand.cs
@@ -0,0 +1,15 @@
+using _116.Shared.Contracts.Application.CQRS;
+
+namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.HardDeletePermission;
+
+/// <summary>
+/// Command for permanently deleting a permission.
+/// </summary>
+/// <param name="PermissionId">The unique identifier of the permission to permanently delete.</param>
+public record AdminHardDeletePermissionCommand(Guid PermissionId) : ICommand<AdminHardDeletePermissionResult>;
+
+/// <summary>
+/// Result of the <see cref="AdminHardDeletePermissionCommand" />.
+/// </summary>
+/// <param name="Success">Indicates whether the permission was successfully deleted.</param>
+public record AdminHardDeletePermissionResult(bool Success);
