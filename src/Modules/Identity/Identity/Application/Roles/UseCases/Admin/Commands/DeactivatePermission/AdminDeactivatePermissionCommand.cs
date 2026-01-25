68a4c43179062a178a827d13a0075e56929dc93a
coolbeatz71 <sigmacool@gmail.com>
2026-01-25 09:10:00 +0200

    feat: create AdminDeactivatePermissionCommand

---
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivatePermission/AdminDeactivatePermissionCommand.cs
@@ -0,0 +1,16 @@
+using _116.Identity.Application.Shared.DTOs;
+using _116.Shared.Contracts.Application.CQRS;
+
+namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.DeactivatePermission;
+
+/// <summary>
+/// Command for deactivating a permission.
+/// </summary>
+/// <param name="PermissionId">The unique identifier of the permission to deactivate.</param>
+public record AdminDeactivatePermissionCommand(Guid PermissionId) : ICommand<AdminDeactivatePermissionResult>;
+
+/// <summary>
+/// Result of the <see cref="AdminDeactivatePermissionCommand" /> containing the deactivated permission details.
+/// </summary>
+/// <param name="Permission">The deactivated permission information.</param>
+public record AdminDeactivatePermissionResult(PermissionDto Permission);
