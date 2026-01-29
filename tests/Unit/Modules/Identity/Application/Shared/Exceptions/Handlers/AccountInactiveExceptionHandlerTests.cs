6320fa5a4c277172650f058539b8321033dc613e
coolbeatz71 <sigmacool@gmail.com>
2026-01-29 19:26:10 +0200

    test(identity): add account inactive exception handler tests

---
+++ tests/Unit/Modules/Identity/Application/Shared/Exceptions/Handlers/AccountInactiveExceptionHandlerTests.cs
@@ -0,0 +1,29 @@
+using _116.Identity.Application.Shared.Exceptions;
+using _116.Identity.Application.Shared.Exceptions.Handlers;
+using AwesomeAssertions;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.Shared.Exceptions.Handlers;
+
+public class AccountInactiveExceptionHandlerTests
+{
+    private readonly AccountInactiveExceptionHandler _handler = new();
+
+    [Fact]
+    public void ExceptionType_ShouldReturnAccountInactiveExceptionType()
+    {
+        _handler.ExceptionType.Should().Be(typeof(AccountInactiveException));
+    }
+
+    [Fact]
+    public void CreateProblemDetails_ShouldReturn423StatusCode()
+    {
+        var exception = new AccountInactiveException("Account inactive");
+        var context = new DefaultHttpContext { Request = { Path = "/api/test" } };
+
+        var problemDetails = _handler.CreateProblemDetails(exception, context);
+
+        problemDetails.Status.Should().Be(StatusCodes.Status423Locked);
+    }
+}
