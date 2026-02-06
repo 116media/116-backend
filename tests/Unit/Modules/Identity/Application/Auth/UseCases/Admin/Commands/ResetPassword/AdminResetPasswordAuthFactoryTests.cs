b431cde40e4378a4fade1baa397bbd199c455481
coolbeatz71 <sigmacool@gmail.com>
2026-02-06 09:15:00 +0200

    refactor(test): use AwesomeAssertions in AdminResetPasswordAuthFactoryTests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResetPassword/AdminResetPasswordAuthFactoryTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResetPassword/AdminResetPasswordAuthFactoryTests.cs
@@ -57,7 +57,7 @@ public class AdminResetPasswordAuthFactoryTests
 
         // Assert
         result.Should().NotBeNull();
-        Assert.Equal(user, result.User);
+        result.User.Should().Be(user);
     }
 
     [Fact]
@@ -180,7 +180,7 @@ public class AdminResetPasswordAuthFactoryTests
 
         // Assert
         result.Should().NotBeNull();
-        Assert.Equal(user, result);
+        result.Should().Be(user);
     }
 
     [Fact]
