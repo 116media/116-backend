31e2366ae08226d397ade95100c42439f6f2684f
coolbeatz71 <sigmacool@gmail.com>
2026-02-08 10:17:56 +0200

    test: inherit BaseHandlerTest to eliminate mapper duplication

--- tests/Unit/Modules/Identity/Application/Roles/Admin/Commands/ActivateRole/AdminActivateRoleHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/Admin/Commands/ActivateRole/AdminActivateRoleHandlerTests.cs
@@ -3,6 +3,7 @@ using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
 using _116.Shared.Application.Exceptions;
+using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Builders.Entities;
 using _116.Unit.Tests.Common.Constants;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
@@ -16,7 +17,7 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Roles.Admin.Commands.Acti
 /// <summary>
 /// Unit tests for <see cref="AdminActivateRoleHandler"/>.
 /// </summary>
-public class AdminActivateRoleHandlerTests
+public class AdminActivateRoleHandlerTests : BaseHandlerTest
 {
     private readonly Mock<IRoleRepository> _roleRepositoryMock;
     private readonly Mock<IIdentityUnitOfWork> _unitOfWorkMock;
@@ -26,7 +27,8 @@ public class AdminActivateRoleHandlerTests
     {
         _roleRepositoryMock = MockRoleRepository.Create();
         _unitOfWorkMock = MockIdentityUnitOfWork.Create();
-        _handler = new AdminActivateRoleHandler(_roleRepositoryMock.Object, _unitOfWorkMock.Object);
+
+        _handler = new AdminActivateRoleHandler(_roleRepositoryMock.Object, _unitOfWorkMock.Object, Mapper);
     }
 
     #region Success Cases
