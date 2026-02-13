5402b40931f4e9931edebc9575458686c09a708b
coolbeatz71 <sigmacool@gmail.com>
2026-02-13 23:12:51 +0200

    refactor(test): replace var with explicit types in MethodNotAllowedExceptionHandlerTests

--- tests/Unit/Shared/Exceptions/Handlers/Strategies/MethodNotAllowedExceptionHandlerTests.cs
+++ tests/Unit/Shared/Exceptions/Handlers/Strategies/MethodNotAllowedExceptionHandlerTests.cs
@@ -3,6 +3,7 @@ using _116.Shared.Application.Exceptions.Handlers.Strategies;
 using _116.Unit.Tests.Common.Helpers;
 using AwesomeAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace _116.Unit.Tests.Shared.Exceptions.Handlers.Strategies;
@@ -38,7 +39,7 @@ public class MethodNotAllowedExceptionHandlerTests
         DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
 
         // Act
-        var problemDetails = _handler.CreateProblemDetails(exception, context);
+        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
         problemDetails.Title.Should().Be(nameof(MethodNotAllowedException));
@@ -53,7 +54,7 @@ public class MethodNotAllowedExceptionHandlerTests
         DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
 
         // Act
-        var problemDetails = _handler.CreateProblemDetails(exception, context);
+        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
         problemDetails.Detail.Should().Be(errorMessage);
@@ -67,7 +68,7 @@ public class MethodNotAllowedExceptionHandlerTests
         DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
 
         // Act
-        var problemDetails = _handler.CreateProblemDetails(exception, context);
+        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
         problemDetails.Status.Should().Be(StatusCodes.Status405MethodNotAllowed);
@@ -83,7 +84,7 @@ public class MethodNotAllowedExceptionHandlerTests
         context.Request.Path = requestPath;
 
         // Act
-        var problemDetails = _handler.CreateProblemDetails(exception, context);
+        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
         problemDetails.Instance.Should().Be(requestPath);
@@ -99,7 +100,7 @@ public class MethodNotAllowedExceptionHandlerTests
         context.TraceIdentifier = traceId;
 
         // Act
-        var problemDetails = _handler.CreateProblemDetails(exception, context);
+        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
         problemDetails.Extensions.Should().ContainKey("traceId");
@@ -114,7 +115,7 @@ public class MethodNotAllowedExceptionHandlerTests
         DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
 
         // Act
-        var problemDetails = _handler.CreateProblemDetails(exception, context);
+        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
         problemDetails.Extensions.Should().ContainKey("timestamp");
