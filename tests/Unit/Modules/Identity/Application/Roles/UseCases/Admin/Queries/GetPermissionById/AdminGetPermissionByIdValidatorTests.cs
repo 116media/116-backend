211d1a2c7c9f87382352ec50114e642643e2e6ea
coolbeatz71 <sigmacool@gmail.com>
2026-02-17 15:30:00 +0000

    refactor(test): update AdminGetPermissionByIdValidatorTests to use _116.Tests.Fixtures namespace

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Queries/GetPermissionById/AdminGetPermissionByIdValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Queries/GetPermissionById/AdminGetPermissionByIdValidatorTests.cs
@@ -1,5 +1,5 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Queries.GetPermissionById;
-using _116.Unit.Tests.Common.Constants;
+using _116.Tests.Fixtures.Constants;
 using AwesomeAssertions;
 using FluentValidation.Results;
 using Xunit;
