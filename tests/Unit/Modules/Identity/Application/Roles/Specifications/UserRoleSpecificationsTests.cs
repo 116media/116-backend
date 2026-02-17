0a5b7ed1dabe135e893ad8cd29efb49f75dda154
coolbeatz71 <sigmacool@gmail.com>
2026-02-17 11:20:00 +0000

    refactor(test): update UserRoleSpecificationsTests to use _116.Tests.Fixtures namespace

--- tests/Unit/Modules/Identity/Application/Roles/Specifications/UserRoleSpecificationsTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/Specifications/UserRoleSpecificationsTests.cs
@@ -1,8 +1,8 @@
 using _116.Identity.Application.Roles.Specifications;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
-using _116.Unit.Tests.Common.Builders.Entities;
-using _116.Unit.Tests.Common.Factories;
+using _116.Tests.Fixtures.Builders.Entities;
+using _116.Tests.Fixtures.Factories;
 using AwesomeAssertions;
 using Xunit;
 
