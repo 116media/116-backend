a2f97c7630dd7887d97248c3cfa674fb3069a751
coolbeatz71 <sigmacool@gmail.com>
2026-02-17 10:00:00 +0000

    refactor(test): update PublicSocialLoginAuthFactoryTests to use _116.Tests.Fixtures namespace

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/SocialLogin/PublicSocialLoginAuthFactoryTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/SocialLogin/PublicSocialLoginAuthFactoryTests.cs
@@ -8,7 +8,7 @@ using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
 using _116.Identity.Domain.ValueObjects;
-using _116.Unit.Tests.Common.Factories;
+using _116.Tests.Fixtures.Factories;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
