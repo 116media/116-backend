cf6a9e470e7cb8ea9091425b02bbf713cd48e548
coolbeatz71 <sigmacool@gmail.com>
2026-02-18 13:20:00 +0000

    refactor(test): update UserEntityTests to use _116.Tests.Fixtures namespace

--- tests/Unit/Modules/Identity/Domain/Entities/UserEntityTests.cs
+++ tests/Unit/Modules/Identity/Domain/Entities/UserEntityTests.cs
@@ -1,9 +1,9 @@
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
 using _116.Shared.Application.Exceptions;
-using _116.Unit.Tests.Common.Builders.Entities;
-using _116.Unit.Tests.Common.Constants;
-using _116.Unit.Tests.Common.Factories;
+using _116.Tests.Fixtures.Builders.Entities;
+using _116.Tests.Fixtures.Constants;
+using _116.Tests.Fixtures.Factories;
 using AwesomeAssertions;
 using Xunit;
 
