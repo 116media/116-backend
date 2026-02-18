efbccac6b9aa2ed2439229588cc3d4fd19b005e8
coolbeatz71 <sigmacool@gmail.com>
2026-02-18 14:00:00 +0000

    refactor(test): update OtpRepositoryTests to use _116.Tests.Fixtures namespace

--- tests/Unit/Modules/Identity/Infrastructure/Repositories/OtpRepositoryTests.cs
+++ tests/Unit/Modules/Identity/Infrastructure/Repositories/OtpRepositoryTests.cs
@@ -4,7 +4,7 @@ using _116.Identity.Domain.Enums;
 using _116.Identity.Infrastructure.Persistence;
 using _116.Identity.Infrastructure.Repositories;
 using _116.Shared.Application.Exceptions;
-using _116.Unit.Tests.Common.Factories;
+using _116.Tests.Fixtures.Factories;
 using AwesomeAssertions;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
