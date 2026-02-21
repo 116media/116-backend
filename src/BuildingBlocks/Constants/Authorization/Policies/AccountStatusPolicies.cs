1c3c6158851603030ff56008bdacc6d95444526d
coolbeatz71 <sigmacool@gmail.com>
2026-02-21 08:32:00 +0200

    feat(buildingblocks): add AccountStatusPolicies authorization constants

--- src/BuildingBlocks/Constants/Authorization/Policies/AccountStatusPolicies.cs
+++ src/BuildingBlocks/Constants/Authorization/Policies/AccountStatusPolicies.cs
@@ -1,4 +1,4 @@
-namespace _116.Identity.Application.Shared.Authorizations.Policies;
+namespace _116.BuildingBlocks.Constants.Authorization.Policies;
 
 /// <summary>
 /// Defines authorization policy names for accounts status-based access control.
