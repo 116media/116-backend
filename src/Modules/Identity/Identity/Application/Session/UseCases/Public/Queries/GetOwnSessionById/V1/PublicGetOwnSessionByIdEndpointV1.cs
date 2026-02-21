bf81d9b8ef8d257f5c6f8d219585eae8bdfaa2ae
coolbeatz71 <sigmacool@gmail.com>
2026-02-21 15:36:28 +0200

    refactor(identity): update PublicGetOwnSessionById auth policies import

--- src/Modules/Identity/Identity/Application/Session/UseCases/Public/Queries/GetOwnSessionById/V1/PublicGetOwnSessionByIdEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/Session/UseCases/Public/Queries/GetOwnSessionById/V1/PublicGetOwnSessionByIdEndpointV1.cs
@@ -1,7 +1,7 @@
 using System.Security.Claims;
+using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Identity.Application.Session.Constants;
-using _116.Identity.Application.Shared.Authorizations.Policies;
 using _116.Identity.Application.Shared.DTOs;
 using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Constants;
