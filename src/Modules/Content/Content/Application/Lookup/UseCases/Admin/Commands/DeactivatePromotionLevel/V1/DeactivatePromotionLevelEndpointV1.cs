5b7c9ec2e092a82312a5856bf238f86dfb00e3bf
coolbeatz71 <sigmacool@gmail.com>
2026-02-21 19:25:44 +0200

    feat(content): add authorization to DeactivatePromotionLevel endpoint:    - add BuildingBlocks Authorization.Policies using reference    - require active user via AccountStatusPolicies    - restrict access to SuperAdmin only via UserRolePolicies

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePromotionLevel/V1/DeactivatePromotionLevelEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePromotionLevel/V1/DeactivatePromotionLevelEndpointV1.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
 using _116.Content.Domain.Constants;
@@ -35,7 +36,8 @@ public class DeactivatePromotionLevelEndpointV1 : ICarterModule
             .WithName(endpointName: DeactivatePromotionLevelMetaField.DeactivatePromotionLevel.Name)
             .WithSummary(summary: DeactivatePromotionLevelMetaField.DeactivatePromotionLevel.Summary)
             .WithDescription(description: DeactivatePromotionLevelMetaField.DeactivatePromotionLevel.Description)
-            .RequireAuthorization()
+            .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
+            .RequireAuthorization(UserRolePolicies.RequireSuperAdminOnly)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .Produces(statusCode: StatusCodes.Status204NoContent)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
