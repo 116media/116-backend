7f460872c05d17cc1517cebe21b5202cf87ce933
coolbeatz71 <sigmacool@gmail.com>
2026-02-21 18:39:41 +0200

    feat(content): add authorization to DeactivatePricingTier endpoint:    - add BuildingBlocks Authorization.Policies using reference    - require active user via AccountStatusPolicies    - restrict access to SuperAdmin only via UserRolePolicies

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/V1/DeactivatePricingTierEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/V1/DeactivatePricingTierEndpointV1.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
 using _116.Content.Domain.Constants;
@@ -35,7 +36,8 @@ public class DeactivatePricingTierEndpointV1 : ICarterModule
             .WithName(endpointName: DeactivatePricingTierMetaField.DeactivatePricingTier.Name)
             .WithSummary(summary: DeactivatePricingTierMetaField.DeactivatePricingTier.Summary)
             .WithDescription(description: DeactivatePricingTierMetaField.DeactivatePricingTier.Description)
-            .RequireAuthorization()
+            .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
+            .RequireAuthorization(UserRolePolicies.RequireSuperAdminOnly)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .Produces(statusCode: StatusCodes.Status204NoContent)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
