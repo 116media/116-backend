ac5b75cfe17712246dd2446a7a08d69b4a8f65bb
coolbeatz71 <sigmacool@gmail.com>
2026-02-21 18:28:13 +0200

    feat(content): add authorization to ActivatePricingTier endpoint:    - add BuildingBlocks Authorization.Policies using reference    - require active user via AccountStatusPolicies    - restrict access to SuperAdmin only via UserRolePolicies

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/V1/ActivatePricingTierEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/V1/ActivatePricingTierEndpointV1.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
 using _116.Content.Domain.Constants;
@@ -35,7 +36,8 @@ public class ActivatePricingTierEndpointV1 : ICarterModule
             .WithName(endpointName: ActivatePricingTierMetaField.ActivatePricingTier.Name)
             .WithSummary(summary: ActivatePricingTierMetaField.ActivatePricingTier.Summary)
             .WithDescription(description: ActivatePricingTierMetaField.ActivatePricingTier.Description)
-            .RequireAuthorization()
+            .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
+            .RequireAuthorization(UserRolePolicies.RequireSuperAdminOnly)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .Produces(statusCode: StatusCodes.Status204NoContent)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
