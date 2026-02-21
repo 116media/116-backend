26afc3d8d7c23f9f9bf5e2ff1eccde5fcaf75e6d
coolbeatz71 <sigmacool@gmail.com>
2026-02-21 17:53:48 +0200

    feat(content): add authorization to ActivateContentType endpoint:    - add BuildingBlocks Authorization.Policies using reference    - require active user via AccountStatusPolicies    - restrict access to AdminOrSuperAdmin via UserRolePolicies

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/ActivateContentTypeEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/ActivateContentTypeEndpointV1.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
 using _116.Content.Domain.Constants;
@@ -35,7 +36,8 @@ public class ActivateContentTypeEndpointV1 : ICarterModule
             .WithName(endpointName: ActivateContentTypeMetaField.ActivateContentType.Name)
             .WithSummary(summary: ActivateContentTypeMetaField.ActivateContentType.Summary)
             .WithDescription(description: ActivateContentTypeMetaField.ActivateContentType.Description)
-            .RequireAuthorization()
+            .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
+            .RequireAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .Produces(statusCode: StatusCodes.Status204NoContent)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
