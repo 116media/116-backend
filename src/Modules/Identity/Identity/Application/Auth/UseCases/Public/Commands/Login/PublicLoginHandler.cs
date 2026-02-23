08e48963c0c824c03c66710061dc16b35b4564cc
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 16:59:51 +0200

    refactor(auth): derive roles and permissions from UserRoles in PublicLoginHandler

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/Login/PublicLoginHandler.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/Login/PublicLoginHandler.cs
@@ -60,8 +60,8 @@ public class PublicLoginHandler(
         var avatarDto = avatarFile?.ToFileDto(mapper);
         var userDto = authData.User.ToUserResponseDto(
             mapper: mapper,
-            roles: authData.Roles,
-            permissions: authData.Permissions,
+            roles: authData.User.UserRoles.ToRoleDtos(mapper),
+            permissions: authData.User.UserRoles.ToPermissionDtos(mapper),
             avatar: avatarDto
         );
 
