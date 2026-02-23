869f899f7c2bbef61a021906e64f136b82fa2d70
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 14:01:33 +0200

    refactor(content): replace Adapt with ToPricingTierDto mapper in UpdatePricingTier handler

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/UpdatePricingTier/UpdatePricingTierHandler.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/UpdatePricingTier/UpdatePricingTierHandler.cs
@@ -1,10 +1,10 @@
-using _116.Content.Application.Shared.DTOs;
 using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Shared.Contracts.Application.CQRS;
-using Mapster;
+using MapsterMapper;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.UpdatePricingTier;
 
@@ -13,7 +13,8 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.UpdatePricingT
 /// </summary>
 /// <param name="lookupRepository">Repository for lookup data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-public class UpdatePricingTierHandler(ILookupRepository lookupRepository, IContentUnitOfWork unitOfWork)
+/// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
+public class UpdatePricingTierHandler(ILookupRepository lookupRepository, IContentUnitOfWork unitOfWork, IMapper mapper)
     : ICommandHandler<UpdatePricingTierCommand, UpdatePricingTierResult>
 {
     /// <inheritdoc />
@@ -41,8 +42,7 @@ public class UpdatePricingTierHandler(ILookupRepository lookupRepository, IConte
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
-        var dto = pricingTier.Adapt<PricingTierDto>();
-
+        var dto = pricingTier.ToPricingTierDto(mapper);
         return new UpdatePricingTierResult(PricingTier: dto);
     }
 }
