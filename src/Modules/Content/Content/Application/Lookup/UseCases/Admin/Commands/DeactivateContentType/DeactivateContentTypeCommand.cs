8debf66a73a7a6f1d75e0e239861c52387449594
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 12:51:38 +0200

    refactor(content): update DeactivateContentType command to return result

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivateContentType/DeactivateContentTypeCommand.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivateContentType/DeactivateContentTypeCommand.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.DTOs;
 using _116.Shared.Contracts.Application.CQRS;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.DeactivateContentType;
@@ -6,4 +7,10 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.DeactivateCont
 /// Command to deactivate a content type, preventing it from being used.
 /// </summary>
 /// <param name="Id">The unique identifier of the content type to deactivate.</param>
-public record DeactivateContentTypeCommand(Guid Id) : ICommand;
+public record DeactivateContentTypeCommand(Guid Id) : ICommand<DeactivateContentTypeResult>;
+
+/// <summary>
+/// Result returned after successfully deactivating a content type.
+/// </summary>
+/// <param name="ContentType">The updated content type information.</param>
+public record DeactivateContentTypeResult(ContentTypeDto ContentType);
