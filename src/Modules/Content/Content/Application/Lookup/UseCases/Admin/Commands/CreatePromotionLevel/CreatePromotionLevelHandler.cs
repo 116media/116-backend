9a4098ed20cb47d3774c9a01ddd08c80199cba67
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 12:30:40 +0200

    refactor(content): replace Adapt with ToPromotionLevelDto mapper in CreatePromotionLevel handler

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreatePromotionLevel/CreatePromotionLevelHandler.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreatePromotionLevel/CreatePromotionLevelHandler.cs
@@ -1,10 +1,10 @@
-using _116.Content.Application.Shared.DTOs;
 using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Shared.Contracts.Application.CQRS;
-using Mapster;
+using MapsterMapper;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.CreatePromotionLevel;
 
@@ -13,8 +13,12 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.CreatePromotio
 /// </summary>
 /// <param name="lookupRepository">Repository for lookup data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-public class CreatePromotionLevelHandler(ILookupRepository lookupRepository, IContentUnitOfWork unitOfWork)
-    : ICommandHandler<CreatePromotionLevelCommand, CreatePromotionLevelResult>
+/// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
+public class CreatePromotionLevelHandler(
+    ILookupRepository lookupRepository,
+    IContentUnitOfWork unitOfWork,
+    IMapper mapper
+) : ICommandHandler<CreatePromotionLevelCommand, CreatePromotionLevelResult>
 {
     /// <inheritdoc />
     public async Task<CreatePromotionLevelResult> Handle(
@@ -45,8 +49,7 @@ public class CreatePromotionLevelHandler(ILookupRepository lookupRepository, ICo
         );
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
-        var dto = promotionLevel.Adapt<PromotionLevelDto>();
-
+        var dto = promotionLevel.ToPromotionLevelDto(mapper);
         return new CreatePromotionLevelResult(PromotionLevel: dto);
     }
 }
