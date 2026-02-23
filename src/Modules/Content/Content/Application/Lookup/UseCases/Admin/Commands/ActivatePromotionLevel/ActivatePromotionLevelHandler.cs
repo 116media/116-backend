ec63208c346c9fabc6259ec235e41bfb64787c6a
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 11:44:35 +0200

    refactor(content): update ActivatePromotionLevel handler to return mapped result

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePromotionLevel/ActivatePromotionLevelHandler.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePromotionLevel/ActivatePromotionLevelHandler.cs
@@ -1,8 +1,10 @@
 using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Shared.Contracts.Application.CQRS;
+using MapsterMapper;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivatePromotionLevel;
 
@@ -11,11 +13,18 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivatePromot
 /// </summary>
 /// <param name="lookupRepository">Repository for lookup data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-public class ActivatePromotionLevelHandler(ILookupRepository lookupRepository, IContentUnitOfWork unitOfWork)
-    : ICommandHandler<ActivatePromotionLevelCommand>
+/// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
+public class ActivatePromotionLevelHandler(
+    ILookupRepository lookupRepository,
+    IContentUnitOfWork unitOfWork,
+    IMapper mapper
+) : ICommandHandler<ActivatePromotionLevelCommand, ActivatePromotionLevelResult>
 {
     /// <inheritdoc />
-    public async Task Handle(ActivatePromotionLevelCommand command, CancellationToken cancellationToken)
+    public async Task<ActivatePromotionLevelResult> Handle(
+        ActivatePromotionLevelCommand command,
+        CancellationToken cancellationToken
+    )
     {
         PromotionLevelEntity promotionLevel = await lookupRepository.GetPromotionLevelByIdOrThrowAsync(
             id: command.Id,
@@ -30,5 +39,8 @@ public class ActivatePromotionLevelHandler(ILookupRepository lookupRepository, I
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
+
+        var dto = promotionLevel.ToPromotionLevelDto(mapper);
+        return new ActivatePromotionLevelResult(PromotionLevel: dto);
     }
 }
