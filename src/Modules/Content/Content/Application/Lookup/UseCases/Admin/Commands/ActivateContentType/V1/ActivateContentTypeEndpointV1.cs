f52d83eeed3f6f1995e967273c4777339586d230
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 11:06:46 +0200

    refactor(content): update ActivateContentType endpoint to return 200 with mapped content type

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/ActivateContentTypeEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/ActivateContentTypeEndpointV1.cs
@@ -1,6 +1,7 @@
 using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
+using _116.Content.Application.Shared.DTOs;
 using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using _116.Shared.Contracts.Application.CQRS;
@@ -10,6 +11,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivateContentType.V1;
+
+/// <summary>Response model for a successful content type activation.</summary>
+/// <param name="ContentType">The updated content type information.</param>
+public record ActivateContentTypeResponse(ContentTypeDto ContentType);
 
 /// <summary>
 /// Defines the admin activate content type endpoint.
@@ -29,8 +34,8 @@ public class ActivateContentTypeEndpointV1 : ICarterModule
                 async (Guid id, IDispatcher dispatcher) =>
                 {
                     var command = new ActivateContentTypeCommand(Id: id);
-                    await dispatcher.Send(request: command);
-                    return Results.NoContent();
+                    ActivateContentTypeResult result = await dispatcher.Send(request: command);
+                    return Results.Ok(new ActivateContentTypeResponse(ContentType: result.ContentType));
                 }
             )
             .WithName(endpointName: ActivateContentTypeMetaField.ActivateContentType.Name)
@@ -39,7 +44,7 @@ public class ActivateContentTypeEndpointV1 : ICarterModule
             .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
             .RequireAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
-            .Produces(statusCode: StatusCodes.Status204NoContent)
+            .Produces<ActivateContentTypeResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
             .ProducesProblem(statusCode: StatusCodes.Status404NotFound)
