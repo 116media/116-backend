55f4b20116a9437a40192eb836317396a973a2f3
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 13:08:17 +0200

    refactor(content): update DeactivateContentType endpoint to return 200 with mapped content type

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivateContentType/V1/DeactivateContentTypeEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivateContentType/V1/DeactivateContentTypeEndpointV1.cs
@@ -1,6 +1,7 @@
 using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
+using _116.Content.Application.Shared.DTOs;
 using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using _116.Shared.Contracts.Application.CQRS;
@@ -10,6 +11,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.DeactivateContentType.V1;
+
+/// <summary>Response model for a successful content type deactivation.</summary>
+/// <param name="ContentType">The updated content type information.</param>
+public record DeactivateContentTypeResponse(ContentTypeDto ContentType);
 
 /// <summary>
 /// Defines the admin deactivate content type endpoint.
@@ -29,8 +34,8 @@ public class DeactivateContentTypeEndpointV1 : ICarterModule
                 async (Guid id, IDispatcher dispatcher) =>
                 {
                     var command = new DeactivateContentTypeCommand(Id: id);
-                    await dispatcher.Send(request: command);
-                    return Results.NoContent();
+                    DeactivateContentTypeResult result = await dispatcher.Send(request: command);
+                    return Results.Ok(new DeactivateContentTypeResponse(ContentType: result.ContentType));
                 }
             )
             .WithName(endpointName: DeactivateContentTypeMetaField.DeactivateContentType.Name)
@@ -39,7 +44,7 @@ public class DeactivateContentTypeEndpointV1 : ICarterModule
             .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
             .RequireAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
-            .Produces(statusCode: StatusCodes.Status204NoContent)
+            .Produces<DeactivateContentTypeResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
             .ProducesProblem(statusCode: StatusCodes.Status404NotFound)
