d2e10d4395155cd0c7e28b8bbb1185d07f943984
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 11:31:17 +0200

    refactor(content): update ActivatePricingTier endpoint to return 200 with mapped pricing tier

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/V1/ActivatePricingTierEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/V1/ActivatePricingTierEndpointV1.cs
@@ -1,6 +1,7 @@
 using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
+using _116.Content.Application.Shared.DTOs;
 using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using _116.Shared.Contracts.Application.CQRS;
@@ -10,6 +11,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivatePricingTier.V1;
+
+/// <summary>Response model for a successful pricing tier activation.</summary>
+/// <param name="PricingTier">The updated pricing tier information.</param>
+public record ActivatePricingTierResponse(PricingTierDto PricingTier);
 
 /// <summary>
 /// Defines the admin activate pricing tier endpoint.
@@ -29,8 +34,8 @@ public class ActivatePricingTierEndpointV1 : ICarterModule
                 async (Guid id, IDispatcher dispatcher) =>
                 {
                     var command = new ActivatePricingTierCommand(Id: id);
-                    await dispatcher.Send(request: command);
-                    return Results.NoContent();
+                    ActivatePricingTierResult result = await dispatcher.Send(request: command);
+                    return Results.Ok(new ActivatePricingTierResponse(PricingTier: result.PricingTier));
                 }
             )
             .WithName(endpointName: ActivatePricingTierMetaField.ActivatePricingTier.Name)
@@ -39,7 +44,7 @@ public class ActivatePricingTierEndpointV1 : ICarterModule
             .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
             .RequireAuthorization(UserRolePolicies.RequireSuperAdminOnly)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
-            .Produces(statusCode: StatusCodes.Status204NoContent)
+            .Produces<ActivatePricingTierResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
             .ProducesProblem(statusCode: StatusCodes.Status404NotFound)
