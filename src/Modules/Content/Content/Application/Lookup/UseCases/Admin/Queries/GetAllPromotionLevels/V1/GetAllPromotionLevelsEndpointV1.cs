b50528f807608b89fffe87499eefaf9b9568d7d8
coolbeatz71 <sigmacool@gmail.com>
2026-02-23 09:41:09 +0200

    feat(content): add authorization to GetAllPromotionLevels endpoint:    - add BuildingBlocks Authorization.Policies using reference    - require active user via AccountStatusPolicies    - restrict access to AdminOrSuperAdmin via UserRolePolicies

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Queries/GetAllPromotionLevels/V1/GetAllPromotionLevelsEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Queries/GetAllPromotionLevels/V1/GetAllPromotionLevelsEndpointV1.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants.Authorization.Policies;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Lookup.Constants;
 using _116.Content.Application.Shared.DTOs;
@@ -47,7 +48,8 @@ public class GetAllPromotionLevelsEndpointV1 : ICarterModule
             .WithName(endpointName: GetAllPromotionLevelsMetaField.GetAllPromotionLevels.Name)
             .WithSummary(summary: GetAllPromotionLevelsMetaField.GetAllPromotionLevels.Summary)
             .WithDescription(description: GetAllPromotionLevelsMetaField.GetAllPromotionLevels.Description)
-            .RequireAuthorization()
+            .RequireAuthorization(AccountStatusPolicies.RequireActiveUser)
+            .RequireAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .Produces<GetAllPromotionLevelsResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
