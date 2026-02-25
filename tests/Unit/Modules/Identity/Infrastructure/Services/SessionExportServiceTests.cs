e28cf06b594fb1f6f0c91c7cc964e81db6e1e2b1
coolbeatz71 <sigmacool@gmail.com>
2026-02-25 12:54:38 +0200

    refactor(test): update SessionExportServiceTests import to use shared DTOs namespace

--- tests/Unit/Modules/Identity/Infrastructure/Services/SessionExportServiceTests.cs
+++ tests/Unit/Modules/Identity/Infrastructure/Services/SessionExportServiceTests.cs
@@ -1,4 +1,4 @@
-using _116.Identity.Application.Session.UseCases.Admin.Queries.ExportSessionData;
+using _116.Identity.Application.Shared.DTOs;
 using _116.Identity.Domain.Enums;
 using _116.Identity.Infrastructure.Services;
 using AwesomeAssertions;
