1455356fa30ba5d8edcd284859e8873b44773c81
coolbeatz71 <sigmacool@gmail.com>
2026-03-01 10:42:31 +0200

    refactor(identity): replace RequireAuthorization with WithAuthorization in AdminRestoreRole endpoint

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestoreRole/V1/AdminRestoreRoleEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestoreRole/V1/AdminRestoreRoleEndpointV1.cs
@@ -51,7 +51,7 @@ public class AdminRestoreRoleEndpointV1 : ICarterModule
             .WithName(endpointName: AdminRestoreRoleMetaField.AdminRestoreRole.Name)
             .WithSummary(summary: AdminRestoreRoleMetaField.AdminRestoreRole.Summary)
             .WithDescription(description: AdminRestoreRoleMetaField.AdminRestoreRole.Description)
-            .RequireAuthorization(UserRolePolicies.RequireSuperAdminOnly)
+            .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .ProducesValidationProblem()
             .Produces<AdminRestoreRoleResponse>()
