303ac2d6daaf040ef42b2727fb3ad46fbe60e201
coolbeatz71 <sigmacool@gmail.com>
2026-03-01 12:50:16 +0200

    refactor(identity): replace RequireAuthorization with WithAuthorization in AdminAssignRoleToUser endpoint

--- src/Modules/Identity/Identity/Application/User/UseCases/Admin/Commands/AssignRoleToUser/V1/AdminAssignRoleToUserEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/User/UseCases/Admin/Commands/AssignRoleToUser/V1/AdminAssignRoleToUserEndpointV1.cs
@@ -56,7 +56,7 @@ public class AdminAssignRoleToUserEndpointV1 : ICarterModule
             .WithName(endpointName: AdminAssignRoleToUserMetaField.AdminAssignRoleToUser.Name)
             .WithSummary(summary: AdminAssignRoleToUserMetaField.AdminAssignRoleToUser.Summary)
             .WithDescription(description: AdminAssignRoleToUserMetaField.AdminAssignRoleToUser.Description)
-            .RequireAuthorization(UserRolePolicies.RequireSuperAdminOnly)
+            .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
             .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
             .ProducesValidationProblem()
             .Produces<AdminAssignRoleToUserResponse>()
