8d1455024af6c9bb8ffbde8b76ac962284fceef1
coolbeatz71 <sigmacool@gmail.com>
2026-03-03 08:28:44 +0200

    test(identity): add RolePermissionRepository tests for bulk-update paths

--- tests/Unit/Modules/Identity/Infrastructure/Repositories/RolePermissionRepositoryTests.cs
+++ tests/Unit/Modules/Identity/Infrastructure/Repositories/RolePermissionRepositoryTests.cs
@@ -1,7 +1,6 @@
 using _116.Identity.Domain.Entities;
 using _116.Identity.Infrastructure.Persistence;
 using _116.Identity.Infrastructure.Repositories;
-using _116.Tests.Fixtures.Builders.Entities;
 using _116.Tests.Fixtures.Factories;
 using AwesomeAssertions;
 using Microsoft.EntityFrameworkCore;
