7a8124c4ca010850edc8b8e1761cb73419177fde
coolbeatz71 <sigmacool@gmail.com>
2026-03-09 17:08:47 +0200

    test(identity): add AdminGetAllSessions endpoint v1 tests

---
+++ tests/Unit/Modules/Identity/Application/Session/UseCases/Admin/Queries/GetAllSessions/V1/AdminGetAllSessionsEndpointV1Tests.cs
@@ -0,0 +1,35 @@
+using _116.Identity.Application.Session.UseCases.Admin.Queries.GetAllSessions.V1;
+using _116.Identity.Application.Shared.DTOs;
+using _116.Shared.Application.Pagination;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.Session.UseCases.Admin.Queries.GetAllSessions.V1;
+
+public class AdminGetAllSessionsEndpointV1Tests
+{
+    private static SessionDto CreateSessionDto() =>
+        new(
+            Guid.NewGuid(),
+            "127.0.0.1",
+            "Mozilla/5.0",
+            "Chrome",
+            "Desktop",
+            "Windows",
+            "WebApp",
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddDays(1),
+            true
+        );
+
+    [Fact]
+    public void AdminGetAllSessionsResponse_ShouldConstructCorrectly()
+    {
+        var paginated = new PaginatedResult<SessionDto>(1, 10, 1, [CreateSessionDto()]);
+
+        var response = new AdminGetAllSessionsResponse(Sessions: paginated);
+
+        response.Should().NotBeNull();
+        response.Sessions.Should().Be(paginated);
+    }
+}
