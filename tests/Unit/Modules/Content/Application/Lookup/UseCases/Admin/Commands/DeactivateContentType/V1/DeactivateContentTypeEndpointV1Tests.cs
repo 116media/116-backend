b27066da4668548ae28d461348f75d7f2c5eedec
coolbeatz71 <sigmacool@gmail.com>
2026-03-09 08:44:12 +0200

    test(content): add DeactivateContentType endpoint v1 tests

---
+++ tests/Unit/Modules/Content/Application/Lookup/UseCases/Admin/Commands/DeactivateContentType/V1/DeactivateContentTypeEndpointV1Tests.cs
@@ -0,0 +1,28 @@
+using _116.Content.Application.Lookup.UseCases.Admin.Commands.DeactivateContentType.V1;
+using _116.Content.Application.Shared.DTOs;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Content.Application.Lookup.UseCases.Admin.Commands.DeactivateContentType.V1;
+
+/// <summary>
+/// Unit tests for <see cref="DeactivateContentTypeResponse"/>.
+/// </summary>
+public class DeactivateContentTypeEndpointV1Tests
+{
+    [Fact]
+    public void DeactivateContentTypeResponse_ShouldConstructCorrectly()
+    {
+        // Arrange
+        ContentTypeDto contentType = CreateContentTypeDto();
+
+        // Act
+        var response = new DeactivateContentTypeResponse(ContentType: contentType);
+
+        // Assert
+        response.ContentType.Should().NotBeNull();
+        response.ContentType.Should().Be(contentType);
+    }
+
+    private static ContentTypeDto CreateContentTypeDto() => new(Guid.NewGuid(), "Article", true);
+}
