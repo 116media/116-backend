f53a66e7ed1e57ac2d418d3d69a1cde873ff9f4a
coolbeatz71 <sigmacool@gmail.com>
2026-03-09 15:42:17 +0200

    test(content): add PackageErrors tests for AlreadyExists and NotFound factories:
    - Assert AlreadyExists returns ConflictException with expected error message
    - Assert NotFoundById returns NotFoundException with expected error message

---
+++ tests/Unit/Modules/Content/Application/Shared/Errors/PackageErrorsTests.cs
@@ -0,0 +1,95 @@
+using _116.Content.Application.Shared.Errors;
+using _116.Shared.Application.Exceptions;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Content.Application.Shared.Errors;
+
+/// <summary>
+/// Unit tests for <see cref="PackageErrors"/>.
+/// </summary>
+public class PackageErrorsTests
+{
+    [Fact]
+    public void NotFound_WithId_ShouldReturnNotFoundException()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+
+        // Act
+        NotFoundException exception = PackageErrors.NotFound(id);
+
+        // Assert
+        exception.Should().BeOfType<NotFoundException>();
+        exception.Message.Should().Contain(id.ToString());
+    }
+
+    [Fact]
+    public void AlreadyActive_ShouldReturnConflictException()
+    {
+        // Act
+        ConflictException exception = PackageErrors.AlreadyActive();
+
+        // Assert
+        exception.Should().BeOfType<ConflictException>();
+        exception.Message.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void AlreadyInactive_ShouldReturnConflictException()
+    {
+        // Act
+        ConflictException exception = PackageErrors.AlreadyInactive();
+
+        // Assert
+        exception.Should().BeOfType<ConflictException>();
+        exception.Message.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void NameRequired_ShouldReturnBadRequestException()
+    {
+        // Act
+        BadRequestException exception = PackageErrors.NameRequired();
+
+        // Assert
+        exception.Should().BeOfType<BadRequestException>();
+        exception.Message.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void PriceMustBeNonNegative_ShouldReturnBadRequestException()
+    {
+        // Act
+        BadRequestException exception = PackageErrors.PriceMustBeNonNegative();
+
+        // Assert
+        exception.Should().BeOfType<BadRequestException>();
+        exception.Message.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void SlotQuantityMustBePositive_ShouldReturnBadRequestException()
+    {
+        // Act
+        BadRequestException exception = PackageErrors.SlotQuantityMustBePositive();
+
+        // Assert
+        exception.Should().BeOfType<BadRequestException>();
+        exception.Message.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void SlotNotFound_WithSlotId_ShouldReturnNotFoundException()
+    {
+        // Arrange
+        Guid slotId = Guid.NewGuid();
+
+        // Act
+        NotFoundException exception = PackageErrors.SlotNotFound(slotId);
+
+        // Assert
+        exception.Should().BeOfType<NotFoundException>();
+        exception.Message.Should().Contain(slotId.ToString());
+    }
+}
