ead928f26de71351bf7f89f90527b0f0acdbbc0c
coolbeatz71 <sigmacool@gmail.com>
2026-03-09 16:54:22 +0200

    test(identity): add AdminExportSessionData endpoint v1 tests

---
+++ tests/Unit/Modules/Identity/Application/Session/UseCases/Admin/Queries/ExportSessionData/V1/AdminExportSessionDataEndpointV1Tests.cs
@@ -0,0 +1,37 @@
+using _116.Identity.Application.Session.UseCases.Admin.Queries.ExportSessionData.V1;
+using _116.Identity.Application.Shared.DTOs;
+using _116.Identity.Domain.Enums;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.Session.UseCases.Admin.Queries.ExportSessionData.V1;
+
+public class AdminExportSessionDataEndpointV1Tests
+{
+    [Fact]
+    public void AdminExportSessionDataResponse_ShouldConstructCorrectly()
+    {
+        var data = new List<SessionExportDto>
+        {
+            new(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "127.0.0.1",
+                "Mozilla/5.0",
+                default,
+                default,
+                default,
+                default,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddDays(1),
+                true,
+                null
+            ),
+        };
+
+        var response = new AdminExportSessionDataResponse(SessionData: data);
+
+        response.Should().NotBeNull();
+        response.SessionData.Should().ContainSingle();
+    }
+}
