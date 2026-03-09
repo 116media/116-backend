1d7ae42c000aa60e1b23d8ed88889136e91326fd
coolbeatz71 <sigmacool@gmail.com>
2026-03-09 18:20:52 +0200

    test(identity): add PublicGetOwnSessions endpoint v1 tests

---
+++ tests/Unit/Modules/Identity/Application/Session/UseCases/Public/Queries/GetOwnSessions/V1/PublicGetOwnSessionsEndpointV1Tests.cs
@@ -0,0 +1,34 @@
+using _116.Identity.Application.Session.UseCases.Public.Queries.GetOwnSessions.V1;
+using _116.Identity.Application.Shared.DTOs;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.Session.UseCases.Public.Queries.GetOwnSessions.V1;
+
+public class PublicGetOwnSessionsEndpointV1Tests
+{
+    private static SessionDto CreateSessionDto() =>
+        new(
+            Guid.NewGuid(),
+            "127.0.0.1",
+            "Mozilla/5.0",
+            "Chrome",
+            "Desktop",
+            "Windows",
+            "WebApp",
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddDays(1),
+            true
+        );
+
+    [Fact]
+    public void PublicGetOwnSessionsResponse_ShouldConstructCorrectly()
+    {
+        IReadOnlyCollection<SessionDto> sessions = [CreateSessionDto()];
+
+        var response = new PublicGetOwnSessionsResponse(Sessions: sessions);
+
+        response.Should().NotBeNull();
+        response.Sessions.Should().HaveCount(1);
+    }
+}
