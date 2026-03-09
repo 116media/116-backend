99ca5bfbd752ebd653173d53620709a82806f050
coolbeatz71 <sigmacool@gmail.com>
2026-03-09 17:37:37 +0200

    test(identity): add PublicRefreshToken endpoint v1 tests

---
+++ tests/Unit/Modules/Identity/Application/Session/UseCases/Public/Commands/RefreshToken/V1/PublicRefreshTokenEndpointV1Tests.cs
@@ -0,0 +1,49 @@
+using _116.Identity.Application.Session.UseCases.Public.Commands.RefreshToken.V1;
+using _116.Identity.Application.Shared.DTOs;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.Session.UseCases.Public.Commands.RefreshToken.V1;
+
+public class PublicRefreshTokenEndpointV1Tests
+{
+    private static UserResponseDto CreateUserResponseDto() =>
+        new(
+            Guid.NewGuid(),
+            "user@example.com",
+            "testuser",
+            [],
+            [],
+            "Local",
+            true,
+            true,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+
+    [Fact]
+    public void PublicRefreshTokenResponse_ShouldConstructCorrectly()
+    {
+        UserResponseDto user = CreateUserResponseDto();
+        const string tokenType = "Bearer";
+
+        var response = new PublicRefreshTokenResponse(
+            User: user,
+            AccessToken: "access-token",
+            AccessTokenExpiresAt: DateTime.UtcNow.AddMinutes(60),
+            RefreshToken: "refresh-token",
+            RefreshTokenExpiresAt: DateTime.UtcNow.AddDays(30),
+            TokenType: tokenType
+        );
+
+        response.Should().NotBeNull();
+        response.User.Should().Be(user);
+        response.TokenType.Should().Be(tokenType);
+    }
+}
