25838b97806d4ab7c7b11551d4054572380eaa3c
coolbeatz71 <sigmacool@gmail.com>
2026-03-10 19:39:53 +0200

    test(identity): add PublicUpdateAvatar endpoint v1 tests

---
+++ tests/Unit/Modules/Identity/Application/User/UseCases/Public/Commands/UpdateAvatar/V1/PublicUpdateAvatarEndpointV1Tests.cs
@@ -0,0 +1,40 @@
+using _116.Identity.Application.Shared.DTOs;
+using _116.Identity.Application.User.UseCases.Public.Commands.UpdateAvatar.V1;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.User.UseCases.Public.Commands.UpdateAvatar.V1;
+
+public class PublicUpdateAvatarEndpointV1Tests
+{
+    private static UserResponseDto CreateUserResponseDto() =>
+        new(
+            Guid.NewGuid(),
+            "user@example.com",
+            "testuser",
+            [],
+            [],
+            "Local",
+            true,
+            true,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+
+    [Fact]
+    public void PublicUpdateAvatarResponse_ShouldConstructCorrectly()
+    {
+        UserResponseDto user = CreateUserResponseDto();
+
+        var response = new PublicUpdateAvatarResponse(User: user);
+
+        response.Should().NotBeNull();
+        response.User.Should().NotBeNull();
+    }
+}
