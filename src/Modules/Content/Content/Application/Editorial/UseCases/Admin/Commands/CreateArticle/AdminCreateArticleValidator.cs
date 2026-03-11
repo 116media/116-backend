9b465c58b99710edbe150778be3a94de5cc31fcf
coolbeatz71 <sigmacool@gmail.com>
2026-03-11 08:53:14 +0200

    feat(content): add admin create article validator

---
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateArticle/AdminCreateArticleValidator.cs
@@ -0,0 +1,30 @@
+using _116.Content.Application.Shared.Validators;
+using FluentValidation;
+
+namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateArticle;
+
+/// <summary>
+/// Validator for the <see cref="AdminCreateArticleCommand" /> ensuring proper article draft creation data.
+/// </summary>
+public class AdminCreateArticleValidator : AbstractValidator<AdminCreateArticleCommand>
+{
+    /// <summary>
+    /// Configures validation rules for article draft creation.
+    /// </summary>
+    public AdminCreateArticleValidator()
+    {
+        RuleFor(x => x.CategoryId).ValidArticleCategoryId();
+        RuleFor(x => x.Title).ValidArticleTitle();
+        RuleFor(x => x.Slug).ValidArticleSlug();
+
+        RuleFor(x => x.OrderItemId)
+            .NotEmpty()
+            .When(x => x.CustomerId.HasValue)
+            .WithMessage("Order item ID is required when customer ID is provided.");
+
+        RuleFor(x => x.CustomerId)
+            .NotEmpty()
+            .When(x => x.OrderItemId.HasValue)
+            .WithMessage("Customer ID is required when order item ID is provided.");
+    }
+}
