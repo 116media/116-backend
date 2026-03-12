cdb5bea7a414cd6b29b83f79859e2452a0063e29
coolbeatz71 <sigmacool@gmail.com>
2026-03-12 02:53:26 +0200

    feat(content): add admin get all pricing tiers query

---
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Queries/GetAllPricingTiers/AdminGetAllPricingTiersQuery.cs
@@ -0,0 +1,15 @@
+using _116.Content.Application.Shared.DTOs;
+using _116.Shared.Contracts.Application.CQRS;
+
+namespace _116.Content.Application.Lookup.UseCases.Admin.Queries.GetAllPricingTiers;
+
+/// <summary>
+/// Query for retrieving all pricing tiers.
+/// </summary>
+public record AdminGetAllPricingTiersQuery : IQuery<AdminGetAllPricingTiersResult>;
+
+/// <summary>
+/// Result of the <see cref="AdminGetAllPricingTiersQuery" /> containing all pricing tiers.
+/// </summary>
+/// <param name="PricingTiers">The list of all pricing tiers.</param>
+public record AdminGetAllPricingTiersResult(IReadOnlyList<PricingTierDto> PricingTiers);
