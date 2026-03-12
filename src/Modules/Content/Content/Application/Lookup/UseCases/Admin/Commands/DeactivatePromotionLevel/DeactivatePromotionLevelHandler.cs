f31f9ab4f8957e1ed9afca0d03aa6cd7f29ab6b9
coolbeatz71 <sigmacool@gmail.com>
2026-03-12 06:14:16 +0000

    refactor(content): parse string Id to Guid in DeactivatePromotionLevelHandler

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePromotionLevel/DeactivatePromotionLevelHandler.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePromotionLevel/DeactivatePromotionLevelHandler.cs
@@ -26,8 +26,10 @@ public class DeactivatePromotionLevelHandler(
         CancellationToken cancellationToken
     )
     {
+        Guid id = Guid.Parse(command.Id);
+
         PromotionLevelEntity promotionLevel = await lookupRepository.GetPromotionLevelByIdOrThrowAsync(
-            id: command.Id,
+            id: id,
             cancellationToken: cancellationToken
         );
 
