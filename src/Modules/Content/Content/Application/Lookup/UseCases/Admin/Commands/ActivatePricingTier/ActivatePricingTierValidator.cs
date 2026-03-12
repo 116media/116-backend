a3c543a150d06dce04178100daa49b136b60113e
coolbeatz71 <sigmacool@gmail.com>
2026-03-12 02:06:59 +0000

    refactor(content): use IsValidGuid in ActivatePricingTierValidator

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/ActivatePricingTierValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/ActivatePricingTierValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Validators;
+using _116.Shared.Application.Extensions;
 using FluentValidation;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivatePricingTier;
@@ -13,6 +13,6 @@ public class ActivatePricingTierValidator : AbstractValidator<ActivatePricingTie
     /// </summary>
     public ActivatePricingTierValidator()
     {
-        RuleFor(x => x.Id).ValidPricingTierId();
+        RuleFor(x => x.Id).IsValidGuid("Pricing tier ID");
     }
 }
