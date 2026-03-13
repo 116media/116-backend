c974efbd7a00d846e217322d2afe92a9330207bd
coolbeatz71 <sigmacool@gmail.com>
2026-03-13 01:08:51 +0000

    refactor(identity): parse string Id to Guid in AdminRestorePermissionHandler

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestorePermission/AdminRestorePermissionHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestorePermission/AdminRestorePermissionHandler.cs
@@ -31,8 +31,10 @@ public class AdminRestorePermissionHandler(
         CancellationToken cancellationToken
     )
     {
+        Guid permissionId = Guid.Parse(input: command.PermissionId);
+
         PermissionEntity? permission = await permissionRepository.GetPermissionByIdOrThrowAsync(
-            permissionId: command.PermissionId,
+            permissionId: permissionId,
             cancellationToken: cancellationToken
         );
 
