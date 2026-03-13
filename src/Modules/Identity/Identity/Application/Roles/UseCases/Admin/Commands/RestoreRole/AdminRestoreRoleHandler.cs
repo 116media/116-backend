3b34a50dff666accc3618d1a31d76f312fdbe738
coolbeatz71 <sigmacool@gmail.com>
2026-03-13 01:56:07 +0000

    refactor(identity): parse string Id to Guid in AdminRestoreRoleHandler

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestoreRole/AdminRestoreRoleHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestoreRole/AdminRestoreRoleHandler.cs
@@ -28,8 +28,10 @@ public class AdminRestoreRoleHandler(IRoleRepository roleRepository, IIdentityUn
         CancellationToken cancellationToken
     )
     {
+        Guid roleId = Guid.Parse(input: command.RoleId);
+
         RoleEntity? role = await roleRepository.GetRoleByIdOrThrowAsync(
-            roleId: command.RoleId,
+            roleId: roleId,
             cancellationToken: cancellationToken
         );
 
