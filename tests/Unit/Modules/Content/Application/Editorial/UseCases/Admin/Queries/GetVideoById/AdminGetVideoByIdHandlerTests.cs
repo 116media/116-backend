7ec6771154e9ecdb67844776ec581b41982e2e08
coolbeatz71 <sigmacool@gmail.com>
2026-03-14 08:25:19 +0200

    test(content): add admin get video by id handler tests

---
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/AdminGetVideoByIdHandlerTests.cs
@@ -0,0 +1,62 @@
+using _116.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById;
+using _116.Content.Application.Shared.Repositories;
+using _116.Content.Domain.Entities;
+using _116.Shared.Application.Exceptions;
+using _116.Tests.Fixtures.Factories.Content;
+using _116.Unit.Tests.Common;
+using _116.Unit.Tests.Common.Mocks.Repositories;
+using AwesomeAssertions;
+using Moq;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById;
+
+/// <summary>
+/// Unit tests for <see cref="AdminGetVideoByIdHandler"/>.
+/// </summary>
+public class AdminGetVideoByIdHandlerTests : BaseContentHandlerTest
+{
+    private readonly Mock<IVideoRepository> _videoRepositoryMock;
+    private readonly AdminGetVideoByIdHandler _handler;
+
+    private static readonly Guid CategoryId = Guid.NewGuid();
+
+    public AdminGetVideoByIdHandlerTests()
+    {
+        _videoRepositoryMock = MockVideoRepository.Create();
+        _handler = new AdminGetVideoByIdHandler(_videoRepositoryMock.Object, Mapper);
+    }
+
+    [Fact]
+    public async Task Handle_WhenVideoExists_ShouldReturnVideoDetail()
+    {
+        // Arrange
+        CategoryEntity category = CategoryFactory.Create(CategoryId);
+        VideoEntity video = VideoFactory.CreateWithCategory(CategoryId, category);
+        var query = new AdminGetVideoByIdQuery(Id: video.Id.ToString());
+        _videoRepositoryMock.SetupGetByIdOrThrow(video);
+
+        // Act
+        AdminGetVideoByIdResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Video.Should().NotBeNull();
+        result.Video.Id.Should().Be(video.Id);
+    }
+
+    [Fact]
+    public async Task Handle_WhenVideoNotFound_ShouldThrowNotFoundException()
+    {
+        // Arrange
+        Guid nonExistentId = Guid.NewGuid();
+        var query = new AdminGetVideoByIdQuery(Id: nonExistentId.ToString());
+        _videoRepositoryMock.SetupGetByIdOrThrowNotFound(nonExistentId);
+
+        // Act
+        Func<Task> act = async () => await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<NotFoundException>();
+    }
+}
