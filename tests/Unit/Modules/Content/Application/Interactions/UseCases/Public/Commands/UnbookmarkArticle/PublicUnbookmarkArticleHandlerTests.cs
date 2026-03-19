953e18cea4a18023f98819c1f0a20cf853e26e63
coolbeatz71 <sigmacool@gmail.com>
2026-03-19 09:10:17 +0200

    test(interactions): add PublicUnbookmarkArticle handler tests

---
+++ tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/UnbookmarkArticle/PublicUnbookmarkArticleHandlerTests.cs
@@ -0,0 +1,91 @@
+using _116.Content.Application.Interactions.UseCases.Public.Commands.UnbookmarkArticle;
+using _116.Content.Application.Shared.Persistence;
+using _116.Content.Application.Shared.Repositories;
+using _116.Content.Domain.Entities;
+using _116.Shared.Application.Exceptions;
+using _116.Tests.Fixtures.Factories.Content;
+using _116.Unit.Tests.Common.Mocks.Infrastructure;
+using _116.Unit.Tests.Common.Mocks.Repositories;
+using AwesomeAssertions;
+using Moq;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Content.Application.Interactions.UseCases.Public.Commands.UnbookmarkArticle;
+
+/// <summary>
+/// Unit tests for <see cref="PublicUnbookmarkArticleHandler"/>.
+/// </summary>
+public class PublicUnbookmarkArticleHandlerTests
+{
+    private readonly Mock<IArticleRepository> _articleRepositoryMock;
+    private readonly Mock<IContentUnitOfWork> _unitOfWorkMock;
+    private readonly PublicUnbookmarkArticleHandler _handler;
+
+    private static readonly Guid CategoryId = Guid.NewGuid();
+
+    public PublicUnbookmarkArticleHandlerTests()
+    {
+        _articleRepositoryMock = MockArticleRepository.Create();
+        _unitOfWorkMock = MockContentUnitOfWork.Create();
+        _handler = new PublicUnbookmarkArticleHandler(_articleRepositoryMock.Object, _unitOfWorkMock.Object);
+    }
+
+    #region Success Cases
+
+    [Fact]
+    public async Task Handle_WhenArticleExistsAndBookmarked_ShouldRemoveBookmarkDecrementAndCommit()
+    {
+        // Arrange
+        ArticleEntity article = ArticleFactory.CreatePublished(CategoryId);
+        Guid userId = Guid.NewGuid();
+        var command = new PublicUnbookmarkArticleCommand(ArticleId: article.Id, UserId: userId);
+        _articleRepositoryMock.SetupGetByIdOrThrow(article);
+        _articleRepositoryMock.SetupHasBookmarkedAsync(true);
+
+        // Act
+        PublicUnbookmarkArticleResult result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        _articleRepositoryMock.VerifyRemoveBookmarkCalled(userId, article.Id);
+        _articleRepositoryMock.VerifyUpdateCalled();
+        _unitOfWorkMock.VerifyCommitCalled();
+    }
+
+    #endregion
+
+    #region Failure Cases
+
+    [Fact]
+    public async Task Handle_WhenArticleNotFound_ShouldThrowNotFoundException()
+    {
+        // Arrange
+        Guid articleId = Guid.NewGuid();
+        var command = new PublicUnbookmarkArticleCommand(ArticleId: articleId, UserId: Guid.NewGuid());
+        _articleRepositoryMock.SetupGetByIdOrThrowNotFound(articleId);
+
+        // Act
+        Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Fact]
+    public async Task Handle_WhenBookmarkNotFound_ShouldThrowBadRequestException()
+    {
+        // Arrange
+        ArticleEntity article = ArticleFactory.CreatePublished(CategoryId);
+        var command = new PublicUnbookmarkArticleCommand(ArticleId: article.Id, UserId: Guid.NewGuid());
+        _articleRepositoryMock.SetupGetByIdOrThrow(article);
+        _articleRepositoryMock.SetupHasBookmarkedAsync(false);
+
+        // Act
+        Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<BadRequestException>();
+    }
+
+    #endregion
+}
