05a257c575ba0256dab06882213c85d519dedaa5
coolbeatz71 <sigmacool@gmail.com>
2026-03-21 12:07:39 +0200

    refactor(catalog): add :guid constraint to GetPackageById route

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Queries/GetPackageById/V1/AdminGetPackageByIdEndpointV1.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Queries/GetPackageById/V1/AdminGetPackageByIdEndpointV1.cs
@@ -37,8 +37,8 @@ public class AdminGetPackageByIdEndpointV1 : ICarterModule
 
         group
             .MapGet(
-                "/{id}",
-                async (string id, IDispatcher dispatcher) =>
+                "/{id:guid}",
+                async (Guid id, IDispatcher dispatcher) =>
                 {
                     var query = new AdminGetPackageByIdQuery(Id: id);
 
