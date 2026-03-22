2245b8b4d8e3047ca646c53a5327ad762209e76f
coolbeatz71 <sigmacool@gmail.com>
2026-03-22 13:11:32 +0200

    test(session): add get own session by id endpoint tests

---
+++ tests/Unit/Modules/Identity/Application/Session/UseCases/Admin/Queries/GetOwnSessionById/V1/AdminGetOwnSessionByIdEndpointV1Tests.cs
@@ -0,0 +1,34 @@
+using _116.Identity.Application.Session.UseCases.Admin.Queries.GetOwnSessionById.V1;
+using _116.Identity.Application.Shared.DTOs;
+using _116.Identity.Domain.Enums;
+using AwesomeAssertions;
+using Xunit;
+
+namespace _116.Unit.Tests.Modules.Identity.Application.Session.UseCases.Admin.Queries.GetOwnSessionById.V1;
+
+public class AdminGetOwnSessionByIdEndpointV1Tests
+{
+    private static SessionDto CreateSessionDto() =>
+        new(
+            Guid.NewGuid(),
+            "127.0.0.1",
+            "Mozilla/5.0",
+            EnumBrowser.Chrome,
+            EnumDevice.Desktop,
+            EnumPlatform.Windows,
+            EnumClient.WebApp,
+            DateTime.UtcNow.AddDays(1),
+            true
+        );
+
+    [Fact]
+    public void AdminGetOwnSessionByIdResponse_ShouldConstructCorrectly()
+    {
+        SessionDto session = CreateSessionDto();
+
+        var response = new AdminGetOwnSessionByIdResponse(Session: session);
+
+        response.Should().NotBeNull();
+        response.Session.Should().NotBeNull();
+    }
+}
