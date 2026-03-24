cd5647c3580a1df95733c6049c1de1e4f1896acd
coolbeatz71 <sigmacool@gmail.com>
2026-03-24 05:14:53 +0200

    refactor(session): update ISessionExportService to use EnumSessionExportFormat

--- src/Modules/Identity/Identity/Application/Session/Services/ISessionExportService.cs
+++ src/Modules/Identity/Identity/Application/Session/Services/ISessionExportService.cs
@@ -15,14 +15,14 @@ public interface ISessionExportService
     /// <param name="format">The export format.</param>
     /// <param name="columns">Optional list of column names to export. If null, export all columns.</param>
     /// <returns>File content as a byte array.</returns>
-    byte[] Export(List<SessionExportDto> sessions, SessionExportFormat format, List<string>? columns = null);
+    byte[] Export(List<SessionExportDto> sessions, EnumSessionExportFormat format, List<string>? columns = null);
 
     /// <summary>
     /// Gets the content type (MIME type) for the export format.
     /// </summary>
     /// <param name="format">The export format.</param>
     /// <returns>Content type string.</returns>
-    string GetContentType(SessionExportFormat format);
+    string GetContentType(EnumSessionExportFormat format);
 
     /// <summary>
     /// Generates a timestamped filename for the export.
@@ -30,5 +30,5 @@ public interface ISessionExportService
     /// <param name="format">The export format.</param>
     /// <param name="baseFileName">The base file name without extension. Uses constant if null.</param>
     /// <returns>Filename with timestamp and extension.</returns>
-    string GenerateFileName(SessionExportFormat format, string? baseFileName = null);
+    string GenerateFileName(EnumSessionExportFormat format, string? baseFileName = null);
 }
