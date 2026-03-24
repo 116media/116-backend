fbc19b851dd26bb7188242b2ae2c44fffa9417ed
coolbeatz71 <sigmacool@gmail.com>
2026-03-24 17:25:39 +0200

    test(editorial): use EnumContentStatus in PublicGetPublishedArticlesEndpointV1Tests

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPublishedArticles/V1/PublicGetPublishedArticlesEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPublishedArticles/V1/PublicGetPublishedArticlesEndpointV1Tests.cs
@@ -1,5 +1,6 @@
 using _116.Content.Application.Editorial.UseCases.Public.Queries.GetPublishedArticles.V1;
 using _116.Content.Application.Shared.DTOs;
+using _116.Content.Domain.Enums;
 using _116.Shared.Application.Pagination;
 using AwesomeAssertions;
 using Xunit;
@@ -35,7 +36,7 @@ public class PublicGetPublishedArticlesEndpointV1Tests
             Headline: "Test",
             CoverImageUrl: null,
             AuthorId: "Test",
-            Status: "Published",
+            Status: EnumContentStatus.Published,
             IsFeatured: false,
             PublishedAt: null
         );
