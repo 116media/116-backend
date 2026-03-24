b58e18160790dffc20665d3c2983f84a5302091b
coolbeatz71 <sigmacool@gmail.com>
2026-03-24 16:49:51 +0200

    test(editorial): use EnumContentStatus in PublicGetArticleBySlugEndpointV1Tests

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetArticleBySlug/V1/PublicGetArticleBySlugEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetArticleBySlug/V1/PublicGetArticleBySlugEndpointV1Tests.cs
@@ -1,5 +1,6 @@
 using _116.Content.Application.Editorial.UseCases.Public.Queries.GetArticleBySlug.V1;
 using _116.Content.Application.Shared.DTOs;
+using _116.Content.Domain.Enums;
 using AwesomeAssertions;
 using Xunit;
 
@@ -35,7 +36,7 @@ public class PublicGetArticleBySlugEndpointV1Tests
             Body: "Test",
             CoverImageUrl: null,
             AuthorId: "Test",
-            Status: "Published",
+            Status: EnumContentStatus.Published,
             RejectionReason: null,
             IsFeatured: false,
             FeaturedUntil: null,
