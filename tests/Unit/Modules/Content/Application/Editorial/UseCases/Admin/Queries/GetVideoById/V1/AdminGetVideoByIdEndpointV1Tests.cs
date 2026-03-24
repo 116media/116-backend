7297e5db69b669d0d26f5271334b85c63710c97e
coolbeatz71 <sigmacool@gmail.com>
2026-03-24 16:38:09 +0200

    test(editorial): use EnumContentStatus in AdminGetVideoByIdEndpointV1Tests

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
@@ -1,5 +1,6 @@
 using _116.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById.V1;
 using _116.Content.Application.Shared.DTOs;
+using _116.Content.Domain.Enums;
 using AwesomeAssertions;
 using Xunit;
 
@@ -35,7 +36,7 @@ public class AdminGetVideoByIdEndpointV1Tests
             ThumbnailUrl: null,
             ThumbnailStorageKey: null,
             AuthorId: "Test",
-            Status: "Draft",
+            Status: EnumContentStatus.Draft,
             RejectionReason: null,
             YoutubeVideoId: null,
             IsFeatured: false,
