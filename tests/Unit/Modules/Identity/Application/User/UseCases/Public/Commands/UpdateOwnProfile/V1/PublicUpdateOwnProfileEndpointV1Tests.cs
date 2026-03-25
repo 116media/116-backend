6551f953625241366b2ede2b9d00f37ebfe2240d
coolbeatz71 <sigmacool@gmail.com>
2026-03-25 11:35:23 +0200

    test(user): use EnumAuthProvider in PublicUpdateOwnProfileEndpointV1Tests

--- tests/Unit/Modules/Identity/Application/User/UseCases/Public/Commands/UpdateOwnProfile/V1/PublicUpdateOwnProfileEndpointV1Tests.cs
+++ tests/Unit/Modules/Identity/Application/User/UseCases/Public/Commands/UpdateOwnProfile/V1/PublicUpdateOwnProfileEndpointV1Tests.cs
@@ -1,5 +1,6 @@
 using _116.Identity.Application.Shared.DTOs;
 using _116.Identity.Application.User.UseCases.Public.Commands.UpdateOwnProfile.V1;
+using _116.Identity.Domain.Enums;
 using AwesomeAssertions;
 using Xunit;
 
@@ -14,7 +15,7 @@ public class PublicUpdateOwnProfileEndpointV1Tests
             "testuser",
             [],
             [],
-            "Local",
+            EnumAuthProvider.Local,
             true,
             true,
             null,
