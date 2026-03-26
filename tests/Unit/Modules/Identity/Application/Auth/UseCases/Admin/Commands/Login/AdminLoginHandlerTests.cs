7886f56b8c8ef46f3c055cb8498aa8947fc7cfc7
coolbeatz71 <sigmacool@gmail.com>
2026-03-26 10:38:11 +0200

    test(admin-login): update factory import path

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginHandlerTests.cs
@@ -1,7 +1,7 @@
 using _116.Core.Application.Shared.Repositories;
 using _116.Identity.Application.Auth.UseCases.Admin.Commands.Login;
 using _116.Identity.Application.Auth.UseCases.Admin.Commands.Login.Contracts;
-using _116.Identity.Application.Session.Factories;
+using _116.Identity.Application.Session.Factories.Contracts;
 using _116.Identity.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
