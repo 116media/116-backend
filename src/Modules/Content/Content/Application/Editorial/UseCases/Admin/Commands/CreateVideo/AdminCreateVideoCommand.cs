4bb87b5705168fd97a5479d61d3e3784bcd5bd67
coolbeatz71 <sigmacool@gmail.com>
2026-04-08 13:55:48 +0200

    refactor(create-video): make description non-nullable on command

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateVideo/AdminCreateVideoCommand.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateVideo/AdminCreateVideoCommand.cs
@@ -14,7 +14,7 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateVideo
 /// <param name="AuthorId">The identity user UUID read from JWT claims.</param>
 /// <param name="CustomerId">The B2B customer who commissioned this video. Null for free content.</param>
 /// <param name="OrderItemId">The order item this video fulfils. Null for free content.</param>
-/// <param name="Description">Optional description shown below the video player.</param>
+/// <param name="Description">The description shown below the video player.</param>
 /// <param name="ShootingScheduledAt">Optional scheduled shooting date for pre-booked productions.</param>
 public record AdminCreateVideoCommand(
     Guid CategoryId,
@@ -23,7 +23,7 @@ public record AdminCreateVideoCommand(
     Guid AuthorId,
     Guid? CustomerId,
     Guid? OrderItemId,
-    string? Description,
+    string Description,
     DateTimeOffset? ShootingScheduledAt
 ) : ICommand<AdminCreateVideoResult>;
 
