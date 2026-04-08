a068660902d512511e763cfcfc3b0644447f42d2
coolbeatz71 <sigmacool@gmail.com>
2026-04-08 21:01:09 +0200

    test(get-video-by-id): include description in dto fixture

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
@@ -32,7 +32,7 @@ public class AdminGetVideoByIdEndpointV1Tests
             CategoryName: "Test",
             Title: "Test",
             Slug: "test",
-            Description: null,
+            Description: "Test video description",
             ThumbnailUrl: null,
             ThumbnailStorageKey: null,
             AuthorId: "Test",
