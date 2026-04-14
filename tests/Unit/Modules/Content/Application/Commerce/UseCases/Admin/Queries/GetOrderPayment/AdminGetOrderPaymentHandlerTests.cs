3304b7b281dcd4fb4bda429ee4057eb3e988a52e
coolbeatz71 <sigmacool@gmail.com>
2026-04-14 10:22:48 +0200

    test(payment): cover verifier user name resolution in get order payment

--- tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetOrderPayment/AdminGetOrderPaymentHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetOrderPayment/AdminGetOrderPaymentHandlerTests.cs
@@ -3,11 +3,14 @@ using _116.Content.Application.Commerce.UseCases.Admin.Queries.GetOrderPayment;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
 using _116.Core.Domain.Entities;
+using _116.Identity.Contracts.Application;
+using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Factories;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -21,13 +24,20 @@ public class AdminGetOrderPaymentHandlerTests : BaseContentHandlerTest
 {
     private readonly Mock<IOrderPaymentFactory> _orderPaymentFactoryMock;
     private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IUserLookupService> _userLookupMock;
     private readonly AdminGetOrderPaymentHandler _handler;
 
     public AdminGetOrderPaymentHandlerTests()
     {
         _orderPaymentFactoryMock = MockOrderPaymentFactory.Create();
         _fileRepositoryMock = MockFileRepository.Create();
-        _handler = new AdminGetOrderPaymentHandler(_orderPaymentFactoryMock.Object, _fileRepositoryMock.Object, Mapper);
+        _userLookupMock = MockUserLookupService.Create();
+        _handler = new AdminGetOrderPaymentHandler(
+            _orderPaymentFactoryMock.Object,
+            _fileRepositoryMock.Object,
+            Mapper,
+            _userLookupMock.Object
+        );
     }
 
     #region Success Cases
@@ -74,6 +84,47 @@ public class AdminGetOrderPaymentHandlerTests : BaseContentHandlerTest
         result.Payment.PaymentProof.Should().BeNull();
 
         _fileRepositoryMock.Verify(x => x.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task Handle_WithVerifiedPayment_ShouldResolveVerifierUserName()
+    {
+        // Arrange
+        Guid orderId = Guid.NewGuid();
+        ContentPaymentEntity payment = ContentPaymentFactory.CreateVerified(orderId);
+        Guid verifierId = payment.VerifiedById!.Value;
+
+        _orderPaymentFactoryMock.SetupGetByOrderId(orderId, payment);
+        _userLookupMock.SetupGetUserNameById(verifierId, TestConstants.User.ValidUserName);
+
+        var query = new AdminGetOrderPaymentQuery(OrderId: orderId);
+
+        // Act
+        AdminGetOrderPaymentResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Payment.Should().NotBeNull();
+        result.Payment.VerifiedByUserName.Should().Be(TestConstants.User.ValidUserName);
+        _userLookupMock.VerifyGetUserNameByIdCalled(verifierId);
+    }
+
+    [Fact]
+    public async Task Handle_WithUnverifiedPayment_ShouldNotCallUserLookup()
+    {
+        // Arrange
+        Guid orderId = Guid.NewGuid();
+        ContentPaymentEntity payment = ContentPaymentFactory.Create(orderId);
+
+        _orderPaymentFactoryMock.SetupGetByOrderId(orderId, payment);
+
+        var query = new AdminGetOrderPaymentQuery(OrderId: orderId);
+
+        // Act
+        AdminGetOrderPaymentResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Payment.VerifiedByUserName.Should().BeNull();
+        _userLookupMock.VerifyGetUserNameByIdNotCalled();
     }
 
     #endregion
