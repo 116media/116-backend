b840897d2c1fbd9fd85c5346c04817735a9f7777
coolbeatz71 <sigmacool@gmail.com>
2026-04-14 10:08:24 +0200

    test(order): cover verifier user name resolution in get order by id

--- tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetOrderById/AdminGetOrderByIdHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetOrderById/AdminGetOrderByIdHandlerTests.cs
@@ -2,10 +2,15 @@ using _116.Content.Application.Commerce.UseCases.Admin.Queries.GetOrderById;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Domain.Entities;
+using _116.Identity.Contracts.Application;
 using _116.Shared.Application.Exceptions;
+using _116.Tests.Fixtures.Constants;
+using _116.Tests.Fixtures.Factories;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -19,13 +24,20 @@ public class AdminGetOrderByIdHandlerTests : BaseContentHandlerTest
 {
     private readonly Mock<IContentOrderRepository> _orderRepositoryMock;
     private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IUserLookupService> _userLookupMock;
     private readonly AdminGetOrderByIdHandler _handler;
 
     public AdminGetOrderByIdHandlerTests()
     {
         _orderRepositoryMock = MockContentOrderRepository.Create();
         _fileRepositoryMock = MockFileRepository.Create();
-        _handler = new AdminGetOrderByIdHandler(_orderRepositoryMock.Object, _fileRepositoryMock.Object, Mapper);
+        _userLookupMock = MockUserLookupService.Create();
+        _handler = new AdminGetOrderByIdHandler(
+            _orderRepositoryMock.Object,
+            _fileRepositoryMock.Object,
+            Mapper,
+            _userLookupMock.Object
+        );
     }
 
     #region Success Cases
@@ -47,6 +59,68 @@ public class AdminGetOrderByIdHandlerTests : BaseContentHandlerTest
         result.Order.Should().NotBeNull();
         result.Order.Id.Should().Be(order.Id);
         result.Order.Payment.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task Handle_WithVerifiedPayment_ShouldResolveVerifierUserName()
+    {
+        // Arrange
+        var orderId = Guid.NewGuid();
+        ContentOrderEntity order = ContentOrderFactory.CreateWithId(orderId);
+        ContentPaymentEntity payment = ContentPaymentFactory.CreateVerified(orderId);
+        Guid verifierId = payment.VerifiedById!.Value;
+        Guid proofFileId = Guid.NewGuid();
+
+        typeof(ContentOrderEntity).GetProperty(nameof(ContentOrderEntity.Payment))!.SetValue(order, payment);
+        typeof(ContentPaymentEntity)
+            .GetProperty(nameof(ContentPaymentEntity.PaymentProofFileId))!
+            .SetValue(payment, (Guid?)proofFileId);
+
+        FileEntity proofFile = FileFactory.CreateWithId(proofFileId);
+
+        _orderRepositoryMock.SetupGetByIdWithItems(order);
+        _fileRepositoryMock.SetupGetById(proofFile);
+        _userLookupMock.SetupGetUserNameById(verifierId, TestConstants.User.ValidUserName);
+
+        var query = new AdminGetOrderByIdQuery(Id: orderId);
+
+        // Act
+        AdminGetOrderByIdResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Order.Payment.Should().NotBeNull();
+        result.Order.Payment!.VerifiedByUserName.Should().Be(TestConstants.User.ValidUserName);
+        _userLookupMock.VerifyGetUserNameByIdCalled(verifierId);
+    }
+
+    [Fact]
+    public async Task Handle_WithUnverifiedPayment_ShouldNotCallUserLookup()
+    {
+        // Arrange
+        var orderId = Guid.NewGuid();
+        ContentOrderEntity order = ContentOrderFactory.CreateWithId(orderId);
+        ContentPaymentEntity payment = ContentPaymentFactory.Create(orderId);
+        Guid proofFileId = Guid.NewGuid();
+
+        typeof(ContentOrderEntity).GetProperty(nameof(ContentOrderEntity.Payment))!.SetValue(order, payment);
+        typeof(ContentPaymentEntity)
+            .GetProperty(nameof(ContentPaymentEntity.PaymentProofFileId))!
+            .SetValue(payment, (Guid?)proofFileId);
+
+        FileEntity proofFile = FileFactory.CreateWithId(proofFileId);
+
+        _orderRepositoryMock.SetupGetByIdWithItems(order);
+        _fileRepositoryMock.SetupGetById(proofFile);
+
+        var query = new AdminGetOrderByIdQuery(Id: orderId);
+
+        // Act
+        AdminGetOrderByIdResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Order.Payment.Should().NotBeNull();
+        result.Order.Payment!.VerifiedByUserName.Should().BeNull();
+        _userLookupMock.VerifyGetUserNameByIdNotCalled();
     }
 
     #endregion
