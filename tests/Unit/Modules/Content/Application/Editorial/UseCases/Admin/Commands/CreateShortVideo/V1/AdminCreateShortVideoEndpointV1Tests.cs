2642bcfc8a14b1688435bfb381c3073816d11789
coolbeatz71 <sigmacool@gmail.com>
2026-04-21 11:51:55 +0200

    fix(tests): add AuthorId to ShortVideoDto constructor

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/CreateShortVideo/V1/AdminCreateShortVideoEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/CreateShortVideo/V1/AdminCreateShortVideoEndpointV1Tests.cs
@@ -36,6 +36,7 @@ public class AdminCreateShortVideoEndpointV1Tests
             ViewCount: 0,
             LikeCount: 0,
             ShareCount: 0,
-            BookmarkCount: 0
+            BookmarkCount: 0,
+            AuthorId: Guid.NewGuid().ToString()
         );
 }
