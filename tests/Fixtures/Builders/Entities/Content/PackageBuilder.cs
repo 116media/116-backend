1423e49aea7b5f2b7faaedcbd4a880efb4efe09c
coolbeatz71 <sigmacool@gmail.com>
2026-04-25 05:52:46 +0200

    refactor(fixtures): remove FlatPriceUsd from builder

--- tests/Fixtures/Builders/Entities/Content/PackageBuilder.cs
+++ tests/Fixtures/Builders/Entities/Content/PackageBuilder.cs
@@ -15,7 +15,6 @@ internal class PackageBuilder
     private Guid _id;
     private string _name;
     private string _description = "Default package description";
-    private decimal _flatPriceUsd;
     private bool _isActive = true;
 
     /// <summary>
@@ -26,48 +25,50 @@ internal class PackageBuilder
         _id = Guid.NewGuid();
         string word = _faker.Lorem.Word();
         _name = word[..Math.Min(TestConstants.Content.Package.NameMaxLength, word.Length)];
-        _flatPriceUsd = _faker.Random.Decimal(0, 1000);
     }
 
-    /// <summary>Sets the package ID.</summary>
+    /// <summary>
+    /// Sets the package ID.
+    /// </summary>
     public PackageBuilder WithId(Guid id)
     {
         _id = id;
         return this;
     }
 
-    /// <summary>Sets the package name.</summary>
+    /// <summary>
+    /// Sets the package name.
+    /// </summary>
     public PackageBuilder WithName(string name)
     {
         _name = name;
         return this;
     }
 
-    /// <summary>Sets the package description.</summary>
+    /// <summary>
+    /// Sets the package description.
+    /// </summary>
     public PackageBuilder WithDescription(string description)
     {
         _description = description;
         return this;
     }
 
-    /// <summary>Sets the flat price in USD.</summary>
-    public PackageBuilder WithFlatPriceUsd(decimal flatPriceUsd)
-    {
-        _flatPriceUsd = flatPriceUsd;
-        return this;
-    }
-
-    /// <summary>Marks the package as inactive.</summary>
+    /// <summary>
+    /// Marks the package as inactive.
+    /// </summary>
     public PackageBuilder AsInactive()
     {
         _isActive = false;
         return this;
     }
 
-    /// <summary>Builds the <see cref="PackageEntity"/> instance.</summary>
+    /// <summary>
+    /// Builds the <see cref="PackageEntity"/> instance.
+    /// </summary>
     public PackageEntity Build()
     {
-        var entity = PackageEntity.Create(_id, _name, _description, _flatPriceUsd);
+        var entity = PackageEntity.Create(_id, _name, _description);
 
         if (!_isActive)
         {
