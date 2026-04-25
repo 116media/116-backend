f210101c128ee376abf7a0528e7aa6786d3c6a65
coolbeatz71 <sigmacool@gmail.com>
2026-04-25 11:38:53 +0200

    style: convert single-line XML summaries to multiline

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/AddVideoToPlaylist/PublicAddVideoToPlaylistHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/AddVideoToPlaylist/PublicAddVideoToPlaylistHandler.cs
@@ -31,47 +31,47 @@ public class PublicAddVideoToPlaylistHandler(
             cancellationToken: cancellationToken
         );
 
-        if (playlist is null)
+        if (playlist is not null)
         {
-            throw PlaylistErrors.NotFound(id: command.PlaylistId);
+            if (playlist.UserId != command.UserId)
+            {
+                throw PlaylistErrors.NotOwner();
+            }
+
+            VideoEntity video = await videoRepository.GetByIdOrThrowAsync(
+                id: command.VideoId,
+                cancellationToken: cancellationToken
+            );
+
+            if (video.Status != EnumContentStatus.Published)
+            {
+                throw VideoErrors.NotFound(id: command.VideoId);
+            }
+
+            bool alreadyExists = await playlistRepository.VideoExistsInPlaylistAsync(
+                playlistId: command.PlaylistId,
+                videoId: command.VideoId,
+                cancellationToken: cancellationToken
+            );
+
+            if (alreadyExists)
+            {
+                throw PlaylistErrors.VideoAlreadyInPlaylist();
+            }
+
+            var playlistVideo = PlaylistVideoEntity.Create(
+                id: Guid.NewGuid(),
+                playlistId: command.PlaylistId,
+                videoId: command.VideoId,
+                sortOrder: command.SortOrder
+            );
+
+            await playlistRepository.AddVideoAsync(playlistVideo: playlistVideo, cancellationToken: cancellationToken);
+            await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
+
+            return new PublicAddVideoToPlaylistResult(IsSuccess: true);
         }
 
-        if (playlist.UserId != command.UserId)
-        {
-            throw PlaylistErrors.NotOwner();
-        }
-
-        VideoEntity video = await videoRepository.GetByIdOrThrowAsync(
-            id: command.VideoId,
-            cancellationToken: cancellationToken
-        );
-
-        if (video.Status != EnumContentStatus.Published)
-        {
-            throw VideoErrors.NotFound(id: command.VideoId);
-        }
-
-        bool alreadyExists = await playlistRepository.VideoExistsInPlaylistAsync(
-            playlistId: command.PlaylistId,
-            videoId: command.VideoId,
-            cancellationToken: cancellationToken
-        );
-
-        if (alreadyExists)
-        {
-            throw PlaylistErrors.VideoAlreadyInPlaylist();
-        }
-
-        var playlistVideo = PlaylistVideoEntity.Create(
-            id: Guid.NewGuid(),
-            playlistId: command.PlaylistId,
-            videoId: command.VideoId,
-            sortOrder: command.SortOrder
-        );
-
-        await playlistRepository.AddVideoAsync(playlistVideo: playlistVideo, cancellationToken: cancellationToken);
-        await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
-
-        return new PublicAddVideoToPlaylistResult(IsSuccess: true);
+        throw PlaylistErrors.NotFound(id: command.PlaylistId);
     }
 }
