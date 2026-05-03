45e5b57a4720a3743b5d5ccf51b9329751034811
coolbeatz71 <sigmacool@gmail.com>
2026-05-03 07:54:03 +0200

    feat(update-video-tags): accept tag names instead of tag IDs

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideoTags/AdminUpdateVideoTagsCommand.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideoTags/AdminUpdateVideoTagsCommand.cs
@@ -5,10 +5,11 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.UpdateVideo
 /// <summary>
 /// Command for replacing the full set of tags assigned to a video.
 /// All existing tag associations are removed and replaced with the provided tag set.
+/// Tags that do not yet exist are automatically created by name.
 /// </summary>
 /// <param name="VideoId">The unique identifier of the video whose tags are being updated.</param>
-/// <param name="TagIds">The complete set of tag identifiers to assign to this video.</param>
-public record AdminUpdateVideoTagsCommand(string VideoId, IReadOnlyList<Guid> TagIds)
+/// <param name="TagNames">The complete set of tag display names to assign to this video.</param>
+public record AdminUpdateVideoTagsCommand(string VideoId, IReadOnlyList<string> TagNames)
     : ICommand<AdminUpdateVideoTagsResult>;
 
 /// <summary>
