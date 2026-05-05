280f0969dd3558810c0519793f1ac1ca9dae2aae
coolbeatz71 <sigmacool@gmail.com>
2026-05-05 22:17:52 +0200

    test(lyrics): inject author mocks in get all lyrics handler tests

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllLyrics/AdminGetAllLyricsHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllLyrics/AdminGetAllLyricsHandlerTests.cs
@@ -1,10 +1,13 @@
 using _116.Content.Application.Editorial.UseCases.Admin.Queries.GetAllLyrics;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
+using _116.Core.Application.Shared.Repositories;
+using _116.Identity.Contracts.Application;
 using _116.Shared.Application.Pagination;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -22,7 +25,14 @@ public class AdminGetAllLyricsHandlerTests : BaseContentHandlerTest
     public AdminGetAllLyricsHandlerTests()
     {
         _lyricsRepositoryMock = MockLyricsRepository.Create();
-        _handler = new AdminGetAllLyricsHandler(_lyricsRepositoryMock.Object, Mapper);
+        Mock<IUserLookupService> userLookupMock = MockUserLookupService.Create();
+        Mock<IFileRepository> fileRepositoryMock = MockFileRepository.Create();
+        _handler = new AdminGetAllLyricsHandler(
+            _lyricsRepositoryMock.Object,
+            userLookupMock.Object,
+            fileRepositoryMock.Object,
+            Mapper
+        );
     }
 
     [Fact]
