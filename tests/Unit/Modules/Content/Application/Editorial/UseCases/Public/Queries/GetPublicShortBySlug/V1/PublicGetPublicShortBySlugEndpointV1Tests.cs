0431617def2162dfe2cf02c5181d28806a163463
coolbeatz71 <sigmacool@gmail.com>
2026-05-05 15:14:18 +0200

    fix(tests): add VideoId param to ShortVideoDto in GetPublicShortBySlug test

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPublicShortBySlug/V1/PublicGetPublicShortBySlugEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPublicShortBySlug/V1/PublicGetPublicShortBySlugEndpointV1Tests.cs
@@ -31,6 +31,7 @@ public class PublicGetPublicShortBySlugEndpointV1Tests
             Slug: "test",
             VideoUrl: "Test",
             ThumbnailUrl: null,
+            VideoId: null,
             HasFullVideo: false,
             IsActive: false,
             ViewCount: 0,
