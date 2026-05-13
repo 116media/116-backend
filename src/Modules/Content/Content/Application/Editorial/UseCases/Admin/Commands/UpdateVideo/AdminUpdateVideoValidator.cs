9be2936292932c71baa9a5ce1c7969ad2d165787
coolbeatz71 <sigmacool@gmail.com>
2026-05-13 13:59:28 +0200

    refactor(update-video): use When block with ValidMetaTitle/ValidMetaDescription

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideo/AdminUpdateVideoValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideo/AdminUpdateVideoValidator.cs
@@ -27,8 +27,7 @@ public class AdminUpdateVideoValidator : AbstractValidator<AdminUpdateVideoComma
         When(x => x.CustomerId.HasValue, () => RuleFor(x => x.OrderItemId).ValidOrderItemId());
         When(x => x.OrderItemId.HasValue, () => RuleFor(x => x.CustomerId).ValidCustomerId());
 
-        RuleFor(x => x.MetaTitle).ValidOptionalMetaTitle(x => x.MetaTitle is not null);
-
-        RuleFor(x => x.MetaDescription).ValidOptionalMetaDescription(x => x.MetaDescription is not null);
+        When(x => x.MetaTitle is not null, () => RuleFor(x => x.MetaTitle).ValidMetaTitle());
+        When(x => x.MetaDescription is not null, () => RuleFor(x => x.MetaDescription).ValidMetaDescription());
     }
 }
