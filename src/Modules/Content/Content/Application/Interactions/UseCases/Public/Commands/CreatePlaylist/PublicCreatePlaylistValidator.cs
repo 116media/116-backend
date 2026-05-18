89a9f32379a126fdb59eff209d3b12f7cbf52372
coolbeatz71 <sigmacool@gmail.com>
2026-05-18 23:22:44 +0200

    refactor(validation): inject i18n messages into PublicCreatePlaylistValidator

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/CreatePlaylist/PublicCreatePlaylistValidator.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/CreatePlaylist/PublicCreatePlaylistValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -9,10 +10,11 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.CreateP
 public class PublicCreatePlaylistValidator : AbstractValidator<PublicCreatePlaylistCommand>
 {
     /// <summary>
-    /// Configures validation rules for creating a playlist.
+    /// Initializes a new instance of <see cref="PublicCreatePlaylistValidator" /> with the specified error message provider.
     /// </summary>
-    public PublicCreatePlaylistValidator()
+    /// <param name="msg">Playlist validation error messages.</param>
+    public PublicCreatePlaylistValidator(PlaylistErrorMessage msg)
     {
-        RuleFor(x => x.Name).ValidPlaylistName();
+        RuleFor(x => x.Name).ValidPlaylistName(msg);
     }
 }
