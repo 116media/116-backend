0ff269f83c13a1c85afe447151da8f14cf027358
coolbeatz71 <sigmacool@gmail.com>
2026-05-18 18:14:47 +0200

    refactor(validation): inject i18n messages into AdminCreateLyricsValidator

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateLyrics/AdminCreateLyricsValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateLyrics/AdminCreateLyricsValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -9,16 +10,17 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateLyric
 public class AdminCreateLyricsValidator : AbstractValidator<AdminCreateLyricsCommand>
 {
     /// <summary>
-    /// Configures validation rules for lyrics creation.
+    /// Initializes a new instance of <see cref="AdminCreateLyricsValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminCreateLyricsValidator()
+    /// <param name="msg">Lyrics validation error messages.</param>
+    public AdminCreateLyricsValidator(LyricsErrorMessage msg)
     {
-        RuleFor(x => x.SongTitle).ValidSongTitle();
+        RuleFor(x => x.SongTitle).ValidSongTitle(msg);
 
-        RuleFor(x => x.ArtistName).ValidArtistName();
+        RuleFor(x => x.ArtistName).ValidArtistName(msg);
 
-        RuleFor(x => x.LyricsText).ValidLyricsText();
+        RuleFor(x => x.LyricsText).ValidLyricsText(msg);
 
-        RuleFor(x => x.Language).ValidLyricsLanguage();
+        RuleFor(x => x.Language).ValidLyricsLanguage(msg);
     }
 }
