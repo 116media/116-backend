6364a2dc7b64c1cd434a4b96dacb6e65fcd94168
coolbeatz71 <sigmacool@gmail.com>
2026-05-18 17:46:05 +0200

    refactor(validation): inject i18n messages into AdminAttachYoutubeVideoUrlValidator

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/AttachYoutubeVideoUrl/AdminAttachYoutubeVideoUrlValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/AttachYoutubeVideoUrl/AdminAttachYoutubeVideoUrlValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
@@ -10,12 +11,13 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.AttachYoutu
 public class AdminAttachYoutubeVideoUrlValidator : AbstractValidator<AdminAttachYoutubeVideoUrlCommand>
 {
     /// <summary>
-    /// Configures validation rules for attaching a YouTube video Url.
+    /// Initializes a new instance of <see cref="AdminAttachYoutubeVideoUrlValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminAttachYoutubeVideoUrlValidator()
+    /// <param name="msg">Video validation error messages.</param>
+    public AdminAttachYoutubeVideoUrlValidator(VideoErrorMessage msg)
     {
         RuleFor(x => x.VideoId).IsValidGuid("Video ID");
 
-        RuleFor(x => x.YoutubeVideoUrl).ValidYoutubeVideoUrl();
+        RuleFor(x => x.YoutubeVideoUrl).ValidYoutubeVideoUrl(msg);
     }
 }
