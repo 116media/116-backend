de0e187c0dcb8b56a6dda378bd8ee3697cd2116b
coolbeatz71 <sigmacool@gmail.com>
2026-05-19 11:57:04 +0200

    refactor(validation): inject i18n messages into AdminCreateTagValidator

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreateTag/AdminCreateTagValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreateTag/AdminCreateTagValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -9,11 +10,12 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.CreateTag;
 public class AdminCreateTagValidator : AbstractValidator<AdminCreateTagCommand>
 {
     /// <summary>
-    /// Configures validation rules for tag creation.
+    /// Initializes a new instance of <see cref="AdminCreateTagValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminCreateTagValidator()
+    /// <param name="msg">Tag validation error messages.</param>
+    public AdminCreateTagValidator(TagErrorMessage msg)
     {
-        RuleFor(x => x.Name).ValidTagName();
-        RuleFor(x => x.Slug).ValidTagSlug();
+        RuleFor(x => x.Name).ValidTagName(msg);
+        RuleFor(x => x.Slug).ValidTagSlug(msg);
     }
 }
