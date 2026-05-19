4d5417aedf262e6710389a4e773069ef6a1606f4
coolbeatz71 <sigmacool@gmail.com>
2026-05-19 16:02:24 +0200

    refactor(i18n): convert PlaylistErrors to DI-injectable instance

--- src/Modules/Content/Content/Application/Shared/Errors/PlaylistErrors.cs
+++ src/Modules/Content/Content/Application/Shared/Errors/PlaylistErrors.cs
@@ -6,12 +6,12 @@ namespace _116.Content.Application.Shared.Errors;
 /// <summary>
 /// Playlist error factory providing simple, readable exception creation.
 /// </summary>
-public static class PlaylistErrors
+public class PlaylistErrors(PlaylistErrorMessage msg)
 {
     /// <summary>
     /// Throws when a playlist is not found by its identifier.
     /// </summary>
-    public static NotFoundException NotFound(Guid id)
+    public NotFoundException NotFound(Guid id)
     {
         return new NotFoundException("Playlist", "id", keyValue: id);
     }
@@ -19,16 +19,16 @@ public static class PlaylistErrors
     /// <summary>
     /// Throws when a user attempts to manage a playlist they do not own.
     /// </summary>
-    public static BadRequestException NotOwner()
+    public BadRequestException NotOwner()
     {
-        return new BadRequestException(PlaylistErrorMessage.NotOwner());
+        return new BadRequestException(msg.NotOwner());
     }
 
     /// <summary>
     /// Throws when a video is already present in the target playlist.
     /// </summary>
-    public static ConflictException VideoAlreadyInPlaylist()
+    public ConflictException VideoAlreadyInPlaylist()
     {
-        return new ConflictException(PlaylistErrorMessage.VideoAlreadyInPlaylist());
+        return new ConflictException(msg.VideoAlreadyInPlaylist());
     }
 }
