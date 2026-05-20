5bbb904aaa8be971632b659d09cfd6883ee71c82
coolbeatz71 <sigmacool@gmail.com>
2026-05-20 14:02:38 +0200

    refactor(validation): inject i18n messages into AdminCreateRoleValidator

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/CreateRole/AdminCreateRoleValidator.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/CreateRole/AdminCreateRoleValidator.cs
@@ -1,4 +1,5 @@
 using _116.Identity.Application.Auth.Validators;
+using _116.Identity.Application.Shared.Errors.Messages;
 using FluentValidation;
 
 namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.CreateRole;
@@ -9,11 +10,14 @@ namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.CreateRole;
 public class AdminCreateRoleValidator : AbstractValidator<AdminCreateRoleCommand>
 {
     /// <summary>
-    /// Configure validation rules for role creation.
+    /// Initializes a new instance of <see cref="AdminCreateRoleValidator" /> with validation rules.
     /// </summary>
-    public AdminCreateRoleValidator()
+    /// <param name="msg">
+    /// Validation error messages for rule configuration.
+    /// </param>
+    public AdminCreateRoleValidator(ValidationErrorMessage msg)
     {
-        RuleFor(x => x.Name).ValidRoleName();
-        RuleFor(x => x.Description).ValidRoleDescription();
+        RuleFor(x => x.Name).ValidRoleName(msg);
+        RuleFor(x => x.Description).ValidRoleDescription(msg);
     }
 }
