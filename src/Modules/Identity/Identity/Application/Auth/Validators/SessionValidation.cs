149b284ec457d68ab361dbf3e993270c93326415
coolbeatz71 <sigmacool@gmail.com>
2026-05-20 13:06:38 +0200

    refactor(validation): inject i18n messages into SessionValidation

--- src/Modules/Identity/Identity/Application/Auth/Validators/SessionValidation.cs
+++ src/Modules/Identity/Identity/Application/Auth/Validators/SessionValidation.cs
@@ -1,3 +1,4 @@
+using _116.Identity.Application.Shared.Errors.Messages;
 using FluentValidation;
 
 namespace _116.Identity.Application.Auth.Validators;
@@ -12,9 +13,13 @@ public static class SessionValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the refresh token property.</param>
+    /// <param name="msg">The validation error message provider.</param>
     /// <returns>The configured rule builder.</returns>
-    public static IRuleBuilderOptions<T, string> ValidRefreshToken<T>(this IRuleBuilderInitial<T, string> ruleBuilder)
+    public static IRuleBuilderOptions<T, string> ValidRefreshToken<T>(
+        this IRuleBuilderInitial<T, string> ruleBuilder,
+        ValidationErrorMessage msg
+    )
     {
-        return ruleBuilder.NotEmpty().WithMessage("Refresh token is required.");
+        return ruleBuilder.NotEmpty().WithMessage(msg.RefreshTokenRequired());
     }
 }
