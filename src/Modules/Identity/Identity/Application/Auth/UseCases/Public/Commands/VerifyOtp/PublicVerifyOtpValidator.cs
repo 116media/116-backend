cc1343910886de0fdabc1a46786f5ad1794caece
coolbeatz71 <sigmacool@gmail.com>
2026-05-20 12:17:11 +0200

    refactor(validation): inject i18n messages into PublicVerifyOtpValidator

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/VerifyOtp/PublicVerifyOtpValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/VerifyOtp/PublicVerifyOtpValidator.cs
@@ -1,4 +1,5 @@
 using _116.Identity.Application.Auth.Validators;
+using _116.Identity.Application.Shared.Errors.Messages;
 using FluentValidation;
 
 namespace _116.Identity.Application.Auth.UseCases.Public.Commands.VerifyOtp;
@@ -15,12 +16,15 @@ namespace _116.Identity.Application.Auth.UseCases.Public.Commands.VerifyOtp;
 public class PublicVerifyOtpValidator : AbstractValidator<PublicVerifyOtpCommand>
 {
     /// <summary>
-    /// Configure validation rules for OTP verification.
+    /// Initializes a new instance of <see cref="PublicVerifyOtpValidator" /> with validation rules.
     /// </summary>
-    public PublicVerifyOtpValidator()
+    /// <param name="msg">
+    /// Validation error messages for rule configuration.
+    /// </param>
+    public PublicVerifyOtpValidator(ValidationErrorMessage msg)
     {
-        RuleFor(x => x.Email).ValidEmail();
-        RuleFor(x => x.Code).ValidOtpCode();
-        RuleFor(x => x.Purpose).ValidOtpPurpose();
+        RuleFor(x => x.Email).ValidEmail(msg);
+        RuleFor(x => x.Code).ValidOtpCode(msg);
+        RuleFor(x => x.Purpose).ValidOtpPurpose(msg);
     }
 }
