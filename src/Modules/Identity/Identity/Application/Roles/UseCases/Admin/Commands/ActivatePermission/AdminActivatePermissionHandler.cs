8a78e61e69a42ffe2f480b6a4969f157be5488f6
coolbeatz71 <sigmacool@gmail.com>
2026-05-20 13:20:38 +0200

    refactor: inject i18n error class into AdminActivatePermissionHandler

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/ActivatePermission/AdminActivatePermissionHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/ActivatePermission/AdminActivatePermissionHandler.cs
@@ -14,10 +14,12 @@ namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.ActivatePermis
 /// <param name="permissionRepository">Repository for permission data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
+/// <param name="userErrors">User domain error factory for generating domain exceptions.</param>
 public class AdminActivatePermissionHandler(
     IPermissionRepository permissionRepository,
     IIdentityUnitOfWork unitOfWork,
-    IMapper mapper
+    IMapper mapper,
+    UserErrors userErrors
 ) : ICommandHandler<AdminActivatePermissionCommand, AdminActivatePermissionResult>
 {
     /// <summary>
@@ -42,7 +44,7 @@ public class AdminActivatePermissionHandler(
 
         if (!wasActivated)
         {
-            throw UserErrors.PermissionAlreadyActive();
+            throw userErrors.PermissionAlreadyActive();
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
