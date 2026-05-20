c7413254c71744bc1ee63215b4b2df2368337bc8
coolbeatz71 <sigmacool@gmail.com>
2026-05-20 14:37:50 +0200

    refactor: inject i18n error class into AdminRestorePermissionHandler

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestorePermission/AdminRestorePermissionHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestorePermission/AdminRestorePermissionHandler.cs
@@ -14,10 +14,12 @@ namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.RestorePermiss
 /// <param name="permissionRepository">Repository for permission data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
+/// <param name="userErrors">User domain error factory for generating domain exceptions.</param>
 public class AdminRestorePermissionHandler(
     IPermissionRepository permissionRepository,
     IIdentityUnitOfWork unitOfWork,
-    IMapper mapper
+    IMapper mapper,
+    UserErrors userErrors
 ) : ICommandHandler<AdminRestorePermissionCommand, AdminRestorePermissionResult>
 {
     /// <summary>
@@ -42,7 +44,7 @@ public class AdminRestorePermissionHandler(
 
         if (!wasRestored)
         {
-            throw UserErrors.PermissionNotDeleted();
+            throw userErrors.PermissionNotDeleted();
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
