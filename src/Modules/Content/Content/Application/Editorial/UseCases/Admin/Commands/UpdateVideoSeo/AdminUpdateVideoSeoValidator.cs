35ebffee7ef23e89d78918d193e4a96abea62eef
coolbeatz71 <sigmacool@gmail.com>
2026-05-21 11:44:41 +0200

    refactor(i18n): use localized msgs in UpdateVideoSeo validator

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideoSeo/AdminUpdateVideoSeoValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideoSeo/AdminUpdateVideoSeoValidator.cs
@@ -1,18 +1,42 @@
+using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Validators;
+using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
 namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.UpdateVideoSeo;
 
 /// <summary>
-/// Validator for the <see cref="AdminUpdateVideoSeoCommand" /> ensuring a valid video ID is provided.
+/// Validator for the <see cref="AdminUpdateVideoSeoCommand" /> ensuring a valid video ID is provided
+/// and that SEO fields respect their length constraints when supplied.
 /// </summary>
 public class AdminUpdateVideoSeoValidator : AbstractValidator<AdminUpdateVideoSeoCommand>
 {
     /// <summary>
-    /// Configures validation rules for video SEO update.
+    /// Initializes a new instance of <see cref="AdminUpdateVideoSeoValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminUpdateVideoSeoValidator()
+    /// <param name="msg">Article validation error messages.</param>
+    public AdminUpdateVideoSeoValidator(ArticleErrorMessage msg)
     {
         RuleFor(x => x.Id).IsValidGuid("Video ID");
+
+        When(
+            x => x.MetaTitle is not null,
+            () =>
+                RuleFor(x => x.MetaTitle)
+                    .ValidMetaTitle(
+                        metaTitleTooShort: msg.MetaTitleTooShort(ContentConstants.MinMetaTitleLength),
+                        metaTitleTooLong: msg.MetaTitleTooLong(ContentConstants.MaxMetaTitleLength)
+                    )
+        );
+        When(
+            x => x.MetaDescription is not null,
+            () =>
+                RuleFor(x => x.MetaDescription)
+                    .ValidMetaDescription(
+                        metaDescriptionTooShort: msg.MetaDescriptionTooShort(ContentConstants.MinMetaDescriptionLength),
+                        metaDescriptionTooLong: msg.MetaDescriptionTooLong(ContentConstants.MaxMetaDescriptionLength)
+                    )
+        );
     }
 }
