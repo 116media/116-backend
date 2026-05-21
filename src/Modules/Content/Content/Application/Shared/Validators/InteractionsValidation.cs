13b042c45590d32c1f64c9b36633e85b786b38b1
coolbeatz71 <sigmacool@gmail.com>
2026-05-21 18:58:47 +0200

    refactor(i18n): update InteractionsValidation for i18n

--- src/Modules/Content/Content/Application/Shared/Validators/InteractionsValidation.cs
+++ src/Modules/Content/Content/Application/Shared/Validators/InteractionsValidation.cs
@@ -1,4 +1,3 @@
-using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Domain.Constants;
 using FluentValidation;
 
@@ -14,19 +13,21 @@ public static class InteractionsValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the comment body property.</param>
-    /// <param name="msg">The error message provider for article interaction validation messages.</param>
+    /// <param name="commentBodyRequired">Error message used when the comment body is empty.</param>
+    /// <param name="commentBodyTooLong">Error message used when the comment body exceeds the maximum length.</param>
     /// <returns>The configured rule builder.</returns>
     public static IRuleBuilderOptions<T, string?> ValidCommentBody<T>(
         this IRuleBuilderInitial<T, string?> ruleBuilder,
-        ArticleInteractionErrorMessage msg
+        string commentBodyRequired,
+        string commentBodyTooLong
     )
     {
         return ruleBuilder
             .Cascade(cascadeMode: CascadeMode.Stop)
             .NotEmpty()
-            .WithMessage(msg.CommentBodyRequired())
+            .WithMessage(commentBodyRequired)
             .MaximumLength(maximumLength: ContentConstants.MaxCommentBodyLength)
-            .WithMessage(msg.CommentBodyTooLong(ContentConstants.MaxCommentBodyLength));
+            .WithMessage(commentBodyTooLong);
     }
 
     /// <summary>
@@ -34,19 +35,21 @@ public static class InteractionsValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the playlist name property.</param>
-    /// <param name="msg">The error message provider for playlist validation messages.</param>
+    /// <param name="nameRequired">Error message used when the playlist name is empty.</param>
+    /// <param name="nameTooLong">Error message used when the playlist name exceeds the maximum length.</param>
     /// <returns>The configured rule builder.</returns>
     public static IRuleBuilderOptions<T, string?> ValidPlaylistName<T>(
         this IRuleBuilderInitial<T, string?> ruleBuilder,
-        PlaylistErrorMessage msg
+        string nameRequired,
+        string nameTooLong
     )
     {
         return ruleBuilder
             .Cascade(cascadeMode: CascadeMode.Stop)
             .NotEmpty()
-            .WithMessage(msg.NameRequired())
+            .WithMessage(nameRequired)
             .MaximumLength(maximumLength: ContentConstants.MaxPlaylistNameLength)
-            .WithMessage(msg.NameTooLong(ContentConstants.MaxPlaylistNameLength));
+            .WithMessage(nameTooLong);
     }
 
     /// <summary>
@@ -54,13 +57,13 @@ public static class InteractionsValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the star rating property.</param>
-    /// <param name="msg">The error message provider for article interaction validation messages.</param>
+    /// <param name="invalidStarRating">Error message used when the star rating is outside the allowed range.</param>
     /// <returns>The configured rule builder.</returns>
     public static IRuleBuilderOptions<T, short> ValidVideoStarRating<T>(
         this IRuleBuilder<T, short> ruleBuilder,
-        ArticleInteractionErrorMessage msg
+        string invalidStarRating
     )
     {
-        return ruleBuilder.InclusiveBetween(from: (short)1, to: (short)5).WithMessage(msg.InvalidStarRating());
+        return ruleBuilder.InclusiveBetween(from: (short)1, to: (short)5).WithMessage(invalidStarRating);
     }
 }
