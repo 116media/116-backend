d3dee776e703652e2946e4825b1606d65e404c77
coolbeatz71 <sigmacool@gmail.com>
2026-05-21 19:15:36 +0200

    refactor(i18n): update PricingTierValidation for i18n

--- src/Modules/Content/Content/Application/Shared/Validators/PricingTierValidation.cs
+++ src/Modules/Content/Content/Application/Shared/Validators/PricingTierValidation.cs
@@ -1,4 +1,3 @@
-using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Domain.Constants;
 using FluentValidation;
 
@@ -14,10 +13,10 @@ public static class PricingTierValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the ID property.</param>
-    /// <param name="msg">The error message provider for pricing tier validation messages.</param>
-    public static void ValidPricingTierId<T>(this IRuleBuilder<T, Guid> ruleBuilder, PricingTierErrorMessage msg)
+    /// <param name="nameRequired">Error message used when the ID is empty.</param>
+    public static void ValidPricingTierId<T>(this IRuleBuilder<T, Guid> ruleBuilder, string nameRequired)
     {
-        ruleBuilder.NotEmpty().WithMessage(msg.NameRequired());
+        ruleBuilder.NotEmpty().WithMessage(nameRequired);
     }
 
     /// <summary>
@@ -25,12 +24,14 @@ public static class PricingTierValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the name property.</param>
-    /// <param name="msg">The error message provider for pricing tier validation messages.</param>
+    /// <param name="nameRequired">Error message used when the name is empty.</param>
+    /// <param name="nameTooLong">Error message used when the name exceeds the maximum length.</param>
     /// <param name="isRequired">Whether the name is required (default: true).</param>
     /// <returns>The configured rule builder.</returns>
     public static IRuleBuilderOptions<T, string?> ValidPricingTierName<T>(
         this IRuleBuilderInitial<T, string?> ruleBuilder,
-        PricingTierErrorMessage msg,
+        string nameRequired,
+        string nameTooLong,
         bool isRequired = true
     )
     {
@@ -39,15 +40,15 @@ public static class PricingTierValidation
             return ruleBuilder
                 .Cascade(cascadeMode: CascadeMode.Stop)
                 .NotEmpty()
-                .WithMessage(msg.NameRequired())
+                .WithMessage(nameRequired)
                 .MaximumLength(maximumLength: ContentConstants.MaxPricingTierNameLength)
-                .WithMessage(msg.NameTooLong(ContentConstants.MaxPricingTierNameLength));
+                .WithMessage(nameTooLong);
         }
 
         return ruleBuilder
             .Cascade(cascadeMode: CascadeMode.Stop)
             .MaximumLength(maximumLength: ContentConstants.MaxPricingTierNameLength)
-            .WithMessage(msg.NameTooLong(ContentConstants.MaxPricingTierNameLength))
+            .WithMessage(nameTooLong)
             .When(x => !string.IsNullOrWhiteSpace(ValidationUtils.GetPropertyValue(instance: x, "Name")));
     }
 
@@ -56,18 +57,20 @@ public static class PricingTierValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the description property.</param>
-    /// <param name="msg">The error message provider for pricing tier validation messages.</param>
+    /// <param name="descriptionRequired">Error message used when the description is empty.</param>
+    /// <param name="descriptionTooLong">Error message used when the description exceeds the maximum length.</param>
     /// <returns>The configured rule builder.</returns>
     public static IRuleBuilderOptions<T, string?> ValidPricingTierDescription<T>(
         this IRuleBuilderInitial<T, string?> ruleBuilder,
-        PricingTierErrorMessage msg
+        string descriptionRequired,
+        string descriptionTooLong
     )
     {
         return ruleBuilder
             .Cascade(cascadeMode: CascadeMode.Stop)
             .NotEmpty()
-            .WithMessage(msg.DescriptionRequired())
+            .WithMessage(descriptionRequired)
             .MaximumLength(maximumLength: ContentConstants.MaxPricingTierDescriptionLength)
-            .WithMessage(msg.DescriptionTooLong(ContentConstants.MaxPricingTierDescriptionLength));
+            .WithMessage(descriptionTooLong);
     }
 }
