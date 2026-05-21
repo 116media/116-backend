200236be395411aa37fef724941ca6d103912b9f
coolbeatz71 <sigmacool@gmail.com>
2026-05-21 09:51:07 +0200

    refactor(i18n): use localized msgs in AttachYoutubeVideoUrl validator

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/AttachYoutubeVideoUrl/AdminAttachYoutubeVideoUrlValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/AttachYoutubeVideoUrl/AdminAttachYoutubeVideoUrlValidator.cs
@@ -1,5 +1,6 @@
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
+using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -18,6 +19,11 @@ public class AdminAttachYoutubeVideoUrlValidator : AbstractValidator<AdminAttach
     {
         RuleFor(x => x.VideoId).IsValidGuid("Video ID");
 
-        RuleFor(x => x.YoutubeVideoUrl).ValidYoutubeVideoUrl(msg);
+        RuleFor(x => x.YoutubeVideoUrl)
+            .ValidYoutubeVideoUrl(
+                youtubeUrlRequired: msg.YoutubeUrlRequired(),
+                youtubeUrlTooLong: msg.YoutubeUrlTooLong(ContentConstants.MaxYoutubeVideoUrlLength),
+                youtubeUrlInvalidFormat: msg.YoutubeUrlInvalidFormat()
+            );
     }
 }
