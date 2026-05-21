73cf54f023eebce372ec440df2a4975f294dfa23
coolbeatz71 <sigmacool@gmail.com>
2026-05-21 08:27:36 +0200

    refactor(i18n): use localized msgs in CreateCategory validator

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/AdminCreateCategoryValidator.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/AdminCreateCategoryValidator.cs
@@ -1,5 +1,6 @@
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
+using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -17,8 +18,21 @@ public class AdminCreateCategoryValidator : AbstractValidator<AdminCreateCategor
     public AdminCreateCategoryValidator(CategoryErrorMessage msg)
     {
         RuleFor(x => x.ContentTypeId).IsValidGuid("Content type ID");
-        RuleFor(x => x.Name).ValidCategoryName(msg);
-        RuleFor(x => x.Slug).ValidCategorySlug(msg);
-        RuleFor(x => x.Description).ValidCategoryDescription(msg);
+        RuleFor(x => x.Name)
+            .ValidCategoryName(
+                nameRequired: msg.NameRequired(),
+                nameTooLong: msg.NameTooLong(ContentConstants.MaxCategoryNameLength)
+            );
+        RuleFor(x => x.Slug)
+            .ValidCategorySlug(
+                slugRequired: msg.SlugRequired(),
+                slugTooLong: msg.SlugTooLong(ContentConstants.MaxCategorySlugLength),
+                slugInvalidFormat: msg.SlugInvalidFormat()
+            );
+        RuleFor(x => x.Description)
+            .ValidCategoryDescription(
+                descriptionRequired: msg.DescriptionRequired(),
+                descriptionTooLong: msg.DescriptionTooLong(ContentConstants.MaxCategoryDescriptionLength)
+            );
     }
 }
