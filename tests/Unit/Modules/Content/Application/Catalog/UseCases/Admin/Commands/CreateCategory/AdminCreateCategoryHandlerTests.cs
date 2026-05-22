a8c613a5148c0ee006cffeed1dee138603598600
coolbeatz71 <sigmacool@gmail.com>
2026-05-22 20:43:37 +0200

    test(i18n): update CreateCategory handler tests

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/AdminCreateCategoryHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/AdminCreateCategoryHandlerTests.cs
@@ -5,6 +5,7 @@ using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -33,7 +34,8 @@ public class AdminCreateCategoryHandlerTests : BaseContentHandlerTest
             _lookupRepositoryMock.Object,
             _categoryRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            Mapper
+            Mapper,
+            TestErrorsFactory.CreateCategoryErrors()
         );
     }
 
