1845d8d5a5f37e7e90f309c5298d52620ac3a4a3
coolbeatz71 <sigmacool@gmail.com>
2026-05-22 10:10:01 +0200

    refactor(i18n): use localized msgs in SocialLogin validator

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/SocialLogin/PublicSocialLoginValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/SocialLogin/PublicSocialLoginValidator.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants;
 using _116.Identity.Application.Auth.Validators;
 using _116.Identity.Application.Shared.Errors.Messages;
 using FluentValidation;
@@ -24,9 +25,24 @@ public class PublicSocialLoginValidator : AbstractValidator<PublicSocialLoginCom
     /// </param>
     public PublicSocialLoginValidator(ValidationErrorMessage msg)
     {
-        RuleFor(x => x.Email).ValidEmail(msg);
-        RuleFor(x => x.UserName).ValidUsername(msg);
-        RuleFor(x => x.AvatarUrl).ValidAvatarUrl(msg);
-        RuleFor(x => x.Provider).ValidAuthProvider(msg);
+        RuleFor(x => x.Email)
+            .ValidEmail(
+                emailRequired: msg.EmailRequired(),
+                emailTooLong: msg.EmailTooLong(UserConstants.MaxEmailLength),
+                invalidEmailFormat: msg.InvalidEmailFormatMsg()
+            );
+        RuleFor(x => x.UserName)
+            .ValidUsername(
+                usernameRequired: msg.UsernameRequired(),
+                usernameTooShort: msg.UsernameTooShort(UserConstants.MinUserNameLength),
+                usernameTooLong: msg.UsernameTooLong(UserConstants.MaxUserNameLength),
+                usernameInvalidChars: msg.UsernameInvalidChars()
+            );
+        RuleFor(x => x.AvatarUrl).ValidAvatarUrl(avatarUrlInvalid: msg.AvatarUrlInvalid());
+        RuleFor(x => x.Provider)
+            .ValidAuthProvider(
+                authProviderRequired: msg.AuthProviderRequired(),
+                authProviderInvalid: msg.AuthProviderInvalid()
+            );
     }
 }
