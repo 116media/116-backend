203ad59fea4a5f12a045d900ae3191782d224610
coolbeatz71 <sigmacool@gmail.com>
2026-05-22 19:58:55 +0200

    test(i18n): update ActivateCategory handler tests

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/ActivateCategory/AdminActivateCategoryHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/ActivateCategory/AdminActivateCategoryHandlerTests.cs
@@ -4,6 +4,7 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -26,7 +27,12 @@ public class AdminActivateCategoryHandlerTests : BaseContentHandlerTest
     {
         _categoryRepositoryMock = MockCategoryRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
-        _handler = new AdminActivateCategoryHandler(_categoryRepositoryMock.Object, _unitOfWorkMock.Object, Mapper);
+        _handler = new AdminActivateCategoryHandler(
+            _categoryRepositoryMock.Object,
+            _unitOfWorkMock.Object,
+            Mapper,
+            TestErrorsFactory.CreateCategoryErrors()
+        );
     }
 
     #region Success Cases
