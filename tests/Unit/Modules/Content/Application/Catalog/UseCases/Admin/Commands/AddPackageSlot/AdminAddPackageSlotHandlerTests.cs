d11de1e7f845cf3ab9461582a915bd6a04cc484f
coolbeatz71 <sigmacool@gmail.com>
2026-05-22 20:28:56 +0200

    test(i18n): update AddPackageSlot handler tests

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/AddPackageSlot/AdminAddPackageSlotHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/AddPackageSlot/AdminAddPackageSlotHandlerTests.cs
@@ -5,6 +5,7 @@ using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -33,7 +34,9 @@ public class AdminAddPackageSlotHandlerTests : BaseContentHandlerTest
             _packageRepositoryMock.Object,
             _categoryRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            Mapper
+            Mapper,
+            TestErrorsFactory.CreateCategoryErrors(),
+            TestErrorsFactory.CreatePackageErrors()
         );
     }
 
