f2e890b32b19c4a46cc76b723460977f4b796b54
coolbeatz71 <sigmacool@gmail.com>
2026-05-22 20:57:32 +0200

    test(i18n): update CreateCustomer handler tests

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCustomer/AdminCreateCustomerHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCustomer/AdminCreateCustomerHandlerTests.cs
@@ -5,6 +5,7 @@ using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -27,7 +28,12 @@ public class AdminCreateCustomerHandlerTests : BaseContentHandlerTest
     {
         _customerRepositoryMock = MockCustomerRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
-        _handler = new AdminCreateCustomerHandler(_customerRepositoryMock.Object, _unitOfWorkMock.Object, Mapper);
+        _handler = new AdminCreateCustomerHandler(
+            _customerRepositoryMock.Object,
+            _unitOfWorkMock.Object,
+            Mapper,
+            TestErrorsFactory.CreateCustomerErrors()
+        );
     }
 
     #region Success Cases
