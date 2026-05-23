8d575bebd15e7fd51909045800df474d883be349
coolbeatz71 <sigmacool@gmail.com>
2026-05-23 05:48:54 +0200

    test(i18n): update UpdateCategoryPricing handler tests

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCategoryPricing/AdminUpdateCategoryPricingHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCategoryPricing/AdminUpdateCategoryPricingHandlerTests.cs
@@ -5,6 +5,7 @@ using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -30,7 +31,8 @@ public class AdminUpdateCategoryPricingHandlerTests : BaseContentHandlerTest
         _handler = new AdminUpdateCategoryPricingHandler(
             _categoryRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            Mapper
+            Mapper,
+            TestErrorsFactory.CreateCategoryErrors()
         );
     }
 
