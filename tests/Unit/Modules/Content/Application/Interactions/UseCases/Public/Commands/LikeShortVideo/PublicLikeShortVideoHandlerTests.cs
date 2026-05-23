0033d2078fc74f2cfc9b7c782c5b3670bf87bc49
coolbeatz71 <sigmacool@gmail.com>
2026-05-23 17:30:24 +0200

    test(i18n): update LikeShortVideo handler tests

--- tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/LikeShortVideo/PublicLikeShortVideoHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/LikeShortVideo/PublicLikeShortVideoHandlerTests.cs
@@ -4,6 +4,7 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
@@ -25,7 +26,11 @@ public class PublicLikeShortVideoHandlerTests
     {
         _shortVideoRepositoryMock = MockShortVideoRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
-        _handler = new PublicLikeShortVideoHandler(_shortVideoRepositoryMock.Object, _unitOfWorkMock.Object);
+        _handler = new PublicLikeShortVideoHandler(
+            _shortVideoRepositoryMock.Object,
+            _unitOfWorkMock.Object,
+            TestErrorsFactory.CreateShortVideoInteractionErrors()
+        );
     }
 
     #region Success Cases
