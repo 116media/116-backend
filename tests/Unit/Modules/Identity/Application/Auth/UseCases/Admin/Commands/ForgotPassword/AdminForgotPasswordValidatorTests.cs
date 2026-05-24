4b16bca322cea4773d406f98ce9222802ab24da2
coolbeatz71 <sigmacool@gmail.com>
2026-05-24 20:37:08 +0200

    test(i18n): update ForgotPassword validator assertions

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ForgotPassword/AdminForgotPasswordValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ForgotPassword/AdminForgotPasswordValidatorTests.cs
@@ -1,6 +1,8 @@
 using _116.BuildingBlocks.Constants;
 using _116.Identity.Application.Auth.UseCases.Admin.Commands.ForgotPassword;
+using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
+using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
 using FluentValidation.TestHelper;
 using Xunit;
@@ -12,7 +14,9 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Auth.UseCases.Admin.Comma
 /// </summary>
 public class AdminForgotPasswordValidatorTests
 {
-    private readonly AdminForgotPasswordValidator _validator = new();
+    private readonly AdminForgotPasswordValidator _validator = new(
+        LocalizerFactory.CreateMessage<ValidationErrorMessage>()
+    );
 
     #region Valid Command Tests
 
