976d55b6a9306ea056ea9c7f3f02f1a831232795
coolbeatz71 <sigmacool@gmail.com>
2026-05-24 20:52:26 +0200

    test(i18n): update Login handler tests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginHandlerTests.cs
@@ -5,7 +5,7 @@ using _116.Identity.Application.Session.Factories.Contracts;
 using _116.Identity.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
 using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
