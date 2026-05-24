ea7e7c5550132fec292a9b3ab533dea31a13b660
coolbeatz71 <sigmacool@gmail.com>
2026-05-24 22:20:35 +0200

    test(i18n): update VerifyOtp handler tests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/VerifyOtp/AdminVerifyOtpHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/VerifyOtp/AdminVerifyOtpHandlerTests.cs
@@ -6,7 +6,7 @@ using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
 using _116.Identity.Domain.ValueObjects;
 using _116.Shared.Application.Exceptions;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
