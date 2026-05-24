0280aaaf6664a6a25d0fcae5a7eec2dcdbf70305
coolbeatz71 <sigmacool@gmail.com>
2026-05-24 20:45:34 +0200

    test(i18n): update LoginAuthFactory for i18n refactor

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginAuthFactoryTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginAuthFactoryTests.cs
@@ -5,7 +5,8 @@ using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.ValueObjects;
 using _116.Shared.Application.Exceptions;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
@@ -28,7 +29,11 @@ public class AdminLoginAuthFactoryTests
         _authRepositoryMock = MockAuthRepository.Create();
         _passwordServiceMock = MockPasswordService.Create();
 
-        _factory = new AdminLoginAuthFactory(_authRepositoryMock.Object, _passwordServiceMock.Object);
+        _factory = new AdminLoginAuthFactory(
+            _authRepositoryMock.Object,
+            _passwordServiceMock.Object,
+            TestErrorsFactory.CreateUserErrors()
+        );
     }
 
     #region Success Cases
