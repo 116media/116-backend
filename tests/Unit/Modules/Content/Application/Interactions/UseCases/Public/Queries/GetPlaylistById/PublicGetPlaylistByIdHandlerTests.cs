2ea39868a529f1d3da6701aa1132c8d722e7bf45
coolbeatz71 <sigmacool@gmail.com>
2026-05-24 11:05:08 +0200

    test(i18n): update GetPlaylistById handler tests

--- tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Queries/GetPlaylistById/PublicGetPlaylistByIdHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Queries/GetPlaylistById/PublicGetPlaylistByIdHandlerTests.cs
@@ -3,6 +3,7 @@ using _116.Content.Application.Interactions.UseCases.Public.Queries.GetPlaylistB
 using _116.Content.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
@@ -24,7 +25,11 @@ public class PublicGetPlaylistByIdHandlerTests : BaseContentHandlerTest
     public PublicGetPlaylistByIdHandlerTests()
     {
         _playlistRepositoryMock = MockPlaylistRepository.Create();
-        _handler = new PublicGetPlaylistByIdHandler(_playlistRepositoryMock.Object, Mapper);
+        _handler = new PublicGetPlaylistByIdHandler(
+            _playlistRepositoryMock.Object,
+            Mapper,
+            TestErrorsFactory.CreatePlaylistErrors()
+        );
     }
 
     #region Success Cases
