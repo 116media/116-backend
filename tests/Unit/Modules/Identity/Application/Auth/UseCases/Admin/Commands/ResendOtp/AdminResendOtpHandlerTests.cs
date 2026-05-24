43053ed3964f7485c4454488fa6b3b2c6045feae
coolbeatz71 <sigmacool@gmail.com>
2026-05-24 21:15:45 +0200

    test(i18n): update ResendOtp handler tests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResendOtp/AdminResendOtpHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResendOtp/AdminResendOtpHandlerTests.cs
@@ -4,7 +4,7 @@ using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
 using _116.Identity.Domain.ValueObjects;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
 using Moq;
