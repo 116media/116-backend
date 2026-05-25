028cfed51672e1d1de98275b751799b7ea156e74
coolbeatz71 <sigmacool@gmail.com>
2026-05-25 15:53:04 +0200

    test(i18n): update RefreshToken validator assertions

--- tests/Unit/Modules/Identity/Application/Session/UseCases/Public/Commands/RefreshToken/PublicRefreshTokenValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Session/UseCases/Public/Commands/RefreshToken/PublicRefreshTokenValidatorTests.cs
@@ -1,5 +1,7 @@
 using _116.Identity.Application.Session.UseCases.Public.Commands.RefreshToken;
+using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
+using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
 using FluentValidation.TestHelper;
 using Xunit;
@@ -11,7 +13,9 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Session.UseCases.Public.C
 /// </summary>
 public class PublicRefreshTokenValidatorTests
 {
-    private readonly PublicRefreshTokenValidator _validator = new();
+    private readonly PublicRefreshTokenValidator _validator = new(
+        LocalizerFactory.CreateMessage<ValidationErrorMessage>()
+    );
 
     #region Valid Command Tests
 
