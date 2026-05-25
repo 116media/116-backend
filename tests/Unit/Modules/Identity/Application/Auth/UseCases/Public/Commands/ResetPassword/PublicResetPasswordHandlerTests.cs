c7f8664c5e55cbaf832879bc744abf7f4c728e75
coolbeatz71 <sigmacool@gmail.com>
2026-05-25 07:34:29 +0200

    test(i18n): update ResetPassword handler tests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordHandlerTests.cs
@@ -4,7 +4,7 @@ using _116.Identity.Application.Auth.UseCases.Public.Commands.ResetPassword.Cont
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
 using _116.Shared.Application.Exceptions;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
 using Moq;
