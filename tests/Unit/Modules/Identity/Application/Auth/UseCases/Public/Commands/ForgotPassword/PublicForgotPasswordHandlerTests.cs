267b8cc1edc85c065e898347be3c547db965a5c3
coolbeatz71 <sigmacool@gmail.com>
2026-05-25 06:20:55 +0200

    test(i18n): update ForgotPassword handler tests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ForgotPassword/PublicForgotPasswordHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ForgotPassword/PublicForgotPasswordHandlerTests.cs
@@ -3,7 +3,7 @@ using _116.Identity.Application.Auth.UseCases.Public.Commands.ForgotPassword.Con
 using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.ValueObjects;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
 using Moq;
