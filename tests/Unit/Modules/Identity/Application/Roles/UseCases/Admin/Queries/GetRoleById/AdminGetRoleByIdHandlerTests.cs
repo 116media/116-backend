a9aa8a7ab9454c610d9e567bc3c94b903404f9c6
coolbeatz71 <sigmacool@gmail.com>
2026-05-25 14:04:50 +0200

    test(i18n): update GetRoleById handler tests

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Queries/GetRoleById/AdminGetRoleByIdHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Queries/GetRoleById/AdminGetRoleByIdHandlerTests.cs
@@ -4,7 +4,7 @@ using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
 using AwesomeAssertions;
