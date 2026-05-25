b34c0078fc194f15d931ce3997e27911a7c33e0f
coolbeatz71 <sigmacool@gmail.com>
2026-05-25 11:30:22 +0200

    test(i18n): update CreatePermission handler tests

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/CreatePermission/AdminCreatePermissionHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/CreatePermission/AdminCreatePermissionHandlerTests.cs
@@ -1,9 +1,11 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.CreatePermission;
+using _116.Identity.Application.Shared.Errors;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
-using _116.Tests.Fixtures.Factories;
+using _116.Tests.Fixtures.Factories.Identity;
+using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -20,14 +22,21 @@ public class AdminCreatePermissionHandlerTests : BaseHandlerTest
 {
     private readonly Mock<IPermissionRepository> _permissionRepositoryMock;
     private readonly Mock<IIdentityUnitOfWork> _unitOfWorkMock;
+    private readonly UserErrors _userErrors;
     private readonly AdminCreatePermissionHandler _handler;
 
     public AdminCreatePermissionHandlerTests()
     {
         _permissionRepositoryMock = MockPermissionRepository.Create();
         _unitOfWorkMock = MockIdentityUnitOfWork.Create();
+        _userErrors = TestErrorsFactory.CreateUserErrors();
 
-        _handler = new AdminCreatePermissionHandler(_permissionRepositoryMock.Object, _unitOfWorkMock.Object, Mapper);
+        _handler = new AdminCreatePermissionHandler(
+            _permissionRepositoryMock.Object,
+            _unitOfWorkMock.Object,
+            Mapper,
+            _userErrors
+        );
     }
 
     #region Success Cases
