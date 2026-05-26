ced80bdb9dd96fb819726dedb3b856c2171a474d
coolbeatz71 <sigmacool@gmail.com>
2026-05-26 21:08:22 +0200

    refactor(catalog): inject CategoryErrorMessage into AdminRemoveCategoryPricingValidator

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/RemoveCategoryPricing/AdminRemoveCategoryPricingValidator.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/RemoveCategoryPricing/AdminRemoveCategoryPricingValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -9,11 +10,13 @@ namespace _116.Content.Application.Catalog.UseCases.Admin.Commands.RemoveCategor
 public class AdminRemoveCategoryPricingValidator : AbstractValidator<AdminRemoveCategoryPricingCommand>
 {
     /// <summary>
-    /// Configures validation rules for category pricing removal.
+    /// Initializes a new instance of <see cref="AdminRemoveCategoryPricingValidator" /> with the specified error message providers.
     /// </summary>
-    public AdminRemoveCategoryPricingValidator()
+    /// <param name="categoryMsg">Category validation error messages.</param>
+    /// <param name="pricingTierMsg">Pricing tier validation error messages.</param>
+    public AdminRemoveCategoryPricingValidator(CategoryErrorMessage categoryMsg, PricingTierErrorMessage pricingTierMsg)
     {
-        RuleFor(x => x.CategoryId).IsValidGuid("Category ID");
-        RuleFor(x => x.PricingTierId).IsValidGuid("Pricing Tier ID");
+        RuleFor(x => x.CategoryId).IsValidGuid(categoryMsg.Localizer);
+        RuleFor(x => x.PricingTierId).IsValidGuid(pricingTierMsg.Localizer);
     }
 }
