af7918e42dba6738503429e4c7f804ce0df919aa
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 00:47:54 +0200

    refactor(editorial): inject ArticleErrorMessage into AdminApproveArticleValidator

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ApproveArticle/AdminApproveArticleValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ApproveArticle/AdminApproveArticleValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -9,10 +10,11 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.ApproveArti
 public class AdminApproveArticleValidator : AbstractValidator<AdminApproveArticleCommand>
 {
     /// <summary>
-    /// Configures validation rules for article approval.
+    /// Initializes a new instance of <see cref="AdminApproveArticleValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminApproveArticleValidator()
+    /// <param name="i18n">Article validation error messages.</param>
+    public AdminApproveArticleValidator(ArticleErrorMessage i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid("Article ID");
+        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
     }
 }
