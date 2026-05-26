fe4079c0d4ae59c297e4bda638b5cb4dd7fd60fe
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 00:02:35 +0200

    refactor(commerce): inject ContentOrderErrorMessage into AdminRemoveOrderItemValidator

--- src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/RemoveOrderItem/AdminRemoveOrderItemValidator.cs
+++ src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/RemoveOrderItem/AdminRemoveOrderItemValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -9,11 +10,12 @@ namespace _116.Content.Application.Commerce.UseCases.Admin.Commands.RemoveOrderI
 public class AdminRemoveOrderItemValidator : AbstractValidator<AdminRemoveOrderItemCommand>
 {
     /// <summary>
-    /// Configures validation rules for removing an order item.
+    /// Initializes a new instance of <see cref="AdminRemoveOrderItemValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminRemoveOrderItemValidator()
+    /// <param name="i18n">Content order validation error messages.</param>
+    public AdminRemoveOrderItemValidator(ContentOrderErrorMessage i18n)
     {
-        RuleFor(x => x.OrderId).IsValidGuid("Order ID");
-        RuleFor(x => x.ItemId).IsValidGuid("Item ID");
+        RuleFor(x => x.OrderId).IsValidGuid(i18n.Localizer);
+        RuleFor(x => x.ItemId).IsValidGuid(i18n.Localizer, "ItemIdRequired", "ItemIdInvalid");
     }
 }
