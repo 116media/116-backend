9a44cfc58cb806e7254e94f23b06a4c7a2ab96a7
coolbeatz71 <sigmacool@gmail.com>
2026-05-26 20:12:44 +0200

    refactor(catalog): inject CustomerErrorMessage into AdminCreateCustomerValidator

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/CreateCustomer/AdminCreateCustomerValidator.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/CreateCustomer/AdminCreateCustomerValidator.cs
@@ -1,6 +1,5 @@
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
-using _116.Content.Domain.Constants;
 using FluentValidation;
 
 namespace _116.Content.Application.Catalog.UseCases.Admin.Commands.CreateCustomer;
@@ -13,25 +12,13 @@ public class AdminCreateCustomerValidator : AbstractValidator<AdminCreateCustome
     /// <summary>
     /// Initializes a new instance of <see cref="AdminCreateCustomerValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="msg">Customer validation error messages.</param>
-    public AdminCreateCustomerValidator(CustomerErrorMessage msg)
+    /// <param name="i18n">Customer validation error messages.</param>
+    public AdminCreateCustomerValidator(CustomerErrorMessage i18n)
     {
-        RuleFor(x => x.FullName)
-            .ValidCustomerFullName(
-                fullNameRequired: msg.FullNameRequired(),
-                fullNameTooLong: msg.FullNameTooLong(ContentConstants.MaxCustomerFullNameLength)
-            );
-        RuleFor(x => x.Email)
-            .ValidCustomerEmail(
-                emailRequired: msg.EmailRequired(),
-                emailInvalidFormat: msg.EmailInvalidFormat(),
-                emailTooLong: msg.EmailTooLong(ContentConstants.MaxCustomerEmailLength)
-            );
-        RuleFor(x => x.Phone)
-            .ValidCustomerPhone(phoneTooLong: msg.PhoneTooLong(ContentConstants.MaxCustomerPhoneLength));
-        RuleFor(x => x.Company)
-            .ValidCustomerCompany(companyTooLong: msg.CompanyTooLong(ContentConstants.MaxCustomerCompanyLength));
-        RuleFor(x => x.Notes)
-            .ValidCustomerNotes(notesTooLong: msg.NotesTooLong(ContentConstants.MaxCustomerNotesLength));
+        RuleFor(x => x.FullName).ValidCustomerFullName(i18n);
+        RuleFor(x => x.Email).ValidCustomerEmail(i18n);
+        RuleFor(x => x.Phone).ValidCustomerPhone(i18n);
+        RuleFor(x => x.Company).ValidCustomerCompany(i18n);
+        RuleFor(x => x.Notes).ValidCustomerNotes(i18n);
     }
 }
