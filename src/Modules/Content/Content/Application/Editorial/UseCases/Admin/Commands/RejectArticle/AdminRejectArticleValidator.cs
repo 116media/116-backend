2a200a24392adf0c340d7cc13d4d55cd6943bc48
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 05:04:21 +0200

    refactor(editorial): inject ArticleErrorMessage into AdminRejectArticleValidator

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/RejectArticle/AdminRejectArticleValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/RejectArticle/AdminRejectArticleValidator.cs
@@ -1,6 +1,5 @@
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
-using _116.Content.Domain.Constants;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -14,15 +13,11 @@ public class AdminRejectArticleValidator : AbstractValidator<AdminRejectArticleC
     /// <summary>
     /// Initializes a new instance of <see cref="AdminRejectArticleValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="msg">Article validation error messages.</param>
-    public AdminRejectArticleValidator(ArticleErrorMessage msg)
+    /// <param name="i18n">Article validation error messages.</param>
+    public AdminRejectArticleValidator(ArticleErrorMessage i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid("Article ID");
+        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
 
-        RuleFor(x => x.Reason)
-            .ValidRejectionReason(
-                reasonRequired: msg.RejectionReasonRequired(),
-                reasonTooLong: msg.RejectionReasonTooLong(ContentConstants.MaxRejectionReasonLength)
-            );
+        RuleFor(x => x.Reason).ValidRejectionReason(i18n);
     }
 }
