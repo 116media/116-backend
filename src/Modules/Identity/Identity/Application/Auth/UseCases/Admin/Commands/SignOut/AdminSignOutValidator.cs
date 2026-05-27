29b464509b0555517ad856fb15875575e5dea1cf
coolbeatz71 <sigmacool@gmail.com>
2026-05-28 00:13:12 +0200

    refactor(auth): inject ValidationErrorMessage into AdminSignOutValidator

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/SignOut/AdminSignOutValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/SignOut/AdminSignOutValidator.cs
@@ -15,11 +15,11 @@ public class AdminSignOutValidator : AbstractValidator<AdminSignOutCommand>
     /// <summary>
     /// Initializes a new instance of <see cref="AdminSignOutValidator" /> with validation rules.
     /// </summary>
-    /// <param name="msg">
+    /// <param name="i18n">
     /// Validation error messages for rule configuration.
     /// </param>
-    public AdminSignOutValidator(ValidationErrorMessage msg)
+    public AdminSignOutValidator(ValidationErrorMessage i18n)
     {
-        RuleFor(x => x.RefreshToken).ValidRefreshToken(refreshTokenRequired: msg.RefreshTokenRequired());
+        RuleFor(x => x.RefreshToken).ValidRefreshToken(i18n);
     }
 }
