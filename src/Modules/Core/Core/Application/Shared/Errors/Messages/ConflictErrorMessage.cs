ec344e5eb8e317499d352106459874db30988300
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 22:16:29 +0200

    refactor(core-i18n): expose Localizer property on ConflictErrorMessage

--- src/Modules/Core/Core/Application/Shared/Errors/Messages/ConflictErrorMessage.cs
+++ src/Modules/Core/Core/Application/Shared/Errors/Messages/ConflictErrorMessage.cs
@@ -9,6 +9,11 @@ namespace _116.Core.Application.Shared.Errors.Messages;
 public class ConflictErrorMessage(IStringLocalizer<ConflictErrorMessage> localizer)
 {
     /// <summary>
+    /// Exposes the underlying localizer for shared validation extensions.
+    /// </summary>
+    public IStringLocalizer Localizer => localizer;
+
+    /// <summary>
     /// Gets error message for file upload failure.
     /// </summary>
     /// <param name="fileName">The name of the file that failed to upload.</param>
