cec0fafd318e8ed333bb2bc1253c67af7178478c
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 23:58:37 +0200

    refactor(auth): inject ValidationErrorMessage into AdminResetPasswordValidator

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/ResetPassword/AdminResetPasswordValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/ResetPassword/AdminResetPasswordValidator.cs
@@ -1,4 +1,3 @@
-using _116.BuildingBlocks.Constants;
 using _116.Identity.Application.Auth.Validators;
 using _116.Identity.Application.Shared.Errors.Messages;
 using FluentValidation;
@@ -19,28 +18,13 @@ public class AdminResetPasswordValidator : AbstractValidator<AdminResetPasswordC
     /// <summary>
     /// Initializes a new instance of <see cref="AdminResetPasswordValidator" /> with validation rules.
     /// </summary>
-    /// <param name="msg">
+    /// <param name="i18n">
     /// Validation error messages for rule configuration.
     /// </param>
-    public AdminResetPasswordValidator(ValidationErrorMessage msg)
+    public AdminResetPasswordValidator(ValidationErrorMessage i18n)
     {
-        RuleFor(x => x.Email)
-            .ValidEmail(
-                emailRequired: msg.EmailRequired(),
-                emailTooLong: msg.EmailTooLong(UserConstants.MaxEmailLength),
-                invalidEmailFormat: msg.InvalidEmailFormatMsg()
-            );
-        RuleFor(x => x.Code)
-            .ValidOtpCode(
-                otpCodeRequired: msg.OtpCodeRequired(),
-                otpCodeWrongLength: msg.OtpCodeWrongLength(UserConstants.OtpCodeLength),
-                otpCodeNotNumeric: msg.OtpCodeNotNumeric()
-            );
-        RuleFor(x => x.NewPassword)
-            .ValidPassword(
-                passwordRequired: msg.PasswordRequired(),
-                passwordTooShort: msg.PasswordTooShort("New password", UserConstants.MinPasswordLength),
-                passwordComplexity: msg.PasswordComplexity("New password")
-            );
+        RuleFor(x => x.Email).ValidEmail(i18n);
+        RuleFor(x => x.Code).ValidOtpCode(i18n);
+        RuleFor(x => x.NewPassword).ValidPassword(i18n, fieldName: "New password");
     }
 }
