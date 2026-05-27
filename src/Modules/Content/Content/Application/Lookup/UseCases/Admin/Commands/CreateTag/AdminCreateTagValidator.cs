fdfdf2702827b5863e077d46b5b38bbcc27810ca
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 12:00:57 +0200

    refactor(lookup): inject TagErrorMessage into AdminCreateTagValidator

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreateTag/AdminCreateTagValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreateTag/AdminCreateTagValidator.cs
@@ -1,6 +1,5 @@
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Application.Shared.Validators;
-using _116.Content.Domain.Constants;
 using FluentValidation;
 
 namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.CreateTag;
@@ -13,19 +12,10 @@ public class AdminCreateTagValidator : AbstractValidator<AdminCreateTagCommand>
     /// <summary>
     /// Initializes a new instance of <see cref="AdminCreateTagValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="msg">Tag validation error messages.</param>
-    public AdminCreateTagValidator(TagErrorMessage msg)
+    /// <param name="i18n">Tag validation error messages.</param>
+    public AdminCreateTagValidator(TagErrorMessage i18n)
     {
-        RuleFor(x => x.Name)
-            .ValidTagName(
-                nameRequired: msg.NameRequired(),
-                nameTooLong: msg.NameTooLong(ContentConstants.MaxTagNameLength)
-            );
-        RuleFor(x => x.Slug)
-            .ValidTagSlug(
-                slugRequired: msg.SlugRequired(),
-                slugTooLong: msg.SlugTooLong(ContentConstants.MaxTagSlugLength),
-                slugInvalidFormat: msg.SlugInvalidFormat()
-            );
+        RuleFor(x => x.Name).ValidTagName(i18n);
+        RuleFor(x => x.Slug).ValidTagSlug(i18n);
     }
 }
