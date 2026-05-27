d2dda70998421ee4ac184d36cd186272ae91b58b
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 10:47:57 +0200

    refactor(lookup): inject PricingTierErrorMessage into AdminActivatePricingTierValidator

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/AdminActivatePricingTierValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/AdminActivatePricingTierValidator.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -9,10 +10,11 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivatePricin
 public class AdminActivatePricingTierValidator : AbstractValidator<AdminActivatePricingTierCommand>
 {
     /// <summary>
-    /// Configures validation rules for pricing tier activation.
+    /// Initializes a new instance of <see cref="AdminActivatePricingTierValidator" /> with the specified error message provider.
     /// </summary>
-    public AdminActivatePricingTierValidator()
+    /// <param name="i18n">Pricing tier validation error messages.</param>
+    public AdminActivatePricingTierValidator(PricingTierErrorMessage i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid("Pricing tier ID");
+        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
     }
 }
