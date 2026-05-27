b183e2d690557f96b0bed05fff47411de709c1e2
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 20:19:46 +0200

    refactor(content-validators): replace string params with i18n in ContentTypeValidation

--- src/Modules/Content/Content/Application/Shared/Validators/ContentTypeValidation.cs
+++ src/Modules/Content/Content/Application/Shared/Validators/ContentTypeValidation.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Domain.Constants;
 using FluentValidation;
 
@@ -13,14 +14,12 @@ public static class ContentTypeValidation
     /// </summary>
     /// <typeparam name="T">The type being validated.</typeparam>
     /// <param name="ruleBuilder">The rule builder for the name property.</param>
-    /// <param name="nameRequired">Error message used when the name is empty.</param>
-    /// <param name="nameTooLong">Error message used when the name exceeds the maximum length.</param>
+    /// <param name="i18n">The content type error message provider.</param>
     /// <param name="isRequired">Whether the name is required (default: true).</param>
     /// <returns>The configured rule builder.</returns>
     public static IRuleBuilderOptions<T, string?> ValidContentTypeName<T>(
         this IRuleBuilderInitial<T, string?> ruleBuilder,
-        string nameRequired,
-        string nameTooLong,
+        ContentTypeErrorMessage i18n,
         bool isRequired = true
     )
     {
@@ -29,15 +28,15 @@ public static class ContentTypeValidation
             return ruleBuilder
                 .Cascade(cascadeMode: CascadeMode.Stop)
                 .NotEmpty()
-                .WithMessage(nameRequired)
+                .WithMessage(i18n.NameRequired())
                 .MaximumLength(maximumLength: ContentConstants.MaxContentTypeNameLength)
-                .WithMessage(nameTooLong);
+                .WithMessage(i18n.NameTooLong(ContentConstants.MaxContentTypeNameLength));
         }
 
         return ruleBuilder
             .Cascade(cascadeMode: CascadeMode.Stop)
             .MaximumLength(maximumLength: ContentConstants.MaxContentTypeNameLength)
-            .WithMessage(nameTooLong)
+            .WithMessage(i18n.NameTooLong(ContentConstants.MaxContentTypeNameLength))
             .When(x => !string.IsNullOrWhiteSpace(ValidationUtils.GetPropertyValue(instance: x, "Name")));
     }
 }
