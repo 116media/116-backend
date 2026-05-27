1eaa97c7e456d49c9a31661d3007ae9dcad1367b
coolbeatz71 <sigmacool@gmail.com>
2026-05-27 19:06:49 +0200

    refactor(content-errors): rename msg param to i18n in ShortVideoInteractionErrors

--- src/Modules/Content/Content/Application/Shared/Errors/ShortVideoInteractionErrors.cs
+++ src/Modules/Content/Content/Application/Shared/Errors/ShortVideoInteractionErrors.cs
@@ -7,14 +7,14 @@ namespace _116.Content.Application.Shared.Errors;
 /// Short video interaction error factory providing simple, readable exception creation.
 /// Covers likes and bookmarks on short videos.
 /// </summary>
-public class ShortVideoInteractionErrors(ShortVideoInteractionErrorMessage msg)
+public class ShortVideoInteractionErrors(ShortVideoInteractionErrorMessage i18n)
 {
     /// <summary>
     /// Throws when a user attempts to like a short video they have already liked.
     /// </summary>
     public ConflictException AlreadyLiked()
     {
-        return new ConflictException(msg.AlreadyLiked());
+        return new ConflictException(i18n.AlreadyLiked());
     }
 
     /// <summary>
@@ -22,7 +22,7 @@ public class ShortVideoInteractionErrors(ShortVideoInteractionErrorMessage msg)
     /// </summary>
     public BadRequestException LikeNotFound()
     {
-        return new BadRequestException(msg.LikeNotFound());
+        return new BadRequestException(i18n.LikeNotFound());
     }
 
     /// <summary>
@@ -30,7 +30,7 @@ public class ShortVideoInteractionErrors(ShortVideoInteractionErrorMessage msg)
     /// </summary>
     public ConflictException AlreadyBookmarked()
     {
-        return new ConflictException(msg.AlreadyBookmarked());
+        return new ConflictException(i18n.AlreadyBookmarked());
     }
 
     /// <summary>
@@ -38,6 +38,6 @@ public class ShortVideoInteractionErrors(ShortVideoInteractionErrorMessage msg)
     /// </summary>
     public BadRequestException BookmarkNotFound()
     {
-        return new BadRequestException(msg.BookmarkNotFound());
+        return new BadRequestException(i18n.BookmarkNotFound());
     }
 }
