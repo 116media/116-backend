1a8e99e3e48ad1dbb3d5c84ea13c2a3401d44521
coolbeatz71 <sigmacool@gmail.com>
2026-05-28 12:54:59 +0200

    refactor(roles): inject ValidationErrorMessage into AdminAssignRoleToUserValidator

--- src/Modules/Identity/Identity/Application/User/UseCases/Admin/Commands/AssignRoleToUser/AdminAssignRoleToUserValidator.cs
+++ src/Modules/Identity/Identity/Application/User/UseCases/Admin/Commands/AssignRoleToUser/AdminAssignRoleToUserValidator.cs
@@ -1,3 +1,4 @@
+using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -11,8 +12,11 @@ public class AdminAssignRoleToUserValidator : AbstractValidator<AdminAssignRoleT
     /// <summary>
     /// Configure validation rules for assigning a role to a user.
     /// </summary>
-    public AdminAssignRoleToUserValidator()
+    /// <param name="i18n">
+    /// Validation error messages for rule configuration.
+    /// </param>
+    public AdminAssignRoleToUserValidator(ValidationErrorMessage i18n)
     {
-        RuleFor(x => x.UserId).IsValidGuid("User ID");
+        RuleFor(x => x.UserId).IsValidGuid(i18n.Localizer, "UserIdRequired", "UserIdInvalid");
     }
 }
