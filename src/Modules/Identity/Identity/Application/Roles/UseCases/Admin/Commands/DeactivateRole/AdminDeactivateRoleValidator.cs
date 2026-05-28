dcd61797a831a51e7bc903ab4eb7af92a6b8d3f6
coolbeatz71 <sigmacool@gmail.com>
2026-05-28 06:47:08 +0200

    refactor(roles): inject ValidationErrorMessage into AdminDeactivateRoleValidator

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivateRole/AdminDeactivateRoleValidator.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivateRole/AdminDeactivateRoleValidator.cs
@@ -1,3 +1,4 @@
+using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -11,8 +12,11 @@ public class AdminDeactivateRoleValidator : AbstractValidator<AdminDeactivateRol
     /// <summary>
     /// Configure validation rules for role deactivation.
     /// </summary>
-    public AdminDeactivateRoleValidator()
+    /// <param name="i18n">
+    /// Validation error messages for rule configuration.
+    /// </param>
+    public AdminDeactivateRoleValidator(ValidationErrorMessage i18n)
     {
-        RuleFor(x => x.RoleId).IsValidGuid("Role ID");
+        RuleFor(x => x.RoleId).IsValidGuid(i18n.Localizer, "RoleIdRequired", "RoleIdInvalid");
     }
 }
