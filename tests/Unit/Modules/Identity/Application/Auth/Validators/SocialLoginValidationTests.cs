5fa281ffb004af75c1beef82708ca2a72043ac8f
coolbeatz71 <sigmacool@gmail.com>
2026-05-29 20:35:47 +0200

    test(identity-tests): replace hardcoded strings and add culture tests in SocialLoginValidationTests

--- tests/Unit/Modules/Identity/Application/Auth/Validators/SocialLoginValidationTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/Validators/SocialLoginValidationTests.cs
@@ -1,4 +1,6 @@
 using _116.Identity.Application.Auth.Validators;
+using _116.Identity.Application.Shared.Errors.Messages;
+using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
 using FluentValidation;
 using FluentValidation.TestHelper;
@@ -11,6 +13,8 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Auth.Validators;
 /// </summary>
 public class SocialLoginValidationTests
 {
+    private readonly ValidationErrorMessage _enMsg = LocalizerFactory.CreateMessage<ValidationErrorMessage>("en");
+
     private class TestAvatarUrlCommand
     {
         public string? AvatarUrl { get; set; }
@@ -23,21 +27,17 @@ public class SocialLoginValidationTests
 
     private class TestAvatarUrlCommandValidator : AbstractValidator<TestAvatarUrlCommand>
     {
-        public TestAvatarUrlCommandValidator()
+        public TestAvatarUrlCommandValidator(ValidationErrorMessage i18n)
         {
-            RuleFor(x => x.AvatarUrl).ValidAvatarUrl(avatarUrlInvalid: "Avatar URL must be a valid HTTP or HTTPS URL.");
+            RuleFor(x => x.AvatarUrl).ValidAvatarUrl(i18n);
         }
     }
 
     private class TestAuthProviderCommandValidator : AbstractValidator<TestAuthProviderCommand>
     {
-        public TestAuthProviderCommandValidator()
+        public TestAuthProviderCommandValidator(ValidationErrorMessage i18n)
         {
-            RuleFor(x => x.Provider)
-                .ValidAuthProvider(
-                    authProviderRequired: "Authentication provider is required.",
-                    authProviderInvalid: "Invalid authentication provider specified."
-                );
+            RuleFor(x => x.Provider).ValidAuthProvider(i18n);
         }
     }
 
@@ -46,7 +46,7 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAvatarUrl_WithValidUrl_ShouldPass()
     {
-        var validator = new TestAvatarUrlCommandValidator();
+        var validator = new TestAvatarUrlCommandValidator(_enMsg);
         var command = new TestAvatarUrlCommand { AvatarUrl = "https://avatar.url/image.jpg" };
 
         TestValidationResult<TestAvatarUrlCommand> result = validator.TestValidate(command);
@@ -57,7 +57,7 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAvatarUrl_WithNullUrl_ShouldPass()
     {
-        var validator = new TestAvatarUrlCommandValidator();
+        var validator = new TestAvatarUrlCommandValidator(_enMsg);
         var command = new TestAvatarUrlCommand { AvatarUrl = null };
 
         TestValidationResult<TestAvatarUrlCommand> result = validator.TestValidate(command);
@@ -68,7 +68,7 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAvatarUrl_WithEmptyUrl_ShouldPass()
     {
-        var validator = new TestAvatarUrlCommandValidator();
+        var validator = new TestAvatarUrlCommandValidator(_enMsg);
         var command = new TestAvatarUrlCommand { AvatarUrl = string.Empty };
 
         TestValidationResult<TestAvatarUrlCommand> result = validator.TestValidate(command);
@@ -79,20 +79,18 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAvatarUrl_WithInvalidUrl_ShouldFail()
     {
-        var validator = new TestAvatarUrlCommandValidator();
+        var validator = new TestAvatarUrlCommandValidator(_enMsg);
         var command = new TestAvatarUrlCommand { AvatarUrl = "not-a-valid-url" };
 
         TestValidationResult<TestAvatarUrlCommand> result = validator.TestValidate(command);
 
-        result
-            .ShouldHaveValidationErrorFor(x => x.AvatarUrl)
-            .WithErrorMessage("Avatar URL must be a valid HTTP or HTTPS URL.");
+        result.ShouldHaveValidationErrorFor(x => x.AvatarUrl).WithErrorMessage(_enMsg.AvatarUrlInvalid());
     }
 
     [Fact]
     public void ValidAvatarUrl_WithHttpUrl_ShouldPass()
     {
-        var validator = new TestAvatarUrlCommandValidator();
+        var validator = new TestAvatarUrlCommandValidator(_enMsg);
         var command = new TestAvatarUrlCommand { AvatarUrl = "http://avatar.url/image.jpg" };
 
         TestValidationResult<TestAvatarUrlCommand> result = validator.TestValidate(command);
@@ -107,7 +105,7 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAuthProvider_WithGoogle_ShouldPass()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = "Google" };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
@@ -118,7 +116,7 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAuthProvider_WithFacebook_ShouldPass()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = "Facebook" };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
@@ -129,29 +127,29 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAuthProvider_WithEmptyProvider_ShouldFail()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = string.Empty };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
 
-        result.ShouldHaveValidationErrorFor(x => x.Provider).WithErrorMessage("Authentication provider is required.");
+        result.ShouldHaveValidationErrorFor(x => x.Provider).WithErrorMessage(_enMsg.AuthProviderRequired());
     }
 
     [Fact]
     public void ValidAuthProvider_WithNullProvider_ShouldFail()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = null! };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
 
-        result.ShouldHaveValidationErrorFor(x => x.Provider).WithErrorMessage("Authentication provider is required.");
+        result.ShouldHaveValidationErrorFor(x => x.Provider).WithErrorMessage(_enMsg.AuthProviderRequired());
     }
 
     [Fact]
     public void ValidAuthProvider_WithEmptyProvider_ShouldStopCascading()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = string.Empty };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
@@ -162,20 +160,18 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAuthProvider_WithInvalidProvider_ShouldFail()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = "GitHub" };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
 
-        result
-            .ShouldHaveValidationErrorFor(x => x.Provider)
-            .WithErrorMessage("Invalid authentication provider specified.");
+        result.ShouldHaveValidationErrorFor(x => x.Provider).WithErrorMessage(_enMsg.AuthProviderInvalid());
     }
 
     [Fact]
     public void ValidAuthProvider_WithLocalProvider_ShouldPass()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = "Local" };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
@@ -186,14 +182,12 @@ public class SocialLoginValidationTests
     [Fact]
     public void ValidAuthProvider_WithLowercaseProvider_ShouldFail()
     {
-        var validator = new TestAuthProviderCommandValidator();
+        var validator = new TestAuthProviderCommandValidator(_enMsg);
         var command = new TestAuthProviderCommand { Provider = "google" };
 
         TestValidationResult<TestAuthProviderCommand> result = validator.TestValidate(command);
 
-        result
-            .ShouldHaveValidationErrorFor(x => x.Provider)
-            .WithErrorMessage("Invalid authentication provider specified.");
+        result.ShouldHaveValidationErrorFor(x => x.Provider).WithErrorMessage(_enMsg.AuthProviderInvalid());
     }
 
     #endregion
