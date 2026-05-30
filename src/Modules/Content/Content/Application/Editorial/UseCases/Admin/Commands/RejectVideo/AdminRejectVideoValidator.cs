6832569c773f0fbdcbb5d1952a2c77eb6c5ed6b0
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 19:37:31 +0000

    refactor(editorial): migrate AdminRejectVideo validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/RejectVideo/AdminRejectVideoValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/RejectVideo/AdminRejectVideoValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
@@ -13,11 +13,11 @@ public class AdminRejectVideoValidator : AbstractValidator<AdminRejectVideoComma
     /// <summary>
     /// Initializes a new instance of <see cref="AdminRejectVideoValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Article validation error messages.</param>
-    public AdminRejectVideoValidator(ArticleErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminRejectVideoValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer, "VideoIdRequired", "VideoIdInvalid");
+        RuleFor(x => x.Id).IsValidGuid(i18n.Article.Msg.Localizer, "VideoIdRequired", "VideoIdInvalid");
 
-        RuleFor(x => x.Reason).ValidRejectionReason(i18n);
+        RuleFor(x => x.Reason).ValidRejectionReason(i18n.Article.Msg);
     }
 }
