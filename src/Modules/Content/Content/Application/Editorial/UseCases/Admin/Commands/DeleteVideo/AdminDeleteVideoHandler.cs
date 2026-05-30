a87955e726383b719abce22a027be2e3bb726c02
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 17:44:09 +0000

    refactor(editorial): migrate AdminDeleteVideo handler to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeleteVideo/AdminDeleteVideoHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeleteVideo/AdminDeleteVideoHandler.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -15,12 +15,12 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.DeleteVideo
 /// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="cloudinaryService">Service for deleting Cloudinary image assets.</param>
-/// <param name="videoErrors">Video domain error factory.</param>
+/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminDeleteVideoHandler(
     IVideoRepository videoRepository,
     IContentUnitOfWork unitOfWork,
     ICloudinaryService cloudinaryService,
-    VideoErrors videoErrors
+    ContentI18n i18n
 ) : ICommandHandler<AdminDeleteVideoCommand, AdminDeleteVideoResult>
 {
     /// <inheritdoc />
@@ -35,7 +35,7 @@ public class AdminDeleteVideoHandler(
 
         if (video.Status != EnumContentStatus.Draft && video.Status != EnumContentStatus.Rejected)
         {
-            throw videoErrors.CannotDeletePublishedVideo();
+            throw i18n.Video.CannotDeletePublishedVideo();
         }
 
         if (video.ThumbnailStorageKey is not null)
