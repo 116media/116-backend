7ea7f42ab1263f0ae4e5380c8527ffaf787a7fb3
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 11:07:56 +0000

    refactor(commerce): migrate AdminAddItemTier validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/AddItemTier/AdminAddItemTierValidator.cs
+++ src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/AddItemTier/AdminAddItemTierValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -10,14 +10,14 @@ namespace _116.Content.Application.Commerce.UseCases.Admin.Commands.AddItemTier;
 public class AdminAddItemTierValidator : AbstractValidator<AdminAddItemTierCommand>
 {
     /// <summary>
-    /// Initializes a new instance of <see cref="AdminAddItemTierValidator" /> with the specified error message providers.
+    /// Initializes a new instance of <see cref="AdminAddItemTierValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="orderMsg">Content order validation error messages.</param>
-    /// <param name="pricingTierMsg">Pricing tier validation error messages.</param>
-    public AdminAddItemTierValidator(ContentOrderErrorMessage orderMsg, PricingTierErrorMessage pricingTierMsg)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminAddItemTierValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.OrderId).IsValidGuid(orderMsg.Localizer);
-        RuleFor(x => x.OrderItemId).IsValidGuid(orderMsg.Localizer, "OrderItemIdRequired", "OrderItemIdInvalid");
-        RuleFor(x => x.PricingTierId).IsValidGuid(pricingTierMsg.Localizer);
+        RuleFor(x => x.OrderId).IsValidGuid(i18n.ContentOrder.Msg.Localizer);
+        RuleFor(x => x.OrderItemId)
+            .IsValidGuid(i18n.ContentOrder.Msg.Localizer, "OrderItemIdRequired", "OrderItemIdInvalid");
+        RuleFor(x => x.PricingTierId).IsValidGuid(i18n.PricingTier.Msg.Localizer);
     }
 }
