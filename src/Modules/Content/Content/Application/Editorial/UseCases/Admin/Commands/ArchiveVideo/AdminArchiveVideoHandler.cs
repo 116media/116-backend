f3375344067cdc12a8e088582d111ef6bd2bb49b
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 15:03:38 +0000

    refactor(editorial): migrate AdminArchiveVideo handler to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveVideo/AdminArchiveVideoHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveVideo/AdminArchiveVideoHandler.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -12,12 +12,9 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.ArchiveVide
 /// </summary>
 /// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-/// <param name="videoErrors">Video domain error factory.</param>
-public class AdminArchiveVideoHandler(
-    IVideoRepository videoRepository,
-    IContentUnitOfWork unitOfWork,
-    VideoErrors videoErrors
-) : ICommandHandler<AdminArchiveVideoCommand, AdminArchiveVideoResult>
+/// <param name="i18n">Single i18n entry point for the Content module.</param>
+public class AdminArchiveVideoHandler(IVideoRepository videoRepository, IContentUnitOfWork unitOfWork, ContentI18n i18n)
+    : ICommandHandler<AdminArchiveVideoCommand, AdminArchiveVideoResult>
 {
     /// <inheritdoc />
     public async Task<AdminArchiveVideoResult> Handle(
@@ -33,7 +30,7 @@ public class AdminArchiveVideoHandler(
 
         if (!archived)
         {
-            throw videoErrors.AlreadyArchived();
+            throw i18n.Video.AlreadyArchived();
         }
 
         videoRepository.Update(video: video);
