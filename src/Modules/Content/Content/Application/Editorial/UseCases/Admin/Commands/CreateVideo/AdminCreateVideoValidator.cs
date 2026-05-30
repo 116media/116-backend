a4706deeff10fd9bd134ff727ff9bf02c3a88ba2
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 16:42:24 +0000

    refactor(editorial): migrate AdminCreateVideo validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateVideo/AdminCreateVideoValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateVideo/AdminCreateVideoValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -10,27 +10,19 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateVideo
 public class AdminCreateVideoValidator : AbstractValidator<AdminCreateVideoCommand>
 {
     /// <summary>
-    /// Initializes a new instance of <see cref="AdminCreateVideoValidator" /> with the specified error message providers.
+    /// Initializes a new instance of <see cref="AdminCreateVideoValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="articleMsg">Article validation error messages.</param>
-    /// <param name="videoMsg">Video validation error messages.</param>
-    /// <param name="orderMsg">Content order validation error messages.</param>
-    /// <param name="customerMsg">Customer validation error messages.</param>
-    public AdminCreateVideoValidator(
-        ArticleErrorMessage articleMsg,
-        VideoErrorMessage videoMsg,
-        ContentOrderErrorMessage orderMsg,
-        CustomerErrorMessage customerMsg
-    )
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminCreateVideoValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.CategoryId).ValidArticleCategoryId(articleMsg.CategoryIdRequired());
+        RuleFor(x => x.CategoryId).ValidArticleCategoryId(i18n.Article.Msg.CategoryIdRequired());
 
-        RuleFor(x => x.Title).ValidVideoTitle(videoMsg);
-        RuleFor(x => x.Slug).ValidVideoSlug(videoMsg);
+        RuleFor(x => x.Title).ValidVideoTitle(i18n.Video.Msg);
+        RuleFor(x => x.Slug).ValidVideoSlug(i18n.Video.Msg);
 
-        RuleFor(x => x.Description).ValidVideoDescription(videoMsg);
+        RuleFor(x => x.Description).ValidVideoDescription(i18n.Video.Msg);
 
-        When(x => x.CustomerId.HasValue, () => RuleFor(x => x.OrderItemId).ValidOrderItemId(orderMsg));
-        When(x => x.OrderItemId.HasValue, () => RuleFor(x => x.CustomerId).ValidCustomerId(customerMsg));
+        When(x => x.CustomerId.HasValue, () => RuleFor(x => x.OrderItemId).ValidOrderItemId(i18n.ContentOrder.Msg));
+        When(x => x.OrderItemId.HasValue, () => RuleFor(x => x.CustomerId).ValidCustomerId(i18n.Customer.Msg));
     }
 }
