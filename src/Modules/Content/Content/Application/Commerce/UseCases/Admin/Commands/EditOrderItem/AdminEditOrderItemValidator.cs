37bf692bc8a10c16134ff249ba8d0286647941f6
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 12:25:23 +0000

    refactor(commerce): migrate AdminEditOrderItem validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/EditOrderItem/AdminEditOrderItemValidator.cs
+++ src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/EditOrderItem/AdminEditOrderItemValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
@@ -13,15 +13,17 @@ public class AdminEditOrderItemValidator : AbstractValidator<AdminEditOrderItemC
     /// <summary>
     /// Initializes a new instance of <see cref="AdminEditOrderItemValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Content order validation error messages.</param>
-    /// <param name="categoryMsg">Category validation error messages.</param>
-    public AdminEditOrderItemValidator(ContentOrderErrorMessage i18n, CategoryErrorMessage categoryMsg)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminEditOrderItemValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.OrderId).IsValidGuid(i18n.Localizer);
-        RuleFor(x => x.ItemId).IsValidGuid(i18n.Localizer, "ItemIdRequired", "ItemIdInvalid");
+        RuleFor(x => x.OrderId).IsValidGuid(i18n.ContentOrder.Msg.Localizer);
+        RuleFor(x => x.ItemId).IsValidGuid(i18n.ContentOrder.Msg.Localizer, "ItemIdRequired", "ItemIdInvalid");
 
-        When(x => x.CategoryId is not null, () => RuleFor(x => x.CategoryId!).IsValidGuid(categoryMsg.Localizer));
+        When(x => x.CategoryId is not null, () => RuleFor(x => x.CategoryId!).IsValidGuid(i18n.Category.Msg.Localizer));
 
-        When(x => x.ContentKind.HasValue, () => RuleFor(x => x.ContentKind!.Value).ValidOrderItemContentKind(i18n));
+        When(
+            x => x.ContentKind.HasValue,
+            () => RuleFor(x => x.ContentKind!.Value).ValidOrderItemContentKind(i18n.ContentOrder.Msg)
+        );
     }
 }
