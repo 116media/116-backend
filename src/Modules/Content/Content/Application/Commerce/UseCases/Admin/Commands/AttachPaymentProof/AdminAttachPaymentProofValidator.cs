53bf7a54db417a9e90bc32684352cfa77a6ca728
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 11:25:32 +0000

    refactor(commerce): migrate AdminAttachPaymentProof validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/AttachPaymentProof/AdminAttachPaymentProofValidator.cs
+++ src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/AttachPaymentProof/AdminAttachPaymentProofValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
@@ -13,11 +13,11 @@ public class AdminAttachPaymentProofValidator : AbstractValidator<AdminAttachPay
     /// <summary>
     /// Initializes a new instance of <see cref="AdminAttachPaymentProofValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Content order validation error messages.</param>
-    public AdminAttachPaymentProofValidator(ContentOrderErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminAttachPaymentProofValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.OrderId).IsValidGuid(i18n.Localizer);
-        RuleFor(x => x.File).ValidPaymentProofFile(i18n);
-        RuleFor(x => x.PaymentMethod).ValidPaymentMethod(i18n);
+        RuleFor(x => x.OrderId).IsValidGuid(i18n.ContentOrder.Msg.Localizer);
+        RuleFor(x => x.File).ValidPaymentProofFile(i18n.ContentOrder.Msg);
+        RuleFor(x => x.PaymentMethod).ValidPaymentMethod(i18n.ContentOrder.Msg);
     }
 }
