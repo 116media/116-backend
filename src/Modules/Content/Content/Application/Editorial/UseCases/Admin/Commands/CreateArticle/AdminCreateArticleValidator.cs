a295d42e092984f2df699dd752e818b86ff701d1
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 15:48:36 +0000

    refactor(editorial): migrate AdminCreateArticle validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateArticle/AdminCreateArticleValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateArticle/AdminCreateArticleValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -10,22 +10,16 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateArtic
 public class AdminCreateArticleValidator : AbstractValidator<AdminCreateArticleCommand>
 {
     /// <summary>
-    /// Initializes a new instance of <see cref="AdminCreateArticleValidator" /> with the specified error message providers.
+    /// Initializes a new instance of <see cref="AdminCreateArticleValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="articleMsg">Article validation error messages.</param>
-    /// <param name="orderMsg">Content order validation error messages.</param>
-    /// <param name="customerMsg">Customer validation error messages.</param>
-    public AdminCreateArticleValidator(
-        ArticleErrorMessage articleMsg,
-        ContentOrderErrorMessage orderMsg,
-        CustomerErrorMessage customerMsg
-    )
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminCreateArticleValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.CategoryId).ValidArticleCategoryId(articleMsg.CategoryIdRequired());
-        RuleFor(x => x.Title).ValidArticleTitle(articleMsg);
-        RuleFor(x => x.Slug).ValidArticleSlug(articleMsg);
+        RuleFor(x => x.CategoryId).ValidArticleCategoryId(i18n.Article.Msg.CategoryIdRequired());
+        RuleFor(x => x.Title).ValidArticleTitle(i18n.Article.Msg);
+        RuleFor(x => x.Slug).ValidArticleSlug(i18n.Article.Msg);
 
-        When(x => x.CustomerId.HasValue, () => RuleFor(x => x.OrderItemId).ValidOrderItemId(orderMsg));
-        When(x => x.OrderItemId.HasValue, () => RuleFor(x => x.CustomerId).ValidCustomerId(customerMsg));
+        When(x => x.CustomerId.HasValue, () => RuleFor(x => x.OrderItemId).ValidOrderItemId(i18n.ContentOrder.Msg));
+        When(x => x.OrderItemId.HasValue, () => RuleFor(x => x.CustomerId).ValidCustomerId(i18n.Customer.Msg));
     }
 }
