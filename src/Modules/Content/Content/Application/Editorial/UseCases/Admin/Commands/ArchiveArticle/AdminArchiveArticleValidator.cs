56eea111b12b26f256423cad3e0c36f4e0f3f78d
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 14:54:13 +0000

    refactor(editorial): migrate AdminArchiveArticle validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveArticle/AdminArchiveArticleValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveArticle/AdminArchiveArticleValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -12,9 +12,9 @@ public class AdminArchiveArticleValidator : AbstractValidator<AdminArchiveArticl
     /// <summary>
     /// Initializes a new instance of <see cref="AdminArchiveArticleValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Article validation error messages.</param>
-    public AdminArchiveArticleValidator(ArticleErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminArchiveArticleValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
+        RuleFor(x => x.Id).IsValidGuid(i18n.Article.Msg.Localizer);
     }
 }
