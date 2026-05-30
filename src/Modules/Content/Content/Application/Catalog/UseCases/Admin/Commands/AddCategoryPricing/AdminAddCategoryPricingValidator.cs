9546852f89037032dd603729278ab048b617749e
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 07:44:13 +0000

    refactor(catalog): migrate AdminAddCategoryPricing validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/AddCategoryPricing/AdminAddCategoryPricingValidator.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/AddCategoryPricing/AdminAddCategoryPricingValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
@@ -11,14 +11,13 @@ namespace _116.Content.Application.Catalog.UseCases.Admin.Commands.AddCategoryPr
 public class AdminAddCategoryPricingValidator : AbstractValidator<AdminAddCategoryPricingCommand>
 {
     /// <summary>
-    /// Initializes a new instance of <see cref="AdminAddCategoryPricingValidator" /> with the specified error message providers.
+    /// Initializes a new instance of <see cref="AdminAddCategoryPricingValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="categoryMsg">Category validation error messages.</param>
-    /// <param name="pricingTierMsg">Pricing tier validation error messages.</param>
-    public AdminAddCategoryPricingValidator(CategoryErrorMessage categoryMsg, PricingTierErrorMessage pricingTierMsg)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminAddCategoryPricingValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.CategoryId).IsValidGuid(categoryMsg.Localizer);
-        RuleFor(x => x.PricingTierId).ValidPricingTierId(pricingTierMsg);
-        RuleFor(x => x.PriceUsd).ValidCategoryPriceUsd(categoryMsg);
+        RuleFor(x => x.CategoryId).IsValidGuid(i18n.Category.Msg.Localizer);
+        RuleFor(x => x.PricingTierId).ValidPricingTierId(i18n.PricingTier.Msg);
+        RuleFor(x => x.PriceUsd).ValidCategoryPriceUsd(i18n.Category.Msg);
     }
 }
