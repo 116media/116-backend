52bcdd336a6c75c34d86b656a1f1fefc0ad43461
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 18:27:11 +0000

    refactor(editorial): migrate AdminForceUnpromoteVideo validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ForceUnpromoteVideo/AdminForceUnpromoteVideoValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ForceUnpromoteVideo/AdminForceUnpromoteVideoValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using _116.Content.Domain.Constants;
 using FluentValidation;
@@ -11,17 +11,16 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.ForceUnprom
 public class AdminForceUnpromoteVideoValidator : AbstractValidator<AdminForceUnpromoteVideoCommand>
 {
     /// <summary>
-    /// Initializes a new instance of <see cref="AdminForceUnpromoteVideoValidator" /> with the specified error message providers.
+    /// Initializes a new instance of <see cref="AdminForceUnpromoteVideoValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="articleMsg">Article validation error messages.</param>
-    /// <param name="videoMsg">Video validation error messages.</param>
-    public AdminForceUnpromoteVideoValidator(ArticleErrorMessage articleMsg, VideoErrorMessage videoMsg)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminForceUnpromoteVideoValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Slug).ValidVideoSlug(videoMsg);
+        RuleFor(x => x.Slug).ValidVideoSlug(i18n.Video.Msg);
         RuleFor(x => x.Reason)
             .ValidUnpromoteReason(
-                reasonRequired: articleMsg.RejectionReasonRequired(),
-                reasonTooLong: articleMsg.RejectionReasonTooLong(ContentConstants.MaxRejectionReasonLength)
+                reasonRequired: i18n.Article.Msg.RejectionReasonRequired(),
+                reasonTooLong: i18n.Article.Msg.RejectionReasonTooLong(ContentConstants.MaxRejectionReasonLength)
             );
     }
 }
