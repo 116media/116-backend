9ca3615e3fa69d4ad4659f1bbb8e229deed8bcc0
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 10:14:32 +0000

    refactor(catalog): migrate AdminUpdateCategory handler to ContentI18n facade

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCategory/AdminUpdateCategoryHandler.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCategory/AdminUpdateCategoryHandler.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -14,12 +14,12 @@ namespace _116.Content.Application.Catalog.UseCases.Admin.Commands.UpdateCategor
 /// <param name="categoryRepository">Repository for category data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
-/// <param name="categoryErrors">Category domain error factory.</param>
+/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminUpdateCategoryHandler(
     ICategoryRepository categoryRepository,
     IContentUnitOfWork unitOfWork,
     IMapper mapper,
-    CategoryErrors categoryErrors
+    ContentI18n i18n
 ) : ICommandHandler<AdminUpdateCategoryCommand, AdminUpdateCategoryResult>
 {
     /// <inheritdoc />
@@ -42,7 +42,7 @@ public class AdminUpdateCategoryHandler(
 
         if (slugConflict is not null && slugConflict.Id != id)
         {
-            throw categoryErrors.AlreadyExists(slug: command.Slug);
+            throw i18n.Category.AlreadyExists(slug: command.Slug);
         }
 
         category.Update(
@@ -50,7 +50,7 @@ public class AdminUpdateCategoryHandler(
             slug: command.Slug,
             description: command.Description,
             isGossip: command.IsGossip,
-            errors: categoryErrors
+            errors: i18n.Category
         );
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
