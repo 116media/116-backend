a924527be86f8b39a3e0a841c0116b9d883d2e42
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 17:26:21 +0000

    refactor(editorial): migrate AdminDeleteLyrics validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeleteLyrics/AdminDeleteLyricsValidator.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeleteLyrics/AdminDeleteLyricsValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -12,9 +12,9 @@ public class AdminDeleteLyricsValidator : AbstractValidator<AdminDeleteLyricsCom
     /// <summary>
     /// Initializes a new instance of <see cref="AdminDeleteLyricsValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Lyrics validation error messages.</param>
-    public AdminDeleteLyricsValidator(LyricsErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminDeleteLyricsValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
+        RuleFor(x => x.Id).IsValidGuid(i18n.Lyrics.Msg.Localizer);
     }
 }
