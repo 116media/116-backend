8ea40f0e1aeef13c26a26ea5f54162e131b3fe49
coolbeatz71 <sigmacool@gmail.com>
2026-05-30 09:22:00 +0000

    refactor(catalog): migrate AdminDeactivatePackage handler to ContentI18n facade

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/DeactivatePackage/AdminDeactivatePackageHandler.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/DeactivatePackage/AdminDeactivatePackageHandler.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -14,12 +14,12 @@ namespace _116.Content.Application.Catalog.UseCases.Admin.Commands.DeactivatePac
 /// <param name="packageRepository">Repository for package data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
-/// <param name="packageErrors">Package domain error factory.</param>
+/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminDeactivatePackageHandler(
     IPackageRepository packageRepository,
     IContentUnitOfWork unitOfWork,
     IMapper mapper,
-    PackageErrors packageErrors
+    ContentI18n i18n
 ) : ICommandHandler<AdminDeactivatePackageCommand, AdminDeactivatePackageResult>
 {
     /// <inheritdoc />
@@ -39,7 +39,7 @@ public class AdminDeactivatePackageHandler(
 
         if (!deactivated)
         {
-            throw packageErrors.AlreadyInactive();
+            throw i18n.Package.AlreadyInactive();
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
