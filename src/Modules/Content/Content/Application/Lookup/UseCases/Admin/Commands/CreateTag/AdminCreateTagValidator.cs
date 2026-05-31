2dad7837e56a2643832d71b88759c34c2bb1965d
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 14:23:50 +0000

    refactor(lookup): migrate AdminCreateTag validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreateTag/AdminCreateTagValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreateTag/AdminCreateTagValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -12,10 +12,10 @@ public class AdminCreateTagValidator : AbstractValidator<AdminCreateTagCommand>
     /// <summary>
     /// Initializes a new instance of <see cref="AdminCreateTagValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Tag validation error messages.</param>
-    public AdminCreateTagValidator(TagErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminCreateTagValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Name).ValidTagName(i18n);
-        RuleFor(x => x.Slug).ValidTagSlug(i18n);
+        RuleFor(x => x.Name).ValidTagName(i18n.Tag.Msg);
+        RuleFor(x => x.Slug).ValidTagSlug(i18n.Tag.Msg);
     }
 }
