e0ed484b8107542bdadbfb0892fa873f7d81052a
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 15:16:54 +0000

    refactor(lookup): migrate AdminDeactivatePromotionLevel validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePromotionLevel/AdminDeactivatePromotionLevelValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePromotionLevel/AdminDeactivatePromotionLevelValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -12,9 +12,9 @@ public class AdminDeactivatePromotionLevelValidator : AbstractValidator<AdminDea
     /// <summary>
     /// Initializes a new instance of <see cref="AdminDeactivatePromotionLevelValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Promotion level validation error messages.</param>
-    public AdminDeactivatePromotionLevelValidator(PromotionLevelErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminDeactivatePromotionLevelValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
+        RuleFor(x => x.Id).IsValidGuid(i18n.PromotionLevel.Msg.Localizer);
     }
 }
