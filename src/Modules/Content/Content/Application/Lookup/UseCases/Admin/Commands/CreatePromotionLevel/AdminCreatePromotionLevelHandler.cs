c7813ade4fc0c065a3078edbbd4b420a2cef3e36
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 13:58:05 +0000

    refactor(lookup): migrate AdminCreatePromotionLevel handler to ContentI18n facade

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreatePromotionLevel/AdminCreatePromotionLevelHandler.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/CreatePromotionLevel/AdminCreatePromotionLevelHandler.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -14,12 +14,12 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.CreatePromotio
 /// <param name="lookupRepository">Repository for lookup data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
-/// <param name="promotionLevelErrors">Promotion level domain error factory.</param>
+/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminCreatePromotionLevelHandler(
     ILookupRepository lookupRepository,
     IContentUnitOfWork unitOfWork,
     IMapper mapper,
-    PromotionLevelErrors promotionLevelErrors
+    ContentI18n i18n
 ) : ICommandHandler<AdminCreatePromotionLevelCommand, AdminCreatePromotionLevelResult>
 {
     /// <inheritdoc />
@@ -35,7 +35,7 @@ public class AdminCreatePromotionLevelHandler(
 
         if (exists)
         {
-            throw promotionLevelErrors.AlreadyExists(name: command.Name);
+            throw i18n.PromotionLevel.AlreadyExists(name: command.Name);
         }
 
         var promotionLevel = PromotionLevelEntity.Create(
@@ -44,7 +44,7 @@ public class AdminCreatePromotionLevelHandler(
             durationDays: command.DurationDays,
             priceUsd: command.PriceUsd,
             spotPriority: command.SpotPriority,
-            errors: promotionLevelErrors
+            errors: i18n.PromotionLevel
         );
 
         await lookupRepository.AddPromotionLevelAsync(
