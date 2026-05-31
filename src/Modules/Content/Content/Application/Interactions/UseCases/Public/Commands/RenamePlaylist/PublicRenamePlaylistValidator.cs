8f16aef210623ed6ac3b7abcd26ca569ff61636e
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 11:37:56 +0000

    refactor(interactions): migrate PublicRenamePlaylist validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/RenamePlaylist/PublicRenamePlaylistValidator.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/RenamePlaylist/PublicRenamePlaylistValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -12,9 +12,9 @@ public class PublicRenamePlaylistValidator : AbstractValidator<PublicRenamePlayl
     /// <summary>
     /// Initializes a new instance of <see cref="PublicRenamePlaylistValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Playlist validation error messages.</param>
-    public PublicRenamePlaylistValidator(PlaylistErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public PublicRenamePlaylistValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Name).ValidPlaylistName(i18n);
+        RuleFor(x => x.Name).ValidPlaylistName(i18n.Playlist.Msg);
     }
 }
