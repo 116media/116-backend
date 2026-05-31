44ca60877907a20f6d511fcb1998b28c64c1d939
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 17:22:55 +0000

    refactor(shared): update ContentTypeErrors to use ContentI18n facade

--- src/Modules/Content/Content/Application/Shared/Errors/ContentTypeErrors.cs
+++ src/Modules/Content/Content/Application/Shared/Errors/ContentTypeErrors.cs
@@ -10,6 +10,11 @@ namespace _116.Content.Application.Shared.Errors;
 public class ContentTypeErrors(ContentTypeErrorMessage i18n)
 {
     /// <summary>
+    /// Exposes the localized message provider for use in validator extensions.
+    /// </summary>
+    public ContentTypeErrorMessage Msg => i18n;
+
+    /// <summary>
     /// Throws when a content type with the given name already exists.
     /// </summary>
     public ConflictException AlreadyExists(string name)
