c9ec2f2858c2ad7c0d3e0af682790dcbbeb1a988
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 20:36:25 +0000

    refactor(identity): migrate AdminResendOtp validator to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/ResendOtp/AdminResendOtpValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/ResendOtp/AdminResendOtpValidator.cs
@@ -1,5 +1,5 @@
 using _116.Identity.Application.Auth.Validators;
-using _116.Identity.Application.Shared.Errors.Messages;
+using _116.Identity.Application.Shared.Errors.Facade;
 using FluentValidation;
 
 namespace _116.Identity.Application.Auth.UseCases.Admin.Commands.ResendOtp;
@@ -18,11 +18,11 @@ public class AdminResendOtpValidator : AbstractValidator<AdminResendOtpCommand>
     /// Initializes a new instance of <see cref="AdminResendOtpValidator" /> with validation rules.
     /// </summary>
     /// <param name="i18n">
-    /// Validation error messages for rule configuration.
+    /// Identity module i18n facade for rule configuration.
     /// </param>
-    public AdminResendOtpValidator(ValidationErrorMessage i18n)
+    public AdminResendOtpValidator(IdentityI18n i18n)
     {
-        RuleFor(x => x.Email).ValidEmail(i18n);
-        RuleFor(x => x.Purpose).ValidOtpPurpose(i18n);
+        RuleFor(x => x.Email).ValidEmail(i18n.User.Validation);
+        RuleFor(x => x.Purpose).ValidOtpPurpose(i18n.User.Validation);
     }
 }
