0753c5216624ec18bed7d30e06ab7285e4616112
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 11:10:58 +0000

    refactor(interactions): migrate PublicRateVideo validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/RateVideo/PublicRateVideoValidator.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/RateVideo/PublicRateVideoValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using FluentValidation;
 
@@ -12,9 +12,9 @@ public class PublicRateVideoValidator : AbstractValidator<PublicRateVideoCommand
     /// <summary>
     /// Initializes a new instance of <see cref="PublicRateVideoValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Article interaction validation error messages.</param>
-    public PublicRateVideoValidator(ArticleInteractionErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public PublicRateVideoValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Stars).ValidVideoStarRating(i18n);
+        RuleFor(x => x.Stars).ValidVideoStarRating(i18n.ArticleInteraction.Msg);
     }
 }
