58f6098ac6133d784708f17256ffbb559a0b9d3a
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 12:56:34 +0000

    refactor(lookup): migrate AdminActivatePricingTier validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/AdminActivatePricingTierValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePricingTier/AdminActivatePricingTierValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -12,9 +12,9 @@ public class AdminActivatePricingTierValidator : AbstractValidator<AdminActivate
     /// <summary>
     /// Initializes a new instance of <see cref="AdminActivatePricingTierValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Pricing tier validation error messages.</param>
-    public AdminActivatePricingTierValidator(PricingTierErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminActivatePricingTierValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
+        RuleFor(x => x.Id).IsValidGuid(i18n.PricingTier.Msg.Localizer);
     }
 }
