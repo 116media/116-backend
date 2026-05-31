8f1c8241d67abe019dca9c9c5e97470c14c2c2f2
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 21:02:44 +0000

    refactor(identity): migrate AdminVerifyOtp validator to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/VerifyOtp/AdminVerifyOtpValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/VerifyOtp/AdminVerifyOtpValidator.cs
@@ -1,5 +1,5 @@
 using _116.Identity.Application.Auth.Validators;
-using _116.Identity.Application.Shared.Errors.Messages;
+using _116.Identity.Application.Shared.Errors.Facade;
 using FluentValidation;
 
 namespace _116.Identity.Application.Auth.UseCases.Admin.Commands.VerifyOtp;
@@ -19,12 +19,12 @@ public class AdminVerifyOtpValidator : AbstractValidator<AdminVerifyOtpCommand>
     /// Initializes a new instance of <see cref="AdminVerifyOtpValidator" /> with validation rules.
     /// </summary>
     /// <param name="i18n">
-    /// Validation error messages for rule configuration.
+    /// Identity module i18n facade for rule configuration.
     /// </param>
-    public AdminVerifyOtpValidator(ValidationErrorMessage i18n)
+    public AdminVerifyOtpValidator(IdentityI18n i18n)
     {
-        RuleFor(x => x.Email).ValidEmail(i18n);
-        RuleFor(x => x.Code).ValidOtpCode(i18n);
-        RuleFor(x => x.Purpose).ValidOtpPurpose(i18n);
+        RuleFor(x => x.Email).ValidEmail(i18n.User.Validation);
+        RuleFor(x => x.Code).ValidOtpCode(i18n.User.Validation);
+        RuleFor(x => x.Purpose).ValidOtpPurpose(i18n.User.Validation);
     }
 }
