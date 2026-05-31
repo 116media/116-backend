647755bdcf4f6dcb74fabbb117bcee7ceb6bbc08
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 20:27:50 +0000

    refactor(identity): migrate AdminLogin validator to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Admin/Commands/Login/AdminLoginValidator.cs
@@ -1,5 +1,5 @@
 using _116.Identity.Application.Auth.Validators;
-using _116.Identity.Application.Shared.Errors.Messages;
+using _116.Identity.Application.Shared.Errors.Facade;
 using FluentValidation;
 
 namespace _116.Identity.Application.Auth.UseCases.Admin.Commands.Login;
@@ -13,14 +13,14 @@ public class AdminLoginValidator : AbstractValidator<AdminLoginCommand>
     /// Initializes a new instance of <see cref="AdminLoginValidator" /> with validation rules.
     /// </summary>
     /// <param name="i18n">
-    /// Validation error messages for rule configuration.
+    /// Identity module i18n facade for rule configuration.
     /// </param>
     /// <remarks>
     /// Validates email and password presence for admin login attempts.
     /// </remarks>
-    public AdminLoginValidator(ValidationErrorMessage i18n)
+    public AdminLoginValidator(IdentityI18n i18n)
     {
-        RuleFor(x => x.Email).ValidEmail(i18n);
-        RuleFor(x => x.Password).ValidPassword(i18n, isStrong: false);
+        RuleFor(x => x.Email).ValidEmail(i18n.User.Validation);
+        RuleFor(x => x.Password).ValidPassword(i18n.User.Validation, isStrong: false);
     }
 }
