c960e9395e39d37961034155622ad2d5a19a9b9a
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 16:19:52 +0000

    refactor(lookup): migrate AdminUpdatePromotionLevel validator to ContentI18n facade

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/UpdatePromotionLevel/AdminUpdatePromotionLevelValidator.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/UpdatePromotionLevel/AdminUpdatePromotionLevelValidator.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Validators;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
@@ -13,13 +13,16 @@ public class AdminUpdatePromotionLevelValidator : AbstractValidator<AdminUpdateP
     /// <summary>
     /// Initializes a new instance of <see cref="AdminUpdatePromotionLevelValidator" /> with the specified error message provider.
     /// </summary>
-    /// <param name="i18n">Promotion level validation error messages.</param>
-    public AdminUpdatePromotionLevelValidator(PromotionLevelErrorMessage i18n)
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminUpdatePromotionLevelValidator(ContentI18n i18n)
     {
-        RuleFor(x => x.Id).IsValidGuid(i18n.Localizer);
-        RuleFor(x => x.Name).ValidPromotionLevelName(i18n);
-        RuleFor(x => x.DurationDays).ValidDurationDays(i18n);
-        RuleFor(x => x.PriceUsd).ValidPriceUsd(i18n);
-        When(x => x.SpotPriority.HasValue, () => RuleFor(x => x.SpotPriority).ValidSpotPriority(i18n));
+        RuleFor(x => x.Id).IsValidGuid(i18n.PromotionLevel.Msg.Localizer);
+        RuleFor(x => x.Name).ValidPromotionLevelName(i18n.PromotionLevel.Msg);
+        RuleFor(x => x.DurationDays).ValidDurationDays(i18n.PromotionLevel.Msg);
+        RuleFor(x => x.PriceUsd).ValidPriceUsd(i18n.PromotionLevel.Msg);
+        When(
+            x => x.SpotPriority.HasValue,
+            () => RuleFor(x => x.SpotPriority).ValidSpotPriority(i18n.PromotionLevel.Msg)
+        );
     }
 }
