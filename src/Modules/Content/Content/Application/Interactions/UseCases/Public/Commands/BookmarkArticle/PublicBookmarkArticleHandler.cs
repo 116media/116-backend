956950ec1d6cf7d67152100fb901148ea6cd3983
coolbeatz71 <sigmacool@gmail.com>
2026-05-31 09:49:58 +0000

    refactor(interactions): migrate PublicBookmarkArticle handler to ContentI18n facade

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/PublicBookmarkArticleHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/PublicBookmarkArticleHandler.cs
@@ -1,4 +1,4 @@
-using _116.Content.Application.Shared.Errors;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -11,11 +11,11 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.Bookmar
 /// </summary>
 /// <param name="articleRepository">Repository for article data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-/// <param name="articleInteractionErrors">Article interaction domain error factory.</param>
+/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class PublicBookmarkArticleHandler(
     IArticleRepository articleRepository,
     IContentUnitOfWork unitOfWork,
-    ArticleInteractionErrors articleInteractionErrors
+    ContentI18n i18n
 ) : ICommandHandler<PublicBookmarkArticleCommand, PublicBookmarkArticleResult>
 {
     /// <inheritdoc />
@@ -37,7 +37,7 @@ public class PublicBookmarkArticleHandler(
 
         if (alreadyBookmarked)
         {
-            throw articleInteractionErrors.AlreadyBookmarked();
+            throw i18n.ArticleInteraction.AlreadyBookmarked();
         }
 
         var bookmark = ArticleBookmarkEntity.Create(
