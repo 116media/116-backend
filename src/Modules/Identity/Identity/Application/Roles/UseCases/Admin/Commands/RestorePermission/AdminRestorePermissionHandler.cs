14f30c9c7d1d2fee037c06f57f9c46e7f6d9f4ff
coolbeatz71 <sigmacool@gmail.com>
2026-06-01 11:06:27 +0000

    refactor(identity): migrate AdminRestorePermission handler to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestorePermission/AdminRestorePermissionHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/RestorePermission/AdminRestorePermissionHandler.cs
@@ -1,4 +1,4 @@
-using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Mappers;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
@@ -14,12 +14,12 @@ namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.RestorePermiss
 /// <param name="permissionRepository">Repository for permission data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
-/// <param name="userErrors">User domain error factory for generating domain exceptions.</param>
+/// <param name="i18n">Single i18n entry point for the Identity module.</param>
 public class AdminRestorePermissionHandler(
     IPermissionRepository permissionRepository,
     IIdentityUnitOfWork unitOfWork,
     IMapper mapper,
-    UserErrors userErrors
+    IdentityI18n i18n
 ) : ICommandHandler<AdminRestorePermissionCommand, AdminRestorePermissionResult>
 {
     /// <summary>
@@ -44,7 +44,7 @@ public class AdminRestorePermissionHandler(
 
         if (!wasRestored)
         {
-            throw userErrors.PermissionNotDeleted();
+            throw i18n.User.PermissionNotDeleted();
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
