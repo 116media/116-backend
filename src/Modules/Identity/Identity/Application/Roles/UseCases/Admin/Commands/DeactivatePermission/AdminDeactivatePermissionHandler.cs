823453204cddad26779a2960a9287c63cee38163
coolbeatz71 <sigmacool@gmail.com>
2026-06-01 09:48:49 +0000

    refactor(identity): migrate AdminDeactivatePermission handler to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivatePermission/AdminDeactivatePermissionHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivatePermission/AdminDeactivatePermissionHandler.cs
@@ -1,4 +1,4 @@
-using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Mappers;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
@@ -14,12 +14,12 @@ namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.DeactivatePerm
 /// <param name="permissionRepository">Repository for permission data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
-/// <param name="userErrors">User domain error factory for generating domain exceptions.</param>
+/// <param name="i18n">Single i18n entry point for the Identity module.</param>
 public class AdminDeactivatePermissionHandler(
     IPermissionRepository permissionRepository,
     IIdentityUnitOfWork unitOfWork,
     IMapper mapper,
-    UserErrors userErrors
+    IdentityI18n i18n
 ) : ICommandHandler<AdminDeactivatePermissionCommand, AdminDeactivatePermissionResult>
 {
     /// <summary>
@@ -44,7 +44,7 @@ public class AdminDeactivatePermissionHandler(
 
         if (!wasDeactivated)
         {
-            throw userErrors.PermissionAlreadyInactive();
+            throw i18n.User.PermissionAlreadyInactive();
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
