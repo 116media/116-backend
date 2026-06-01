8a63afbafb11cd6ff055961b79016cadd0ac0198
coolbeatz71 <sigmacool@gmail.com>
2026-06-01 11:59:35 +0000

    refactor(identity): migrate AdminSoftDeleteRole handler to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/SoftDeleteRole/AdminSoftDeleteRoleHandler.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/SoftDeleteRole/AdminSoftDeleteRoleHandler.cs
@@ -1,4 +1,4 @@
-using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Mappers;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
@@ -14,12 +14,12 @@ namespace _116.Identity.Application.Roles.UseCases.Admin.Commands.SoftDeleteRole
 /// <param name="roleRepository">Repository for role data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
-/// <param name="userErrors">User domain error factory for generating domain exceptions.</param>
+/// <param name="i18n">Single i18n entry point for the Identity module.</param>
 public class AdminSoftDeleteRoleHandler(
     IRoleRepository roleRepository,
     IIdentityUnitOfWork unitOfWork,
     IMapper mapper,
-    UserErrors userErrors
+    IdentityI18n i18n
 ) : ICommandHandler<AdminSoftDeleteRoleCommand, AdminSoftDeleteRoleResult>
 {
     /// <summary>
@@ -44,7 +44,7 @@ public class AdminSoftDeleteRoleHandler(
 
         if (!wasSoftDeleted)
         {
-            throw userErrors.RoleAlreadyDeleted();
+            throw i18n.User.RoleAlreadyDeleted();
         }
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
