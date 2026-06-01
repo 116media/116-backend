55f8c3f32707d2b967a31d21a7eef99c0a18d1ab
coolbeatz71 <sigmacool@gmail.com>
2026-06-01 07:01:50 +0000

    refactor(identity): migrate PublicResendOtp validator to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/ResendOtp/PublicResendOtpValidator.cs
+++ src/Modules/Identity/Identity/Application/Auth/UseCases/Public/Commands/ResendOtp/PublicResendOtpValidator.cs
@@ -1,5 +1,5 @@
 using _116.Identity.Application.Auth.Validators;
-using _116.Identity.Application.Shared.Errors.Messages;
+using _116.Identity.Application.Shared.Errors.Facade;
 using FluentValidation;
 
 namespace _116.Identity.Application.Auth.UseCases.Public.Commands.ResendOtp;
@@ -18,11 +18,11 @@ public class PublicResendOtpValidator : AbstractValidator<PublicResendOtpCommand
     /// Initializes a new instance of <see cref="PublicResendOtpValidator" /> with validation rules.
     /// </summary>
     /// <param name="i18n">
-    /// Validation error messages for rule configuration.
+    /// Identity module i18n facade for rule configuration.
     /// </param>
-    public PublicResendOtpValidator(ValidationErrorMessage i18n)
+    public PublicResendOtpValidator(IdentityI18n i18n)
     {
-        RuleFor(x => x.Email).ValidEmail(i18n);
-        RuleFor(x => x.Purpose).ValidOtpPurpose(i18n);
+        RuleFor(x => x.Email).ValidEmail(i18n.User.Validation);
+        RuleFor(x => x.Purpose).ValidOtpPurpose(i18n.User.Validation);
     }
 }
