2b1e50a1857dc7aecf65b260b0a8d96e2cc9af1d
coolbeatz71 <sigmacool@gmail.com>
2026-06-01 10:22:51 +0000

    refactor(identity): migrate AdminHardDeletePermission validator to ContentI18n facade

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/HardDeletePermission/AdminHardDeletePermissionValidator.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/HardDeletePermission/AdminHardDeletePermissionValidator.cs
@@ -1,4 +1,4 @@
-using _116.Identity.Application.Shared.Errors.Messages;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Shared.Application.Extensions;
 using FluentValidation;
 
@@ -13,10 +13,11 @@ public class AdminHardDeletePermissionValidator : AbstractValidator<AdminHardDel
     /// Configure validation rules for permission hard deletion.
     /// </summary>
     /// <param name="i18n">
-    /// Validation error messages for rule configuration.
+    /// Identity module i18n facade for rule configuration.
     /// </param>
-    public AdminHardDeletePermissionValidator(ValidationErrorMessage i18n)
+    public AdminHardDeletePermissionValidator(IdentityI18n i18n)
     {
-        RuleFor(x => x.PermissionId).IsValidGuid(i18n.Localizer, "PermissionIdRequired", "PermissionIdInvalid");
+        RuleFor(x => x.PermissionId)
+            .IsValidGuid(i18n.User.Validation.Localizer, "PermissionIdRequired", "PermissionIdInvalid");
     }
 }
