0fc6cd40c670cd5f4bcf9375388780c932378031
coolbeatz71 <sigmacool@gmail.com>
2026-06-02 07:52:48 +0000

    test(commerce): fix AdminEditOrderItem validator tests to use ContentI18n facade

--- tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Commands/EditOrderItem/AdminEditOrderItemValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Commands/EditOrderItem/AdminEditOrderItemValidatorTests.cs
@@ -1,4 +1,5 @@
 using _116.Content.Application.Commerce.UseCases.Admin.Commands.EditOrderItem;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Content.Domain.Enums;
 using _116.Tests.Fixtures.Helpers;
@@ -13,10 +14,7 @@ namespace _116.Unit.Tests.Modules.Content.Application.Commerce.UseCases.Admin.Co
 /// </summary>
 public class AdminEditOrderItemValidatorTests
 {
-    private readonly AdminEditOrderItemValidator _validator = new(
-        LocalizerFactory.CreateMessage<ContentOrderErrorMessage>(),
-        LocalizerFactory.CreateMessage<CategoryErrorMessage>()
-    );
+    private readonly AdminEditOrderItemValidator _validator = new(TestErrorsFactory.CreateContentI18n());
 
     #region Validate
 
