94702846d0e645a215aa0eb0bfcbb3f2e7b8c54d
coolbeatz71 <sigmacool@gmail.com>
2026-06-02 11:43:43 +0000

    test(editorial): fix AdminCreateShortVideo handler tests to use ContentI18n facade

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/CreateShortVideo/AdminCreateShortVideoHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/CreateShortVideo/AdminCreateShortVideoHandlerTests.cs
@@ -38,7 +38,7 @@ public class AdminCreateShortVideoHandlerTests : BaseContentHandlerTest
             _cloudinaryMock.Object,
             _unitOfWorkMock.Object,
             Mapper,
-            TestErrorsFactory.CreateShortVideoErrors()
+            TestErrorsFactory.CreateContentI18n()
         );
     }
 
