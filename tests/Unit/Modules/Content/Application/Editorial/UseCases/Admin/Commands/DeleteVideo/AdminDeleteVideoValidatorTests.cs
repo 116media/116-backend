4d3268737e6f92c80ce4a26d02c20b042d17a3fb
coolbeatz71 <sigmacool@gmail.com>
2026-06-02 13:20:58 +0000

    test(editorial): fix AdminDeleteVideo validator tests to use ContentI18n facade

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/DeleteVideo/AdminDeleteVideoValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/DeleteVideo/AdminDeleteVideoValidatorTests.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using _116.Content.Application.Editorial.UseCases.Admin.Commands.DeleteVideo;
+using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
@@ -12,7 +14,7 @@ namespace _116.Unit.Tests.Modules.Content.Application.Editorial.UseCases.Admin.C
 /// </summary>
 public class AdminDeleteVideoValidatorTests
 {
-    private readonly VideoErrorMessage _i18n = LocalizerFactory.CreateMessage<VideoErrorMessage>();
+    private readonly ContentI18n _i18n = TestErrorsFactory.CreateContentI18n();
 
     private readonly AdminDeleteVideoValidator _validator;
 
@@ -56,7 +58,7 @@ public class AdminDeleteVideoValidatorTests
             .Errors.Should()
             .Contain(e =>
                 e.PropertyName == nameof(AdminDeleteVideoCommand.Id)
-                && e.ErrorMessage == _i18n.Localizer["IdRequired"].Value
+                && e.ErrorMessage == _i18n.Video.Msg.Localizer["IdRequired"].Value
             );
     }
 
@@ -75,7 +77,7 @@ public class AdminDeleteVideoValidatorTests
             .Errors.Should()
             .Contain(e =>
                 e.PropertyName == nameof(AdminDeleteVideoCommand.Id)
-                && e.ErrorMessage == _i18n.Localizer["IdInvalid"].Value
+                && e.ErrorMessage == _i18n.Video.Msg.Localizer["IdInvalid"].Value
             );
     }
 
@@ -89,8 +91,9 @@ public class AdminDeleteVideoValidatorTests
     public async Task Validate_ErrorMessages_ShouldBeLocalizedForCulture(string culture)
     {
         // Arrange
-        var i18n = LocalizerFactory.CreateMessage<VideoErrorMessage>(culture);
-        var validator = new AdminDeleteVideoValidator(i18n);
+        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+        var validator = new AdminDeleteVideoValidator(_i18n);
         var command = new AdminDeleteVideoCommand(Id: string.Empty);
 
         // Act
@@ -102,7 +105,7 @@ public class AdminDeleteVideoValidatorTests
             .Errors.Should()
             .Contain(e =>
                 e.PropertyName == nameof(AdminDeleteVideoCommand.Id)
-                && e.ErrorMessage == i18n.Localizer["IdRequired"].Value
+                && e.ErrorMessage == _i18n.Video.Msg.Localizer["IdRequired"].Value
             );
     }
 
