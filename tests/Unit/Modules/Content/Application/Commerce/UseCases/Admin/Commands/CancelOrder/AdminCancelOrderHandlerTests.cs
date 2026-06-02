b8e8e771c30a735accba962e3a874e04cc16d2d2
coolbeatz71 <sigmacool@gmail.com>
2026-06-02 07:01:21 +0000

    test(commerce): fix AdminCancelOrder handler tests to use ContentI18n facade

--- tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Commands/CancelOrder/AdminCancelOrderHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Commands/CancelOrder/AdminCancelOrderHandlerTests.cs
@@ -29,7 +29,7 @@ public class AdminCancelOrderHandlerTests
         _handler = new AdminCancelOrderHandler(
             _orderRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            TestErrorsFactory.CreateContentOrderErrors()
+            TestErrorsFactory.CreateContentI18n()
         );
     }
 
