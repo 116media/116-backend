f21ad5ba83f5920e7271b76d0a142e05c9188364
coolbeatz71 <sigmacool@gmail.com>
2026-06-02 10:14:09 +0000

    test(editorial): fix AdminArchiveArticle handler tests to use ContentI18n facade

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveArticle/AdminArchiveArticleHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveArticle/AdminArchiveArticleHandlerTests.cs
@@ -31,7 +31,7 @@ public class AdminArchiveArticleHandlerTests
         _handler = new AdminArchiveArticleHandler(
             _articleRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            TestErrorsFactory.CreateArticleErrors()
+            TestErrorsFactory.CreateContentI18n()
         );
     }
 
