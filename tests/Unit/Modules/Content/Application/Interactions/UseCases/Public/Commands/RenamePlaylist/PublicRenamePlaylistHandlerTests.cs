dabd4aa2e552e7a869aa8855423b3e60e89a8677
coolbeatz71 <sigmacool@gmail.com>
2026-06-03 06:52:17 +0000

    test(interactions): fix PublicRenamePlaylist handler tests to use ContentI18n facade

--- tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/RenamePlaylist/PublicRenamePlaylistHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/RenamePlaylist/PublicRenamePlaylistHandlerTests.cs
@@ -29,7 +29,7 @@ public class PublicRenamePlaylistHandlerTests
         _handler = new PublicRenamePlaylistHandler(
             _playlistRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            TestErrorsFactory.CreatePlaylistErrors()
+            TestErrorsFactory.CreateContentI18n()
         );
     }
 
