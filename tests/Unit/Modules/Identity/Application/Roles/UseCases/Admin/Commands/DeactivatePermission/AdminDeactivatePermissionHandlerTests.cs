842f982ccff3ed27b663d25fd9b5f0bce32a4c1c
coolbeatz71 <sigmacool@gmail.com>
2026-06-03 17:42:53 +0000

    test(identity): fix AdminDeactivatePermission handler tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/DeactivatePermission/AdminDeactivatePermissionHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/DeactivatePermission/AdminDeactivatePermissionHandlerTests.cs
@@ -1,5 +1,6 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.DeactivatePermission;
 using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
@@ -23,14 +24,14 @@ public class AdminDeactivatePermissionHandlerTests : BaseHandlerTest
 {
     private readonly Mock<IPermissionRepository> _permissionRepositoryMock;
     private readonly Mock<IIdentityUnitOfWork> _unitOfWorkMock;
-    private readonly UserErrors _userErrors;
+    private readonly IdentityI18n _userErrors;
     private readonly AdminDeactivatePermissionHandler _handler;
 
     public AdminDeactivatePermissionHandlerTests()
     {
         _permissionRepositoryMock = MockPermissionRepository.Create();
         _unitOfWorkMock = MockIdentityUnitOfWork.Create();
-        _userErrors = TestErrorsFactory.CreateUserErrors();
+        _userErrors = TestErrorsFactory.CreateIdentityI18n();
 
         _handler = new AdminDeactivatePermissionHandler(
             _permissionRepositoryMock.Object,
