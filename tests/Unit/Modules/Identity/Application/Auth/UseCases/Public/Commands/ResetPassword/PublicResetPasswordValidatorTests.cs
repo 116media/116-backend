3d05b2c53879d45e17a16f1482a294253cbea4e6
coolbeatz71 <sigmacool@gmail.com>
2026-06-03 15:00:48 +0000

    test(identity): fix PublicResetPassword validator tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordValidatorTests.cs
@@ -1,4 +1,5 @@
 using _116.Identity.Application.Auth.UseCases.Public.Commands.ResetPassword;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
@@ -13,9 +14,7 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Auth.UseCases.Public.Comm
 /// </summary>
 public class PublicResetPasswordValidatorTests
 {
-    private readonly PublicResetPasswordValidator _validator = new(
-        LocalizerFactory.CreateMessage<ValidationErrorMessage>()
-    );
+    private readonly PublicResetPasswordValidator _validator = new(TestErrorsFactory.CreateIdentityI18n());
 
     #region Valid Command Tests
 
