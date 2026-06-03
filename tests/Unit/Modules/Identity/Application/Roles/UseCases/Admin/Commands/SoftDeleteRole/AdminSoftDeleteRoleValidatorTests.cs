69124c8ad2811b027eefab53f000a300b163af8b
coolbeatz71 <sigmacool@gmail.com>
2026-06-03 20:02:00 +0000

    test(identity): fix AdminSoftDeleteRole validator tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/SoftDeleteRole/AdminSoftDeleteRoleValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/SoftDeleteRole/AdminSoftDeleteRoleValidatorTests.cs
@@ -1,4 +1,5 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.SoftDeleteRole;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
@@ -13,9 +14,7 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Roles.UseCases.Admin.Comm
 /// </summary>
 public class AdminSoftDeleteRoleValidatorTests
 {
-    private readonly AdminSoftDeleteRoleValidator _validator = new(
-        LocalizerFactory.CreateMessage<ValidationErrorMessage>()
-    );
+    private readonly AdminSoftDeleteRoleValidator _validator = new(TestErrorsFactory.CreateIdentityI18n());
 
     #region Valid Command Tests
 
