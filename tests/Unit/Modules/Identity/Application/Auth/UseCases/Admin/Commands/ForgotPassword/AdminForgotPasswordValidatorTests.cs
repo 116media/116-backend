2bddb8f2dc74801b61d202726c9a08fa8b780d1a
coolbeatz71 <sigmacool@gmail.com>
2026-06-03 13:24:44 +0000

    test(identity): fix AdminForgotPassword validator tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ForgotPassword/AdminForgotPasswordValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ForgotPassword/AdminForgotPasswordValidatorTests.cs
@@ -1,5 +1,6 @@
 using _116.BuildingBlocks.Constants;
 using _116.Identity.Application.Auth.UseCases.Admin.Commands.ForgotPassword;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
@@ -14,9 +15,7 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Auth.UseCases.Admin.Comma
 /// </summary>
 public class AdminForgotPasswordValidatorTests
 {
-    private readonly AdminForgotPasswordValidator _validator = new(
-        LocalizerFactory.CreateMessage<ValidationErrorMessage>()
-    );
+    private readonly AdminForgotPasswordValidator _validator = new(TestErrorsFactory.CreateIdentityI18n());
 
     #region Valid Command Tests
 
