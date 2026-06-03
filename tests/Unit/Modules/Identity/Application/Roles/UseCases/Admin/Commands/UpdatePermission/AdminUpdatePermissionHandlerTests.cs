aba553efc83e47c6e41c7236ea0b55e15dda485d
coolbeatz71 <sigmacool@gmail.com>
2026-06-03 20:10:21 +0000

    test(identity): fix AdminUpdatePermission handler tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/UpdatePermission/AdminUpdatePermissionHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/UpdatePermission/AdminUpdatePermissionHandlerTests.cs
@@ -1,5 +1,6 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.UpdatePermission;
 using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Domain.Entities;
@@ -23,14 +24,14 @@ public class AdminUpdatePermissionHandlerTests : BaseHandlerTest
 {
     private readonly Mock<IPermissionRepository> _permissionRepositoryMock;
     private readonly Mock<IIdentityUnitOfWork> _unitOfWorkMock;
-    private readonly UserErrors _userErrors;
+    private readonly IdentityI18n _userErrors;
     private readonly AdminUpdatePermissionHandler _handler;
 
     public AdminUpdatePermissionHandlerTests()
     {
         _permissionRepositoryMock = MockPermissionRepository.Create();
         _unitOfWorkMock = MockIdentityUnitOfWork.Create();
-        _userErrors = TestErrorsFactory.CreateUserErrors();
+        _userErrors = TestErrorsFactory.CreateIdentityI18n();
 
         _handler = new AdminUpdatePermissionHandler(
             _permissionRepositoryMock.Object,
