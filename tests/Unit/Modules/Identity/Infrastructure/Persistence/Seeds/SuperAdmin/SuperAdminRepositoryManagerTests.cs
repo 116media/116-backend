d6e661c026087b561e74c2e36600434d65154da2
coolbeatz71 <sigmacool@gmail.com>
2026-06-04 10:09:59 +0000

    test(identity): fix SuperAdminRepositoryManager tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Infrastructure/Persistence/Seeds/SuperAdmin/SuperAdminRepositoryManagerTests.cs
+++ tests/Unit/Modules/Identity/Infrastructure/Persistence/Seeds/SuperAdmin/SuperAdminRepositoryManagerTests.cs
@@ -1,4 +1,5 @@
 using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Infrastructure.Persistence;
 using _116.Identity.Infrastructure.Persistence.Seeds.SuperAdmin;
