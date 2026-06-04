6d049a5d296cd3ad7dce0d91fb5be121111634e4
coolbeatz71 <sigmacool@gmail.com>
2026-06-04 10:01:31 +0000

    test(identity): fix SuperAdminEntityFactory tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Infrastructure/Persistence/Seeds/SuperAdmin/SuperAdminEntityFactoryTests.cs
+++ tests/Unit/Modules/Identity/Infrastructure/Persistence/Seeds/SuperAdmin/SuperAdminEntityFactoryTests.cs
@@ -1,5 +1,6 @@
 using _116.Identity.Application.Auth.Services;
 using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Infrastructure.Persistence.Seeds.SuperAdmin;
 using _116.Tests.Fixtures.Helpers;
