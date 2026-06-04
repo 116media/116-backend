6e7b82b900faf9fcbdb2fecf507ef08231fe9c10
coolbeatz71 <sigmacool@gmail.com>
2026-06-04 09:52:19 +0000

    test(identity): fix VisitorPermissions tests to use ContentI18n facade

--- tests/Unit/Modules/Identity/Domain/ValueObjects/VisitorPermissionsTests.cs
+++ tests/Unit/Modules/Identity/Domain/ValueObjects/VisitorPermissionsTests.cs
@@ -1,4 +1,5 @@
 using _116.Identity.Application.Shared.Errors;
+using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.ValueObjects;
 using _116.Tests.Fixtures.Helpers;
