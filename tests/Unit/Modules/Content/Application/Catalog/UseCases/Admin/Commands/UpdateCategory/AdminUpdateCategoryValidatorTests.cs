ba63f52c1b51a54bf1f4048ab0fd2683ba3548d6
coolbeatz71 <sigmacool@gmail.com>
2026-06-05 13:17:32 +0000

    refactor(content-tests): remove unused using in AdminUpdateCategoryValidatorTests

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCategory/AdminUpdateCategoryValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCategory/AdminUpdateCategoryValidatorTests.cs
@@ -1,7 +1,6 @@
 using System.Globalization;
 using _116.Content.Application.Catalog.UseCases.Admin.Commands.UpdateCategory;
 using _116.Content.Application.Shared.Errors.Facade;
-using _116.Content.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
