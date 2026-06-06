415e6615bdebca62ccddaca695af04f89976d0c9
coolbeatz71 <sigmacool@gmail.com>
2026-06-06 08:04:57 +0000

    refactor(identity-tests): remove unused using in AdminUpdatePermissionValidatorTests

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/UpdatePermission/AdminUpdatePermissionValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/UpdatePermission/AdminUpdatePermissionValidatorTests.cs
@@ -1,6 +1,4 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.UpdatePermission;
-using _116.Identity.Application.Shared.Errors.Facade;
-using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Identity;
 using _116.Tests.Fixtures.Helpers;
