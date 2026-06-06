334adacb50db5b0c47a432e65f9d7744cf8765b4
coolbeatz71 <sigmacool@gmail.com>
2026-06-06 04:56:47 +0000

    refactor(identity-tests): remove unused using in AdminRemovePermissionFromRoleHandlerTests

--- tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/RemovePermissionFromRole/AdminRemovePermissionFromRoleHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Roles/UseCases/Admin/Commands/RemovePermissionFromRole/AdminRemovePermissionFromRoleHandlerTests.cs
@@ -1,5 +1,4 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.RemovePermissionFromRole;
-using _116.Identity.Application.Shared.Errors;
 using _116.Identity.Application.Shared.Errors.Facade;
 using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
