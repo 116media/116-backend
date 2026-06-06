ebe50f99fbccf3140f0dde41bc347f29db38dc68
coolbeatz71 <sigmacool@gmail.com>
2026-06-06 12:38:52 +0000

    refactor(identity-tests): remove unused using in PublicResetPasswordValidatorTests

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordValidatorTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordValidatorTests.cs
@@ -1,6 +1,4 @@
 using _116.Identity.Application.Auth.UseCases.Public.Commands.ResetPassword;
-using _116.Identity.Application.Shared.Errors.Facade;
-using _116.Identity.Application.Shared.Errors.Messages;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
