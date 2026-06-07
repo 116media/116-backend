c1f57965cc1fcd32dba9547d906ddde0ad543a7b
coolbeatz71 <sigmacool@gmail.com>
2026-06-07 11:25:52 +0000

    feat(shared): use localized message in NotFoundExceptionHandler

--- src/Shared/Shared/Application/Exceptions/Handlers/Strategies/NotFoundExceptionHandler.cs
+++ src/Shared/Shared/Application/Exceptions/Handlers/Strategies/NotFoundExceptionHandler.cs
@@ -1,6 +1,8 @@
 using _116.Shared.Application.Exceptions.Handlers.Contracts;
+using _116.Shared.Application.Exceptions.Messages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace _116.Shared.Application.Exceptions.Handlers.Strategies;
 
@@ -12,9 +14,19 @@ public sealed class NotFoundExceptionHandler : BaseExceptionStrategy<NotFoundExc
     /// <inheritdoc />
     public override ProblemDetails CreateProblemDetails(NotFoundException exception, HttpContext context)
     {
+        string detail = exception.Message;
+
+        if (exception.EntityName is not null)
+        {
+            var msg = context.RequestServices.GetRequiredService<SharedExceptionMessage>();
+            detail = exception.KeyName is not null
+                ? msg.EntityNotFoundByKey(exception.EntityName, exception.KeyName, exception.KeyValue!)
+                : msg.EntityNotFoundById(exception.EntityName, exception.Key);
+        }
+
         return CreateStandardProblemDetails(
             title: nameof(NotFoundException),
-            detail: exception.Message,
+            detail: detail,
             statusCode: StatusCodes.Status404NotFound,
             context: context
         );
