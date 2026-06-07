24623568ec0841525e5db2c0ee24e38a43a42251
coolbeatz71 <sigmacool@gmail.com>
2026-06-07 11:12:19 +0000

    feat(shared): use localized message in RateLimitExceededExceptionHandler

--- src/Shared/Shared/Application/Exceptions/Handlers/Strategies/RateLimitExceededExceptionHandler.cs
+++ src/Shared/Shared/Application/Exceptions/Handlers/Strategies/RateLimitExceededExceptionHandler.cs
@@ -1,6 +1,8 @@
 using _116.Shared.Application.Exceptions.Handlers.Contracts;
+using _116.Shared.Application.Exceptions.Messages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace _116.Shared.Application.Exceptions.Handlers.Strategies;
 
@@ -14,9 +16,20 @@ public sealed class RateLimitExceededExceptionHandler : BaseExceptionStrategy<Ra
     {
         context.Response.Headers.RetryAfter = ((int)exception.RetryAfter.TotalSeconds).ToString();
 
+        string detail;
+        if (exception.HasCustomMessage)
+        {
+            detail = exception.Message;
+        }
+        else
+        {
+            var msg = context.RequestServices.GetRequiredService<SharedExceptionMessage>();
+            detail = msg.RateLimitExceeded((int)exception.RetryAfter.TotalSeconds);
+        }
+
         return CreateStandardProblemDetails(
             title: nameof(RateLimitExceededException),
-            detail: exception.Message,
+            detail: detail,
             statusCode: StatusCodes.Status429TooManyRequests,
             context: context
         );
