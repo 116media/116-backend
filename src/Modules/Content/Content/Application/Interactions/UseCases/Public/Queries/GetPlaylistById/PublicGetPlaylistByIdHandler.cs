fdc15e1243a7c0ac7d40289bd7b240a8a5703463
coolbeatz71 <sigmacool@gmail.com>
2026-06-08 12:54:35 +0200

    refactor(playlist): use async mapper in get by id

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetPlaylistById/PublicGetPlaylistByIdHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetPlaylistById/PublicGetPlaylistByIdHandler.cs
@@ -2,6 +2,7 @@ using _116.Content.Application.Interactions.Persistence;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Domain.Entities;
+using _116.Core.Application.Shared.Repositories;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -11,10 +12,15 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Queries.GetPlayl
 /// Handles the <see cref="PublicGetPlaylistByIdQuery" /> to retrieve a playlist with its videos.
 /// </summary>
 /// <param name="playlistRepository">Repository for playlist data access operations.</param>
+/// <param name="fileRepository">Repository for resolving file URLs.</param>
 /// <param name="mapper">The mapper used to project entities to DTOs.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
-public class PublicGetPlaylistByIdHandler(IPlaylistRepository playlistRepository, IMapper mapper, ContentI18n i18n)
-    : IQueryHandler<PublicGetPlaylistByIdQuery, PublicGetPlaylistByIdResult>
+public class PublicGetPlaylistByIdHandler(
+    IPlaylistRepository playlistRepository,
+    IFileRepository fileRepository,
+    IMapper mapper,
+    ContentI18n i18n
+) : IQueryHandler<PublicGetPlaylistByIdQuery, PublicGetPlaylistByIdResult>
 {
     /// <inheritdoc />
     public async Task<PublicGetPlaylistByIdResult> Handle(
@@ -34,7 +40,7 @@ public class PublicGetPlaylistByIdHandler(IPlaylistRepository playlistRepository
                 throw i18n.Playlist.NotOwner();
             }
 
-            var dto = playlist.ToPlaylistDetailDto(mapper);
+            var dto = await playlist.ToPlaylistDetailDtoAsync(mapper, fileRepository, cancellationToken);
             return new PublicGetPlaylistByIdResult(Playlist: dto);
         }
 
