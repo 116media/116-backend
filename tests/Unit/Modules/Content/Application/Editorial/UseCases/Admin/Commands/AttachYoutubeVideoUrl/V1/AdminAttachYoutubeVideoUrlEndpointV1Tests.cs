b9f3d7eeb9d61340e9855065aabd8d532b504729
coolbeatz71 <sigmacool@gmail.com>
2026-06-08 17:17:56 +0200

    test(video): remove ThumbnailStorageKey from endpoint

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/AttachYoutubeVideoUrl/V1/AdminAttachYoutubeVideoUrlEndpointV1Tests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/AttachYoutubeVideoUrl/V1/AdminAttachYoutubeVideoUrlEndpointV1Tests.cs
@@ -34,7 +34,6 @@ public class AdminAttachYoutubeVideoUrlEndpointV1Tests
             Slug: "test",
             Description: "Test video description",
             ThumbnailUrl: null,
-            ThumbnailStorageKey: null,
             AuthorId: "Test",
             Status: EnumContentStatus.Draft,
             RejectionReason: null,
