411a8083c214e34bdcd56f1e9cdb45a1bc25411d
coolbeatz71 <sigmacool@gmail.com>
2026-06-08 11:38:26 +0200

    refactor(article): use async mapper in get by slug

--- src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetArticleBySlug/PublicGetArticleBySlugHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetArticleBySlug/PublicGetArticleBySlugHandler.cs
@@ -3,6 +3,7 @@ using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
+using _116.Core.Application.Shared.Repositories;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -12,10 +13,15 @@ namespace _116.Content.Application.Editorial.UseCases.Public.Queries.GetArticleB
 /// Handles the <see cref="PublicGetArticleBySlugQuery" /> to retrieve a single published article by its slug.
 /// </summary>
 /// <param name="articleRepository">Repository for article data access operations.</param>
+/// <param name="fileRepository">Repository for resolving file URLs.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
-public class PublicGetArticleBySlugHandler(IArticleRepository articleRepository, IMapper mapper, ContentI18n i18n)
-    : IQueryHandler<PublicGetArticleBySlugQuery, PublicGetArticleBySlugResult>
+public class PublicGetArticleBySlugHandler(
+    IArticleRepository articleRepository,
+    IFileRepository fileRepository,
+    IMapper mapper,
+    ContentI18n i18n
+) : IQueryHandler<PublicGetArticleBySlugQuery, PublicGetArticleBySlugResult>
 {
     /// <inheritdoc />
     public async Task<PublicGetArticleBySlugResult> Handle(
@@ -33,7 +39,7 @@ public class PublicGetArticleBySlugHandler(IArticleRepository articleRepository,
             throw i18n.Article.NotFound(Guid.Empty);
         }
 
-        var dto = article.ToArticleDetailDto(mapper);
+        var dto = await article.ToArticleDetailDtoAsync(mapper, fileRepository, cancellationToken);
         return new PublicGetArticleBySlugResult(Article: dto);
     }
 }
