7a02fa333b305554ccb1ccc0d12b8debd4b4e2d3
coolbeatz71 <sigmacool@gmail.com>
2026-06-09 13:30:22 +0200

    feat(core): add AddStorageKeyToFileEntity migration

---
+++ src/Modules/Core/Core/Infrastructure/Persistence/Migrations/20260618215519_AddStorageKeyToFileEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _116.Core.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStorageKeyToFileEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "storage_key",
+                schema: "core",
+                table: "files",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "storage_key",
+                schema: "core",
+                table: "files");
+        }
+    }
+}
