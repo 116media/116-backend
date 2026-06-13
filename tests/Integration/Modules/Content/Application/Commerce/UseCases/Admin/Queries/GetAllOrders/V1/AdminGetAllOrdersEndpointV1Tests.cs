107e0995fe4a3d254bbb4847f66be0ff498acf3d
coolbeatz71 <sigmacool@gmail.com>
2026-06-13 10:47:09 +0200

    test(content-commerce): add AdminGetAllOrders integration tests

---
+++ tests/Integration/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetAllOrders/V1/AdminGetAllOrdersEndpointV1Tests.cs
@@ -0,0 +1,51 @@
+using System.Net.Http.Json;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
+namespace _116.Integration.Tests.Modules.Content.Application.Commerce.UseCases.Admin.Queries.GetAllOrders.V1;
+
+/// <summary>
+/// Integration tests for the AdminGetAllOrders endpoint.
+/// </summary>
+[Collection("Database")]
+public class AdminGetAllOrdersEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
+{
+    [Fact]
+    public async Task GetAllOrders_AsSuperAdmin_ReturnsOk()
+    {
+        await using var seedContext = CreateDbContext<ContentDbContext>();
+        var customer = CustomerFactory.Create();
+        seedContext.Customers.Add(customer);
+        await seedContext.SaveChangesAsync();
+
+        var order = ContentOrderFactory.CreateForCustomer(customer.Id);
+        seedContext.ContentOrders.Add(order);
+        await seedContext.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Orders}?pageIndex=0&pageSize=10");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetAllOrders_AsAdmin_ReturnsOk()
+    {
+        Client.AuthenticateAsAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Orders}?pageIndex=0&pageSize=10");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetAllOrders_WithNoAuth_ReturnsUnauthorized()
+    {
+        Client.ClearAuthentication();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Orders}?pageIndex=0&pageSize=10");
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
