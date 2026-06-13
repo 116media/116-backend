21bd6a5284bcdcc73fe39cea11f1264c604bd1b6
coolbeatz71 <sigmacool@gmail.com>
2026-06-13 15:08:52 +0200

    test(content-editorial): add AdminSubmitVideo integration tests

---
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/SubmitVideo/V1/AdminSubmitVideoEndpointV1Tests.cs
@@ -0,0 +1,52 @@
+namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Commands.SubmitVideo.V1;
+
+/// <summary>
+/// Integration tests for the AdminSubmitVideo endpoint.
+/// </summary>
+[Collection("Database")]
+public class AdminSubmitVideoEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
+{
+    [Fact]
+    public async Task SubmitVideo_WithNoAuth_ReturnsUnauthorized()
+    {
+        Client.ClearAuthentication();
+        var nonExistentId = Guid.NewGuid();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.Videos}/{nonExistentId}/submit", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task SubmitVideo_AsVisitor_ReturnsForbidden()
+    {
+        Client.AuthenticateAsVisitor();
+        var nonExistentId = Guid.NewGuid();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.Videos}/{nonExistentId}/submit", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task SubmitVideo_AsAdmin_ReturnsForbidden()
+    {
+        Client.AuthenticateAsAdmin();
+        var nonExistentId = Guid.NewGuid();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.Videos}/{nonExistentId}/submit", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task SubmitVideo_AsSuperAdmin_WithNonExistentId_ReturnsError()
+    {
+        Client.AuthenticateAsSuperAdmin();
+        var nonExistentId = Guid.NewGuid();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.Videos}/{nonExistentId}/submit", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
