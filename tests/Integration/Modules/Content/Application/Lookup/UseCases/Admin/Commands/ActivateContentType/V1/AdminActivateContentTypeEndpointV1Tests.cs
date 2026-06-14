b22bc5195bca024f2c115a736eb1872fc963452a
coolbeatz71 <sigmacool@gmail.com>
2026-06-14 08:59:26 +0200

    test(content-lookup): add AdminActivateContentType integration tests

---
+++ tests/Integration/Modules/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/AdminActivateContentTypeEndpointV1Tests.cs
@@ -0,0 +1,47 @@
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
+namespace _116.Integration.Tests.Modules.Content.Application.Lookup.UseCases.Admin.Commands.ActivateContentType.V1;
+
+/// <summary>
+/// Integration tests for the AdminActivateContentType endpoint.
+/// </summary>
+[Collection("Database")]
+public class AdminActivateContentTypeEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
+{
+    [Fact]
+    public async Task ActivateContentType_WithNoAuth_ReturnsUnauthorized()
+    {
+        Client.ClearAuthentication();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.ContentTypes}/{Guid.NewGuid()}/activate", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task ActivateContentType_AsSuperAdmin_NonExistent_ReturnsNotFound()
+    {
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.ContentTypes}/{Guid.NewGuid()}/activate", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task ActivateContentType_AsSuperAdmin_WithValidId_ReturnsOk()
+    {
+        await using var context = CreateDbContext<ContentDbContext>();
+        var contentType = ContentTypeFactory.Create();
+        contentType.Deactivate();
+        context.ContentTypes.Add(contentType);
+        await context.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.ContentTypes}/{contentType.Id}/activate", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
