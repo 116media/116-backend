924acd8391c14d60f1cf4725eba181b74e099115
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 12:27:44 +0200

    test(get-all-shorts): add error path coverage tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllShorts/V1/AdminGetAllShortsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllShorts/V1/AdminGetAllShortsEndpointV1Tests.cs
@@ -1,3 +1,6 @@
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Queries.GetAllShorts.V1;
 
 /// <summary>
@@ -32,6 +35,36 @@ public class AdminGetAllShortsEndpointV1Tests(PostgresFixture db) : BaseApiTest(
         Client.AuthenticateAsAdmin();
 
         var response = await Client.GetAsync(ApiRoutes.Admin.Shorts);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetAllShorts_WithSearch_ReturnsOk()
+    {
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Shorts}?search=test");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    /// <summary>
+    /// Verifies that the search query parameter filters short videos by title,
+    /// returning only short videos whose title matches the search term.
+    /// </summary>
+    [Fact]
+    public async Task GetAllShorts_WithSearchQuery_ReturnsFilteredResults()
+    {
+        await using var context = CreateDbContext<ContentDbContext>();
+        var matchingShort = ShortVideoFactory.Create();
+        var otherShort = ShortVideoFactory.Create();
+        context.ShortVideos.AddRange(matchingShort, otherShort);
+        await context.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Shorts}?search=UniqueSearchTerm");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
