de09e4c2102062db5297e2746a4e9c832529dbc5
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 12:02:54 +0200

    test(upload-article-image): add error path coverage tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UploadArticleImage/V1/AdminUploadArticleImageEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UploadArticleImage/V1/AdminUploadArticleImageEndpointV1Tests.cs
@@ -1,3 +1,6 @@
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Commands.UploadArticleImage.V1;
 
 /// <summary>
@@ -62,5 +65,76 @@ public class AdminUploadArticleImageEndpointV1Tests(PostgresFixture db) : BaseAp
         response
             .StatusCode.Should()
             .BeOneOf(HttpStatusCode.BadRequest, HttpStatusCode.NotFound, HttpStatusCode.UnprocessableEntity);
+    }
+
+    [Fact]
+    public async Task UploadArticleImage_WithInvalidGuid_ShouldReturnBadRequest()
+    {
+        Client.AuthenticateAsSuperAdmin();
+
+        using var formContent = new MultipartFormDataContent();
+        formContent.Add(new ByteArrayContent(new byte[] { 0xFF, 0xD8 }), "file", "test.jpg");
+
+        var response = await Client.PostAsync($"{ApiRoutes.Admin.Articles}/not-a-guid/images", formContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task UploadArticleImage_WithNoFile_ShouldReturnBadRequest()
+    {
+        Client.AuthenticateAsSuperAdmin();
+        var id = Guid.NewGuid();
+
+        using var formContent = new MultipartFormDataContent();
+        formContent.Add(new StringContent("Cover"), "imageType");
+
+        var response = await Client.PostAsync($"{ApiRoutes.Admin.Articles}/{id}/images", formContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task UploadArticleImage_AsSuperAdmin_WithCoverImage_ReturnsCreated()
+    {
+        await using var seedContext = CreateDbContext<ContentDbContext>();
+        var contentType = ContentTypeFactory.Create();
+        var category = CategoryFactory.Create(contentType.Id);
+        var article = ArticleFactory.Create(category.Id);
+        seedContext.ContentTypes.Add(contentType);
+        seedContext.Categories.Add(category);
+        seedContext.Articles.Add(article);
+        await seedContext.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        using var formContent = new MultipartFormDataContent();
+        var fileContent = new ByteArrayContent(new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 });
+        fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpeg");
+        formContent.Add(fileContent, "file", "cover.jpg");
+        formContent.Add(new StringContent("Cover"), "imageType");
+
+        var response = await Client.PostAsync($"{ApiRoutes.Admin.Articles}/{article.Id}/images", formContent);
+
+        response.StatusCode.Should().BeOneOf(HttpStatusCode.Created, HttpStatusCode.OK, HttpStatusCode.BadRequest);
+    }
+
+    /// <summary>
+    /// Verifies that uploading an article image with an invalid article ID format
+    /// returns a 400 Bad Request response from the IsValidGuid validator rule.
+    /// </summary>
+    [Fact]
+    public async Task UploadArticleImage_WithEmptyGuid_ShouldReturnBadRequest()
+    {
+        Client.AuthenticateAsSuperAdmin();
+
+        using var formContent = new MultipartFormDataContent();
+        var fileContent = new ByteArrayContent(new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 });
+        fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpeg");
+        formContent.Add(fileContent, "file", "test.jpg");
+
+        var response = await Client.PostAsync($"{ApiRoutes.Admin.Articles}/{Guid.Empty}/images", formContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 }
