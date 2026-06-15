7a68c7c0d964a8e6a9b59e4e8ba835a21e269af2
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 18:14:29 +0200

    test(deactivate-role): add error path coverage tests

--- tests/Integration/Modules/Identity/Application/Roles/UseCases/Admin/Commands/DeactivateRole/V1/AdminDeactivateRoleEndpointV1Tests.cs
+++ tests/Integration/Modules/Identity/Application/Roles/UseCases/Admin/Commands/DeactivateRole/V1/AdminDeactivateRoleEndpointV1Tests.cs
@@ -25,4 +25,22 @@ public class AdminDeactivateRoleEndpointV1Tests(PostgresFixture db) : BaseApiTes
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
+
+    /// <summary>
+    /// Verifies that deactivating a role that is already inactive returns a 409 Conflict.
+    /// </summary>
+    [Fact]
+    public async Task DeactivateRole_WhenAlreadyInactive_ReturnsConflict()
+    {
+        await using var seedContext = CreateDbContext<IdentityDbContext>();
+        var role = RoleFactory.CreateInactive(ShortName("di"));
+        seedContext.Roles.Add(role);
+        await seedContext.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.Roles}/{role.Id}/deactivate", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
 }
