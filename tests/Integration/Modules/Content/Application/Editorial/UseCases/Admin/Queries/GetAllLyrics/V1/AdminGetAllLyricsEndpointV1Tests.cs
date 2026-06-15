afb4810ab439285f900ca2e9b942acce859a0648
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 12:18:05 +0200

    test(get-all-lyrics): add error path coverage tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllLyrics/V1/AdminGetAllLyricsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllLyrics/V1/AdminGetAllLyricsEndpointV1Tests.cs
@@ -1,3 +1,6 @@
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Queries.GetAllLyrics.V1;
 
 /// <summary>
@@ -32,6 +35,36 @@ public class AdminGetAllLyricsEndpointV1Tests(PostgresFixture db) : BaseApiTest(
         Client.AuthenticateAsAdmin();
 
         var response = await Client.GetAsync(ApiRoutes.Admin.Lyrics);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetAllLyrics_WithSearch_ReturnsOk()
+    {
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Lyrics}?search=test");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    /// <summary>
+    /// Verifies that the search query parameter filters lyrics by song title,
+    /// returning only lyrics whose song title matches the search term.
+    /// </summary>
+    [Fact]
+    public async Task GetAllLyrics_WithSearchQuery_ReturnsFilteredResults()
+    {
+        await using var context = CreateDbContext<ContentDbContext>();
+        var matchingLyrics = LyricsFactory.Create("UniqueSearchTerm Song", "Test Artist");
+        var otherLyrics = LyricsFactory.Create("Completely Different Song", "Other Artist");
+        context.Lyrics.AddRange(matchingLyrics, otherLyrics);
+        await context.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Lyrics}?search=UniqueSearchTerm");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
