479e61e9f747ad175d1fde4960cd2e463b8d8e9e
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 12:57:12 +0200

    test(bookmark-article): add error path coverage tests

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/V1/PublicBookmarkArticleEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/V1/PublicBookmarkArticleEndpointV1Tests.cs
@@ -51,4 +51,29 @@ public class PublicBookmarkArticleEndpointV1Tests(PostgresFixture db) : BaseApiT
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
+
+    [Fact]
+    public async Task BookmarkArticle_AsVisitor_AlreadyBookmarked_ReturnsConflict()
+    {
+        await using var context = CreateDbContext<ContentDbContext>();
+        var contentType = ContentTypeFactory.Create();
+        context.ContentTypes.Add(contentType);
+        await context.SaveChangesAsync();
+
+        var category = CategoryFactory.Create(contentType.Id);
+        context.Categories.Add(category);
+        await context.SaveChangesAsync();
+
+        var article = ArticleFactory.CreatePublished(category.Id);
+        context.Articles.Add(article);
+        await context.SaveChangesAsync();
+
+        Client.AuthenticateAsVisitor();
+
+        await Client.PostAsync($"{ApiRoutes.Public.Articles}/{article.Id}/bookmarks", null);
+
+        var response = await Client.PostAsync($"{ApiRoutes.Public.Articles}/{article.Id}/bookmarks", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
 }
