6dd07b9f97cac955fe601d44cd6f1a9395bac4c0
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 17:49:16 +0200

    test(activate-role): add error path coverage tests

--- tests/Integration/Modules/Identity/Application/Roles/UseCases/Admin/Commands/ActivateRole/V1/AdminActivateRoleEndpointV1Tests.cs
+++ tests/Integration/Modules/Identity/Application/Roles/UseCases/Admin/Commands/ActivateRole/V1/AdminActivateRoleEndpointV1Tests.cs
@@ -35,4 +35,22 @@ public class AdminActivateRoleEndpointV1Tests(PostgresFixture db) : BaseApiTest(
 
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
+
+    /// <summary>
+    /// Verifies that activating a role that is already active returns a 409 Conflict.
+    /// </summary>
+    [Fact]
+    public async Task ActivateRole_WhenAlreadyActive_ReturnsConflict()
+    {
+        await using var seedContext = CreateDbContext<IdentityDbContext>();
+        var role = RoleFactory.Create(ShortName("aa"));
+        seedContext.Roles.Add(role);
+        await seedContext.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.PatchAsync($"{ApiRoutes.Admin.Roles}/{role.Id}/activate", null);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
 }
