bfa06a59362a30f9c4b36527918d0497181ac234
coolbeatz71 <sigmacool@gmail.com>
2026-06-15 11:42:58 +0200

    test(update-article-tags): add error path coverage tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UpdateArticleTags/V1/AdminUpdateArticleTagsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UpdateArticleTags/V1/AdminUpdateArticleTagsEndpointV1Tests.cs
@@ -1,3 +1,6 @@
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Commands.UpdateArticleTags.V1;
 
 /// <summary>
@@ -39,5 +42,41 @@ public class AdminUpdateArticleTagsEndpointV1Tests(PostgresFixture db) : BaseApi
         response
             .StatusCode.Should()
             .BeOneOf(HttpStatusCode.BadRequest, HttpStatusCode.UnprocessableEntity, HttpStatusCode.NotFound);
+    }
+
+    /// <summary>
+    /// Verifies that updating article tags replaces existing tags with the new set,
+    /// exercising the ArticleTagByArticleIdSpecification internally.
+    /// </summary>
+    [Fact]
+    public async Task UpdateArticle_WithTags_ReplacesExistingTags()
+    {
+        await using var context = CreateDbContext<ContentDbContext>();
+        var contentType = ContentTypeFactory.Create();
+        context.ContentTypes.Add(contentType);
+        await context.SaveChangesAsync();
+
+        var category = CategoryFactory.Create(contentType.Id);
+        context.Categories.Add(category);
+        await context.SaveChangesAsync();
+
+        var tag1 = TagFactory.Create("OldTag", "old-tag");
+        var tag2 = TagFactory.Create("NewTag", "new-tag");
+        context.Tags.AddRange(tag1, tag2);
+        await context.SaveChangesAsync();
+
+        var article = ArticleFactory.Create(category.Id);
+        context.Articles.Add(article);
+        await context.SaveChangesAsync();
+
+        Client.AuthenticateAsSuperAdmin();
+
+        var assignRequest = new { TagNames = new[] { "OldTag" } };
+        await Client.PutAsJsonAsync($"{ApiRoutes.Admin.Articles}/{article.Id}/tags", assignRequest);
+
+        var replaceRequest = new { TagNames = new[] { "NewTag" } };
+        var response = await Client.PutAsJsonAsync($"{ApiRoutes.Admin.Articles}/{article.Id}/tags", replaceRequest);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
