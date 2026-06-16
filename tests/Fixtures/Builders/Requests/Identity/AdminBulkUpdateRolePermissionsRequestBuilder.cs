16e0f09c014e2ca62d918e2cc785758daac672ee
coolbeatz71 <sigmacool@gmail.com>
2026-06-16 13:33:08 +0200

    test(identity): add AdminBulkUpdateRolePermissions request builder

---
+++ tests/Fixtures/Builders/Requests/Identity/AdminBulkUpdateRolePermissionsRequestBuilder.cs
@@ -0,0 +1,43 @@
+using _116.Identity.Application.Roles.UseCases.Admin.Commands.BulkUpdateRolePermissions.V1;
+using Bogus;
+
+namespace _116.Tests.Fixtures.Builders.Requests.Identity;
+
+/// <summary>
+/// Fluent builder for creating <see cref="AdminBulkUpdateRolePermissionsRequest"/> instances in tests.
+/// </summary>
+public class AdminBulkUpdateRolePermissionsRequestBuilder
+{
+    private readonly Faker _faker = new();
+
+    private List<Guid> _permissionIds;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdminBulkUpdateRolePermissionsRequestBuilder"/> class
+    /// with a valid random list of permission identifiers.
+    /// </summary>
+    public AdminBulkUpdateRolePermissionsRequestBuilder()
+    {
+        _permissionIds = [_faker.Random.Guid(), _faker.Random.Guid()];
+    }
+
+    /// <summary>
+    /// Sets the list of permission identifiers to assign to the role.
+    /// </summary>
+    /// <param name="permissionIds">The permission identifiers.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public AdminBulkUpdateRolePermissionsRequestBuilder WithPermissionIds(List<Guid> permissionIds)
+    {
+        _permissionIds = permissionIds;
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="AdminBulkUpdateRolePermissionsRequest"/> instance.
+    /// </summary>
+    /// <returns>A configured AdminBulkUpdateRolePermissionsRequest instance.</returns>
+    public AdminBulkUpdateRolePermissionsRequest Build()
+    {
+        return new AdminBulkUpdateRolePermissionsRequest(PermissionIds: _permissionIds);
+    }
+}
