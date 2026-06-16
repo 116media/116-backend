d5ddce671dc3572d9522bbece3f4f7e84491a35e
coolbeatz71 <sigmacool@gmail.com>
2026-06-16 12:57:58 +0200

    test(content): add PublicEditArticleComment request builder

---
+++ tests/Fixtures/Builders/Requests/Content/PublicEditArticleCommentRequestBuilder.cs
@@ -0,0 +1,45 @@
+using _116.Content.Application.Interactions.UseCases.Public.Commands.EditArticleComment.V1;
+using _116.Tests.Fixtures.Constants;
+using Bogus;
+
+namespace _116.Tests.Fixtures.Builders.Requests.Content;
+
+/// <summary>
+/// Fluent builder for creating <see cref="PublicEditArticleCommentRequest"/> instances in tests.
+/// </summary>
+public class PublicEditArticleCommentRequestBuilder
+{
+    private readonly Faker _faker = new();
+
+    private string _body;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PublicEditArticleCommentRequestBuilder"/> class
+    /// with a valid random comment body that satisfies the validator.
+    /// </summary>
+    public PublicEditArticleCommentRequestBuilder()
+    {
+        string sentence = _faker.Lorem.Sentence(wordCount: 8);
+        _body = sentence[..Math.Min(TestConstants.Content.Interactions.MaxCommentBodyLength, sentence.Length)];
+    }
+
+    /// <summary>
+    /// Sets the comment body text.
+    /// </summary>
+    /// <param name="body">The new comment body text.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public PublicEditArticleCommentRequestBuilder WithBody(string body)
+    {
+        _body = body;
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="PublicEditArticleCommentRequest"/> instance.
+    /// </summary>
+    /// <returns>A configured PublicEditArticleCommentRequest instance.</returns>
+    public PublicEditArticleCommentRequest Build()
+    {
+        return new PublicEditArticleCommentRequest(Body: _body);
+    }
+}
