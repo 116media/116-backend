f3253cf68d360a4e55cdc59c315a9e8e046e819f
coolbeatz71 <sigmacool@gmail.com>
2026-06-16 12:44:09 +0200

    test(content): add PublicAddVideoToPlaylist request builder

---
+++ tests/Fixtures/Builders/Requests/Content/PublicAddVideoToPlaylistRequestBuilder.cs
@@ -0,0 +1,56 @@
+using _116.Content.Application.Interactions.UseCases.Public.Commands.AddVideoToPlaylist.V1;
+using Bogus;
+
+namespace _116.Tests.Fixtures.Builders.Requests.Content;
+
+/// <summary>
+/// Fluent builder for creating <see cref="PublicAddVideoToPlaylistRequest"/> instances in tests.
+/// </summary>
+public class PublicAddVideoToPlaylistRequestBuilder
+{
+    private readonly Faker _faker = new();
+
+    private Guid _videoId;
+    private int _sortOrder;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PublicAddVideoToPlaylistRequestBuilder"/> class
+    /// with a valid random video identifier and sort order.
+    /// </summary>
+    public PublicAddVideoToPlaylistRequestBuilder()
+    {
+        _videoId = _faker.Random.Guid();
+        _sortOrder = 1;
+    }
+
+    /// <summary>
+    /// Sets the identifier of the video to add to the playlist.
+    /// </summary>
+    /// <param name="videoId">The unique identifier of the video.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public PublicAddVideoToPlaylistRequestBuilder WithVideoId(Guid videoId)
+    {
+        _videoId = videoId;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the display order for the video within the playlist.
+    /// </summary>
+    /// <param name="sortOrder">The sort order for the video.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public PublicAddVideoToPlaylistRequestBuilder WithSortOrder(int sortOrder)
+    {
+        _sortOrder = sortOrder;
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="PublicAddVideoToPlaylistRequest"/> instance.
+    /// </summary>
+    /// <returns>A configured PublicAddVideoToPlaylistRequest instance.</returns>
+    public PublicAddVideoToPlaylistRequest Build()
+    {
+        return new PublicAddVideoToPlaylistRequest(VideoId: _videoId, SortOrder: _sortOrder);
+    }
+}
