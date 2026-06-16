e0b3d70837a3d261459b581eeac04e9cb956d4e6
coolbeatz71 <sigmacool@gmail.com>
2026-06-16 12:16:41 +0200

    test(content): add AdminUpdateTag request builder

---
+++ tests/Fixtures/Builders/Requests/Content/AdminUpdateTagRequestBuilder.cs
@@ -0,0 +1,61 @@
+using _116.Content.Application.Lookup.UseCases.Admin.Commands.UpdateTag.V1;
+using _116.Tests.Fixtures.Constants;
+using Bogus;
+
+namespace _116.Tests.Fixtures.Builders.Requests.Content;
+
+/// <summary>
+/// Fluent builder for creating <see cref="AdminUpdateTagRequest"/> instances in tests.
+/// </summary>
+public class AdminUpdateTagRequestBuilder
+{
+    private readonly Faker _faker = new();
+
+    private string _name;
+    private string _slug;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdminUpdateTagRequestBuilder"/> class
+    /// with valid random default values that satisfy the update tag validator.
+    /// </summary>
+    public AdminUpdateTagRequestBuilder()
+    {
+        string token = _faker.Random.AlphaNumeric(length: 8).ToLowerInvariant();
+        string nameCandidate = $"Tag {token}";
+        _name = nameCandidate[..Math.Min(TestConstants.Content.Tag.NameMaxLength, nameCandidate.Length)];
+
+        string slugCandidate = $"tag-{token}";
+        _slug = slugCandidate[..Math.Min(TestConstants.Content.Tag.SlugMaxLength, slugCandidate.Length)];
+    }
+
+    /// <summary>
+    /// Sets the tag display name.
+    /// </summary>
+    /// <param name="name">The tag name.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public AdminUpdateTagRequestBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the URL-safe tag slug.
+    /// </summary>
+    /// <param name="slug">The tag slug.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public AdminUpdateTagRequestBuilder WithSlug(string slug)
+    {
+        _slug = slug;
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="AdminUpdateTagRequest"/> instance.
+    /// </summary>
+    /// <returns>A configured AdminUpdateTagRequest instance.</returns>
+    public AdminUpdateTagRequest Build()
+    {
+        return new AdminUpdateTagRequest(Name: _name, Slug: _slug);
+    }
+}
