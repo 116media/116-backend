224064778062bb41df5b57b609ce194b8c0e15a7
coolbeatz71 <sigmacool@gmail.com>
2026-06-16 13:15:52 +0200

    test(identity): add AdminAssignPermissionToRole request builder

---
+++ tests/Fixtures/Builders/Requests/Identity/AdminAssignPermissionToRoleRequestBuilder.cs
@@ -0,0 +1,43 @@
+using _116.Identity.Application.Roles.UseCases.Admin.Commands.AssignPermissionToRole.V1;
+using Bogus;
+
+namespace _116.Tests.Fixtures.Builders.Requests.Identity;
+
+/// <summary>
+/// Fluent builder for creating <see cref="AdminAssignPermissionToRoleRequest"/> instances in tests.
+/// </summary>
+public class AdminAssignPermissionToRoleRequestBuilder
+{
+    private readonly Faker _faker = new();
+
+    private Guid _permissionId;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdminAssignPermissionToRoleRequestBuilder"/> class
+    /// with a valid random permission identifier.
+    /// </summary>
+    public AdminAssignPermissionToRoleRequestBuilder()
+    {
+        _permissionId = _faker.Random.Guid();
+    }
+
+    /// <summary>
+    /// Sets the identifier of the permission to assign to the role.
+    /// </summary>
+    /// <param name="permissionId">The permission identifier.</param>
+    /// <returns>The builder instance for chaining.</returns>
+    public AdminAssignPermissionToRoleRequestBuilder WithPermissionId(Guid permissionId)
+    {
+        _permissionId = permissionId;
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="AdminAssignPermissionToRoleRequest"/> instance.
+    /// </summary>
+    /// <returns>A configured AdminAssignPermissionToRoleRequest instance.</returns>
+    public AdminAssignPermissionToRoleRequest Build()
+    {
+        return new AdminAssignPermissionToRoleRequest(PermissionId: _permissionId);
+    }
+}
