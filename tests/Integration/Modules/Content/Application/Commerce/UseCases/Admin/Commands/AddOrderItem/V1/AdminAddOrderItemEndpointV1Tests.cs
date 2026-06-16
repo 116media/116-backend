707faf93801394c1b53adc50c844a18151aa4949
coolbeatz71 <sigmacool@gmail.com>
2026-06-16 19:34:53 +0200

    test(commerce): deepen AdminAddOrderItem tests

--- tests/Integration/Modules/Content/Application/Commerce/UseCases/Admin/Commands/AddOrderItem/V1/AdminAddOrderItemEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Commerce/UseCases/Admin/Commands/AddOrderItem/V1/AdminAddOrderItemEndpointV1Tests.cs
@@ -1,5 +1,8 @@
-using System.Net.Http.Json;
+using _116.Content.Application.Commerce.UseCases.Admin.Commands.AddOrderItem.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Domain.Enums;
 using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Builders.Requests.Content;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Commerce.UseCases.Admin.Commands.AddOrderItem.V1;
@@ -13,61 +16,61 @@ public class AdminAddOrderItemEndpointV1Tests(PostgresFixture db) : BaseApiTest(
     [Fact]
     public async Task AddOrderItem_AsSuperAdmin_WithValidData_ReturnsCreated()
     {
-        await using var seedContext = CreateDbContext<ContentDbContext>();
-        var customer = CustomerFactory.Create();
-        var contentType = ContentTypeFactory.Create();
-        var category = CategoryFactory.Create(contentType.Id);
-        var order = ContentOrderFactory.CreateForCustomer(customer.Id);
-        seedContext.Customers.Add(customer);
-        seedContext.ContentTypes.Add(contentType);
-        seedContext.Categories.Add(category);
-        seedContext.ContentOrders.Add(order);
-        await seedContext.SaveChangesAsync();
+        CustomerEntity customer = CustomerFactory.Create();
+        ContentTypeEntity contentType = ContentTypeFactory.Create();
+        CategoryEntity category = CategoryFactory.Create(contentType.Id);
+        ContentOrderEntity order = ContentOrderFactory.CreateForCustomer(customer.Id);
+        await SeedAsync<ContentDbContext>(ctx =>
+        {
+            ctx.Customers.Add(customer);
+            ctx.ContentTypes.Add(contentType);
+            ctx.Categories.Add(category);
+            ctx.ContentOrders.Add(order);
+        });
 
         Client.AuthenticateAsSuperAdmin();
-        var request = new
-        {
-            ContentKind = 0,
-            CategoryId = category.Id.ToString(),
-            SocialBoost = false,
-            IsBonus = false,
-        };
+        AdminAddOrderItemRequest request = new AdminAddOrderItemRequestBuilder()
+            .WithContentKind(EnumCoreContentType.Article)
+            .WithCategoryId(category.Id.ToString())
+            .WithSocialBoost(false)
+            .WithIsBonus(false)
+            .Build();
 
-        var response = await Client.PostAsJsonAsync($"{ApiRoutes.Admin.Orders}/{order.Id}/items", request);
+        var response = await Client.PostAsJsonAsync(Routes.Admin.Orders.Items(order.Id), request);
 
         response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var body = await response.ReadAsAsync<AdminAddOrderItemResponse>();
+        body.Item.Id.Should().NotBeEmpty();
+        body.Item.CategoryId.Should().Be(category.Id);
+        body.Item.ContentKind.Should().Be(request.ContentKind);
+        body.Item.SocialBoost.Should().Be(request.SocialBoost);
+        body.Item.IsBonus.Should().Be(request.IsBonus);
+
+        await using ContentDbContext db = CreateDbContext<ContentDbContext>();
+        ContentOrderItemEntity? persisted = await db.ContentOrderItems.FindAsync(body.Item.Id);
+        persisted.Should().NotBeNull();
+        persisted!.OrderId.Should().Be(order.Id);
+        persisted.CategoryId.Should().Be(category.Id);
     }
 
     [Fact]
     public async Task AddOrderItem_NonExistentOrder_ReturnsNotFound()
     {
         Client.AuthenticateAsSuperAdmin();
-        var request = new
-        {
-            ContentKind = 0,
-            CategoryId = Guid.NewGuid().ToString(),
-            SocialBoost = false,
-            IsBonus = false,
-        };
+        AdminAddOrderItemRequest request = new AdminAddOrderItemRequestBuilder().Build();
 
-        var response = await Client.PostAsJsonAsync($"{ApiRoutes.Admin.Orders}/{Guid.NewGuid()}/items", request);
+        var response = await Client.PostAsJsonAsync(Routes.Admin.Orders.Items(Guid.NewGuid()), request);
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
     }
 
     [Fact]
     public async Task AddOrderItem_WithNoAuth_ReturnsUnauthorized()
     {
         Client.ClearAuthentication();
-        var request = new
-        {
-            ContentKind = 0,
-            CategoryId = Guid.NewGuid().ToString(),
-            SocialBoost = false,
-            IsBonus = false,
-        };
+        AdminAddOrderItemRequest request = new AdminAddOrderItemRequestBuilder().Build();
 
-        var response = await Client.PostAsJsonAsync($"{ApiRoutes.Admin.Orders}/{Guid.NewGuid()}/items", request);
+        var response = await Client.PostAsJsonAsync(Routes.Admin.Orders.Items(Guid.NewGuid()), request);
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
@@ -78,30 +81,25 @@ public class AdminAddOrderItemEndpointV1Tests(PostgresFixture db) : BaseApiTest(
     [Fact]
     public async Task AddItem_ToSubmittedOrder_ReturnsBadRequest()
     {
-        await using var seedContext = CreateDbContext<ContentDbContext>();
-        var customer = CustomerFactory.Create();
-        var contentType = ContentTypeFactory.Create();
-        var category = CategoryFactory.Create(contentType.Id);
-        var order = ContentOrderFactory.CreateSubmitted();
-        var orderCustomer = CustomerFactory.CreateWithId(order.CustomerId);
-        seedContext.Customers.Add(customer);
-        seedContext.Customers.Add(orderCustomer);
-        seedContext.ContentTypes.Add(contentType);
-        seedContext.Categories.Add(category);
-        seedContext.ContentOrders.Add(order);
-        await seedContext.SaveChangesAsync();
+        ContentTypeEntity contentType = ContentTypeFactory.Create();
+        CategoryEntity category = CategoryFactory.Create(contentType.Id);
+        ContentOrderEntity order = ContentOrderFactory.CreateSubmitted();
+        CustomerEntity orderCustomer = CustomerFactory.CreateWithId(order.CustomerId);
+        await SeedAsync<ContentDbContext>(ctx =>
+        {
+            ctx.Customers.Add(orderCustomer);
+            ctx.ContentTypes.Add(contentType);
+            ctx.Categories.Add(category);
+            ctx.ContentOrders.Add(order);
+        });
 
         Client.AuthenticateAsSuperAdmin();
-        var request = new
-        {
-            ContentKind = 0,
-            CategoryId = category.Id.ToString(),
-            SocialBoost = false,
-            IsBonus = false,
-        };
+        AdminAddOrderItemRequest request = new AdminAddOrderItemRequestBuilder()
+            .WithCategoryId(category.Id.ToString())
+            .Build();
 
-        var response = await Client.PostAsJsonAsync($"{ApiRoutes.Admin.Orders}/{order.Id}/items", request);
+        var response = await Client.PostAsJsonAsync(Routes.Admin.Orders.Items(order.Id), request);
 
-        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        await response.ShouldBeProblem(HttpStatusCode.BadRequest);
     }
 }
