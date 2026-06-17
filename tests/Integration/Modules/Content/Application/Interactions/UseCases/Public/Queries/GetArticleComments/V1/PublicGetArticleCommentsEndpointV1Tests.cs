f99bc2b772798a8c6827ac8e9da12fba9d5f546c
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 19:00:15 +0200

    test(interactions): deepen PublicGetArticleComments tests

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Queries/GetArticleComments/V1/PublicGetArticleCommentsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Queries/GetArticleComments/V1/PublicGetArticleCommentsEndpointV1Tests.cs
@@ -1,3 +1,9 @@
+using _116.Content.Application.Shared.DTOs;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Application.Pagination;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCases.Public.Queries.GetArticleComments.V1;
 
 /// <summary>
@@ -6,25 +12,68 @@ namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCas
 [Collection("Database")]
 public class PublicGetArticleCommentsEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<(ArticleEntity Article, ArticleCommentEntity Comment)> SeedArticleWithCommentAsync()
+    {
+        ArticleEntity article = await SeedAsync<ContentDbContext, ArticleEntity>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+            ArticleEntity created = ArticleFactory.CreatePublished(category.Id);
+            ctx.Articles.Add(created);
+            return created;
+        });
+
+        ArticleCommentEntity comment = await SeedAsync<ContentDbContext, ArticleCommentEntity>(ctx =>
+        {
+            ArticleCommentEntity created = ArticleCommentFactory.Create(article.Id, TestUser.VisitorId);
+            ctx.ArticleComments.Add(created);
+            return created;
+        });
+
+        return (article, comment);
+    }
+
     [Fact]
     public async Task GetArticleComments_AsAnonymous_ReturnsOk()
     {
         Client.ClearAuthentication();
 
-        var response = await Client.GetAsync($"{ApiRoutes.Public.Articles}/{Guid.NewGuid()}/comments");
+        var response = await Client.GetAsync(Routes.Public.Articles.Comments(Guid.NewGuid()));
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+        PaginatedResult<ArticleCommentDto> body = await response.ReadAsAsync<PaginatedResult<ArticleCommentDto>>();
+        body.Items.Should().BeEmpty();
+        body.Count.Should().Be(0);
+    }
+
+    [Fact]
+    public async Task GetArticleComments_WithSeededComment_ReturnsComment()
+    {
+        (ArticleEntity article, ArticleCommentEntity comment) = await SeedArticleWithCommentAsync();
+        Client.ClearAuthentication();
+
+        var response = await Client.GetAsync(Routes.Public.Articles.Comments(article.Id));
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        PaginatedResult<ArticleCommentDto> body = await response.ReadAsAsync<PaginatedResult<ArticleCommentDto>>();
+        body.Count.Should().Be(1);
+        body.Items.Should().ContainSingle(c => c.Id == comment.Id && c.Body == comment.Body);
     }
 
     [Fact]
     public async Task GetArticleComments_WithPagination_ReturnsOk()
     {
+        (ArticleEntity article, _) = await SeedArticleWithCommentAsync();
         Client.ClearAuthentication();
 
-        var response = await Client.GetAsync(
-            $"{ApiRoutes.Public.Articles}/{Guid.NewGuid()}/comments?pageIndex=0&pageSize=5"
-        );
+        var response = await Client.GetAsync($"{Routes.Public.Articles.Comments(article.Id)}?pageIndex=0&pageSize=5");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+        PaginatedResult<ArticleCommentDto> body = await response.ReadAsAsync<PaginatedResult<ArticleCommentDto>>();
+        body.PageIndex.Should().Be(0);
+        body.PageSize.Should().Be(5);
+        body.Count.Should().Be(1);
     }
 }
