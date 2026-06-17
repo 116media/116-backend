087a63a37f35ca5b7050ead9a5e9cfef15c1bcd4
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 16:48:43 +0200

    test(interactions): deepen PublicBookmarkArticle tests

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/V1/PublicBookmarkArticleEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/V1/PublicBookmarkArticleEndpointV1Tests.cs
@@ -1,3 +1,5 @@
+using _116.Content.Application.Interactions.UseCases.Public.Commands.BookmarkArticle.V1;
+using _116.Content.Domain.Entities;
 using _116.Content.Infrastructure.Persistence;
 using _116.Tests.Fixtures.Factories.Content;
 
@@ -9,12 +11,26 @@ namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCas
 [Collection("Database")]
 public class PublicBookmarkArticleEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<ArticleEntity> SeedArticleAsync()
+    {
+        return await SeedAsync<ContentDbContext, ArticleEntity>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+            ArticleEntity article = ArticleFactory.CreatePublished(category.Id);
+            ctx.Articles.Add(article);
+            return article;
+        });
+    }
+
     [Fact]
     public async Task BookmarkArticle_WithNoAuth_ReturnsUnauthorized()
     {
         Client.ClearAuthentication();
 
-        var response = await Client.PostAsync($"{ApiRoutes.Public.Articles}/{Guid.NewGuid()}/bookmarks", null);
+        var response = await Client.PostAsync(Routes.Public.Articles.Bookmarks(Guid.NewGuid()), null);
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
@@ -24,56 +40,44 @@ public class PublicBookmarkArticleEndpointV1Tests(PostgresFixture db) : BaseApiT
     {
         Client.AuthenticateAsVisitor();
 
-        var response = await Client.PostAsync($"{ApiRoutes.Public.Articles}/{Guid.NewGuid()}/bookmarks", null);
+        var response = await Client.PostAsync(Routes.Public.Articles.Bookmarks(Guid.NewGuid()), null);
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
     }
 
     [Fact]
     public async Task BookmarkArticle_AsVisitor_WithValidArticle_ReturnsOk()
     {
-        await using var context = CreateDbContext<ContentDbContext>();
-        var contentType = ContentTypeFactory.Create();
-        context.ContentTypes.Add(contentType);
-        await context.SaveChangesAsync();
-
-        var category = CategoryFactory.Create(contentType.Id);
-        context.Categories.Add(category);
-        await context.SaveChangesAsync();
-
-        var article = ArticleFactory.CreatePublished(category.Id);
-        context.Articles.Add(article);
-        await context.SaveChangesAsync();
-
+        ArticleEntity article = await SeedArticleAsync();
         Client.AuthenticateAsVisitor();
 
-        var response = await Client.PostAsync($"{ApiRoutes.Public.Articles}/{article.Id}/bookmarks", null);
+        var response = await Client.PostAsync(Routes.Public.Articles.Bookmarks(article.Id), null);
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var body = await response.ReadAsAsync<PublicBookmarkArticleResponse>();
+        body.IsSuccess.Should().BeTrue();
+
+        await using var verifyDb = CreateDbContext<ContentDbContext>();
+        (await verifyDb.ArticleBookmarks.AnyAsync(b => b.ArticleId == article.Id && b.UserId == TestUser.VisitorId))
+            .Should()
+            .BeTrue();
     }
 
     [Fact]
     public async Task BookmarkArticle_AsVisitor_AlreadyBookmarked_ReturnsConflict()
     {
-        await using var context = CreateDbContext<ContentDbContext>();
-        var contentType = ContentTypeFactory.Create();
-        context.ContentTypes.Add(contentType);
-        await context.SaveChangesAsync();
-
-        var category = CategoryFactory.Create(contentType.Id);
-        context.Categories.Add(category);
-        await context.SaveChangesAsync();
-
-        var article = ArticleFactory.CreatePublished(category.Id);
-        context.Articles.Add(article);
-        await context.SaveChangesAsync();
-
+        ArticleEntity article = await SeedArticleAsync();
         Client.AuthenticateAsVisitor();
 
-        await Client.PostAsync($"{ApiRoutes.Public.Articles}/{article.Id}/bookmarks", null);
+        await Client.PostAsync(Routes.Public.Articles.Bookmarks(article.Id), null);
 
-        var response = await Client.PostAsync($"{ApiRoutes.Public.Articles}/{article.Id}/bookmarks", null);
+        var response = await Client.PostAsync(Routes.Public.Articles.Bookmarks(article.Id), null);
 
-        response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+        await response.ShouldBeProblem(HttpStatusCode.Conflict);
+
+        await using var verifyDb = CreateDbContext<ContentDbContext>();
+        (await verifyDb.ArticleBookmarks.CountAsync(b => b.ArticleId == article.Id && b.UserId == TestUser.VisitorId))
+            .Should()
+            .Be(1);
     }
 }
