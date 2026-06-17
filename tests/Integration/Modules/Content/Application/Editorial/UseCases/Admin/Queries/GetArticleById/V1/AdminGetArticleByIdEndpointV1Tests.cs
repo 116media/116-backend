d4d0f295a634bb2a24fcd10149fddc99ddec2cfb
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 14:51:45 +0200

    test(editorial): deepen AdminGetArticleById tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetArticleById/V1/AdminGetArticleByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetArticleById/V1/AdminGetArticleByIdEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Editorial.UseCases.Admin.Queries.GetArticleById.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Queries.GetArticleById.V1;
 
 /// <summary>
@@ -6,6 +11,20 @@ namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.
 [Collection("Database")]
 public class AdminGetArticleByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<Guid> SeedCategoryAsync()
+    {
+        return await SeedAsync<ContentDbContext, Guid>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+
+            return category.Id;
+        });
+    }
+
     [Fact]
     public async Task GetArticleById_WithNoAuth_ReturnsUnauthorized()
     {
@@ -31,11 +50,35 @@ public class AdminGetArticleByIdEndpointV1Tests(PostgresFixture db) : BaseApiTes
     [Fact]
     public async Task GetArticleById_AsAdmin_IsAllowed()
     {
+        Guid categoryId = await SeedCategoryAsync();
+        ArticleEntity article = await SeedAsync<ContentDbContext, ArticleEntity>(ctx =>
+        {
+            ArticleEntity entity = ArticleFactory.Create(categoryId);
+            ctx.Articles.Add(entity);
+            return entity;
+        });
+
+        Client.AuthenticateAsAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Articles}/{article.Id}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        AdminGetArticleByIdResponse body = await response.ReadAsAsync<AdminGetArticleByIdResponse>();
+        body.Article.Id.Should().Be(article.Id);
+        body.Article.Title.Should().Be(article.Title);
+        body.Article.Slug.Should().Be(article.Slug);
+        body.Article.CategoryId.Should().Be(categoryId);
+    }
+
+    [Fact]
+    public async Task GetArticleById_WithNonExistent_ReturnsNotFound()
+    {
         Client.AuthenticateAsAdmin();
         var nonExistentId = Guid.NewGuid();
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.Articles}/{nonExistentId}");
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
     }
 }
