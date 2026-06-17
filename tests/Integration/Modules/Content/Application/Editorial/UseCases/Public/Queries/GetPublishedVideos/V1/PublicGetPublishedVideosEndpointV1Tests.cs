f33e5774b6ee10ee5beca09110bc409f6cf20218
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 16:05:38 +0200

    test(editorial): deepen PublicGetPublishedVideos tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPublishedVideos/V1/PublicGetPublishedVideosEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPublishedVideos/V1/PublicGetPublishedVideosEndpointV1Tests.cs
@@ -1,3 +1,9 @@
+using _116.Content.Application.Editorial.UseCases.Public.Queries.GetPublishedVideos.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Domain.Enums;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Public.Queries.GetPublishedVideos.V1;
 
 /// <summary>
@@ -6,13 +12,48 @@ namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.
 [Collection("Database")]
 public class PublicGetPublishedVideosEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<Guid> SeedCategoryAsync()
+    {
+        return await SeedAsync<ContentDbContext, Guid>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+
+            return category.Id;
+        });
+    }
+
     [Fact]
     public async Task GetPublishedVideos_AsAnonymous_ReturnsOk()
     {
+        Guid categoryId = await SeedCategoryAsync();
+        VideoEntity publishedVideo = await SeedAsync<ContentDbContext, VideoEntity>(ctx =>
+        {
+            VideoEntity entity = VideoFactory.CreatePublished(categoryId);
+            ctx.Videos.Add(entity);
+            return entity;
+        });
+        VideoEntity draftVideo = await SeedAsync<ContentDbContext, VideoEntity>(ctx =>
+        {
+            VideoEntity entity = VideoFactory.Create(categoryId);
+            ctx.Videos.Add(entity);
+            return entity;
+        });
+
         Client.ClearAuthentication();
 
         var response = await Client.GetAsync(ApiRoutes.Public.Videos);
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        PublicGetPublishedVideosResponse body = await response.ReadAsAsync<PublicGetPublishedVideosResponse>();
+        body.Videos.Items.Should().Contain(item => item.Id == publishedVideo.Id);
+        body.Videos.Items.Should().NotContain(item => item.Id == draftVideo.Id);
+        body.Videos.Items.Should().OnlyContain(item => item.Status == EnumContentStatus.Published);
+        body.Videos.PageIndex.Should().Be(0);
+        body.Videos.PageSize.Should().Be(10);
     }
 }
