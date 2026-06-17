284f6f4fc7bcdfb65c8eb1af41e1df208377a308
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 15:05:24 +0200

    test(editorial): deepen AdminGetVideoById tests

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById.V1;
 
 /// <summary>
@@ -6,6 +11,20 @@ namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.
 [Collection("Database")]
 public class AdminGetVideoByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<Guid> SeedCategoryAsync()
+    {
+        return await SeedAsync<ContentDbContext, Guid>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+
+            return category.Id;
+        });
+    }
+
     [Fact]
     public async Task GetVideoById_WithNoAuth_ReturnsUnauthorized()
     {
@@ -31,11 +50,35 @@ public class AdminGetVideoByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(
     [Fact]
     public async Task GetVideoById_AsAdmin_IsAllowed()
     {
+        Guid categoryId = await SeedCategoryAsync();
+        VideoEntity video = await SeedAsync<ContentDbContext, VideoEntity>(ctx =>
+        {
+            VideoEntity entity = VideoFactory.Create(categoryId);
+            ctx.Videos.Add(entity);
+            return entity;
+        });
+
+        Client.AuthenticateAsAdmin();
+
+        var response = await Client.GetAsync($"{ApiRoutes.Admin.Videos}/{video.Id}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        AdminGetVideoByIdResponse body = await response.ReadAsAsync<AdminGetVideoByIdResponse>();
+        body.Video.Id.Should().Be(video.Id);
+        body.Video.Title.Should().Be(video.Title);
+        body.Video.Slug.Should().Be(video.Slug);
+        body.Video.CategoryId.Should().Be(categoryId);
+    }
+
+    [Fact]
+    public async Task GetVideoById_WithNonExistent_ReturnsNotFound()
+    {
         Client.AuthenticateAsAdmin();
         var nonExistentId = Guid.NewGuid();
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.Videos}/{nonExistentId}");
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
     }
 }
