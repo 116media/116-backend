4603231690951b1d94ed34ca210d679d5b70e441
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 19:21:26 +0200

    test(interactions): deepen PublicGetPlaylistById tests

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Queries/GetPlaylistById/V1/PublicGetPlaylistByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Queries/GetPlaylistById/V1/PublicGetPlaylistByIdEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Shared.DTOs;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCases.Public.Queries.GetPlaylistById.V1;
 
 /// <summary>
@@ -6,12 +11,37 @@ namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCas
 [Collection("Database")]
 public class PublicGetPlaylistByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<VideoEntity> SeedPublishedVideoAsync()
+    {
+        return await SeedAsync<ContentDbContext, VideoEntity>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+            VideoEntity video = VideoFactory.CreatePublished(category.Id);
+            ctx.Videos.Add(video);
+            return video;
+        });
+    }
+
+    private async Task<PlaylistEntity> SeedPlaylistWithVideoAsync(Guid userId, Guid videoId)
+    {
+        return await SeedAsync<ContentDbContext, PlaylistEntity>(ctx =>
+        {
+            PlaylistEntity playlist = PlaylistFactory.Create(userId);
+            ctx.Playlists.Add(playlist);
+            ctx.PlaylistVideos.Add(PlaylistVideoEntity.Create(Guid.NewGuid(), playlist.Id, videoId, sortOrder: 1));
+            return playlist;
+        });
+    }
+
     [Fact]
     public async Task GetPlaylistById_WithNoAuth_ReturnsUnauthorized()
     {
         Client.ClearAuthentication();
 
-        var response = await Client.GetAsync($"{ApiRoutes.Public.Playlists}/{Guid.NewGuid()}");
+        var response = await Client.GetAsync(Routes.Public.Playlists.ById(Guid.NewGuid()));
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
@@ -21,8 +51,36 @@ public class PublicGetPlaylistByIdEndpointV1Tests(PostgresFixture db) : BaseApiT
     {
         Client.AuthenticateAsVisitor();
 
-        var response = await Client.GetAsync($"{ApiRoutes.Public.Playlists}/{Guid.NewGuid()}");
+        var response = await Client.GetAsync(Routes.Public.Playlists.ById(Guid.NewGuid()));
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetPlaylistById_AsVisitor_NotOwner_ReturnsBadRequest()
+    {
+        VideoEntity video = await SeedPublishedVideoAsync();
+        PlaylistEntity playlist = await SeedPlaylistWithVideoAsync(TestUser.AdminId, video.Id);
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.GetAsync(Routes.Public.Playlists.ById(playlist.Id));
+
+        await response.ShouldBeProblem(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetPlaylistById_AsOwner_ReturnsPlaylistWithVideos()
+    {
+        VideoEntity video = await SeedPublishedVideoAsync();
+        PlaylistEntity playlist = await SeedPlaylistWithVideoAsync(TestUser.VisitorId, video.Id);
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.GetAsync(Routes.Public.Playlists.ById(playlist.Id));
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        PlaylistDetailDto body = await response.ReadAsAsync<PlaylistDetailDto>();
+        body.Id.Should().Be(playlist.Id);
+        body.Name.Should().Be(playlist.Name);
+        body.Videos.Should().ContainSingle(v => v.VideoId == video.Id);
     }
 }
