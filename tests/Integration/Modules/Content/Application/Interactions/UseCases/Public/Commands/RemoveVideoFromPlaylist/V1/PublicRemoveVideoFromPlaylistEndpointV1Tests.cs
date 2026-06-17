b949bc6e567193799f627b20dc91c608373d0991
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 17:58:18 +0200

    test(interactions): deepen PublicRemoveVideoFromPlaylist tests

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/RemoveVideoFromPlaylist/V1/PublicRemoveVideoFromPlaylistEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/RemoveVideoFromPlaylist/V1/PublicRemoveVideoFromPlaylistEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Interactions.UseCases.Public.Commands.RemoveVideoFromPlaylist.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCases.Public.Commands.RemoveVideoFromPlaylist.V1;
 
 /// <summary>
@@ -6,14 +11,37 @@ namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCas
 [Collection("Database")]
 public class PublicRemoveVideoFromPlaylistEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<VideoEntity> SeedPublishedVideoAsync()
+    {
+        return await SeedAsync<ContentDbContext, VideoEntity>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+            VideoEntity video = VideoFactory.CreatePublished(category.Id);
+            ctx.Videos.Add(video);
+            return video;
+        });
+    }
+
+    private async Task<PlaylistEntity> SeedPlaylistWithVideoAsync(Guid userId, Guid videoId)
+    {
+        return await SeedAsync<ContentDbContext, PlaylistEntity>(ctx =>
+        {
+            PlaylistEntity playlist = PlaylistFactory.Create(userId);
+            ctx.Playlists.Add(playlist);
+            ctx.PlaylistVideos.Add(PlaylistVideoEntity.Create(Guid.NewGuid(), playlist.Id, videoId, sortOrder: 1));
+            return playlist;
+        });
+    }
+
     [Fact]
     public async Task RemoveVideoFromPlaylist_WithNoAuth_ReturnsUnauthorized()
     {
         Client.ClearAuthentication();
 
-        var response = await Client.DeleteAsync(
-            $"{ApiRoutes.Public.Playlists}/{Guid.NewGuid()}/videos/{Guid.NewGuid()}"
-        );
+        var response = await Client.DeleteAsync(Routes.Public.Playlists.Video(Guid.NewGuid(), Guid.NewGuid()));
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
@@ -23,10 +51,40 @@ public class PublicRemoveVideoFromPlaylistEndpointV1Tests(PostgresFixture db) : 
     {
         Client.AuthenticateAsVisitor();
 
-        var response = await Client.DeleteAsync(
-            $"{ApiRoutes.Public.Playlists}/{Guid.NewGuid()}/videos/{Guid.NewGuid()}"
-        );
+        var response = await Client.DeleteAsync(Routes.Public.Playlists.Video(Guid.NewGuid(), Guid.NewGuid()));
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task RemoveVideoFromPlaylist_AsVisitor_NotOwner_ReturnsBadRequest()
+    {
+        VideoEntity video = await SeedPublishedVideoAsync();
+        PlaylistEntity playlist = await SeedPlaylistWithVideoAsync(TestUser.AdminId, video.Id);
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.DeleteAsync(Routes.Public.Playlists.Video(playlist.Id, video.Id));
+
+        await response.ShouldBeProblem(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task RemoveVideoFromPlaylist_AsOwner_RemovesJoinRow()
+    {
+        VideoEntity video = await SeedPublishedVideoAsync();
+        PlaylistEntity playlist = await SeedPlaylistWithVideoAsync(TestUser.VisitorId, video.Id);
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.DeleteAsync(Routes.Public.Playlists.Video(playlist.Id, video.Id));
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        PublicRemoveVideoFromPlaylistResponse body =
+            await response.ReadAsAsync<PublicRemoveVideoFromPlaylistResponse>();
+        body.IsSuccess.Should().BeTrue();
+
+        await using ContentDbContext verifyDb = CreateDbContext<ContentDbContext>();
+        (await verifyDb.PlaylistVideos.AnyAsync(pv => pv.PlaylistId == playlist.Id && pv.VideoId == video.Id))
+            .Should()
+            .BeFalse();
     }
 }
