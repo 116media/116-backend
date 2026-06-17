69ebc2a44d50cecb66eb641e2f219618aac49f0d
coolbeatz71 <sigmacool@gmail.com>
2026-06-17 17:08:09 +0200

    test(interactions): deepen PublicDeleteArticleComment tests

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/DeleteArticleComment/V1/PublicDeleteArticleCommentEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/DeleteArticleComment/V1/PublicDeleteArticleCommentEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Interactions.UseCases.Public.Commands.DeleteArticleComment.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCases.Public.Commands.DeleteArticleComment.V1;
 
 /// <summary>
@@ -6,14 +11,36 @@ namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCas
 [Collection("Database")]
 public class PublicDeleteArticleCommentEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private async Task<ArticleEntity> SeedArticleAsync()
+    {
+        return await SeedAsync<ContentDbContext, ArticleEntity>(ctx =>
+        {
+            ContentTypeEntity contentType = ContentTypeFactory.Create();
+            ctx.ContentTypes.Add(contentType);
+            CategoryEntity category = CategoryFactory.Create(contentType.Id);
+            ctx.Categories.Add(category);
+            ArticleEntity article = ArticleFactory.CreatePublished(category.Id);
+            ctx.Articles.Add(article);
+            return article;
+        });
+    }
+
+    private async Task<ArticleCommentEntity> SeedCommentAsync(Guid articleId, Guid authorId)
+    {
+        return await SeedAsync<ContentDbContext, ArticleCommentEntity>(ctx =>
+        {
+            ArticleCommentEntity comment = ArticleCommentFactory.Create(articleId, authorId);
+            ctx.ArticleComments.Add(comment);
+            return comment;
+        });
+    }
+
     [Fact]
     public async Task DeleteArticleComment_WithNoAuth_ReturnsUnauthorized()
     {
         Client.ClearAuthentication();
 
-        var response = await Client.DeleteAsync(
-            $"{ApiRoutes.Public.Articles}/{Guid.NewGuid()}/comments/{Guid.NewGuid()}"
-        );
+        var response = await Client.DeleteAsync(Routes.Public.Articles.Comment(Guid.NewGuid(), Guid.NewGuid()));
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
@@ -23,10 +50,38 @@ public class PublicDeleteArticleCommentEndpointV1Tests(PostgresFixture db) : Bas
     {
         Client.AuthenticateAsVisitor();
 
-        var response = await Client.DeleteAsync(
-            $"{ApiRoutes.Public.Articles}/{Guid.NewGuid()}/comments/{Guid.NewGuid()}"
-        );
+        var response = await Client.DeleteAsync(Routes.Public.Articles.Comment(Guid.NewGuid(), Guid.NewGuid()));
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task DeleteArticleComment_AsVisitor_NotOwner_ReturnsBadRequest()
+    {
+        ArticleEntity article = await SeedArticleAsync();
+        ArticleCommentEntity comment = await SeedCommentAsync(article.Id, TestUser.SuperAdminId);
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.DeleteAsync(Routes.Public.Articles.Comment(article.Id, comment.Id));
+
+        await response.ShouldBeProblem(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task DeleteArticleComment_AsOwner_SoftDeletesComment()
+    {
+        ArticleEntity article = await SeedArticleAsync();
+        ArticleCommentEntity comment = await SeedCommentAsync(article.Id, TestUser.VisitorId);
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.DeleteAsync(Routes.Public.Articles.Comment(article.Id, comment.Id));
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        PublicDeleteArticleCommentResponse body = await response.ReadAsAsync<PublicDeleteArticleCommentResponse>();
+        body.IsSuccess.Should().BeTrue();
+
+        await using ContentDbContext verifyDb = CreateDbContext<ContentDbContext>();
+        ArticleCommentEntity? stored = await verifyDb.ArticleComments.FindAsync(comment.Id);
+        stored!.IsDeleted.Should().BeTrue();
     }
 }
