655ff633f34b0b43125780a9c89f185b676f14f1
coolbeatz71 <sigmacool@gmail.com>
2026-06-18 10:45:47 +0200

    test(lookup): deepen PublicGetPopularTags tests

--- tests/Integration/Modules/Content/Application/Lookup/UseCases/Public/Queries/GetPopularTags/V1/PublicGetPopularTagsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Lookup/UseCases/Public/Queries/GetPopularTags/V1/PublicGetPopularTagsEndpointV1Tests.cs
@@ -1,3 +1,5 @@
+using _116.Content.Application.Lookup.UseCases.Public.Queries.GetPopularTags.V1;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Lookup.UseCases.Public.Queries.GetPopularTags.V1;
 
 /// <summary>
@@ -7,42 +9,55 @@ namespace _116.Integration.Tests.Modules.Content.Application.Lookup.UseCases.Pub
 public class PublicGetPopularTagsEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
     [Fact]
-    public async Task GetPopularTags_AsAnonymous_ReturnsOk()
+    public async Task GetPopularTags_AsAnonymous_ReturnsTagsList()
     {
         Client.ClearAuthentication();
 
         var response = await Client.GetAsync($"{ApiRoutes.Public.Tags}/popular");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<PublicGetPopularTagsResponse>();
+        body.Tags.Should().NotBeNull();
     }
 
     [Fact]
-    public async Task GetPopularTags_WithLimitParam_ReturnsOk()
+    public async Task GetPopularTags_WithLimitParam_RespectsLimit()
     {
         Client.ClearAuthentication();
 
         var response = await Client.GetAsync($"{ApiRoutes.Public.Tags}/popular?limit=5");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<PublicGetPopularTagsResponse>();
+        body.Tags.Should().NotBeNull();
+        body.Tags.Count.Should().BeLessThanOrEqualTo(5);
     }
 
     [Fact]
-    public async Task GetPopularTags_WithContentTypeParam_ReturnsOk()
+    public async Task GetPopularTags_WithContentTypeParam_ReturnsTagsList()
     {
         Client.ClearAuthentication();
 
         var response = await Client.GetAsync($"{ApiRoutes.Public.Tags}/popular?contentType=Article");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<PublicGetPopularTagsResponse>();
+        body.Tags.Should().NotBeNull();
     }
 
     [Fact]
-    public async Task GetPopularTags_WithVideoContentType_ReturnsOk()
+    public async Task GetPopularTags_WithVideoContentType_ReturnsTagsList()
     {
         Client.ClearAuthentication();
 
         var response = await Client.GetAsync($"{ApiRoutes.Public.Tags}/popular?contentType=Video");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<PublicGetPopularTagsResponse>();
+        body.Tags.Should().NotBeNull();
     }
 }
