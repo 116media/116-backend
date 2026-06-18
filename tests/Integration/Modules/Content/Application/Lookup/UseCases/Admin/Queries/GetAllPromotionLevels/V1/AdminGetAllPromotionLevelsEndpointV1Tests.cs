5fac587526e557863c70ac84be3e40820000299a
coolbeatz71 <sigmacool@gmail.com>
2026-06-18 10:14:13 +0200

    test(lookup): deepen AdminGetAllPromotionLevels tests

--- tests/Integration/Modules/Content/Application/Lookup/UseCases/Admin/Queries/GetAllPromotionLevels/V1/AdminGetAllPromotionLevelsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Lookup/UseCases/Admin/Queries/GetAllPromotionLevels/V1/AdminGetAllPromotionLevelsEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Lookup.UseCases.Admin.Queries.GetAllPromotionLevels.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Lookup.UseCases.Admin.Queries.GetAllPromotionLevels.V1;
 
 /// <summary>
@@ -17,22 +22,43 @@ public class AdminGetAllPromotionLevelsEndpointV1Tests(PostgresFixture db) : Bas
     }
 
     [Fact]
-    public async Task GetAllPromotionLevels_AsAdmin_ReturnsOk()
+    public async Task GetAllPromotionLevels_AsAdmin_ReturnsSeededPromotionLevel()
     {
+        PromotionLevelEntity promotionLevel = await SeedAsync<ContentDbContext, PromotionLevelEntity>(ctx =>
+        {
+            PromotionLevelEntity entity = PromotionLevelFactory.Create();
+            ctx.PromotionLevels.Add(entity);
+            return entity;
+        });
+
         Client.AuthenticateAsAdmin();
 
         var response = await Client.GetAsync(ApiRoutes.Admin.PromotionLevels);
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<AdminGetAllPromotionLevelsResponse>();
+        body.PromotionLevels.Should().Contain(p => p.Id == promotionLevel.Id && p.Name == promotionLevel.Name);
     }
 
     [Fact]
-    public async Task GetAllPromotionLevels_AsSuperAdmin_WithSearch_ReturnsOk()
+    public async Task GetAllPromotionLevels_AsSuperAdmin_WithSearch_ReturnsMatchingPromotionLevels()
     {
+        PromotionLevelEntity matching = await SeedAsync<ContentDbContext, PromotionLevelEntity>(ctx =>
+        {
+            PromotionLevelEntity entity = PromotionLevelFactory.Create("Featured Spot", 7, 50m);
+            ctx.PromotionLevels.Add(entity);
+            return entity;
+        });
+
         Client.AuthenticateAsSuperAdmin();
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.PromotionLevels}?search=featured");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<AdminGetAllPromotionLevelsResponse>();
+        body.PromotionLevels.Should().Contain(p => p.Id == matching.Id);
+        body.PromotionLevels.Should().OnlyContain(p => p.Name.Contains("featured", StringComparison.OrdinalIgnoreCase));
     }
 }
