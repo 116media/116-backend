76e42317324d9b7b0d37c8cebf033fc40ff3e299
coolbeatz71 <sigmacool@gmail.com>
2026-06-18 10:20:36 +0200

    test(lookup): deepen AdminGetAllTags tests

--- tests/Integration/Modules/Content/Application/Lookup/UseCases/Admin/Queries/GetAllTags/V1/AdminGetAllTagsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Lookup/UseCases/Admin/Queries/GetAllTags/V1/AdminGetAllTagsEndpointV1Tests.cs
@@ -1,3 +1,8 @@
+using _116.Content.Application.Lookup.UseCases.Admin.Queries.GetAllTags.V1;
+using _116.Content.Domain.Entities;
+using _116.Content.Infrastructure.Persistence;
+using _116.Tests.Fixtures.Factories.Content;
+
 namespace _116.Integration.Tests.Modules.Content.Application.Lookup.UseCases.Admin.Queries.GetAllTags.V1;
 
 /// <summary>
@@ -17,22 +22,47 @@ public class AdminGetAllTagsEndpointV1Tests(PostgresFixture db) : BaseApiTest(db
     }
 
     [Fact]
-    public async Task GetAllTags_AsAdmin_ReturnsOk()
+    public async Task GetAllTags_AsAdmin_ReturnsSeededTag()
     {
+        TagEntity tag = await SeedAsync<ContentDbContext, TagEntity>(ctx =>
+        {
+            TagEntity entity = TagFactory.Create();
+            ctx.Tags.Add(entity);
+            return entity;
+        });
+
         Client.AuthenticateAsAdmin();
 
         var response = await Client.GetAsync(ApiRoutes.Admin.Tags);
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<AdminGetAllTagsResponse>();
+        body.Tags.Should().Contain(t => t.Id == tag.Id && t.Name == tag.Name && t.Slug == tag.Slug);
     }
 
     [Fact]
-    public async Task GetAllTags_AsSuperAdmin_WithSearch_ReturnsOk()
+    public async Task GetAllTags_AsSuperAdmin_WithSearch_ReturnsMatchingTags()
     {
+        TagEntity matching = await SeedAsync<ContentDbContext, TagEntity>(ctx =>
+        {
+            TagEntity entity = TagFactory.Create("test tag", "test-tag");
+            ctx.Tags.Add(entity);
+            return entity;
+        });
+
         Client.AuthenticateAsSuperAdmin();
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.Tags}?search=test");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var body = await response.ReadAsAsync<AdminGetAllTagsResponse>();
+        body.Tags.Should().Contain(t => t.Id == matching.Id);
+        body.Tags.Should()
+            .OnlyContain(t =>
+                t.Name.Contains("test", StringComparison.OrdinalIgnoreCase)
+                || t.Slug.Contains("test", StringComparison.OrdinalIgnoreCase)
+            );
     }
 }
