d2a721289932004cdeac161594fd9d4605d43f52
coolbeatz71 <sigmacool@gmail.com>
2026-06-18 16:41:25 +0200

    test(session): deepen PublicGetOwnSessionById tests

--- tests/Integration/Modules/Identity/Application/Session/UseCases/Public/Queries/GetOwnSessionById/V1/PublicGetOwnSessionByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Identity/Application/Session/UseCases/Public/Queries/GetOwnSessionById/V1/PublicGetOwnSessionByIdEndpointV1Tests.cs
@@ -1,3 +1,6 @@
+using _116.Identity.Application.Session.Constants;
+using _116.Identity.Application.Session.UseCases.Public.Queries.GetOwnSessionById.V1;
+using _116.Identity.Domain.Entities;
 using _116.Identity.Infrastructure.Persistence;
 using _116.Tests.Fixtures.Factories.Identity;
 
@@ -9,13 +12,15 @@ namespace _116.Integration.Tests.Modules.Identity.Application.Session.UseCases.P
 [Collection("Database")]
 public class PublicGetOwnSessionByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 {
+    private const string PublicMeSessions = $"{ApiRoutes.Public.Me}/{SessionRouteConstants.Endpoint}";
+
     [Fact]
     public async Task GetOwnSessionById_WithNoAuth_ReturnsUnauthorized()
     {
         Client.ClearAuthentication();
-        var sessionId = Guid.NewGuid();
+        Guid sessionId = Guid.NewGuid();
 
-        var response = await Client.GetAsync($"{ApiRoutes.Public.Me}/sessions/{sessionId}");
+        var response = await Client.GetAsync($"{PublicMeSessions}/{sessionId}");
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
@@ -24,21 +29,41 @@ public class PublicGetOwnSessionByIdEndpointV1Tests(PostgresFixture db) : BaseAp
     public async Task GetOwnSessionById_AsVisitor_WithNonExistentId_ReturnsNotFound()
     {
         Client.AuthenticateAsVisitor();
-        var nonExistentId = Guid.NewGuid();
+        Guid nonExistentId = Guid.NewGuid();
 
-        var response = await Client.GetAsync($"{ApiRoutes.Public.Me}/sessions/{nonExistentId}");
+        var response = await Client.GetAsync($"{PublicMeSessions}/{nonExistentId}");
 
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem(HttpStatusCode.NotFound);
     }
 
     [Fact]
     public async Task GetOwnSessionById_AsAdmin_ReturnsForbidden()
     {
         Client.AuthenticateAsAdmin();
-        var sessionId = Guid.NewGuid();
+        Guid sessionId = Guid.NewGuid();
 
-        var response = await Client.GetAsync($"{ApiRoutes.Public.Me}/sessions/{sessionId}");
+        var response = await Client.GetAsync($"{PublicMeSessions}/{sessionId}");
 
         response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task GetOwnSessionById_AsVisitor_WithOwnSession_ReturnsOk()
+    {
+        SessionEntity session = await SeedAsync<IdentityDbContext, SessionEntity>(ctx =>
+        {
+            SessionEntity entity = SessionFactory.Create(TestUser.VisitorId);
+            ctx.Sessions.Add(entity);
+            return entity;
+        });
+
+        Client.AuthenticateAsVisitor();
+
+        var response = await Client.GetAsync($"{PublicMeSessions}/{session.Id}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        PublicGetOwnSessionByIdResponse body = await response.ReadAsAsync<PublicGetOwnSessionByIdResponse>();
+        body.Session.Id.Should().Be(session.Id);
     }
 }
