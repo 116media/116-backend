06b09b36cdd6b90f803226a5dc1d13e7b8cd98db
coolbeatz71 <sigmacool@gmail.com>
2026-06-21 10:53:02 +0200

    feat(short-video): pass errors to Activate for file guard

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ActivateShortVideo/AdminActivateShortVideoHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ActivateShortVideo/AdminActivateShortVideoHandler.cs
@@ -31,7 +31,7 @@ public class AdminActivateShortVideoHandler(
             cancellationToken: cancellationToken
         );
 
-        bool activated = shortVideo.Activate();
+        bool activated = shortVideo.Activate(i18n.ShortVideo);
 
         if (!activated)
         {
