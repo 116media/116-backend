38945f3b2ba6056642c679b05d85cfacf2ba40bf
coolbeatz71 <sigmacool@gmail.com>
2026-06-29 09:22:56 +0200

    test(editorial): assert popular cache invalidated on publish

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/PublishArticle/AdminPublishArticleHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/PublishArticle/AdminPublishArticleHandlerTests.cs
@@ -1,4 +1,5 @@
 using _116.Content.Application.Editorial.UseCases.Admin.Commands.PublishArticle;
+using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -20,6 +21,7 @@ public class AdminPublishArticleHandlerTests
 {
     private readonly Mock<IArticleRepository> _articleRepositoryMock;
     private readonly Mock<IContentUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IPopularArticlesCacheInvalidator> _cacheInvalidatorMock;
     private readonly AdminPublishArticleHandler _handler;
 
     private static readonly Guid CategoryId = Guid.NewGuid();
@@ -28,9 +30,11 @@ public class AdminPublishArticleHandlerTests
     {
         _articleRepositoryMock = MockArticleRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
+        _cacheInvalidatorMock = MockPopularArticlesCacheInvalidator.Create();
         _handler = new AdminPublishArticleHandler(
             _articleRepositoryMock.Object,
             _unitOfWorkMock.Object,
+            _cacheInvalidatorMock.Object,
             TestErrorsFactory.CreateContentI18n()
         );
     }
@@ -52,6 +56,7 @@ public class AdminPublishArticleHandlerTests
         result.IsSuccess.Should().BeTrue();
         _articleRepositoryMock.VerifyUpdateCalled();
         _unitOfWorkMock.VerifyCommitCalled();
+        _cacheInvalidatorMock.VerifyInvalidateCalled();
     }
 
     #endregion
