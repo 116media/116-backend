9ceee8b8b511259dd5b11799f507c4a84e545d17
coolbeatz71 <sigmacool@gmail.com>
2026-06-29 09:59:53 +0200

    test(interactions): assert popular cache busted on unbookmark

--- tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/UnbookmarkArticle/PublicUnbookmarkArticleHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Interactions/UseCases/Public/Commands/UnbookmarkArticle/PublicUnbookmarkArticleHandlerTests.cs
@@ -1,4 +1,5 @@
 using _116.Content.Application.Interactions.UseCases.Public.Commands.UnbookmarkArticle;
+using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -20,6 +21,7 @@ public class PublicUnbookmarkArticleHandlerTests
 {
     private readonly Mock<IArticleRepository> _articleRepositoryMock;
     private readonly Mock<IContentUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IPopularArticlesCacheInvalidator> _cacheInvalidatorMock;
     private readonly PublicUnbookmarkArticleHandler _handler;
 
     private static readonly Guid CategoryId = Guid.NewGuid();
@@ -28,9 +30,11 @@ public class PublicUnbookmarkArticleHandlerTests
     {
         _articleRepositoryMock = MockArticleRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
+        _cacheInvalidatorMock = MockPopularArticlesCacheInvalidator.Create();
         _handler = new PublicUnbookmarkArticleHandler(
             _articleRepositoryMock.Object,
             _unitOfWorkMock.Object,
+            _cacheInvalidatorMock.Object,
             TestErrorsFactory.CreateContentI18n()
         );
     }
@@ -55,6 +59,7 @@ public class PublicUnbookmarkArticleHandlerTests
         _articleRepositoryMock.VerifyRemoveBookmarkCalled(userId, article.Id);
         _articleRepositoryMock.VerifyUpdateCalled();
         _unitOfWorkMock.VerifyCommitCalled();
+        _cacheInvalidatorMock.VerifyInvalidateCalled();
     }
 
     #endregion
