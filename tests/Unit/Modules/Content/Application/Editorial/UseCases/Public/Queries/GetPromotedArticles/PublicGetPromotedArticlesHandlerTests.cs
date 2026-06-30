b9b6a6ec7ede12883bca389d957ef051068aa154
coolbeatz71 <sigmacool@gmail.com>
2026-06-30 09:44:31 +0200

    test(content): cover interaction flags in promoted feed

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPromotedArticles/PublicGetPromotedArticlesHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetPromotedArticles/PublicGetPromotedArticlesHandlerTests.cs
@@ -69,4 +69,45 @@ public class PublicGetPromotedArticlesHandlerTests : BaseContentHandlerTest
         // Assert
         result.Articles.Should().BeEmpty();
     }
+
+    [Fact]
+    public async Task Handle_WhenAnonymous_ShouldReturnAllFalseFlagsAndSkipBatchLookups()
+    {
+        // Arrange
+        List<ArticleEntity> promoted = ArticleFactory.CreateManyPublished(CategoryId, 2);
+        var query = new PublicGetPromotedArticlesQuery();
+
+        _articleRepositoryMock.SetupGetPromotedAsync(promoted);
+
+        // Act
+        PublicGetPromotedArticlesResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Articles.Should().OnlyContain(article => !article.IsLiked && !article.IsBookmarked);
+        _articleRepositoryMock.VerifyGetLikedAndBookmarkedIdsCalledWithUser(Times.Never());
+    }
+
+    [Fact]
+    public async Task Handle_WhenAuthenticated_ShouldStampOnlyInteractedItems_WithSingleBatchPerType()
+    {
+        // Arrange
+        List<ArticleEntity> promoted = ArticleFactory.CreateManyPublished(CategoryId, 3);
+        Guid likedId = promoted[0].Id;
+        Guid bookmarkedId = promoted[1].Id;
+        var query = new PublicGetPromotedArticlesQuery(CurrentUserId: Guid.NewGuid());
+
+        _articleRepositoryMock.SetupGetPromotedAsync(promoted);
+        _articleRepositoryMock.SetupGetLikedAndBookmarkedIds([likedId], [bookmarkedId]);
+
+        // Act
+        PublicGetPromotedArticlesResult result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Articles.Single(article => article.Id == likedId).IsLiked.Should().BeTrue();
+        result.Articles.Single(article => article.Id == bookmarkedId).IsBookmarked.Should().BeTrue();
+        result.Articles.Single(article => article.Id == promoted[2].Id).IsLiked.Should().BeFalse();
+        result.Articles.Single(article => article.Id == promoted[2].Id).IsBookmarked.Should().BeFalse();
+
+        _articleRepositoryMock.VerifyGetLikedAndBookmarkedIdsCalledWithUser(Times.Once());
+    }
 }
