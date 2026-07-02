e94b0e03e541cedfc835c5d8f7125cbf296ff7d3
coolbeatz71 <sigmacool@gmail.com>
2026-07-02 22:55:49 +0200

    chore(catalog): remove unused usings

--- tests/Integration/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/V1/AdminCreateCategoryEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/V1/AdminCreateCategoryEndpointV1Tests.cs
@@ -1,4 +1,3 @@
-using System.Net.Http.Json;
 using _116.Content.Application.Catalog.UseCases.Admin.Commands.CreateCategory.V1;
 using _116.Content.Domain.Entities;
 using _116.Content.Infrastructure.Persistence;
