d0d7284571a9ba43ea5d03184db2c006581f71fa
coolbeatz71 <sigmacool@gmail.com>
2026-07-02 15:41:34 +0200

    feat(editorial): invalidate popular videos cache on archive

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveVideo/AdminArchiveVideoHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveVideo/AdminArchiveVideoHandler.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -13,8 +14,13 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.ArchiveVide
 /// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
-public class AdminArchiveVideoHandler(IVideoRepository videoRepository, IContentUnitOfWork unitOfWork, ContentI18n i18n)
-    : ICommandHandler<AdminArchiveVideoCommand, AdminArchiveVideoResult>
+/// <param name="cacheInvalidator">Invalidates the popular-videos cache after the published set changes.</param>
+public class AdminArchiveVideoHandler(
+    IVideoRepository videoRepository,
+    IContentUnitOfWork unitOfWork,
+    ContentI18n i18n,
+    IPopularVideosCacheInvalidator cacheInvalidator
+) : ICommandHandler<AdminArchiveVideoCommand, AdminArchiveVideoResult>
 {
     /// <inheritdoc />
     public async Task<AdminArchiveVideoResult> Handle(
@@ -35,6 +41,8 @@ public class AdminArchiveVideoHandler(IVideoRepository videoRepository, IContent
 
         videoRepository.Update(video: video);
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
+
+        cacheInvalidator.Invalidate();
 
         return new AdminArchiveVideoResult(IsSuccess: true);
     }
