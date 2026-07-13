70bc9d4b1cbe32235b06acf146bcdeef63e9b0c7
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-13 18:14:26 +0200

    refactor(interactions): use ShareChannel on share article command

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/ShareArticle/PublicShareArticleCommand.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/ShareArticle/PublicShareArticleCommand.cs
@@ -1,3 +1,4 @@
+using _116.Content.Domain.Enums;
 using _116.Shared.Contracts.Application.CQRS;
 
 namespace _116.Content.Application.Interactions.UseCases.Public.Commands.ShareArticle;
@@ -7,8 +8,8 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.ShareAr
 /// </summary>
 /// <param name="ArticleId">The unique identifier of the article that was shared.</param>
 /// <param name="UserId">The identity user UUID of the sharer. Null for anonymous shares.</param>
-/// <param name="Platform">The channel the share targeted (e.g. facebook, x, whatsapp). Null when unreported.</param>
-public record PublicShareArticleCommand(Guid ArticleId, Guid? UserId, string? Platform = null)
+/// <param name="ShareChannel">The channel the share targeted. Null when unreported.</param>
+public record PublicShareArticleCommand(Guid ArticleId, Guid? UserId, EnumShareChannel? ShareChannel = null)
     : ICommand<PublicShareArticleResult>;
 
 /// <summary>
