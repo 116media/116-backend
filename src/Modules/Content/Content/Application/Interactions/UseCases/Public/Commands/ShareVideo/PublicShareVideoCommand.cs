6a64ef7cb32867debde36e002f2532b2eb24f66f
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-13 13:17:18 +0200

    feat(interactions): add platform to share video command

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/ShareVideo/PublicShareVideoCommand.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/ShareVideo/PublicShareVideoCommand.cs
@@ -7,7 +7,9 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.ShareVi
 /// </summary>
 /// <param name="VideoId">The unique identifier of the video that was shared.</param>
 /// <param name="UserId">The identity user UUID of the sharer. Null for anonymous shares.</param>
-public record PublicShareVideoCommand(Guid VideoId, Guid? UserId) : ICommand<PublicShareVideoResult>;
+/// <param name="Platform">The channel the share targeted (e.g. facebook, x, whatsapp). Null when unreported.</param>
+public record PublicShareVideoCommand(Guid VideoId, Guid? UserId, string? Platform = null)
+    : ICommand<PublicShareVideoResult>;
 
 /// <summary>
 /// Result of the <see cref="PublicShareVideoCommand" />.
