f424d33813c0df05b5c814718ad4054fca4a87e1
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-15 11:09:36 +0200

    feat(short-video): resolve caller id on by-slug endpoint

--- src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetPublicShortBySlug/V1/PublicGetPublicShortBySlugEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetPublicShortBySlug/V1/PublicGetPublicShortBySlugEndpointV1.cs
@@ -1,7 +1,9 @@
+using System.Security.Claims;
 using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Editorial.Constants;
 using _116.Content.Application.Shared.DTOs;
 using _116.Content.Domain.Constants;
+using _116.Identity.Contracts.Application;
 using _116.Shared.Application.Extensions;
 using _116.Shared.Contracts.Application.CQRS;
 using Carter;
@@ -37,9 +39,16 @@ public class PublicGetPublicShortBySlugEndpointV1 : ICarterModule
         group
             .MapGet(
                 "/{slug}",
-                async (string slug, IDispatcher dispatcher) =>
+                async (string slug, ClaimsPrincipal user, IClaimsProvider claimsProvider, IDispatcher dispatcher) =>
                 {
-                    var query = new PublicGetPublicShortBySlugQuery(Slug: slug);
+                    Guid? userId = null;
+
+                    if (user.Identity?.IsAuthenticated == true)
+                    {
+                        userId = claimsProvider.GetUserIdFromClaims(user: user);
+                    }
+
+                    var query = new PublicGetPublicShortBySlugQuery(Slug: slug, CurrentUserId: userId);
                     PublicGetPublicShortBySlugResult result = await dispatcher.Send(request: query);
 
                     var response = new PublicGetPublicShortBySlugResponse(ShortVideo: result.ShortVideo);
