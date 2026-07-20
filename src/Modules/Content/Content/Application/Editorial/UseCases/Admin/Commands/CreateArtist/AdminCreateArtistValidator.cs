6e847535bc39b69ab13f1cac5ce9cba1855e1bc1
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-20 23:48:10 +0000

    feat(artists): validate new artist profile creation data

---
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateArtist/AdminCreateArtistValidator.cs
@@ -0,0 +1,22 @@
+using _116.Content.Application.Shared.Errors.Facade;
+using _116.Content.Application.Shared.Validators;
+using FluentValidation;
+
+namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateArtist;
+
+/// <summary>
+/// Validator for the <see cref="AdminCreateArtistCommand" /> ensuring proper artist profile creation data.
+/// </summary>
+public class AdminCreateArtistValidator : AbstractValidator<AdminCreateArtistCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="AdminCreateArtistValidator" /> with the specified error message provider.
+    /// </summary>
+    /// <param name="i18n">Content module i18n facade.</param>
+    public AdminCreateArtistValidator(ContentI18n i18n)
+    {
+        RuleFor(x => x.Name).ValidArtistName(i18n.Artist.Msg);
+
+        RuleFor(x => x.Slug).ValidArtistSlug(i18n.Artist.Msg);
+    }
+}
