c137eca735745e120f5c81aae173cfc714e282f3
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-21 09:53:29 +0000

    test(review): add LyricsRevisionVoteBuilder

---
+++ tests/Fixtures/Builders/Entities/Content/LyricsRevisionVoteBuilder.cs
@@ -0,0 +1,80 @@
+using _116.Content.Domain.Entities;
+using _116.Content.Domain.Enums;
+
+namespace _116.Tests.Fixtures.Builders.Entities.Content;
+
+/// <summary>
+/// Fluent builder for creating <see cref="LyricsRevisionVoteEntity"/> instances in tests.
+/// For test code, prefer using LyricsRevisionVoteFactory instead of direct Builder usage.
+/// </summary>
+internal class LyricsRevisionVoteBuilder
+{
+    private Guid _id = Guid.NewGuid();
+    private Guid _revisionId = Guid.NewGuid();
+    private Guid _userId = Guid.NewGuid();
+    private EnumVote _vote = EnumVote.Approve;
+    private string? _comment;
+
+    /// <summary>
+    /// Sets the vote ID.
+    /// </summary>
+    public LyricsRevisionVoteBuilder WithId(Guid id)
+    {
+        _id = id;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the lyrics revision being voted on.
+    /// </summary>
+    public LyricsRevisionVoteBuilder WithRevisionId(Guid revisionId)
+    {
+        _revisionId = revisionId;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the identity user UUID of the voter.
+    /// </summary>
+    public LyricsRevisionVoteBuilder WithUserId(Guid userId)
+    {
+        _userId = userId;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the cast vote direction.
+    /// </summary>
+    public LyricsRevisionVoteBuilder WithVote(EnumVote vote)
+    {
+        _vote = vote;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the optional free-text comment.
+    /// </summary>
+    public LyricsRevisionVoteBuilder WithComment(string? comment)
+    {
+        _comment = comment;
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="LyricsRevisionVoteEntity"/> instance.
+    /// </summary>
+    public LyricsRevisionVoteEntity Build()
+    {
+        LyricsRevisionVoteEntity entity = LyricsRevisionVoteEntity.Create(
+            id: _id,
+            revisionId: _revisionId,
+            userId: _userId,
+            vote: _vote,
+            comment: _comment
+        );
+
+        entity.CreatedAt = DateTime.UtcNow;
+
+        return entity;
+    }
+}
