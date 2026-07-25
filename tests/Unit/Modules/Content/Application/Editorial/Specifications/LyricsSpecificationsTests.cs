ef75c63b5ead91d4dd588aba4bf11429b4190a08
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-25 20:28:20 +0000

    test(lyrics): cover artist, album, similar, and like specifications

--- tests/Unit/Modules/Content/Application/Editorial/Specifications/LyricsSpecificationsTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/Specifications/LyricsSpecificationsTests.cs
@@ -212,4 +212,253 @@ public class LyricsSpecificationsTests
     }
 
     #endregion
+
+    #region LyricsByArtistSpecification
+
+    [Fact]
+    public void LyricsByArtistSpecification_WithMatchingArtistId_ShouldReturnTrue()
+    {
+        // Arrange
+        Guid artistId = Guid.NewGuid();
+        LyricsEntity lyrics = LyricsFactory.CreateForArtist(CategoryId, artistId);
+        var spec = new LyricsByArtistSpecification(artistId);
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsByArtistSpecification_WithDifferentArtistId_ShouldReturnFalse()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.CreateForArtist(CategoryId, Guid.NewGuid());
+        var spec = new LyricsByArtistSpecification(Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
+
+    #region LyricsByAlbumSpecification
+
+    [Fact]
+    public void LyricsByAlbumSpecification_WithMatchingAlbumId_ShouldReturnTrue()
+    {
+        // Arrange
+        Guid albumId = Guid.NewGuid();
+        LyricsEntity lyrics = LyricsFactory.CreateForAlbum(CategoryId, albumId);
+        var spec = new LyricsByAlbumSpecification(albumId);
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsByAlbumSpecification_WithStandaloneLyrics_ShouldReturnFalse()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.Create(CategoryId);
+        var spec = new LyricsByAlbumSpecification(Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
+
+    #region LyricsByOrderItemIdSpecification
+
+    [Fact]
+    public void LyricsByOrderItemIdSpecification_WithMatchingOrderItemId_ShouldReturnTrue()
+    {
+        // Arrange
+        Guid orderItemId = Guid.NewGuid();
+        LyricsEntity lyrics = LyricsFactory.CreatePaid(CategoryId, Guid.NewGuid(), orderItemId);
+        var spec = new LyricsByOrderItemIdSpecification(orderItemId);
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsByOrderItemIdSpecification_WithDifferentOrderItemId_ShouldReturnFalse()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.CreatePaid(CategoryId, Guid.NewGuid(), Guid.NewGuid());
+        var spec = new LyricsByOrderItemIdSpecification(Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
+
+    #region LyricsSimilarByVideoCategorySpecification
+
+    [Fact]
+    public void LyricsSimilarByVideoCategorySpecification_WithPublishedLyricsInSameVideoCategory_ShouldReturnTrue()
+    {
+        // Arrange
+        VideoEntity video = VideoFactory.Create(CategoryId);
+        LyricsEntity lyrics = LyricsFactory.CreatePublishedWithVideoNavigation(CategoryId, video);
+        var spec = new LyricsSimilarByVideoCategorySpecification(CategoryId, Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsSimilarByVideoCategorySpecification_WithExcludedSourceLyrics_ShouldReturnFalse()
+    {
+        // Arrange
+        VideoEntity video = VideoFactory.Create(CategoryId);
+        LyricsEntity lyrics = LyricsFactory.CreatePublishedWithVideoNavigation(CategoryId, video);
+        var spec = new LyricsSimilarByVideoCategorySpecification(CategoryId, lyrics.Id);
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void LyricsSimilarByVideoCategorySpecification_WithStandaloneLyrics_ShouldReturnFalse()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.CreatePublished(CategoryId);
+        var spec = new LyricsSimilarByVideoCategorySpecification(CategoryId, Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
+
+    #region LyricsBySharedTagsSpecification
+
+    [Fact]
+    public void LyricsBySharedTagsSpecification_WithPublishedLyricsSharingATag_ShouldReturnTrue()
+    {
+        // Arrange
+        Guid tagId = Guid.NewGuid();
+        LyricsEntity lyrics = LyricsFactory.CreateWithTags(CategoryId, tagId);
+        lyrics.Publish();
+        var spec = new LyricsBySharedTagsSpecification([tagId], Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsBySharedTagsSpecification_WithNoSharedTag_ShouldReturnFalse()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.CreateWithTags(CategoryId, Guid.NewGuid());
+        lyrics.Publish();
+        var spec = new LyricsBySharedTagsSpecification([Guid.NewGuid()], Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
+
+    #region LyricsStandaloneSpecification
+
+    [Fact]
+    public void LyricsStandaloneSpecification_WithPublishedStandaloneLyrics_ShouldReturnTrue()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.CreatePublished(CategoryId);
+        var spec = new LyricsStandaloneSpecification(Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsStandaloneSpecification_WithDraftStandaloneLyrics_ShouldReturnFalse()
+    {
+        // Arrange
+        LyricsEntity lyrics = LyricsFactory.Create(CategoryId);
+        var spec = new LyricsStandaloneSpecification(Guid.NewGuid());
+
+        // Act
+        bool result = spec.IsSatisfiedBy(lyrics);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
+
+    #region LyricsLikeByUserAndLyricsSpecification
+
+    [Fact]
+    public void LyricsLikeByUserAndLyricsSpecification_WithMatchingUserAndLyrics_ShouldReturnTrue()
+    {
+        // Arrange
+        Guid userId = Guid.NewGuid();
+        Guid lyricsId = Guid.NewGuid();
+        LyricsLikeEntity like = LyricsLikeFactory.Create(userId, lyricsId);
+        var spec = new LyricsLikeByUserAndLyricsSpecification(userId, lyricsId);
+
+        // Act
+        bool result = spec.IsSatisfiedBy(like);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LyricsLikeByUserAndLyricsSpecification_WithDifferentUser_ShouldReturnFalse()
+    {
+        // Arrange
+        Guid lyricsId = Guid.NewGuid();
+        LyricsLikeEntity like = LyricsLikeFactory.Create(Guid.NewGuid(), lyricsId);
+        var spec = new LyricsLikeByUserAndLyricsSpecification(Guid.NewGuid(), lyricsId);
+
+        // Act
+        bool result = spec.IsSatisfiedBy(like);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    #endregion
 }
