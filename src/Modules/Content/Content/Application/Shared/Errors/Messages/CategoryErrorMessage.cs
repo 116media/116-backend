4ea21a172b1220328464c032cc0c09d964836e19
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-26 07:37:18 +0000

    feat(lyrics): add messages for the lyrics default guards

--- src/Modules/Content/Content/Application/Shared/Errors/Messages/CategoryErrorMessage.cs
+++ src/Modules/Content/Content/Application/Shared/Errors/Messages/CategoryErrorMessage.cs
@@ -114,6 +114,16 @@ public class CategoryErrorMessage(IStringLocalizer<CategoryErrorMessage> localiz
     public string OnlyVideoCategoryCanBeExclusive() => localizer["OnlyVideoCategoryCanBeExclusive"];
 
     /// <summary>
+    /// Gets an error message for when an inactive category is set as the lyrics default.
+    /// </summary>
+    public string CannotMakeInactiveDefaultForLyrics() => localizer["CannotMakeInactiveDefaultForLyrics"];
+
+    /// <summary>
+    /// Gets an error message for when a non-lyrics category is set as the lyrics default.
+    /// </summary>
+    public string OnlyLyricsCategoryCanBeDefault() => localizer["OnlyLyricsCategoryCanBeDefault"];
+
+    /// <summary>
     /// Gets an error message for when no exclusive category is currently set.
     /// </summary>
     public string NoExclusiveCategoryFound() => localizer["NoExclusiveCategoryFound"];
