ce4e4b788312c95d9882a55e4199c8247bfac421
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-26 05:40:59 +0000

    refactor(lyrics): drop the uncalled AlreadyExists message

--- src/Modules/Content/Content/Application/Shared/Errors/Messages/LyricsErrorMessage.cs
+++ src/Modules/Content/Content/Application/Shared/Errors/Messages/LyricsErrorMessage.cs
@@ -14,19 +14,6 @@ public class LyricsErrorMessage(IStringLocalizer<LyricsErrorMessage> localizer)
     public IStringLocalizer Localizer => localizer;
 
     /// <summary>
-    /// Gets an error message for when a lyrics entry already exists for the given song and artist.
-    /// </summary>
-    /// <param name="songTitle">The song title that caused the conflict.</param>
-    /// <param name="artistName">The artist name that caused the conflict.</param>
-    /// <returns>
-    /// A formatted error message indicating that lyrics for the specified song and artist already exist.
-    /// </returns>
-    public string AlreadyExists(string songTitle, string artistName)
-    {
-        return string.Format(localizer["AlreadyExists"], songTitle, artistName);
-    }
-
-    /// <summary>
     /// Gets an error message for when a song title is required but not provided.
     /// </summary>
     /// <returns>
