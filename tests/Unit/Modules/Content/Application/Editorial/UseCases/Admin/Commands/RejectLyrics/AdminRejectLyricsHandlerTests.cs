405427f0a8af3841b31678747011bad2d7f02a9a
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-29 19:04:08 +0200

    test(commerce): hand the notifier mock to the lyrics reject tests

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/RejectLyrics/AdminRejectLyricsHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/RejectLyrics/AdminRejectLyricsHandlerTests.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Commerce.Services;
 using _116.Content.Application.Editorial.UseCases.Admin.Commands.RejectLyrics;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -32,7 +33,8 @@ public class AdminRejectLyricsHandlerTests
         _handler = new AdminRejectLyricsHandler(
             _lyricsRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            TestErrorsFactory.CreateContentI18n()
+            TestErrorsFactory.CreateContentI18n(),
+            new Mock<ICommerceCustomerNotifier>().Object
         );
     }
 
