222604f90e3a79beecc4de7cf3d19e9a3448b482
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-29 17:15:09 +0200

    test(role): supply the auth repo and mailer mocks

--- tests/Unit/Modules/Identity/Application/User/UseCases/Admin/Commands/AssignRoleToUser/AdminAssignRoleToUserHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/User/UseCases/Admin/Commands/AssignRoleToUser/AdminAssignRoleToUserHandlerTests.cs
@@ -3,6 +3,7 @@ using _116.Identity.Application.Shared.Persistence;
 using _116.Identity.Application.Shared.Repositories;
 using _116.Identity.Application.User.UseCases.Admin.Commands.AssignRoleToUser;
 using _116.Identity.Domain.Entities;
+using _116.Mailer.Contracts.Application;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Factories.Identity;
 using _116.Tests.Fixtures.Helpers;
@@ -38,7 +39,9 @@ public class AdminAssignRoleToUserHandlerTests : BaseHandlerTest
             _userRoleRepositoryMock.Object,
             _unitOfWorkMock.Object,
             Mapper,
-            _userErrors
+            _userErrors,
+            MockAuthRepository.Create().Object,
+            new Mock<IMailer>().Object
         );
     }
 
