b70525893ae93ebbd4daa8b7cb1ce65f2195348c
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-30 18:44:16 +0200

    refactor(editorial): drop the inline cache invalidation

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveVideo/AdminArchiveVideoHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ArchiveVideo/AdminArchiveVideoHandler.cs
@@ -1,4 +1,3 @@
-using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -14,13 +13,8 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.ArchiveVide
 /// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
-/// <param name="cacheInvalidator">Invalidates the popular-videos cache after the published set changes.</param>
-public class AdminArchiveVideoHandler(
-    IVideoRepository videoRepository,
-    IContentUnitOfWork unitOfWork,
-    ContentI18n i18n,
-    IPopularVideosCacheInvalidator cacheInvalidator
-) : ICommandHandler<AdminArchiveVideoCommand, AdminArchiveVideoResult>
+public class AdminArchiveVideoHandler(IVideoRepository videoRepository, IContentUnitOfWork unitOfWork, ContentI18n i18n)
+    : ICommandHandler<AdminArchiveVideoCommand, AdminArchiveVideoResult>
 {
     /// <inheritdoc />
     public async Task<AdminArchiveVideoResult> Handle(
@@ -41,8 +35,6 @@ public class AdminArchiveVideoHandler(
 
         videoRepository.Update(video: video);
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
-
-        cacheInvalidator.Invalidate();
 
         return new AdminArchiveVideoResult(IsSuccess: true);
     }
