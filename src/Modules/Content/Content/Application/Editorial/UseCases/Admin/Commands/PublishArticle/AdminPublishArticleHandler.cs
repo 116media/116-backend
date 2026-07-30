3b9b9d57c359e066ce8a22928043d0ece711e513
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-30 19:45:28 +0200

    refactor(editorial): drop the inline publish side effects:   - remove the customer notification call   - remove the popular articles cache invalidation

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/PublishArticle/AdminPublishArticleHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/PublishArticle/AdminPublishArticleHandler.cs
@@ -1,6 +1,3 @@
-using _116.Content.Application.Commerce.Services;
-using _116.Content.Application.Editorial.Services;
-using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -15,14 +12,11 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.PublishArti
 /// </summary>
 /// <param name="articleRepository">Repository for article data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-/// <param name="cacheInvalidator">Invalidates the popular-articles cache when the article enters the published set.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminPublishArticleHandler(
     IArticleRepository articleRepository,
     IContentUnitOfWork unitOfWork,
-    IPopularArticlesCacheInvalidator cacheInvalidator,
-    ContentI18n i18n,
-    ICommerceCustomerNotifier customerNotifier
+    ContentI18n i18n
 ) : ICommandHandler<AdminPublishArticleCommand, AdminPublishArticleResult>
 {
     /// <inheritdoc />
@@ -54,15 +48,6 @@ public class AdminPublishArticleHandler(
         article.Publish();
         articleRepository.Update(article: article);
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
-
-        await customerNotifier.NotifyContentPublishedAsync(
-            customerId: article.CustomerId,
-            contentTitle: article.Title,
-            publicUrl: ContentPublicLinks.Article(article.Slug),
-            cancellationToken: cancellationToken
-        );
-
-        cacheInvalidator.Invalidate();
 
         return new AdminPublishArticleResult(IsSuccess: true);
     }
