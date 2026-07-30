113a196b39da9a5992b1d028bcf130462bffc6b9
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-31 01:24:42 +0200

    refactor(interactions): drop the inline bookmark count tail

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/PublicBookmarkArticleHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/PublicBookmarkArticleHandler.cs
@@ -1,4 +1,3 @@
-using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -12,12 +11,10 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.Bookmar
 /// </summary>
 /// <param name="articleRepository">Repository for article data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-/// <param name="cacheInvalidator">Invalidates the popular-articles cache after the bookmark count changes.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class PublicBookmarkArticleHandler(
     IArticleRepository articleRepository,
     IContentUnitOfWork unitOfWork,
-    IPopularArticlesCacheInvalidator cacheInvalidator,
     ContentI18n i18n
 ) : ICommandHandler<PublicBookmarkArticleCommand, PublicBookmarkArticleResult>
 {
@@ -27,10 +24,7 @@ public class PublicBookmarkArticleHandler(
         CancellationToken cancellationToken
     )
     {
-        ArticleEntity article = await articleRepository.GetByIdOrThrowAsync(
-            id: command.ArticleId,
-            cancellationToken: cancellationToken
-        );
+        await articleRepository.GetByIdOrThrowAsync(id: command.ArticleId, cancellationToken: cancellationToken);
 
         bool alreadyBookmarked = await articleRepository.HasBookmarkedAsync(
             userId: command.UserId,
@@ -51,12 +45,7 @@ public class PublicBookmarkArticleHandler(
 
         await articleRepository.AddBookmarkAsync(bookmark: bookmark, cancellationToken: cancellationToken);
 
-        article.IncrementBookmarkCount();
-        articleRepository.Update(article: article);
-
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
-
-        cacheInvalidator.Invalidate();
 
         return new PublicBookmarkArticleResult(IsSuccess: true);
     }
