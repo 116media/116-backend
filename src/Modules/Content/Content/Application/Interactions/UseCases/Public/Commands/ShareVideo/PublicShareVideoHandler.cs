1ffb9c3af590a6f2eab3497813370c9f6d4ef465
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-07-31 02:49:06 +0200

    refactor(interactions): drop the inline share count tail

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/ShareVideo/PublicShareVideoHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/ShareVideo/PublicShareVideoHandler.cs
@@ -1,4 +1,3 @@
-using _116.Content.Application.Shared.Cache;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -11,12 +10,8 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.ShareVi
 /// </summary>
 /// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-/// <param name="cacheInvalidator">Invalidates the popular-videos cache after the share count changes.</param>
-public class PublicShareVideoHandler(
-    IVideoRepository videoRepository,
-    IContentUnitOfWork unitOfWork,
-    IPopularVideosCacheInvalidator cacheInvalidator
-) : ICommandHandler<PublicShareVideoCommand, PublicShareVideoResult>
+public class PublicShareVideoHandler(IVideoRepository videoRepository, IContentUnitOfWork unitOfWork)
+    : ICommandHandler<PublicShareVideoCommand, PublicShareVideoResult>
 {
     /// <inheritdoc />
     public async Task<PublicShareVideoResult> Handle(
@@ -24,10 +19,7 @@ public class PublicShareVideoHandler(
         CancellationToken cancellationToken
     )
     {
-        VideoEntity video = await videoRepository.GetByIdOrThrowAsync(
-            id: command.VideoId,
-            cancellationToken: cancellationToken
-        );
+        await videoRepository.GetByIdOrThrowAsync(id: command.VideoId, cancellationToken: cancellationToken);
 
         var share = VideoShareEntity.Create(
             id: Guid.NewGuid(),
@@ -38,12 +30,7 @@ public class PublicShareVideoHandler(
 
         await videoRepository.AddShareAsync(share: share, cancellationToken: cancellationToken);
 
-        video.IncrementShareCount();
-        videoRepository.Update(video: video);
-
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
-
-        cacheInvalidator.Invalidate();
 
         return new PublicShareVideoResult(IsSuccess: true);
     }
