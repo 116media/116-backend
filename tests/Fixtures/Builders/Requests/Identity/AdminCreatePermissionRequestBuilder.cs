ceb1ea127f95685065838d8b690a1d18f55c68cb
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-05 23:10:23 +0200

    test(fixtures): give the faker its own randomizer

--- tests/Fixtures/Builders/Requests/Identity/AdminCreatePermissionRequestBuilder.cs
+++ tests/Fixtures/Builders/Requests/Identity/AdminCreatePermissionRequestBuilder.cs
@@ -1,5 +1,6 @@
 using _116.Identity.Application.Roles.UseCases.Admin.Commands.CreatePermission.V1;
 using _116.Tests.Fixtures.Constants;
+using _116.Tests.Fixtures.Helpers;
 using Bogus;
 
 namespace _116.Tests.Fixtures.Builders.Requests.Identity;
@@ -9,7 +10,7 @@ namespace _116.Tests.Fixtures.Builders.Requests.Identity;
 /// </summary>
 public class AdminCreatePermissionRequestBuilder
 {
-    private readonly Faker _faker = new();
+    private readonly Faker _faker = TestFaker.Create();
 
     private string _resource;
     private string _action;
