41d6f3203d0f0acfbac494e70b01109e1ecf2649
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-06 19:04:44 +0200

    chore(test-host): update database-collection

--- tests/Integration/Common/Fixtures/DatabaseCollection.cs
+++ tests/Integration/Common/Fixtures/DatabaseCollection.cs
@@ -2,7 +2,7 @@ namespace _116.Integration.Tests.Common.Fixtures;
 
 /// <summary>
 /// Defines the "Database" xUnit test collection so that all integration tests
-/// sharing the <see cref="PostgresFixture" /> run serially against a single container.
+/// sharing the <see cref="PostgresFixture" /> run serially against a single database.
 /// </summary>
 [CollectionDefinition("Database")]
 public class DatabaseCollection : ICollectionFixture<PostgresFixture>;
