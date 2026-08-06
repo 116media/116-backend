ccc92b9b975911351ef0968700bbf2af18de53b7
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-07 01:07:28 +0200

    test(get-customer-by-id): assert problem title and localized detail

--- tests/Integration/Modules/Content/Application/Catalog/UseCases/Admin/Queries/GetCustomerById/V1/AdminGetCustomerByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Catalog/UseCases/Admin/Queries/GetCustomerById/V1/AdminGetCustomerByIdEndpointV1Tests.cs
@@ -1,6 +1,8 @@
 using _116.Content.Application.Catalog.UseCases.Admin.Queries.GetCustomerById.V1;
 using _116.Content.Domain.Entities;
 using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Application.Exceptions;
+using _116.Shared.Application.Exceptions.Messages;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Catalog.UseCases.Admin.Queries.GetCustomerById.V1;
@@ -67,6 +69,9 @@ public class AdminGetCustomerByIdEndpointV1Tests(PostgresFixture db) : BaseApiTe
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.Customers}/{Guid.NewGuid()}");
 
-        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem<NotFoundException>(
+            HttpStatusCode.NotFound,
+            Localized<SharedExceptionMessage>(m => m.EntityNotFound("Customer"))
+        );
     }
 }
