5e357723f3d06431975712f3fe2bdf7b15858074
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-07 18:55:03 +0200

    test(get-video-by-id): assert problem title and localized detail

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetVideoById/V1/AdminGetVideoByIdEndpointV1Tests.cs
@@ -1,6 +1,8 @@
 using _116.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById.V1;
 using _116.Content.Domain.Entities;
 using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Application.Exceptions;
+using _116.Shared.Application.Exceptions.Messages;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Queries.GetVideoById.V1;
@@ -79,6 +81,9 @@ public class AdminGetVideoByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.Videos}/{nonExistentId}");
 
-        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem<NotFoundException>(
+            HttpStatusCode.NotFound,
+            Localized<SharedExceptionMessage>(m => m.EntityNotFound("Video"))
+        );
     }
 }
