27489944995712c2add00e212126b92a48430f32
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-07 04:01:54 +0200

    test(get-order-by-id): assert problem title and localized detail

--- tests/Integration/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetOrderById/V1/AdminGetOrderByIdEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Commerce/UseCases/Admin/Queries/GetOrderById/V1/AdminGetOrderByIdEndpointV1Tests.cs
@@ -2,6 +2,8 @@ using _116.Content.Application.Commerce.UseCases.Admin.Queries.GetOrderById.V1;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Application.Exceptions;
+using _116.Shared.Application.Exceptions.Messages;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Commerce.UseCases.Admin.Queries.GetOrderById.V1;
@@ -49,7 +51,10 @@ public class AdminGetOrderByIdEndpointV1Tests(PostgresFixture db) : BaseApiTest(
 
         var response = await Client.GetAsync($"{ApiRoutes.Admin.Orders}/{Guid.NewGuid()}");
 
-        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem<NotFoundException>(
+            HttpStatusCode.NotFound,
+            Localized<SharedExceptionMessage>(m => m.EntityNotFound("ContentOrder"))
+        );
     }
 
     [Fact]
