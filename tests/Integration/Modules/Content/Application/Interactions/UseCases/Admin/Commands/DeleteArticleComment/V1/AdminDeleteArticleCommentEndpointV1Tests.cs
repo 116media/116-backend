05dd2259e79900c475ad963997bb79ef4a572434
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-07 23:50:31 +0200

    test(delete-article-comment): assert problem title and localized detail

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Admin/Commands/DeleteArticleComment/V1/AdminDeleteArticleCommentEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Admin/Commands/DeleteArticleComment/V1/AdminDeleteArticleCommentEndpointV1Tests.cs
@@ -2,6 +2,8 @@ using _116.Content.Application.Interactions.Constants;
 using _116.Content.Application.Interactions.UseCases.Admin.Commands.DeleteArticleComment.V1;
 using _116.Content.Domain.Entities;
 using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Application.Exceptions;
+using _116.Shared.Application.Exceptions.Messages;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCases.Admin.Commands.DeleteArticleComment.V1;
@@ -65,7 +67,10 @@ public class AdminDeleteArticleCommentEndpointV1Tests(PostgresFixture db) : Base
 
         var response = await Client.DeleteAsync(CommentUrl(Guid.NewGuid(), Guid.NewGuid()));
 
-        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem<NotFoundException>(
+            HttpStatusCode.NotFound,
+            Localized<SharedExceptionMessage>(m => m.EntityNotFound("ArticleComment"))
+        );
     }
 
     [Fact]
@@ -83,5 +88,24 @@ public class AdminDeleteArticleCommentEndpointV1Tests(PostgresFixture db) : Base
         await using ContentDbContext verifyDb = CreateDbContext<ContentDbContext>();
         ArticleCommentEntity? stored = await verifyDb.ArticleComments.FindAsync(comment.Id);
         stored!.IsDeleted.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task DeleteArticleComment_WithCommentBelongingToAnotherArticle_ReturnsNotFound()
+    {
+        (ArticleEntity _, ArticleCommentEntity comment) = await SeedArticleWithCommentAsync(TestUser.VisitorId);
+        (ArticleEntity otherArticle, ArticleCommentEntity _) = await SeedArticleWithCommentAsync(TestUser.VisitorId);
+        Client.AuthenticateAsSuperAdmin();
+
+        var response = await Client.DeleteAsync(CommentUrl(otherArticle.Id, comment.Id));
+
+        await response.ShouldBeProblem<NotFoundException>(
+            HttpStatusCode.NotFound,
+            Localized<SharedExceptionMessage>(m => m.EntityNotFound("ArticleComment"))
+        );
+
+        await using ContentDbContext verifyDb = CreateDbContext<ContentDbContext>();
+        ArticleCommentEntity? persisted = await verifyDb.ArticleComments.FindAsync(comment.Id);
+        persisted!.IsDeleted.Should().BeFalse("a comment under another article must not be deleted");
     }
 }
