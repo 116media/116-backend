ce0a57d3d7d63d1fc3485abc6098bff1cec41a09
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-08 03:51:27 +0200

    test(share-video): assert problem title and localized detail

--- tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/ShareVideo/V1/PublicShareVideoEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Interactions/UseCases/Public/Commands/ShareVideo/V1/PublicShareVideoEndpointV1Tests.cs
@@ -2,6 +2,8 @@ using _116.Content.Application.Interactions.UseCases.Public.Commands.ShareVideo.
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Application.Exceptions;
+using _116.Shared.Application.Exceptions.Messages;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Interactions.UseCases.Public.Commands.ShareVideo.V1;
@@ -67,7 +69,10 @@ public class PublicShareVideoEndpointV1Tests(PostgresFixture db) : BaseApiTest(d
 
         var response = await Client.PostAsync(Routes.Public.Videos.Shares(Guid.NewGuid()), null);
 
-        await response.ShouldBeProblem(HttpStatusCode.NotFound);
+        await response.ShouldBeProblem<NotFoundException>(
+            HttpStatusCode.NotFound,
+            Localized<SharedExceptionMessage>(m => m.EntityNotFound("Video"))
+        );
     }
 
     [Fact]
