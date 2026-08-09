789064b5017e499bec6013029fe6add7c20c2c00
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-09 16:56:17 +0200

    test(force-unpromote-article): drop the self-comparing localization

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/ForceUnpromoteArticle/AdminForceUnpromoteArticleValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/ForceUnpromoteArticle/AdminForceUnpromoteArticleValidatorTests.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using _116.Content.Application.Editorial.UseCases.Admin.Commands.ForceUnpromoteArticle;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Tests.Fixtures.Helpers;
@@ -118,34 +117,6 @@ public class AdminForceUnpromoteArticleValidatorTests
         // Assert
         result.IsValid.Should().BeTrue();
         result.Errors.Should().BeEmpty();
-    }
-
-    #endregion
-
-    #region Culture Tests
-
-    [Theory]
-    [InlineData("en")]
-    [InlineData("fr")]
-    public async Task Validate_ErrorMessages_ShouldBeLocalizedForCulture(string culture)
-    {
-        // Arrange
-        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
-        Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
-        var validator = new AdminForceUnpromoteArticleValidator(_i18n);
-        var command = new AdminForceUnpromoteArticleCommand(Slug: "my-article-slug", Reason: string.Empty);
-
-        // Act
-        ValidationResult result = await validator.ValidateAsync(command);
-
-        // Assert
-        result.IsValid.Should().BeFalse();
-        result
-            .Errors.Should()
-            .Contain(e =>
-                e.PropertyName == nameof(AdminForceUnpromoteArticleCommand.Reason)
-                && e.ErrorMessage == _i18n.Article.Msg.RejectionReasonRequired()
-            );
     }
 
     #endregion
