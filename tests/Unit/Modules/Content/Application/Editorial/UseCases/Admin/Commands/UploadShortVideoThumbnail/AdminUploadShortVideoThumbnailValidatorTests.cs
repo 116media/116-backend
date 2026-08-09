b2bfc8528c97cd6f6597e7e5e032d250de4d8ace
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-10 00:08:25 +0200

    test(upload-short-video-thumbnail): drop the self-comparing

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UploadShortVideoThumbnail/AdminUploadShortVideoThumbnailValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UploadShortVideoThumbnail/AdminUploadShortVideoThumbnailValidatorTests.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using _116.Content.Application.Editorial.UseCases.Admin.Commands.UploadShortVideoThumbnail;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Tests.Fixtures.Helpers;
@@ -88,36 +87,6 @@ public class AdminUploadShortVideoThumbnailValidatorTests
             .Contain(e =>
                 e.PropertyName == nameof(AdminUploadShortVideoThumbnailCommand.ShortVideoId)
                 && e.ErrorMessage == _i18n.ShortVideo.Msg.Localizer["IdInvalid"].Value
-            );
-    }
-
-    #endregion
-
-    #region Culture Tests
-
-    [Theory]
-    [InlineData("en")]
-    [InlineData("fr")]
-    public async Task Validate_ErrorMessages_ShouldBeLocalizedForCulture(string culture)
-    {
-        // Arrange
-        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
-        Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
-        var validator = new AdminUploadShortVideoThumbnailValidator(_i18n);
-        var fileMock = new Mock<IFormFile>();
-        fileMock.Setup(f => f.Length).Returns(1);
-        var command = new AdminUploadShortVideoThumbnailCommand(ShortVideoId: string.Empty, File: fileMock.Object);
-
-        // Act
-        ValidationResult result = await validator.ValidateAsync(command);
-
-        // Assert
-        result.IsValid.Should().BeFalse();
-        result
-            .Errors.Should()
-            .Contain(e =>
-                e.PropertyName == nameof(AdminUploadShortVideoThumbnailCommand.ShortVideoId)
-                && e.ErrorMessage == _i18n.ShortVideo.Msg.Localizer["IdRequired"].Value
             );
     }
 
