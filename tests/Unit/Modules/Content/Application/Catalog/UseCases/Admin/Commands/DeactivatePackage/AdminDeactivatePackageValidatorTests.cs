6816836dd4d3ef4fb712f0ec48f5b95813ba7fd5
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-09 03:46:52 +0200

    test(deactivate-package): drop the self-comparing localization theory

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/DeactivatePackage/AdminDeactivatePackageValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/DeactivatePackage/AdminDeactivatePackageValidatorTests.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using _116.Content.Application.Catalog.UseCases.Admin.Commands.DeactivatePackage;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Tests.Fixtures.Helpers;
@@ -55,34 +54,6 @@ public class AdminDeactivatePackageValidatorTests
         result
             .Errors.Should()
             .ContainSingle(e =>
-                e.PropertyName == nameof(AdminDeactivatePackageCommand.Id)
-                && e.ErrorMessage == _i18n.Package.Msg.Localizer["IdRequired"].Value
-            );
-    }
-
-    #endregion
-
-    #region Culture Tests
-
-    [Theory]
-    [InlineData("en")]
-    [InlineData("fr")]
-    public async Task Validate_ErrorMessages_ShouldBeLocalizedForCulture(string culture)
-    {
-        // Arrange
-        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
-        Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
-        var validator = new AdminDeactivatePackageValidator(_i18n);
-        var command = new AdminDeactivatePackageCommand(Id: "");
-
-        // Act
-        ValidationResult result = await validator.ValidateAsync(command);
-
-        // Assert
-        result.IsValid.Should().BeFalse();
-        result
-            .Errors.Should()
-            .Contain(e =>
                 e.PropertyName == nameof(AdminDeactivatePackageCommand.Id)
                 && e.ErrorMessage == _i18n.Package.Msg.Localizer["IdRequired"].Value
             );
