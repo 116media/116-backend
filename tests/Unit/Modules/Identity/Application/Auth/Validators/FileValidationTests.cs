b51d79dc7e525ccb4a4053c32aac025191f310b8
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-10 23:02:52 +0200

    test(validators): strengthen the assertions

--- tests/Unit/Modules/Identity/Application/Auth/Validators/FileValidationTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/Validators/FileValidationTests.cs
@@ -17,7 +17,7 @@ namespace _116.Unit.Tests.Modules.Identity.Application.Auth.Validators;
 /// </summary>
 public class FileValidationTests
 {
-    private readonly ValidationErrorMessage _enMsg = LocalizerFactory.CreateMessage<ValidationErrorMessage>("en");
+    private readonly ValidationErrorMessage _enMsg = LocalizerFactory.CreateMessage<ValidationErrorMessage>();
 
     private class TestCommand
     {
