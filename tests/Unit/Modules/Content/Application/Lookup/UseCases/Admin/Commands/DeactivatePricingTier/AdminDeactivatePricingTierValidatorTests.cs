7913832ed37c61d84d84e66c3b9dc39937332440
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-10 07:31:13 +0200

    test(deactivate-pricing-tier): drop the self-comparing localization

--- tests/Unit/Modules/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/AdminDeactivatePricingTierValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/AdminDeactivatePricingTierValidatorTests.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using _116.Content.Application.Lookup.UseCases.Admin.Commands.DeactivatePricingTier;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Tests.Fixtures.Helpers;
@@ -58,34 +57,6 @@ public class AdminDeactivatePricingTierValidatorTests
         result
             .Errors.Should()
             .ContainSingle(e =>
-                e.PropertyName == nameof(AdminDeactivatePricingTierCommand.Id)
-                && e.ErrorMessage == _i18n.PricingTier.Msg.Localizer["IdRequired"].Value
-            );
-    }
-
-    #endregion
-
-    #region Culture Tests
-
-    [Theory]
-    [InlineData("en")]
-    [InlineData("fr")]
-    public async Task Validate_ErrorMessages_ShouldBeLocalizedForCulture(string culture)
-    {
-        // Arrange
-        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
-        Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
-        var validator = new AdminDeactivatePricingTierValidator(_i18n);
-        var command = new AdminDeactivatePricingTierCommand(Id: "");
-
-        // Act
-        ValidationResult result = await validator.ValidateAsync(command);
-
-        // Assert
-        result.IsValid.Should().BeFalse();
-        result
-            .Errors.Should()
-            .Contain(e =>
                 e.PropertyName == nameof(AdminDeactivatePricingTierCommand.Id)
                 && e.ErrorMessage == _i18n.PricingTier.Msg.Localizer["IdRequired"].Value
             );
