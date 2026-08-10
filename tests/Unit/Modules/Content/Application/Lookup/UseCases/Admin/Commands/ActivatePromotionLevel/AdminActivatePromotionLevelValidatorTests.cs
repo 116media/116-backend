e9748f83be1982722d981a680e1a218e495bc181
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-10 05:39:17 +0200

    test(activate-promotion-level): drop the self-comparing localization

--- tests/Unit/Modules/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePromotionLevel/AdminActivatePromotionLevelValidatorTests.cs
+++ tests/Unit/Modules/Content/Application/Lookup/UseCases/Admin/Commands/ActivatePromotionLevel/AdminActivatePromotionLevelValidatorTests.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using _116.Content.Application.Lookup.UseCases.Admin.Commands.ActivatePromotionLevel;
 using _116.Content.Application.Shared.Errors.Facade;
 using _116.Tests.Fixtures.Helpers;
@@ -58,34 +57,6 @@ public class AdminActivatePromotionLevelValidatorTests
         result
             .Errors.Should()
             .ContainSingle(e =>
-                e.PropertyName == nameof(AdminActivatePromotionLevelCommand.Id)
-                && e.ErrorMessage == _i18n.PromotionLevel.Msg.Localizer["IdRequired"].Value
-            );
-    }
-
-    #endregion
-
-    #region Culture Tests
-
-    [Theory]
-    [InlineData("en")]
-    [InlineData("fr")]
-    public async Task Validate_ErrorMessages_ShouldBeLocalizedForCulture(string culture)
-    {
-        // Arrange
-        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
-        Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
-        var validator = new AdminActivatePromotionLevelValidator(_i18n);
-        var command = new AdminActivatePromotionLevelCommand(Id: "");
-
-        // Act
-        ValidationResult result = await validator.ValidateAsync(command);
-
-        // Assert
-        result.IsValid.Should().BeFalse();
-        result
-            .Errors.Should()
-            .Contain(e =>
                 e.PropertyName == nameof(AdminActivatePromotionLevelCommand.Id)
                 && e.ErrorMessage == _i18n.PromotionLevel.Msg.Localizer["IdRequired"].Value
             );
