7dbc5652552ecf80c183d8315154c305c912fdb0
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-11 14:27:58 +0200

    refactor(fixtures): drop unused WithId

--- tests/Fixtures/Builders/Entities/Content/ContentItemTierBuilder.cs
+++ tests/Fixtures/Builders/Entities/Content/ContentItemTierBuilder.cs
@@ -22,12 +22,6 @@ public class ContentItemTierBuilder
         _pricingTierId = Guid.NewGuid();
     }
 
-    public ContentItemTierBuilder WithId(Guid id)
-    {
-        _id = id;
-        return this;
-    }
-
     public ContentItemTierBuilder WithOrderItemId(Guid orderItemId)
     {
         _orderItemId = orderItemId;
