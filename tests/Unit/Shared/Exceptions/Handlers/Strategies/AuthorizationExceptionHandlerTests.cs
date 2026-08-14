f73362227dde35b3aa6a5b662bba8fd1469ccd15
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-14 19:09:06 +0200

    test(exceptions): rename to should use the exception message as detail

--- tests/Unit/Shared/Exceptions/Handlers/Strategies/AuthorizationExceptionHandlerTests.cs
+++ tests/Unit/Shared/Exceptions/Handlers/Strategies/AuthorizationExceptionHandlerTests.cs
@@ -1,6 +1,5 @@
 using _116.Shared.Application.Exceptions;
 using _116.Shared.Application.Exceptions.Handlers.Strategies;
-using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
 using Microsoft.AspNetCore.Http;
@@ -10,56 +9,13 @@ using Xunit;
 namespace _116.Unit.Tests.Shared.Exceptions.Handlers.Strategies;
 
 /// <summary>
-/// Unit tests for <see cref="AuthorizationExceptionHandler"/>.
+/// Unit tests for <see cref="AuthorizationExceptionHandler" />.
+/// The title, instance and trace extensions are covered for every strategy by
+/// <see cref="ExceptionStrategyContractTests" />; only the status and detail are asserted here.
 /// </summary>
 public class AuthorizationExceptionHandlerTests
 {
     private readonly AuthorizationExceptionHandler _handler = new();
-
-    #region ExceptionType Tests
-
-    [Fact]
-    public void ExceptionType_ShouldReturnAuthorizationExceptionType()
-    {
-        // Act
-        Type exceptionType = _handler.ExceptionType;
-
-        // Assert
-        exceptionType.Should().Be(typeof(AuthorizationException));
-    }
-
-    #endregion
-
-    #region CreateProblemDetails Tests
-
-    [Fact]
-    public void CreateProblemDetails_ShouldReturnProblemDetailsWithCorrectTitle()
-    {
-        // Arrange
-        AuthorizationException exception = new("Access denied");
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Title.Should().Be(nameof(AuthorizationException));
-    }
-
-    [Fact]
-    public void CreateProblemDetails_ShouldReturnProblemDetailsWithExceptionMessage()
-    {
-        // Arrange
-        string errorMessage = "You do not have permission to access this resource";
-        AuthorizationException exception = new(errorMessage);
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Detail.Should().Be(errorMessage);
-    }
 
     [Fact]
     public void CreateProblemDetails_ShouldReturn403StatusCode()
@@ -76,53 +32,17 @@ public class AuthorizationExceptionHandlerTests
     }
 
     [Fact]
-    public void CreateProblemDetails_ShouldIncludeRequestPath()
+    public void CreateProblemDetails_ShouldUseTheExceptionMessageAsDetail()
     {
         // Arrange
-        AuthorizationException exception = new("Forbidden");
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-        string requestPath = TestConstants.ApiRoutes.Admin.Roles;
-        context.Request.Path = requestPath;
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Instance.Should().Be(requestPath);
-    }
-
-    [Fact]
-    public void CreateProblemDetails_ShouldIncludeTraceIdExtension()
-    {
-        // Arrange
-        AuthorizationException exception = new("Forbidden");
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-        string traceId = "authz-trace-789";
-        context.TraceIdentifier = traceId;
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Extensions.Should().ContainKey("traceId");
-        problemDetails.Extensions["traceId"].Should().Be(traceId);
-    }
-
-    [Fact]
-    public void CreateProblemDetails_ShouldIncludeTimestampExtension()
-    {
-        // Arrange
-        AuthorizationException exception = new("Forbidden");
+        string errorMessage = "You do not have permission to access this resource";
+        AuthorizationException exception = new(errorMessage);
         DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
 
         // Act
         ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
-        problemDetails.Extensions.Should().ContainKey("timestamp");
-        var timestamp = (DateTime)problemDetails.Extensions["timestamp"]!;
-        timestamp.Should().NotBe(default(DateTime));
+        problemDetails.Detail.Should().Be(errorMessage);
     }
-
-    #endregion
 }
