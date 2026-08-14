10fc64dedc3b8b6d1646b60900ee0280e0d29893
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-14 20:33:00 +0200

    test(exceptions): rename to should return405 status code

--- tests/Unit/Shared/Exceptions/Handlers/Strategies/MethodNotAllowedExceptionHandlerTests.cs
+++ tests/Unit/Shared/Exceptions/Handlers/Strategies/MethodNotAllowedExceptionHandlerTests.cs
@@ -1,6 +1,5 @@
 using _116.Shared.Application.Exceptions;
 using _116.Shared.Application.Exceptions.Handlers.Strategies;
-using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Helpers;
 using AwesomeAssertions;
 using Microsoft.AspNetCore.Http;
@@ -10,56 +9,13 @@ using Xunit;
 namespace _116.Unit.Tests.Shared.Exceptions.Handlers.Strategies;
 
 /// <summary>
-/// Unit tests for <see cref="MethodNotAllowedExceptionHandler"/>.
+/// Unit tests for <see cref="MethodNotAllowedExceptionHandler" />.
+/// The title, instance and trace extensions are covered for every strategy by
+/// <see cref="ExceptionStrategyContractTests" />; only the status and detail are asserted here.
 /// </summary>
 public class MethodNotAllowedExceptionHandlerTests
 {
     private readonly MethodNotAllowedExceptionHandler _handler = new();
-
-    #region ExceptionType Tests
-
-    [Fact]
-    public void ExceptionType_ShouldReturnMethodNotAllowedExceptionType()
-    {
-        // Act
-        Type exceptionType = _handler.ExceptionType;
-
-        // Assert
-        exceptionType.Should().Be(typeof(MethodNotAllowedException));
-    }
-
-    #endregion
-
-    #region CreateProblemDetails Tests
-
-    [Fact]
-    public void CreateProblemDetails_ShouldReturnProblemDetailsWithCorrectTitle()
-    {
-        // Arrange
-        MethodNotAllowedException exception = new("Method not allowed");
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Title.Should().Be(nameof(MethodNotAllowedException));
-    }
-
-    [Fact]
-    public void CreateProblemDetails_ShouldReturnProblemDetailsWithExceptionMessage()
-    {
-        // Arrange
-        string errorMessage = "PUT method is not allowed for this endpoint";
-        MethodNotAllowedException exception = new(errorMessage);
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Detail.Should().Be(errorMessage);
-    }
 
     [Fact]
     public void CreateProblemDetails_ShouldReturn405StatusCode()
@@ -76,53 +32,17 @@ public class MethodNotAllowedExceptionHandlerTests
     }
 
     [Fact]
-    public void CreateProblemDetails_ShouldIncludeRequestPath()
+    public void CreateProblemDetails_ShouldUseTheExceptionMessageAsDetail()
     {
         // Arrange
-        MethodNotAllowedException exception = new("Method not allowed");
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-        string requestPath = TestConstants.ApiRoutes.Admin.Roles;
-        context.Request.Path = requestPath;
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Instance.Should().Be(requestPath);
-    }
-
-    [Fact]
-    public void CreateProblemDetails_ShouldIncludeTraceIdExtension()
-    {
-        // Arrange
-        MethodNotAllowedException exception = new("Method not allowed");
-        DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
-        string traceId = "method-trace-101";
-        context.TraceIdentifier = traceId;
-
-        // Act
-        ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
-
-        // Assert
-        problemDetails.Extensions.Should().ContainKey("traceId");
-        problemDetails.Extensions["traceId"].Should().Be(traceId);
-    }
-
-    [Fact]
-    public void CreateProblemDetails_ShouldIncludeTimestampExtension()
-    {
-        // Arrange
-        MethodNotAllowedException exception = new("Method not allowed");
+        string errorMessage = "PUT method is not allowed for this endpoint";
+        MethodNotAllowedException exception = new(errorMessage);
         DefaultHttpContext context = HttpTestHelpers.CreateDefaultHttpContext();
 
         // Act
         ProblemDetails problemDetails = _handler.CreateProblemDetails(exception, context);
 
         // Assert
-        problemDetails.Extensions.Should().ContainKey("timestamp");
-        var timestamp = (DateTime)problemDetails.Extensions["timestamp"]!;
-        timestamp.Should().NotBe(default(DateTime));
+        problemDetails.Detail.Should().Be(errorMessage);
     }
-
-    #endregion
 }
