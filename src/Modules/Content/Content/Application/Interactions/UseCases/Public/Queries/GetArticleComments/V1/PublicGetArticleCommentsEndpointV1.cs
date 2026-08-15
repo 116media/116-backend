1bba0675351a5148c4bbf83cfc6cd7b17322ad27
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-15 07:22:17 +0200

    refactor(content): fix PaginatedRequest arg casing in comments

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetArticleComments/V1/PublicGetArticleCommentsEndpointV1.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetArticleComments/V1/PublicGetArticleCommentsEndpointV1.cs
@@ -45,7 +45,7 @@ public class PublicGetArticleCommentsEndpointV1 : ICarterModule
                         viewerUserId = claimsProvider.GetUserIdFromClaims(user: user);
                     }
 
-                    var paginatedRequest = new PaginatedRequest(PageIndex: pageIndex, PageSize: pageSize);
+                    var paginatedRequest = new PaginatedRequest(pageIndex: pageIndex, pageSize: pageSize);
                     var query = new PublicGetArticleCommentsQuery(
                         ArticleId: id,
                         PaginatedRequest: paginatedRequest,
