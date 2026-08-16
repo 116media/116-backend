4398b897b93bd27596af0c995cc5b9389285bbe3
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-16 16:06:32 +0200

    test(auth): inject account limiter in admin reset-password test

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResetPassword/AdminResetPasswordHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResetPassword/AdminResetPasswordHandlerTests.cs
@@ -3,6 +3,7 @@ using _116.Identity.Application.Auth.UseCases.Admin.Commands.ResetPassword;
 using _116.Identity.Application.Auth.UseCases.Admin.Commands.ResetPassword.Contracts;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
+using _116.Shared.Application.Builders.RateLimit;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -26,7 +27,11 @@ public class AdminResetPasswordHandlerTests
         _authFactoryMock = new Mock<IAdminResetPasswordAuthFactory>();
         _otpRepositoryMock = MockOtpRepository.Create();
 
-        _handler = new AdminResetPasswordHandler(_authFactoryMock.Object, _otpRepositoryMock.Object);
+        _handler = new AdminResetPasswordHandler(
+            _authFactoryMock.Object,
+            _otpRepositoryMock.Object,
+            Mock.Of<IAccountRateLimiter>()
+        );
     }
 
     #region Success Cases
