5961d204be5e219444deb7c30e5acf9af3b6d18b
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-16 20:07:39 +0200

    test(auth): drop account limiter arg from public reset-password test

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordHandlerTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Public/Commands/ResetPassword/PublicResetPasswordHandlerTests.cs
@@ -3,7 +3,6 @@ using _116.Identity.Application.Auth.UseCases.Public.Commands.ResetPassword;
 using _116.Identity.Application.Auth.UseCases.Public.Commands.ResetPassword.Contracts;
 using _116.Identity.Domain.Entities;
 using _116.Identity.Domain.Enums;
-using _116.Shared.Application.Builders.RateLimit;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Factories.Identity;
 using _116.Unit.Tests.Common.Mocks.Repositories;
@@ -27,11 +26,7 @@ public class PublicResetPasswordHandlerTests
         _authFactoryMock = new Mock<IPublicResetPasswordAuthFactory>();
         _otpRepositoryMock = MockOtpRepository.Create();
 
-        _handler = new PublicResetPasswordHandler(
-            _authFactoryMock.Object,
-            _otpRepositoryMock.Object,
-            Mock.Of<IAccountRateLimiter>()
-        );
+        _handler = new PublicResetPasswordHandler(_authFactoryMock.Object, _otpRepositoryMock.Object);
     }
 
     #region Success Cases
