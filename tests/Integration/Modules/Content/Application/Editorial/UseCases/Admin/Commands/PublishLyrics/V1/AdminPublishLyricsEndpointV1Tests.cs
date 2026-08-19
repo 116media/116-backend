0bea9b4c61487ed58a397d558c617efd9b667495
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-19 16:27:42 +0200

    test(content): expect the domain rule on an invalid publish

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/PublishLyrics/V1/AdminPublishLyricsEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/PublishLyrics/V1/AdminPublishLyricsEndpointV1Tests.cs
@@ -6,6 +6,7 @@ using _116.Content.Domain.Enums;
 using _116.Content.Infrastructure.Persistence;
 using _116.Shared.Application.Exceptions;
 using _116.Shared.Application.Exceptions.Messages;
+using _116.Shared.Domain.Exceptions;
 using _116.Tests.Fixtures.Factories.Content;
 
 namespace _116.Integration.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Commands.PublishLyrics.V1;
@@ -121,7 +122,7 @@ public class AdminPublishLyricsEndpointV1Tests(PostgresFixture db) : BaseApiTest
             null
         );
 
-        await response.ShouldBeProblem<BadRequestException>(
+        await response.ShouldBeProblem<DomainRuleException>(
             HttpStatusCode.BadRequest,
             Localized<LyricsErrorMessage>(m =>
                 m.InvalidStatusTransition(
