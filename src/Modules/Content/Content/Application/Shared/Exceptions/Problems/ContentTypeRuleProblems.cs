869764c680c11364ab8e99e099730172112cab0b
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-20 10:06:34 +0200

    feat(content): add the content type rule problem catalog

---
+++ src/Modules/Content/Content/Application/Shared/Exceptions/Problems/ContentTypeRuleProblems.cs
@@ -0,0 +1,25 @@
+using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Domain.StateMachines;
+using _116.Shared.Application.Exceptions;
+using _116.Shared.Application.Exceptions.Problems;
+using _116.Shared.Application.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace _116.Content.Application.Shared.Exceptions.Problems;
+
+/// <summary>
+/// Rule problems owned by the content type aggregate.
+/// </summary>
+public sealed class ContentTypeRuleProblems : IRuleProblemCatalog
+{
+    /// <inheritdoc />
+    public IReadOnlyDictionary<string, RuleProblem> Problems { get; } =
+        new Dictionary<string, RuleProblem>
+        {
+            [ContentRuleCodes.ContentTypeNameRequired] = new(
+                StatusCodes.Status400BadRequest,
+                nameof(BadRequestException),
+                (ctx, _) => ctx.Resolve<ContentTypeErrorMessage>().NameRequired()
+            ),
+        };
+}
