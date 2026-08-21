3e2677589fc15b89dd84e4560b8743edd10fb3dc
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-21 16:27:31 +0200

    test(content): assert coded rules in admin verify payment factory tests

--- tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Commands/VerifyPayment/AdminVerifyPaymentFactoryTests.cs
+++ tests/Unit/Modules/Content/Application/Commerce/UseCases/Admin/Commands/VerifyPayment/AdminVerifyPaymentFactoryTests.cs
@@ -4,6 +4,8 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Content.Domain.Events;
+using _116.Content.Domain.Exceptions;
+using _116.Content.Domain.StateMachines;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
@@ -147,7 +149,9 @@ public class AdminVerifyPaymentFactoryTests
         Func<Task> act = () => _factory.VerifyAsync(order, payment, AdminUserId, ReceiptUrl, CancellationToken.None);
 
         // Assert
-        await act.Should().ThrowAsync<ConflictException>();
+        (await act.Should().ThrowAsync<ContentRuleException>())
+            .Which.Code.Should()
+            .Be(ContentRuleCodes.PaymentAlreadyVerified);
         payment.Status.Should().Be(EnumPaymentStatus.Verified);
         order.Status.Should().Be(EnumOrderStatus.PendingPayment);
         order.DomainEvents.Should().BeEmpty();
@@ -166,7 +170,9 @@ public class AdminVerifyPaymentFactoryTests
         Func<Task> act = () => _factory.VerifyAsync(order, payment, AdminUserId, ReceiptUrl, CancellationToken.None);
 
         // Assert
-        await act.Should().ThrowAsync<ConflictException>();
+        (await act.Should().ThrowAsync<ContentRuleException>())
+            .Which.Code.Should()
+            .Be(ContentRuleCodes.PaymentAlreadyRejected);
         payment.Status.Should().Be(EnumPaymentStatus.Rejected);
         order.Status.Should().Be(EnumOrderStatus.PendingPayment);
         order.DomainEvents.Should().BeEmpty();
@@ -185,7 +191,9 @@ public class AdminVerifyPaymentFactoryTests
         Func<Task> act = () => _factory.VerifyAsync(order, payment, AdminUserId, ReceiptUrl, CancellationToken.None);
 
         // Assert
-        await act.Should().ThrowAsync<ConflictException>();
+        (await act.Should().ThrowAsync<ContentRuleException>())
+            .Which.Code.Should()
+            .Be(ContentRuleCodes.OrderAlreadyPaid);
         order.Status.Should().Be(EnumOrderStatus.Paid);
         order.DomainEvents.Should().BeEmpty();
         _unitOfWorkMock.VerifyCommitNotCalled();
