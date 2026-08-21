c18b30a21e3adb8912d952dc9c65d03a6bc38e64
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-21 16:56:40 +0200

    test(content): assert coded rules

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/ForceUnpromoteLyrics/AdminForceUnpromoteLyricsHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/ForceUnpromoteLyrics/AdminForceUnpromoteLyricsHandlerTests.cs
@@ -4,6 +4,8 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Content.Domain.Events;
+using _116.Content.Domain.Exceptions;
+using _116.Content.Domain.StateMachines;
 using _116.Shared.Application.Exceptions;
 using _116.Shared.Application.Services;
 using _116.Tests.Fixtures.Factories.Content;
@@ -132,7 +134,9 @@ public class AdminForceUnpromoteLyricsHandlerTests
         Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
 
         // Assert
-        await act.Should().ThrowAsync<BadRequestException>();
+        (await act.Should().ThrowAsync<ContentRuleException>())
+            .Which.Code.Should()
+            .Be(ContentRuleCodes.LyricsNotPromoted);
         lyrics.IsPromoted.Should().BeFalse();
         lyrics.UnpromotedAt.Should().BeNull();
         lyrics.UnpromotedBy.Should().BeNull();
