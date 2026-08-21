b27f15f0ec2ea5e186dda1fe0766e15e9656f778
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-22 01:09:26 +0200

    refactor(content): drop the unread i18n dependency from create album

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateAlbum/AdminCreateAlbumHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/CreateAlbum/AdminCreateAlbumHandler.cs
@@ -15,13 +15,11 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.CreateAlbum
 /// <param name="artistRepository">Repository for artist profile data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="fileRepository">Repository for resolving cover image URLs.</param>
-/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminCreateAlbumHandler(
     IAlbumRepository albumRepository,
     IArtistRepository artistRepository,
     IContentUnitOfWork unitOfWork,
-    IFileRepository fileRepository,
-    ContentI18n i18n
+    IFileRepository fileRepository
 ) : ICommandHandler<AdminCreateAlbumCommand, AdminCreateAlbumResult>
 {
     /// <inheritdoc />
