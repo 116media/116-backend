b0f3fbc530ad597e34b5ec683692725777466a14
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-22 02:16:11 +0200

    refactor(content): drop the unread i18n dependency from update artist

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateArtist/AdminUpdateArtistHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateArtist/AdminUpdateArtistHandler.cs
@@ -14,12 +14,10 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.UpdateArtis
 /// <param name="artistRepository">Repository for artist profile data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="fileRepository">Repository for resolving avatar file URLs.</param>
-/// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminUpdateArtistHandler(
     IArtistRepository artistRepository,
     IContentUnitOfWork unitOfWork,
-    IFileRepository fileRepository,
-    ContentI18n i18n
+    IFileRepository fileRepository
 ) : ICommandHandler<AdminUpdateArtistCommand, AdminUpdateArtistResult>
 {
     /// <inheritdoc />
