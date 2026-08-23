abf770cd1e0a70adb7f7d505794d8c5e69791354
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-23 17:37:26 +0200

    refactor(interactions): delegate the not-found throw to the guard

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/PublicBookmarkArticleHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/BookmarkArticle/PublicBookmarkArticleHandler.cs
@@ -2,7 +2,6 @@ using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
-using _116.Shared.Application.Exceptions;
 using _116.Shared.Contracts.Application.CQRS;
 
 namespace _116.Content.Application.Interactions.UseCases.Public.Commands.BookmarkArticle;
@@ -25,15 +24,7 @@ public class PublicBookmarkArticleHandler(
         CancellationToken cancellationToken
     )
     {
-        bool exists = await articleRepository.ExistsAsync(
-            articleId: command.ArticleId,
-            cancellationToken: cancellationToken
-        );
-
-        if (!exists)
-        {
-            throw new NotFoundException(nameof(ArticleEntity), command.ArticleId);
-        }
+        await articleRepository.ExistsOrThrowAsync(articleId: command.ArticleId, cancellationToken: cancellationToken);
 
         bool alreadyBookmarked = await articleRepository.HasBookmarkedAsync(
             userId: command.UserId,
