30f357757431e9be9276fc7f6e5a4eec63d6d60c
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-23 19:26:01 +0200

    test(mocks): arrange the throwing video guard

--- tests/Unit/Common/Mocks/Repositories/MockVideoRepository.cs
+++ tests/Unit/Common/Mocks/Repositories/MockVideoRepository.cs
@@ -24,7 +24,7 @@ public static class MockVideoRepository
     public static Mock<IVideoRepository> SetupGetByIdOrThrow(this Mock<IVideoRepository> mock, VideoEntity entity)
     {
         mock.Setup(x => x.GetByIdOrThrowAsync(entity.Id, It.IsAny<CancellationToken>())).ReturnsAsync(entity);
-        mock.Setup(x => x.ExistsAsync(entity.Id, It.IsAny<CancellationToken>())).ReturnsAsync(true);
+        mock.Setup(x => x.ExistsOrThrowAsync(entity.Id, It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
         return mock;
     }
 
@@ -32,7 +32,8 @@ public static class MockVideoRepository
     {
         mock.Setup(x => x.GetByIdOrThrowAsync(id, It.IsAny<CancellationToken>()))
             .ThrowsAsync(new NotFoundException($"Video with id '{id}' was not found."));
-        mock.Setup(x => x.ExistsAsync(id, It.IsAny<CancellationToken>())).ReturnsAsync(false);
+        mock.Setup(x => x.ExistsOrThrowAsync(id, It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException(nameof(VideoEntity), id));
         return mock;
     }
 
