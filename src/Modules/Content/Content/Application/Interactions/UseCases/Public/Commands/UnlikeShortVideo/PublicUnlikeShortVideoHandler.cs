78cf9a7efd072f6d01369149d513e6ede198671a
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-23 12:25:11 +0200

    perf(interactions): check the short video exists instead of loading it

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/UnlikeShortVideo/PublicUnlikeShortVideoHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/UnlikeShortVideo/PublicUnlikeShortVideoHandler.cs
@@ -2,6 +2,7 @@ using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
+using _116.Shared.Application.Exceptions;
 using _116.Shared.Contracts.Application.CQRS;
 
 namespace _116.Content.Application.Interactions.UseCases.Public.Commands.UnlikeShortVideo;
@@ -24,7 +25,15 @@ public class PublicUnlikeShortVideoHandler(
         CancellationToken cancellationToken
     )
     {
-        await shortVideoRepository.GetByIdOrThrowAsync(id: command.ShortVideoId, cancellationToken: cancellationToken);
+        bool exists = await shortVideoRepository.ExistsAsync(
+            shortVideoId: command.ShortVideoId,
+            cancellationToken: cancellationToken
+        );
+
+        if (!exists)
+        {
+            throw new NotFoundException(nameof(ShortVideoEntity), command.ShortVideoId);
+        }
 
         bool hasLiked = await shortVideoRepository.HasLikedAsync(
             userId: command.UserId,
