2d40507d200103b8d110da6836cc6c0860276ecd
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-23 15:43:26 +0200

    test(mocks): arrange lyrics existence beside the load helpers

--- tests/Unit/Common/Mocks/Repositories/MockLyricsRepository.cs
+++ tests/Unit/Common/Mocks/Repositories/MockLyricsRepository.cs
@@ -24,6 +24,7 @@ public static class MockLyricsRepository
     public static Mock<ILyricsRepository> SetupGetByIdOrThrow(this Mock<ILyricsRepository> mock, LyricsEntity entity)
     {
         mock.Setup(x => x.GetByIdOrThrowAsync(entity.Id, It.IsAny<CancellationToken>())).ReturnsAsync(entity);
+        mock.Setup(x => x.ExistsAsync(entity.Id, It.IsAny<CancellationToken>())).ReturnsAsync(true);
         return mock;
     }
 
@@ -31,6 +32,7 @@ public static class MockLyricsRepository
     {
         mock.Setup(x => x.GetByIdOrThrowAsync(id, It.IsAny<CancellationToken>()))
             .ThrowsAsync(new NotFoundException($"Lyrics with id '{id}' was not found."));
+        mock.Setup(x => x.ExistsAsync(id, It.IsAny<CancellationToken>())).ReturnsAsync(false);
         return mock;
     }
 
