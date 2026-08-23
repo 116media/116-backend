51775107aaf3e262786c0f7862abda6394941548
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-23 19:30:04 +0200

    test(mocks): arrange the throwing role guard

--- tests/Unit/Common/Mocks/Repositories/MockRoleRepository.cs
+++ tests/Unit/Common/Mocks/Repositories/MockRoleRepository.cs
@@ -30,7 +30,7 @@ public static class MockRoleRepository
     public static Mock<IRoleRepository> SetupGetByIdOrThrow(this Mock<IRoleRepository> mock, RoleEntity role)
     {
         mock.Setup(x => x.GetRoleByIdOrThrowAsync(role.Id, It.IsAny<CancellationToken>())).ReturnsAsync(role);
-        mock.Setup(x => x.ExistsByIdAsync(role.Id, It.IsAny<CancellationToken>())).ReturnsAsync(true);
+        mock.Setup(x => x.ExistsByIdOrThrowAsync(role.Id, It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
         return mock;
     }
 
@@ -44,6 +44,8 @@ public static class MockRoleRepository
     {
         mock.Setup(x => x.GetRoleByIdOrThrowAsync(roleId, It.IsAny<CancellationToken>()))
             .ThrowsAsync(new NotFoundException($"Role with ID '{roleId}' was not found."));
+        mock.Setup(x => x.ExistsByIdOrThrowAsync(roleId, It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException(nameof(RoleEntity), roleId));
         return mock;
     }
 
@@ -58,7 +60,7 @@ public static class MockRoleRepository
         RoleEntity role
     )
     {
-        mock.Setup(x => x.ExistsByIdAsync(role.Id, It.IsAny<CancellationToken>())).ReturnsAsync(true);
+        mock.Setup(x => x.ExistsByIdOrThrowAsync(role.Id, It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
         mock.Setup(x => x.GetRoleByIdWithPermissionsOrThrowAsync(role.Id, It.IsAny<CancellationToken>()))
             .ReturnsAsync(role);
         return mock;
@@ -77,6 +79,8 @@ public static class MockRoleRepository
     {
         mock.Setup(x => x.GetRoleByIdWithPermissionsOrThrowAsync(roleId, It.IsAny<CancellationToken>()))
             .ThrowsAsync(new NotFoundException($"Role with ID '{roleId}' was not found."));
+        mock.Setup(x => x.ExistsByIdOrThrowAsync(roleId, It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException(nameof(RoleEntity), roleId));
         return mock;
     }
 
