895f1b7d863b7ef7881eb7badb5b810c98cfee24
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-23 17:55:59 +0200

    refactor(interactions): delegate the not-found throw to the guard

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/LikeShortVideo/PublicLikeShortVideoHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/LikeShortVideo/PublicLikeShortVideoHandler.cs
@@ -2,7 +2,6 @@ using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
-using _116.Shared.Application.Exceptions;
 using _116.Shared.Contracts.Application.CQRS;
 
 namespace _116.Content.Application.Interactions.UseCases.Public.Commands.LikeShortVideo;
@@ -25,15 +24,10 @@ public class PublicLikeShortVideoHandler(
         CancellationToken cancellationToken
     )
     {
-        bool exists = await shortVideoRepository.ExistsAsync(
+        await shortVideoRepository.ExistsOrThrowAsync(
             shortVideoId: command.ShortVideoId,
             cancellationToken: cancellationToken
         );
-
-        if (!exists)
-        {
-            throw new NotFoundException(nameof(ShortVideoEntity), command.ShortVideoId);
-        }
 
         bool alreadyLiked = await shortVideoRepository.HasLikedAsync(
             userId: command.UserId,
