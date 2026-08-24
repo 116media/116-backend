bd0f0e7a7010cf98256fd8cd85c36a0e01566b8a
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-24 16:46:06 +0200

    refactor(lookup): use IPricingTierRepository in deactivation

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/AdminDeactivatePricingTierHandler.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/AdminDeactivatePricingTierHandler.cs
@@ -11,12 +11,12 @@ namespace _116.Content.Application.Lookup.UseCases.Admin.Commands.DeactivatePric
 /// <summary>
 /// Handles the <see cref="AdminDeactivatePricingTierCommand" /> to deactivate a pricing tier.
 /// </summary>
-/// <param name="lookupRepository">Repository for lookup data access operations.</param>
+/// <param name="pricingTierRepository">Repository for pricing tier data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class AdminDeactivatePricingTierHandler(
-    ILookupRepository lookupRepository,
+    IPricingTierRepository pricingTierRepository,
     IContentUnitOfWork unitOfWork,
     IMapper mapper,
     ContentI18n i18n
@@ -30,7 +30,7 @@ public class AdminDeactivatePricingTierHandler(
     {
         Guid id = Guid.Parse(command.Id);
 
-        PricingTierEntity pricingTier = await lookupRepository.GetPricingTierByIdOrThrowAsync(
+        PricingTierEntity pricingTier = await pricingTierRepository.GetByIdOrThrowAsync(
             id: id,
             cancellationToken: cancellationToken
         );
@@ -42,7 +42,7 @@ public class AdminDeactivatePricingTierHandler(
             throw i18n.PricingTier.AlreadyInactive();
         }
 
-        lookupRepository.UpdatePricingTier(pricingTier: pricingTier);
+        pricingTierRepository.Update(pricingTier: pricingTier);
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
