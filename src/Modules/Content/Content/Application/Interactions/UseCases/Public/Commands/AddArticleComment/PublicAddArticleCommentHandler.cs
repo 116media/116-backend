8c9058bca8288351f2072cb92e902b0aa809e1e8
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-24 15:03:40 +0200

    refactor(interactions): use comment repo in add comment handler

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/AddArticleComment/PublicAddArticleCommentHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Commands/AddArticleComment/PublicAddArticleCommentHandler.cs
@@ -10,11 +10,11 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Commands.AddArti
 /// <summary>
 /// Handles the <see cref="PublicAddArticleCommentCommand" /> to post a comment on an article.
 /// </summary>
-/// <param name="articleRepository">Repository for article data access operations.</param>
+/// <param name="articleCommentRepository">Repository for article comment data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">The mapper used to project entities to DTOs.</param>
 public class PublicAddArticleCommentHandler(
-    IArticleRepository articleRepository,
+    IArticleCommentRepository articleCommentRepository,
     IContentUnitOfWork unitOfWork,
     IMapper mapper
 ) : ICommandHandler<PublicAddArticleCommentCommand, PublicAddArticleCommentResult>
@@ -25,7 +25,10 @@ public class PublicAddArticleCommentHandler(
         CancellationToken cancellationToken
     )
     {
-        await articleRepository.ExistsOrThrowAsync(articleId: command.ArticleId, cancellationToken: cancellationToken);
+        await articleCommentRepository.ExistsOrThrowAsync(
+            articleId: command.ArticleId,
+            cancellationToken: cancellationToken
+        );
 
         var comment = ArticleCommentEntity.Create(
             id: Guid.NewGuid(),
@@ -34,7 +37,7 @@ public class PublicAddArticleCommentHandler(
             body: command.Body
         );
 
-        await articleRepository.AddCommentAsync(comment: comment, cancellationToken: cancellationToken);
+        await articleCommentRepository.AddCommentAsync(comment: comment, cancellationToken: cancellationToken);
 
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
