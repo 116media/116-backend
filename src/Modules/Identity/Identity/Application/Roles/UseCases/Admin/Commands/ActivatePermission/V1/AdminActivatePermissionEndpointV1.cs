02353a1ba4364367d1d24c95c8e4ffeba552c567
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-27 11:10:07 +0200

    refactor(admin-activate-permission): pass request token

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/ActivatePermission/V1/AdminActivatePermissionEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/ActivatePermission/V1/AdminActivatePermissionEndpointV1.cs
@@ -38,11 +38,14 @@ public class AdminActivatePermissionEndpointV1 : ICarterModule
         group
             .MapPatch(
                 $"{{id}}/{PermissionRouteConstants.Activate}",
-                async (string id, IDispatcher dispatcher) =>
+                async (string id, IDispatcher dispatcher, CancellationToken cancellationToken) =>
                 {
                     var command = new AdminActivatePermissionCommand(PermissionId: id);
 
-                    AdminActivatePermissionResult result = await dispatcher.Send(request: command);
+                    AdminActivatePermissionResult result = await dispatcher.Send(
+                        request: command,
+                        cancellationToken: cancellationToken
+                    );
 
                     var response = new AdminActivatePermissionResponse(Permission: result.Permission);
                     return Results.Ok(value: response);
