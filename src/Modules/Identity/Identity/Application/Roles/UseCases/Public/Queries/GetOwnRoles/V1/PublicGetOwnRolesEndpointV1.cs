ebbd4b03a61f840388bac201eb3b78654e874949
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-27 12:38:54 +0200

    refactor(public-get-own-roles): pass request token

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Public/Queries/GetOwnRoles/V1/PublicGetOwnRolesEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Public/Queries/GetOwnRoles/V1/PublicGetOwnRolesEndpointV1.cs
@@ -40,12 +40,20 @@ public class PublicGetOwnRolesEndpointV1 : ICarterModule
         group
             .MapGet(
                 RoleRouteConstants.Endpoint,
-                async (ClaimsPrincipal user, IClaimsProvider authProvider, IDispatcher dispatcher) =>
+                async (
+                    ClaimsPrincipal user,
+                    IClaimsProvider authProvider,
+                    IDispatcher dispatcher,
+                    CancellationToken cancellationToken
+                ) =>
                 {
                     Guid userId = authProvider.GetUserIdFromClaims(user: user);
 
                     var query = new PublicGetOwnRolesQuery(UserId: userId);
-                    PublicGetOwnRolesResult result = await dispatcher.Send(request: query);
+                    PublicGetOwnRolesResult result = await dispatcher.Send(
+                        request: query,
+                        cancellationToken: cancellationToken
+                    );
 
                     var response = new PublicGetOwnRolesResponse(Roles: result.Roles);
                     return Results.Ok(value: response);
