a08fa485b8cd4859d29e1ee4c72bd18930797411
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-28 09:49:44 +0200

    refactor(admin-approve-article): pass request token

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ApproveArticle/V1/AdminApproveArticleEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ApproveArticle/V1/AdminApproveArticleEndpointV1.cs
@@ -37,10 +37,13 @@ public class AdminApproveArticleEndpointV1 : ICarterModule
         group
             .MapPatch(
                 $"/{{id}}/{EditorialRouteConstants.Approve}",
-                async (string id, IDispatcher dispatcher) =>
+                async (string id, IDispatcher dispatcher, CancellationToken cancellationToken) =>
                 {
                     var command = new AdminApproveArticleCommand(Id: id);
-                    AdminApproveArticleResult result = await dispatcher.Send(request: command);
+                    AdminApproveArticleResult result = await dispatcher.Send(
+                        request: command,
+                        cancellationToken: cancellationToken
+                    );
 
                     var response = new AdminApproveArticleResponse(IsSuccess: result.IsSuccess);
                     return Results.Ok(response);
