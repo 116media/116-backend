ac8daf69494e65d646d4181036978c1735902775
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-29 12:11:04 +0200

    refactor(admin-activate-content-type): pass request token

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/AdminActivateContentTypeEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/AdminActivateContentTypeEndpointV1.cs
@@ -37,10 +37,13 @@ public class AdminActivateContentTypeEndpointV1 : ICarterModule
         group
             .MapPatch(
                 $"/{{id}}/{LookupRouteConstants.Activate}",
-                async (string id, IDispatcher dispatcher) =>
+                async (string id, IDispatcher dispatcher, CancellationToken cancellationToken) =>
                 {
                     var command = new AdminActivateContentTypeCommand(Id: id);
-                    AdminActivateContentTypeResult result = await dispatcher.Send(request: command);
+                    AdminActivateContentTypeResult result = await dispatcher.Send(
+                        request: command,
+                        cancellationToken: cancellationToken
+                    );
 
                     var response = new AdminActivateContentTypeResponse(ContentType: result.ContentType);
                     return Results.Ok(response);
