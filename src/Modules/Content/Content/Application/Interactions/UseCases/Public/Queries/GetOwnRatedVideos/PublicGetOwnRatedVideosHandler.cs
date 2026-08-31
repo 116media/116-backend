ee74988a3f9416f8ad97ca6f467207c5d0cf6fe9
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-08-31 09:38:29 +0200

    refactor(get-own-rated-videos): map to the public shape

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetOwnRatedVideos/PublicGetOwnRatedVideosHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetOwnRatedVideos/PublicGetOwnRatedVideosHandler.cs
@@ -5,18 +5,14 @@ using _116.Core.Application.Shared.Repositories;
 using _116.Core.Domain.Entities;
 using _116.Shared.Application.Pagination;
 using _116.Shared.Contracts.Application.CQRS;
-using MapsterMapper;
 
 namespace _116.Content.Application.Interactions.UseCases.Public.Queries.GetOwnRatedVideos;
 
 /// <summary>
 /// Handles the current-user rated-video collection query.
 /// </summary>
-public class PublicGetOwnRatedVideosHandler(
-    IVideoRepository videoRepository,
-    IFileRepository fileRepository,
-    IMapper mapper
-) : IQueryHandler<PublicGetOwnRatedVideosQuery, PublicGetOwnRatedVideosResult>
+public class PublicGetOwnRatedVideosHandler(IVideoRepository videoRepository, IFileRepository fileRepository)
+    : IQueryHandler<PublicGetOwnRatedVideosQuery, PublicGetOwnRatedVideosResult>
 {
     /// <inheritdoc />
     public async Task<PublicGetOwnRatedVideosResult> Handle(
@@ -39,10 +35,10 @@ public class PublicGetOwnRatedVideosHandler(
             cancellationToken
         );
 
-        IReadOnlyList<VideoSummaryDto> videoDtos = activities
+        IReadOnlyList<PublicVideoSummaryDto> videoDtos = activities
             .Select(activity => activity.Video)
             .ToList()
-            .ToVideoSummaryDtos(mapper, files);
+            .ToPublicVideoSummaryDtos(files);
 
         IReadOnlyList<UserVideoActivityDto> items = activities
             .Select(
