d298f1a0447f6f8bde0cf3c7c6864fde929036c2
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-03 02:13:04 +0200

    build(mailer-db): update the model snapshot

--- src/Modules/Mailer/Mailer/Infrastructure/Persistence/Migrations/MailerDbContextModelSnapshot.cs
+++ src/Modules/Mailer/Mailer/Infrastructure/Persistence/Migrations/MailerDbContextModelSnapshot.cs
@@ -303,27 +303,6 @@ namespace _116.Mailer.Infrastructure.Persistence.Migrations
 
                     b.ToTable("domain_event_outbox", "mailer");
                 });
-
-            modelBuilder.Entity("_116.Shared.Infrastructure.Outbox.ProcessedDomainEventEntity", b =>
-                {
-                    b.Property<Guid>("EventId")
-                        .HasColumnType("uuid")
-                        .HasColumnName("event_id");
-
-                    b.Property<string>("HandlerName")
-                        .HasMaxLength(200)
-                        .HasColumnType("character varying(200)")
-                        .HasColumnName("handler_name");
-
-                    b.Property<DateTime>("ProcessedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("processed_at");
-
-                    b.HasKey("EventId", "HandlerName")
-                        .HasName("pk_processed_domain_events");
-
-                    b.ToTable("processed_domain_events", "mailer");
-                });
 #pragma warning restore 612, 618
         }
     }
