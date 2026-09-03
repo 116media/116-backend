5221e77e72652ee24c2008bdc69342d62955bf39
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-03 12:54:42 +0200

    refactor(mailer): constrain the repository contract to roots

--- src/Modules/Mailer/Mailer/Application/Shared/Repositories/IMailerRepository.cs
+++ src/Modules/Mailer/Mailer/Application/Shared/Repositories/IMailerRepository.cs
@@ -9,4 +9,4 @@ namespace _116.Mailer.Application.Shared.Repositories;
 /// </summary>
 /// <typeparam name="TEntity">The aggregate type.</typeparam>
 public interface IMailerRepository<TEntity> : IRepository<TEntity, Guid>
-    where TEntity : class, IEntity<Guid>;
+    where TEntity : class, IEntity<Guid>, IAggregateRoot;
