075ef34dff6f41b40d284f86f88cca91ac0b9ef5
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-06 15:15:54 +0200

    test(auth): read admin resend otp purpose via value object

--- tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResendOtp/AdminResendOtpFactoryTests.cs
+++ tests/Unit/Modules/Identity/Application/Auth/UseCases/Admin/Commands/ResendOtp/AdminResendOtpFactoryTests.cs
@@ -193,7 +193,7 @@ public class AdminResendOtpFactoryTests
         // Assert
         result.Should().NotBeNull();
         result!.Otp.UserId.Should().Be(userId);
-        result.Otp.Purpose.Should().Be(purposeEnum);
+        result.Otp.Purpose.Value.Should().Be(purposeEnum);
     }
 
     #endregion
