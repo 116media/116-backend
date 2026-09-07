f311b64869875f88b7f4364cb0c7949372a76501
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-07 16:22:19 +0200

    refactor(mailer): import moved contracts in NotificationRenderer

--- src/Modules/Mailer/Mailer/Application/Notifications/NotificationRenderer.cs
+++ src/Modules/Mailer/Mailer/Application/Notifications/NotificationRenderer.cs
@@ -2,7 +2,7 @@ using System.Globalization;
 using System.Text.RegularExpressions;
 using _116.Mailer.Application.Notifications.Messages;
 using _116.Mailer.Application.Shared.Services;
-using _116.Mailer.Contracts.Domain;
+using _116.Mailer.Contracts.Domain.Enums;
 
 namespace _116.Mailer.Application.Notifications;
 
