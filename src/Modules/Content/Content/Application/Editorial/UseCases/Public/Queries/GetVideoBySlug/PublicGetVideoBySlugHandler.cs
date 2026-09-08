a30c72ad26d740170eab9194ea1ff39f0172d489
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-08 17:08:52 +0200

    refactor(content): use the storage service

--- src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetVideoBySlug/PublicGetVideoBySlugHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetVideoBySlug/PublicGetVideoBySlugHandler.cs
@@ -3,7 +3,7 @@ using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
-using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -14,13 +14,13 @@ namespace _116.Content.Application.Editorial.UseCases.Public.Queries.GetVideoByS
 /// </summary>
 /// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="artistRepository">Repository used to resolve the linked artist's slug.</param>
-/// <param name="fileRepository">Repository for resolving file URLs.</param>
+/// <param name="fileStorage">Core's storage contract.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class PublicGetVideoBySlugHandler(
     IVideoRepository videoRepository,
     IArtistRepository artistRepository,
-    IFileRepository fileRepository,
+    IFileStorageService fileStorage,
     IMapper mapper,
     ContentI18n i18n
 ) : IQueryHandler<PublicGetVideoBySlugQuery, PublicGetVideoBySlugResult>
@@ -65,7 +65,7 @@ public class PublicGetVideoBySlugHandler(
 
         var dto = await video.ToPublicVideoDetailDtoAsync(
             mapper,
-            fileRepository,
+            fileStorage,
             cancellationToken,
             ratedStars: ratedStars
         );
