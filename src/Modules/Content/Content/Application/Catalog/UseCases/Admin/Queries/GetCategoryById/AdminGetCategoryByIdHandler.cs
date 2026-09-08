ea285acb18011b377b3c9098fd67f9e4f746f845
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-08 08:45:46 +0200

    refactor(content): use the storage service

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Queries/GetCategoryById/AdminGetCategoryByIdHandler.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Queries/GetCategoryById/AdminGetCategoryByIdHandler.cs
@@ -1,7 +1,7 @@
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
-using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -11,11 +11,11 @@ namespace _116.Content.Application.Catalog.UseCases.Admin.Queries.GetCategoryByI
 /// Handles the <see cref="AdminGetCategoryByIdQuery" /> to retrieve a category by its identifier.
 /// </summary>
 /// <param name="categoryRepository">Repository for category data access operations.</param>
-/// <param name="fileRepository">Repository for file storage operations.</param>
+/// <param name="fileStorage">Core's storage contract.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
 public class AdminGetCategoryByIdHandler(
     ICategoryRepository categoryRepository,
-    IFileRepository fileRepository,
+    IFileStorageService fileStorage,
     IMapper mapper
 ) : IQueryHandler<AdminGetCategoryByIdQuery, AdminGetCategoryByIdResult>
 {
@@ -30,7 +30,7 @@ public class AdminGetCategoryByIdHandler(
             cancellationToken: cancellationToken
         );
 
-        var dto = await category.ToCategoryDtoAsync(mapper, fileRepository, cancellationToken);
+        var dto = await category.ToCategoryDtoAsync(mapper, fileStorage, cancellationToken);
         return new AdminGetCategoryByIdResult(Category: dto);
     }
 }
