75864ea5ae9588afe28b17b5ac0fe97d46618620
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-08 16:01:54 +0200

    refactor(content): use the storage service

--- src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetPublicShortBySlug/PublicGetPublicShortBySlugHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetPublicShortBySlug/PublicGetPublicShortBySlugHandler.cs
@@ -3,8 +3,8 @@ using _116.Content.Application.Shared.Errors.Facade;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
-using _116.Core.Application.Shared.Repositories;
-using _116.Identity.Contracts.Application;
+using _116.Core.Contracts.Application.Services;
+using _116.Identity.Contracts.Application.Services;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -15,13 +15,13 @@ namespace _116.Content.Application.Editorial.UseCases.Public.Queries.GetPublicSh
 /// </summary>
 /// <param name="shortVideoRepository">Repository for short video data access operations.</param>
 /// <param name="userLookup">Service for resolving the author profile.</param>
-/// <param name="fileRepository">Repository for resolving video and thumbnail file URLs.</param>
+/// <param name="fileStorage">Core's storage contract.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class PublicGetPublicShortBySlugHandler(
     IShortVideoRepository shortVideoRepository,
     IUserLookupService userLookup,
-    IFileRepository fileRepository,
+    IFileStorageService fileStorage,
     IMapper mapper,
     ContentI18n i18n
 ) : IQueryHandler<PublicGetPublicShortBySlugQuery, PublicGetPublicShortBySlugResult>
@@ -54,7 +54,7 @@ public class PublicGetPublicShortBySlugHandler(
         PublicShortVideoDto dto = await shortVideo.ToPublicShortVideoDtoAsync(
             mapper,
             userLookup,
-            fileRepository,
+            fileStorage,
             cancellationToken,
             isLiked: isLiked,
             isBookmarked: isBookmarked
