142d7251e11780f03e80801845995b0a7c7c196d
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-08 14:33:20 +0200

    refactor(content): use the storage service

--- src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetArticleBySlug/PublicGetArticleBySlugHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetArticleBySlug/PublicGetArticleBySlugHandler.cs
@@ -3,7 +3,7 @@ using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
-using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -14,13 +14,13 @@ namespace _116.Content.Application.Editorial.UseCases.Public.Queries.GetArticleB
 /// </summary>
 /// <param name="articleRepository">Repository for article data access operations.</param>
 /// <param name="articleInteractionRepository">Repository for article interaction data access operations.</param>
-/// <param name="fileRepository">Repository for resolving file URLs.</param>
+/// <param name="fileStorage">Core's storage contract.</param>
 /// <param name="mapper">Mapster mapper for entity-to-DTO transformations.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
 public class PublicGetArticleBySlugHandler(
     IArticleRepository articleRepository,
     IArticleInteractionRepository articleInteractionRepository,
-    IFileRepository fileRepository,
+    IFileStorageService fileStorage,
     IMapper mapper,
     ContentI18n i18n
 ) : IQueryHandler<PublicGetArticleBySlugQuery, PublicGetArticleBySlugResult>
@@ -60,7 +60,7 @@ public class PublicGetArticleBySlugHandler(
 
         var dto = await article.ToPublicArticleDetailDtoAsync(
             mapper,
-            fileRepository,
+            fileStorage,
             cancellationToken,
             isLiked: isLiked,
             isBookmarked: isBookmarked
