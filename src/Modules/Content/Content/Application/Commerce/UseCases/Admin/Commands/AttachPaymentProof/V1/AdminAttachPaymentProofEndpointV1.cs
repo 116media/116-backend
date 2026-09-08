13393b352d89f4d3ab838177a9e2780fbf4411aa
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-08 09:15:09 +0200

    refactor(content): import the moved contract namespaces

--- src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/AttachPaymentProof/V1/AdminAttachPaymentProofEndpointV1.cs
+++ src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/AttachPaymentProof/V1/AdminAttachPaymentProofEndpointV1.cs
@@ -3,7 +3,7 @@ using _116.BuildingBlocks.Constants.RateLimit;
 using _116.Content.Application.Commerce.Constants;
 using _116.Content.Domain.Constants;
 using _116.Content.Domain.Enums;
-using _116.Core.Application.Shared.DTOs;
+using _116.Core.Contracts.Application.DTOs;
 using _116.Shared.Application.Extensions;
 using _116.Shared.Contracts.Application.CQRS;
 using Carter;
