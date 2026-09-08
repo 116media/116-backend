11271823e3175e1f5d0b1615b06b15b8af8361cf
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-08 11:59:25 +0200

    refactor(content): use the storage service

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateLyricsMetadata/AdminUpdateLyricsMetadataHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateLyricsMetadata/AdminUpdateLyricsMetadataHandler.cs
@@ -2,8 +2,8 @@ using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
-using _116.Core.Application.Shared.Repositories;
-using _116.Identity.Contracts.Application;
+using _116.Core.Contracts.Application.Services;
+using _116.Identity.Contracts.Application.Services;
 using _116.Shared.Contracts.Application.CQRS;
 using MapsterMapper;
 
@@ -17,13 +17,13 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.UpdateLyric
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="mapper">The Mapster mapper used for tags.</param>
 /// <param name="userLookup">Service for resolving author profiles from the Identity module.</param>
-/// <param name="fileRepository">Repository for resolving avatar and cover image file URLs.</param>
+/// <param name="fileStorage">Core's storage contract.</param>
 public class AdminUpdateLyricsMetadataHandler(
     ILyricsRepository lyricsRepository,
     IContentUnitOfWork unitOfWork,
     IMapper mapper,
     IUserLookupService userLookup,
-    IFileRepository fileRepository
+    IFileStorageService fileStorage
 ) : ICommandHandler<AdminUpdateLyricsMetadataCommand, AdminUpdateLyricsMetadataResult>
 {
     /// <inheritdoc />
@@ -53,7 +53,7 @@ public class AdminUpdateLyricsMetadataHandler(
             cancellationToken: cancellationToken
         );
 
-        var dto = await updated.ToLyricsDetailDtoAsync(mapper, userLookup, fileRepository, cancellationToken);
+        var dto = await updated.ToLyricsDetailDtoAsync(mapper, userLookup, fileStorage, cancellationToken);
         return new AdminUpdateLyricsMetadataResult(Lyrics: dto);
     }
 }
