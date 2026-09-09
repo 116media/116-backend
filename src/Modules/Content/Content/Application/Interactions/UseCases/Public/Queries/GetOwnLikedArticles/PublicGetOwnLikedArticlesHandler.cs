57a2b4f7e8433a81326e7b2c894db2ccec34b75c
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 10:13:58 +0200

    refactor(content): use the storage service

--- src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetOwnLikedArticles/PublicGetOwnLikedArticlesHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Public/Queries/GetOwnLikedArticles/PublicGetOwnLikedArticlesHandler.cs
@@ -1,7 +1,7 @@
 using _116.Content.Application.Shared.DTOs;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
-using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Pagination;
 using _116.Shared.Contracts.Application.CQRS;
 
@@ -12,7 +12,7 @@ namespace _116.Content.Application.Interactions.UseCases.Public.Queries.GetOwnLi
 /// </summary>
 public class PublicGetOwnLikedArticlesHandler(
     IArticleInteractionRepository articleInteractionRepository,
-    IFileRepository fileRepository
+    IFileStorageService fileStorage
 ) : IQueryHandler<PublicGetOwnLikedArticlesQuery, PublicGetOwnLikedArticlesResult>
 {
     /// <inheritdoc />
@@ -42,7 +42,7 @@ public class PublicGetOwnLikedArticlesHandler(
         foreach (ArticleActivity activity in activities)
         {
             PublicArticleSummaryDto article = await activity.Article.ToPublicArticleSummaryDtoAsync(
-                fileRepository,
+                fileStorage,
                 cancellationToken
             );
             article = article with
