22e07eccef323b897ea863f74b2f751d8ef0181d
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 13:33:26 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/AdminCreateCategoryHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/AdminCreateCategoryHandlerTests.cs
@@ -4,13 +4,16 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Factories.Core;
 using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -25,7 +28,7 @@ public class AdminCreateCategoryHandlerTests : BaseContentHandlerTest
     private readonly Mock<IContentTypeRepository> _contentTypeRepositoryMock;
     private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
     private readonly Mock<IContentUnitOfWork> _unitOfWorkMock;
-    private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IFileStorageService> _fileStorageMock;
     private readonly AdminCreateCategoryHandler _handler;
 
     public AdminCreateCategoryHandlerTests()
@@ -33,12 +36,12 @@ public class AdminCreateCategoryHandlerTests : BaseContentHandlerTest
         _contentTypeRepositoryMock = MockContentTypeRepository.Create();
         _categoryRepositoryMock = MockCategoryRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
-        _fileRepositoryMock = MockFileRepository.Create();
+        _fileStorageMock = MockFileStorageService.Create();
         _handler = new AdminCreateCategoryHandler(
             _contentTypeRepositoryMock.Object,
             _categoryRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            _fileRepositoryMock.Object,
+            _fileStorageMock.Object,
             Mapper,
             TestErrorsFactory.CreateContentI18n()
         );
