0d08a5461b4fd752080cc53231b87e2f083b1176
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 14:18:13 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Public/Queries/GetExclusiveCategory/PublicGetExclusiveCategoryHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Public/Queries/GetExclusiveCategory/PublicGetExclusiveCategoryHandlerTests.cs
@@ -3,12 +3,15 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Exceptions;
 using _116.Shared.Application.Pagination;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Factories.Core;
 using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -22,18 +25,18 @@ public class PublicGetExclusiveCategoryHandlerTests : BaseContentHandlerTest
 {
     private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
     private readonly Mock<IVideoRepository> _videoRepositoryMock;
-    private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IFileStorageService> _fileStorageMock;
     private readonly PublicGetExclusiveCategoryHandler _handler;
 
     public PublicGetExclusiveCategoryHandlerTests()
     {
         _categoryRepositoryMock = MockCategoryRepository.Create();
         _videoRepositoryMock = MockVideoRepository.Create();
-        _fileRepositoryMock = MockFileRepository.Create();
+        _fileStorageMock = MockFileStorageService.Create();
         _handler = new PublicGetExclusiveCategoryHandler(
             _categoryRepositoryMock.Object,
             _videoRepositoryMock.Object,
-            _fileRepositoryMock.Object,
+            _fileStorageMock.Object,
             Mapper,
             TestErrorsFactory.CreateContentI18n()
         );
