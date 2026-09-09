f2686e3723a60c6b771f64babe0ca1cb1b23f875
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 15:31:07 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/CreateArtist/AdminCreateArtistHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Commands/CreateArtist/AdminCreateArtistHandlerTests.cs
@@ -3,12 +3,14 @@ using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Exceptions;
 using _116.Tests.Fixtures.Constants;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common.Mocks.Infrastructure;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -28,11 +30,11 @@ public class AdminCreateArtistHandlerTests
     {
         _artistRepositoryMock = MockArtistRepository.Create();
         _unitOfWorkMock = MockContentUnitOfWork.Create();
-        Mock<IFileRepository> fileRepositoryMock = MockFileRepository.Create();
+        Mock<IFileStorageService> fileStorageMock = MockFileStorageService.Create();
         _handler = new AdminCreateArtistHandler(
             _artistRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            fileRepositoryMock.Object,
+            fileStorageMock.Object,
             TestErrorsFactory.CreateContentI18n()
         );
     }
