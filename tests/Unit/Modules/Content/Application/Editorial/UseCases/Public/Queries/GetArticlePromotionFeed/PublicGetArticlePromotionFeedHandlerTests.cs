75b3d82d1e54d907e18cad4ba0b19c64c124dc7a
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 18:00:09 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetArticlePromotionFeed/PublicGetArticlePromotionFeedHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetArticlePromotionFeed/PublicGetArticlePromotionFeedHandlerTests.cs
@@ -4,11 +4,14 @@ using _116.Content.Application.Shared.DTOs;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.DTOs;
+using _116.Core.Contracts.Application.Services;
 using _116.Core.Domain.Entities;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Tests.Fixtures.Factories.Core;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -23,7 +26,7 @@ public class PublicGetArticlePromotionFeedHandlerTests : BaseContentHandlerTest
     private readonly Mock<IArticleRepository> _articleRepositoryMock;
     private readonly Mock<IArticleInteractionRepository> _articleInteractionRepositoryMock;
     private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
-    private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IFileStorageService> _fileStorageMock;
     private readonly PublicGetArticlePromotionFeedHandler _handler;
 
     private static readonly Guid CategoryId = Guid.NewGuid();
@@ -33,14 +36,14 @@ public class PublicGetArticlePromotionFeedHandlerTests : BaseContentHandlerTest
         _articleRepositoryMock = MockArticleRepository.Create();
         _articleInteractionRepositoryMock = MockArticleInteractionRepository.Create();
         _categoryRepositoryMock = MockCategoryRepository.Create();
-        _fileRepositoryMock = MockFileRepository.Create();
-        FileEntity coverFile = FileFactory.CreateImage();
-        _fileRepositoryMock.SetupGetById(coverFile);
+        _fileStorageMock = MockFileStorageService.Create();
+        FileReferenceDto coverFile = FileReferenceDtoFactory.CreateImage();
+        _fileStorageMock.SetupResolve(coverFile);
         _handler = new PublicGetArticlePromotionFeedHandler(
             _articleRepositoryMock.Object,
             _articleInteractionRepositoryMock.Object,
             _categoryRepositoryMock.Object,
-            _fileRepositoryMock.Object,
+            _fileStorageMock.Object,
             Mapper
         );
     }
