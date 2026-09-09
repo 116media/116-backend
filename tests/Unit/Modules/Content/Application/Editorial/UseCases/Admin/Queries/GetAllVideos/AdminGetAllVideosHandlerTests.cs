9478f412a3b26087eaeb4f38c70e21d448a8d47d
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 17:36:59 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllVideos/AdminGetAllVideosHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Admin/Queries/GetAllVideos/AdminGetAllVideosHandlerTests.cs
@@ -3,10 +3,13 @@ using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Pagination;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Factories.Core;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -19,7 +22,7 @@ namespace _116.Unit.Tests.Modules.Content.Application.Editorial.UseCases.Admin.Q
 public class AdminGetAllVideosHandlerTests : BaseContentHandlerTest
 {
     private readonly Mock<IVideoRepository> _videoRepositoryMock;
-    private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IFileStorageService> _fileStorageMock;
     private readonly AdminGetAllVideosHandler _handler;
 
     private static readonly Guid CategoryId = Guid.NewGuid();
@@ -27,8 +30,8 @@ public class AdminGetAllVideosHandlerTests : BaseContentHandlerTest
     public AdminGetAllVideosHandlerTests()
     {
         _videoRepositoryMock = MockVideoRepository.Create();
-        _fileRepositoryMock = MockFileRepository.Create();
-        _handler = new AdminGetAllVideosHandler(_videoRepositoryMock.Object, _fileRepositoryMock.Object, Mapper);
+        _fileStorageMock = MockFileStorageService.Create();
+        _handler = new AdminGetAllVideosHandler(_videoRepositoryMock.Object, _fileStorageMock.Object, Mapper);
     }
 
     [Fact]
