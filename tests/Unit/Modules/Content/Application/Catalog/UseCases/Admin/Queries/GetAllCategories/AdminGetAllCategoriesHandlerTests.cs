6ee267d6c0446ef821fb0ccb30f600d9e7f57a02
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-09 14:08:52 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Queries/GetAllCategories/AdminGetAllCategoriesHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Queries/GetAllCategories/AdminGetAllCategoriesHandlerTests.cs
@@ -2,10 +2,13 @@ using _116.Content.Application.Catalog.UseCases.Admin.Queries.GetAllCategories;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Pagination;
 using _116.Tests.Fixtures.Factories.Content;
+using _116.Tests.Fixtures.Factories.Core;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -18,14 +21,14 @@ namespace _116.Unit.Tests.Modules.Content.Application.Catalog.UseCases.Admin.Que
 public class AdminGetAllCategoriesHandlerTests : BaseContentHandlerTest
 {
     private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
-    private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IFileStorageService> _fileStorageMock;
     private readonly AdminGetAllCategoriesHandler _handler;
 
     public AdminGetAllCategoriesHandlerTests()
     {
         _categoryRepositoryMock = MockCategoryRepository.Create();
-        _fileRepositoryMock = MockFileRepository.Create();
-        _handler = new AdminGetAllCategoriesHandler(_categoryRepositoryMock.Object, _fileRepositoryMock.Object, Mapper);
+        _fileStorageMock = MockFileStorageService.Create();
+        _handler = new AdminGetAllCategoriesHandler(_categoryRepositoryMock.Object, _fileStorageMock.Object, Mapper);
     }
 
     #region Success Cases
