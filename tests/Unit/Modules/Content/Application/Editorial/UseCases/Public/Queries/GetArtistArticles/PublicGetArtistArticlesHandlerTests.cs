c3ec4f18c1e75983028c3d852114b54abb40d497
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-10 08:00:00 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetArtistArticles/PublicGetArtistArticlesHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetArtistArticles/PublicGetArtistArticlesHandlerTests.cs
@@ -2,12 +2,14 @@ using _116.Content.Application.Editorial.UseCases.Public.Queries.GetArtistArticl
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.Services;
 using _116.Shared.Application.Exceptions;
 using _116.Shared.Application.Pagination;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Tests.Fixtures.Helpers;
 using _116.Unit.Tests.Common;
 using _116.Unit.Tests.Common.Mocks.Repositories;
+using _116.Unit.Tests.Common.Mocks.Services;
 using AwesomeAssertions;
 using Moq;
 using Xunit;
@@ -29,11 +31,11 @@ public class PublicGetArtistArticlesHandlerTests : BaseContentHandlerTest
     {
         _artistRepositoryMock = MockArtistRepository.Create();
         _articleRepositoryMock = MockArticleRepository.Create();
-        Mock<IFileRepository> fileRepositoryMock = MockFileRepository.Create();
+        Mock<IFileStorageService> fileStorageMock = MockFileStorageService.Create();
         _handler = new PublicGetArtistArticlesHandler(
             _artistRepositoryMock.Object,
             _articleRepositoryMock.Object,
-            fileRepositoryMock.Object,
+            fileStorageMock.Object,
             TestErrorsFactory.CreateContentI18n()
         );
     }
