2a6e07e98783d05105f9e157d8cdaa4504fde5e2
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-10 09:16:50 +0200

    refactor(test): use the storage service

--- tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetShortsFeed/PublicGetShortsFeedHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/UseCases/Public/Queries/GetShortsFeed/PublicGetShortsFeedHandlerTests.cs
@@ -2,8 +2,10 @@ using _116.Content.Application.Editorial.UseCases.Public.Queries.GetShortsFeed;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Application.Shared.Repositories;
+using _116.Core.Contracts.Application.DTOs;
+using _116.Core.Contracts.Application.Services;
 using _116.Core.Domain.Entities;
-using _116.Identity.Contracts.Application;
+using _116.Identity.Contracts.Application.Services;
 using _116.Tests.Fixtures.Factories.Content;
 using _116.Tests.Fixtures.Factories.Core;
 using _116.Unit.Tests.Common;
@@ -23,23 +25,23 @@ public class PublicGetShortsFeedHandlerTests : BaseContentHandlerTest
 {
     private readonly Mock<IShortVideoRepository> _shortVideoRepositoryMock;
     private readonly Mock<IUserLookupService> _userLookupMock;
-    private readonly Mock<IFileRepository> _fileRepositoryMock;
+    private readonly Mock<IFileStorageService> _fileStorageMock;
     private readonly PublicGetShortsFeedHandler _handler;
 
     public PublicGetShortsFeedHandlerTests()
     {
         _shortVideoRepositoryMock = MockShortVideoRepository.Create();
         _userLookupMock = MockUserLookupService.Create();
-        _fileRepositoryMock = MockFileRepository.Create();
+        _fileStorageMock = MockFileStorageService.Create();
 
-        FileEntity videoFile = FileFactory.CreateVideo();
-        _fileRepositoryMock.SetupGetById(videoFile);
-        _fileRepositoryMock.SetupGetByIds(new Dictionary<Guid, FileEntity>());
+        FileReferenceDto videoFile = FileReferenceDtoFactory.CreateVideo();
+        _fileStorageMock.SetupResolve(videoFile);
+        _fileStorageMock.SetupResolveMany(new Dictionary<Guid, FileReferenceDto>());
 
         _handler = new PublicGetShortsFeedHandler(
             _shortVideoRepositoryMock.Object,
             _userLookupMock.Object,
-            _fileRepositoryMock.Object,
+            _fileStorageMock.Object,
             Mapper
         );
     }
