220a4d118519076bcb861c5389ed3fa7813564e6
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-11 16:33:49 +0200

    test(content): construct the handler with the category dto factory

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/DeactivateCategory/AdminDeactivateCategoryHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Commands/DeactivateCategory/AdminDeactivateCategoryHandlerTests.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Catalog.Factories;
 using _116.Content.Application.Catalog.UseCases.Admin.Commands.DeactivateCategory;
 using _116.Content.Application.Shared.Persistence;
 using _116.Content.Application.Shared.Repositories;
@@ -36,8 +37,7 @@ public class AdminDeactivateCategoryHandlerTests : BaseContentHandlerTest
         _handler = new AdminDeactivateCategoryHandler(
             _categoryRepositoryMock.Object,
             _unitOfWorkMock.Object,
-            _fileStorageMock.Object,
-            Mapper,
+            new CategoryDtoFactory(Mapper, _fileStorageMock.Object),
             TestErrorsFactory.CreateContentI18n()
         );
     }
