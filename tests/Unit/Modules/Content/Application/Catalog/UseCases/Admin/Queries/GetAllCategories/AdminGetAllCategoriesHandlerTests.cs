4800f9de06f87a26de8330932734abcd3d1d988d
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-11 17:14:18 +0200

    test(content): construct the handler with the category dto factory

--- tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Queries/GetAllCategories/AdminGetAllCategoriesHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Catalog/UseCases/Admin/Queries/GetAllCategories/AdminGetAllCategoriesHandlerTests.cs
@@ -1,3 +1,4 @@
+using _116.Content.Application.Catalog.Factories;
 using _116.Content.Application.Catalog.UseCases.Admin.Queries.GetAllCategories;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
@@ -28,7 +29,10 @@ public class AdminGetAllCategoriesHandlerTests : BaseContentHandlerTest
     {
         _categoryRepositoryMock = MockCategoryRepository.Create();
         _fileStorageMock = MockFileStorageService.Create();
-        _handler = new AdminGetAllCategoriesHandler(_categoryRepositoryMock.Object, _fileStorageMock.Object, Mapper);
+        _handler = new AdminGetAllCategoriesHandler(
+            _categoryRepositoryMock.Object,
+            new CategoryDtoFactory(Mapper, _fileStorageMock.Object)
+        );
     }
 
     #region Success Cases
