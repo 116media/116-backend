d6570100bfccd0dbb6136971a83db974a02a3243
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-11 15:13:28 +0200

    refactor(content): build video dtos through the factory

--- src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetPromotedVideos/PublicGetPromotedVideosHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Public/Queries/GetPromotedVideos/PublicGetPromotedVideosHandler.cs
@@ -1,8 +1,8 @@
+using _116.Content.Application.Editorial.Factories;
 using _116.Content.Application.Shared.DTOs;
 using _116.Content.Application.Shared.Mappers;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
-using _116.Core.Contracts.Application.Services;
 using _116.Shared.Contracts.Application.CQRS;
 
 namespace _116.Content.Application.Editorial.UseCases.Public.Queries.GetPromotedVideos;
@@ -11,8 +11,8 @@ namespace _116.Content.Application.Editorial.UseCases.Public.Queries.GetPromoted
 /// Handles the <see cref="PublicGetPromotedVideosQuery" /> to retrieve all currently promoted published videos.
 /// </summary>
 /// <param name="videoRepository">Repository for video data access operations.</param>
-/// <param name="fileStorage">Core's storage contract.</param>
-public class PublicGetPromotedVideosHandler(IVideoRepository videoRepository, IFileStorageService fileStorage)
+/// <param name="videoDtoFactory">Builds video projections with their thumbnails resolved.</param>
+public class PublicGetPromotedVideosHandler(IVideoRepository videoRepository, IVideoDtoFactory videoDtoFactory)
     : IQueryHandler<PublicGetPromotedVideosQuery, PublicGetPromotedVideosResult>
 {
     /// <inheritdoc />
@@ -25,8 +25,8 @@ public class PublicGetPromotedVideosHandler(IVideoRepository videoRepository, IF
             cancellationToken: cancellationToken
         );
 
-        IReadOnlyList<PublicVideoSummaryDto> dtoList = await videos.ToPublicVideoSummaryDtosAsync(
-            fileStorage,
+        IReadOnlyList<PublicVideoSummaryDto> dtoList = await videoDtoFactory.CreatePublicManyAsync(
+            videos,
             cancellationToken
         );
 
