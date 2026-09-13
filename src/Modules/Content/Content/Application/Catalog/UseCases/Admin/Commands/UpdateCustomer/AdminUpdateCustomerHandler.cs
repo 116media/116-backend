98f2eff6116f2e8589e1d94102d9004639f855e9
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-13 12:08:25 +0200

    refactor(catalog): drop attach update in UpdateCustomer

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCustomer/AdminUpdateCustomerHandler.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/UpdateCustomer/AdminUpdateCustomerHandler.cs
@@ -40,9 +40,6 @@ public class AdminUpdateCustomerHandler(
             company: command.Company,
             notes: command.Notes
         );
-
-        customerRepository.Update(customer: customer);
-
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
         var dto = customer.ToCustomerDto(mapper);
