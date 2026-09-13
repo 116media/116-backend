3024bc07e0f3550916d90d7ffe8fc314cbb6da22
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-13 10:59:20 +0200

    refactor(catalog): drop attach update in ActivateCategory

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/ActivateCategory/AdminActivateCategoryHandler.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/ActivateCategory/AdminActivateCategoryHandler.cs
@@ -40,9 +40,6 @@ public class AdminActivateCategoryHandler(
         {
             throw i18n.Category.AlreadyActive();
         }
-
-        categoryRepository.Update(category: category);
-
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
         CategoryEntity updated = await categoryRepository.GetByIdOrThrowAsync(
