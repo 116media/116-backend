1af9aca0cfb1668d979fceccc91684d2d75d8998
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-13 11:53:02 +0200

    refactor(catalog): drop attach update in UnpinCategoryFromFeed

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/UnpinCategoryFromFeed/AdminUnpinCategoryFromFeedHandler.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/UnpinCategoryFromFeed/AdminUnpinCategoryFromFeedHandler.cs
@@ -33,9 +33,6 @@ public class AdminUnpinCategoryFromFeedHandler(
         );
 
         category.UnpinFromFeed();
-
-        categoryRepository.Update(category: category);
-
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
 
         CategoryEntity updated = await categoryRepository.GetByIdOrThrowAsync(
