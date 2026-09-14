0d9506251844b4529d869f2d762ad0127b4f4cb1
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-14 02:06:59 +0200

    refactor(content): inline lyricsrevision predicates

--- src/Modules/Content/Content/Infrastructure/Repositories/LyricsRevisionRepository.cs
+++ src/Modules/Content/Content/Infrastructure/Repositories/LyricsRevisionRepository.cs
@@ -1,4 +1,3 @@
-using _116.Content.Application.Editorial.Specifications;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Infrastructure.Persistence;
