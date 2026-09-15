4da2d317fd3e5138c1fedc9bcb0e2a2fc42bf1fe
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-15 18:51:21 +0200

    refactor(content): query tag through specifications

--- src/Modules/Content/Content/Infrastructure/Repositories/TagRepository.cs
+++ src/Modules/Content/Content/Infrastructure/Repositories/TagRepository.cs
@@ -1,8 +1,10 @@
+using _116.Content.Application.Lookup.Specifications;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Content.Infrastructure.Builders;
 using _116.Content.Infrastructure.Persistence;
+using _116.Shared.Infrastructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace _116.Content.Infrastructure.Repositories;
@@ -16,7 +18,11 @@ public class TagRepository(ContentDbContext context) : ContentRepository<TagEnti
     /// <inheritdoc />
     public async Task<TagEntity?> GetBySlugAsync(string slug, CancellationToken cancellationToken = default)
     {
-        return await Context.Tags.FirstOrDefaultAsync(tag => tag.Slug == slug, cancellationToken);
+        var specification = new TagBySlugSpecification(slug: slug);
+        return await Context.Tags.FirstOrDefaultBySpecificationAsync(
+            specification: specification,
+            cancellationToken: cancellationToken
+        );
     }
 
     /// <inheritdoc />
