66f789b39747d6aae6d9523d755333ebc051cf36
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-15 15:08:45 +0200

    refactor(editorial): restore submission specifications

---
+++ src/Modules/Content/Content/Application/Editorial/Specifications/SubmissionSpecifications.cs
@@ -0,0 +1,19 @@
+using System.Linq.Expressions;
+using _116.Content.Domain.Entities;
+using _116.Content.Domain.Enums;
+using _116.Shared.Application.Specifications;
+
+namespace _116.Content.Application.Editorial.Specifications;
+
+/// <summary>
+/// Specification that matches every lyrics submission currently in the given moderation
+/// status. Used by the admin review queue's status filter.
+/// </summary>
+public class SubmissionByStatusSpecification(EnumSubmissionStatus status) : Specification<LyricsSubmissionEntity>
+{
+    /// <inheritdoc />
+    public override Expression<Func<LyricsSubmissionEntity, bool>> ToExpression()
+    {
+        return submission => submission.Status == status;
+    }
+}
