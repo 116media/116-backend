f9657f8f8022ea6ee830151db6974c9a145bd81f
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-16 17:10:48 +0200

    refactor(content): tighten the short video bookmark aggregate

--- src/Modules/Content/Content/Domain/Entities/ShortVideoBookmarkEntity.cs
+++ src/Modules/Content/Content/Domain/Entities/ShortVideoBookmarkEntity.cs
@@ -20,11 +20,6 @@ public class ShortVideoBookmarkEntity : Aggregate<Guid>
     /// </summary>
     public Guid ShortVideoId { get; private set; }
 
-    /// <summary>
-    /// Navigation property to the short video.
-    /// </summary>
-    public ShortVideoEntity ShortVideo { get; private set; } = null!;
-
     private ShortVideoBookmarkEntity() { }
 
     /// <summary>
@@ -41,7 +36,6 @@ public class ShortVideoBookmarkEntity : Aggregate<Guid>
             Id = id,
             UserId = userId,
             ShortVideoId = shortVideoId,
-            CreatedAt = DateTime.UtcNow,
         };
 
         bookmark.AddDomainEvent(
