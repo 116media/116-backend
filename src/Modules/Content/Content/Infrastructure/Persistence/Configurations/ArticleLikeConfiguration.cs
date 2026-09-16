745c1d6953954e32eab1ecda08d4b40894a7a523
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-16 19:44:36 +0200

    refactor(content): map article like to ids and value objects

--- src/Modules/Content/Content/Infrastructure/Persistence/Configurations/ArticleLikeConfiguration.cs
+++ src/Modules/Content/Content/Infrastructure/Persistence/Configurations/ArticleLikeConfiguration.cs
@@ -20,6 +20,6 @@ public class ArticleLikeConfiguration : IEntityTypeConfiguration<ArticleLikeEnti
 
         builder.HasIndex(x => new { x.UserId, x.ArticleId }).IsUnique();
 
-        builder.HasOne(x => x.Article).WithMany().HasForeignKey(x => x.ArticleId).OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne<ArticleEntity>().WithMany().HasForeignKey(x => x.ArticleId).OnDelete(DeleteBehavior.Cascade);
     }
 }
