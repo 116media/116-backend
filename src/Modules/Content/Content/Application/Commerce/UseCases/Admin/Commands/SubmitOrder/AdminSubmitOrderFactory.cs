82fed7c4969793289480486f217b0ae1e4cf6c0b
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-17 14:00:05 +0200

    refactor(commerce): route submit order factory through the new model

--- src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/SubmitOrder/AdminSubmitOrderFactory.cs
+++ src/Modules/Content/Content/Application/Commerce/UseCases/Admin/Commands/SubmitOrder/AdminSubmitOrderFactory.cs
@@ -1,7 +1,6 @@
 using _116.Content.Application.Commerce.UseCases.Admin.Commands.SubmitOrder.Contracts;
 using _116.Content.Application.Shared.Errors;
 using _116.Content.Application.Shared.Persistence;
-using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 
 namespace _116.Content.Application.Commerce.UseCases.Admin.Commands.SubmitOrder;
@@ -9,14 +8,10 @@ namespace _116.Content.Application.Commerce.UseCases.Admin.Commands.SubmitOrder;
 /// <summary>
 /// Factory implementation for the order submission flow.
 /// </summary>
-/// <param name="contentOrderRepository">Repository for content order data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="contentOrderErrors">Content order domain error factory.</param>
-public class AdminSubmitOrderFactory(
-    IContentOrderRepository contentOrderRepository,
-    IContentUnitOfWork unitOfWork,
-    ContentOrderErrors contentOrderErrors
-) : ISubmitOrderFactory
+public class AdminSubmitOrderFactory(IContentUnitOfWork unitOfWork, ContentOrderErrors contentOrderErrors)
+    : ISubmitOrderFactory
 {
     /// <inheritdoc />
     public async Task SubmitAsync(ContentOrderEntity order, CancellationToken cancellationToken)
@@ -28,14 +23,7 @@ public class AdminSubmitOrderFactory(
         }
 
         order.Submit();
-
-        var payment = ContentPaymentEntity.Create(
-            id: Guid.NewGuid(),
-            orderId: order.Id,
-            amountUsd: order.TotalAmountUsd
-        );
-
-        await contentOrderRepository.AddPaymentAsync(payment: payment, ct: cancellationToken);
+        order.AttachPayment();
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
     }
 }
