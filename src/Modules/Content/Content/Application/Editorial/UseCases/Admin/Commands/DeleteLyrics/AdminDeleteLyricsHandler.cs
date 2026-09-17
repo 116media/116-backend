3df759df2d99d861dc0003c1e5c683c20ccf5c68
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-17 15:30:57 +0200

    refactor(editorial): route delete lyrics handler through the new model

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeleteLyrics/AdminDeleteLyricsHandler.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeleteLyrics/AdminDeleteLyricsHandler.cs
@@ -7,16 +7,11 @@ namespace _116.Content.Application.Editorial.UseCases.Admin.Commands.DeleteLyric
 
 /// <summary>
 /// Handles the <see cref="AdminDeleteLyricsCommand" /> to permanently delete a lyrics page.
-/// Clears the linked video's HasLyrics flag if the lyrics page was linked to a video.
 /// </summary>
 /// <param name="lyricsRepository">Repository for lyrics data access operations.</param>
-/// <param name="videoRepository">Repository for video data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
-public class AdminDeleteLyricsHandler(
-    ILyricsRepository lyricsRepository,
-    IVideoRepository videoRepository,
-    IContentUnitOfWork unitOfWork
-) : ICommandHandler<AdminDeleteLyricsCommand, AdminDeleteLyricsResult>
+public class AdminDeleteLyricsHandler(ILyricsRepository lyricsRepository, IContentUnitOfWork unitOfWork)
+    : ICommandHandler<AdminDeleteLyricsCommand, AdminDeleteLyricsResult>
 {
     /// <inheritdoc />
     public async Task<AdminDeleteLyricsResult> Handle(
@@ -27,15 +22,6 @@ public class AdminDeleteLyricsHandler(
         Guid id = Guid.Parse(command.Id);
 
         LyricsEntity lyrics = await lyricsRepository.GetByIdOrThrowAsync(id: id, cancellationToken: cancellationToken);
-
-        if (lyrics.VideoId.HasValue)
-        {
-            VideoEntity video = await videoRepository.GetByIdOrThrowAsync(
-                id: lyrics.VideoId.Value,
-                cancellationToken: cancellationToken
-            );
-            video.UnmarkHasLyrics();
-        }
 
         lyricsRepository.Remove(lyrics: lyrics);
         await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
