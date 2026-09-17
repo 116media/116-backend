20b56e23726125ab171e8de711c03a22b21856cd
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-17 13:05:49 +0200

    refactor(commerce): route payment dto factory through the new model

--- src/Modules/Content/Content/Application/Commerce/Factories/PaymentDtoFactory.cs
+++ src/Modules/Content/Content/Application/Commerce/Factories/PaymentDtoFactory.cs
@@ -1,5 +1,6 @@
 using _116.Content.Application.Shared.DTOs;
 using _116.Content.Application.Shared.Mappers;
+using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Core.Contracts.Application.DTOs;
 using _116.Identity.Contracts.Application.DTOs;
@@ -13,7 +14,9 @@ namespace _116.Content.Application.Commerce.Factories;
 /// </summary>
 /// <param name="mapper">Injected IMapper instance.</param>
 /// <param name="userLookup">Identity's lookup contract, resolving verifier names.</param>
-public class PaymentDtoFactory(IMapper mapper, IUserLookupService userLookup) : IPaymentDtoFactory
+/// <param name="orderDtoFactory">Factory resolving the ordering customers.</param>
+public class PaymentDtoFactory(IMapper mapper, IUserLookupService userLookup, IContentOrderDtoFactory orderDtoFactory)
+    : IPaymentDtoFactory
 {
     /// <inheritdoc />
     public async Task<PaymentDto> CreateAsync(
@@ -29,13 +32,17 @@ public class PaymentDtoFactory(IMapper mapper, IUserLookupService userLookup) : 
 
     /// <inheritdoc />
     public async Task<IReadOnlyList<PaymentSummaryDto>> CreateManyAsync(
-        IReadOnlyList<ContentPaymentEntity> payments,
+        IReadOnlyList<ContentOrderEntity> orders,
         CancellationToken ct = default
     )
     {
-        IReadOnlyDictionary<Guid, AuthorDto> verifiers = await ResolveVerifiersAsync(payments, ct);
+        IReadOnlyDictionary<Guid, AuthorDto> verifiers = await ResolveVerifiersAsync(
+            [.. orders.Select(order => order.Payment!)],
+            ct
+        );
+        IReadOnlyDictionary<Guid, CustomerEntity> customers = await orderDtoFactory.ResolveCustomersAsync(orders, ct);
 
-        return payments.Select(payment => payment.ToPaymentSummaryDto(mapper, verifiers)).ToList();
+        return orders.Select(order => order.ToPaymentSummaryDto(mapper, verifiers, customers)).ToList();
     }
 
     /// <summary>
