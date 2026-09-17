418ca7e1e54c7909f4588898e94f93f5219bc9dc
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-17 18:51:56 +0200

    refactor(interactions): route delete article comment handler through...

--- src/Modules/Content/Content/Application/Interactions/UseCases/Admin/Commands/DeleteArticleComment/AdminDeleteArticleCommentHandler.cs
+++ src/Modules/Content/Content/Application/Interactions/UseCases/Admin/Commands/DeleteArticleComment/AdminDeleteArticleCommentHandler.cs
@@ -15,10 +15,12 @@ namespace _116.Content.Application.Interactions.UseCases.Admin.Commands.DeleteAr
 /// <param name="articleCommentRepository">Repository for article comment data access operations.</param>
 /// <param name="unitOfWork">Unit of Work for managing database transactions.</param>
 /// <param name="i18n">Single i18n entry point for the Content module.</param>
+/// <param name="timeProvider">Clock stamping the deletion time.</param>
 public class AdminDeleteArticleCommentHandler(
     IArticleCommentRepository articleCommentRepository,
     IContentUnitOfWork unitOfWork,
-    ContentI18n i18n
+    ContentI18n i18n,
+    TimeProvider timeProvider
 ) : ICommandHandler<AdminDeleteArticleCommentCommand, AdminDeleteArticleCommentResult>
 {
     /// <inheritdoc />
@@ -38,7 +40,7 @@ public class AdminDeleteArticleCommentHandler(
             throw i18n.ArticleInteraction.CommentNotFound(commentId: command.CommentId);
         }
 
-        if (comment.SoftDelete())
+        if (comment.SoftDelete(now: timeProvider.GetUtcNow()))
         {
             await unitOfWork.CommitAsync(cancellationToken: cancellationToken);
         }
