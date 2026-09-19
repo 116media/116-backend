af320f57b4244866b6560c3bb7bb63f7fc3bea6f
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-19 21:52:57 +0200

    refactor(approve-video): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ApproveVideo/V1/AdminApproveVideoEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/ApproveVideo/V1/AdminApproveVideoEndpointV1.cs
@@ -54,7 +54,7 @@ public class AdminApproveVideoEndpointV1 : ICarterModule
             .WithDescription(description: AdminApproveVideoMetaField.ApproveVideo.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .Produces<AdminApproveVideoResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
