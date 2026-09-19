18464e45fb10d1d0ac34c13061fa729a1c27dc4f
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-19 18:55:31 +0200

    refactor(create-category): use the admin write rate limit

--- src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/V1/AdminCreateCategoryEndpointV1.cs
+++ src/Modules/Content/Content/Application/Catalog/UseCases/Admin/Commands/CreateCategory/V1/AdminCreateCategoryEndpointV1.cs
@@ -95,7 +95,7 @@ public class AdminCreateCategoryEndpointV1 : ICarterModule
             .WithDescription(description: AdminCreateCategoryMetaField.CreateCategory.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .ProducesValidationProblem()
             .Produces<AdminCreateCategoryResponse>(statusCode: StatusCodes.Status201Created)
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
