0b5cce7d1fc41e5414e649506f39dd639681b88f
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 08:12:30 +0200

    refactor(deactivate-short-video): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeactivateShortVideo/V1/AdminDeactivateShortVideoEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/DeactivateShortVideo/V1/AdminDeactivateShortVideoEndpointV1.cs
@@ -54,7 +54,7 @@ public class AdminDeactivateShortVideoEndpointV1 : ICarterModule
             .WithDescription(description: AdminDeactivateShortVideoMetaField.DeactivateShortVideo.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .Produces<AdminDeactivateShortVideoResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
