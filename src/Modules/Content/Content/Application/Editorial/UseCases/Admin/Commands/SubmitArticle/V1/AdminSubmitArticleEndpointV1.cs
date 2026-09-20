cd8d23ba7a46bf730e8b1e9fc2726cc29aae981e
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 10:50:15 +0200

    refactor(submit-article): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/SubmitArticle/V1/AdminSubmitArticleEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/SubmitArticle/V1/AdminSubmitArticleEndpointV1.cs
@@ -54,7 +54,7 @@ public class AdminSubmitArticleEndpointV1 : ICarterModule
             .WithDescription(description: AdminSubmitArticleMetaField.SubmitArticle.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .Produces<AdminSubmitArticleResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
