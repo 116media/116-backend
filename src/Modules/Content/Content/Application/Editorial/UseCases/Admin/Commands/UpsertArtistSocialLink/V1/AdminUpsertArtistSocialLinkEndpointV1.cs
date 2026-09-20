b621c605bc6c3063e31fdfd696b5c5eedf664c2c
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 12:32:17 +0200

    refactor(upsert-artist-social-link): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpsertArtistSocialLink/V1/AdminUpsertArtistSocialLinkEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpsertArtistSocialLink/V1/AdminUpsertArtistSocialLinkEndpointV1.cs
@@ -72,7 +72,7 @@ public class AdminUpsertArtistSocialLinkEndpointV1 : ICarterModule
             .WithDescription(description: AdminUpsertArtistSocialLinkMetaField.UpsertArtistSocialLink.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .ProducesValidationProblem()
             .Produces<AdminUpsertArtistSocialLinkResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
