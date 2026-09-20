a0aac15266753aa8e88769f3d9475c5c2a0daee0
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 17:07:56 +0200

    refactor(activate-content-type): use the admin write rate limit

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/AdminActivateContentTypeEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/ActivateContentType/V1/AdminActivateContentTypeEndpointV1.cs
@@ -54,7 +54,7 @@ public class AdminActivateContentTypeEndpointV1 : ICarterModule
             .WithDescription(description: AdminActivateContentTypeMetaField.ActivateContentType.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .Produces<AdminActivateContentTypeResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
