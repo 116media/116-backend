38791fedcc9bc28c9488c46a976e143d2d541f0b
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 19:24:18 +0200

    refactor(deactivate-role): use the admin write rate limit

--- src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivateRole/V1/AdminDeactivateRoleEndpointV1.cs
+++ src/Modules/Identity/Identity/Application/Roles/UseCases/Admin/Commands/DeactivateRole/V1/AdminDeactivateRoleEndpointV1.cs
@@ -55,7 +55,7 @@ public class AdminDeactivateRoleEndpointV1 : ICarterModule
             .WithSummary(summary: AdminDeactivateRoleMetaField.DeactivateRole.Summary)
             .WithDescription(description: AdminDeactivateRoleMetaField.DeactivateRole.Description)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .ProducesValidationProblem()
             .Produces<AdminDeactivateRoleResponse>()
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
