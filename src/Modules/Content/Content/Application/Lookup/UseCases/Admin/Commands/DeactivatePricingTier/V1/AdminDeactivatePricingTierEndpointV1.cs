4309cbcb99906b2d12834ea32d475001d85b5f99
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 17:53:57 +0200

    refactor(deactivate-pricing-tier): use the admin write rate limit

--- src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/V1/AdminDeactivatePricingTierEndpointV1.cs
+++ src/Modules/Content/Content/Application/Lookup/UseCases/Admin/Commands/DeactivatePricingTier/V1/AdminDeactivatePricingTierEndpointV1.cs
@@ -54,7 +54,7 @@ public class AdminDeactivatePricingTierEndpointV1 : ICarterModule
             .WithDescription(description: AdminDeactivatePricingTierMetaField.DeactivatePricingTier.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .Produces<AdminDeactivatePricingTierResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
