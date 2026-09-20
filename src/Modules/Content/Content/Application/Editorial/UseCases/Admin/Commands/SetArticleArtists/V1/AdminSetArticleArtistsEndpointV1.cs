a27cae18a7ef91ca92e6de261ad367cc32927de6
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 10:39:40 +0200

    refactor(set-article-artists): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/SetArticleArtists/V1/AdminSetArticleArtistsEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/SetArticleArtists/V1/AdminSetArticleArtistsEndpointV1.cs
@@ -66,7 +66,7 @@ public class AdminSetArticleArtistsEndpointV1 : ICarterModule
             .WithDescription(description: AdminSetArticleArtistsMetaField.SetArticleArtists.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .ProducesValidationProblem()
             .Produces<AdminSetArticleArtistsResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
