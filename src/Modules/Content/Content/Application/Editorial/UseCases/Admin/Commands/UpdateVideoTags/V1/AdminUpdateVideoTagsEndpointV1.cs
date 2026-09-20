6202147c5b740a63bff2a191e033c07e1e5702fd
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 12:20:02 +0200

    refactor(update-video-tags): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideoTags/V1/AdminUpdateVideoTagsEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateVideoTags/V1/AdminUpdateVideoTagsEndpointV1.cs
@@ -65,7 +65,7 @@ public class AdminUpdateVideoTagsEndpointV1 : ICarterModule
             .WithDescription(description: AdminUpdateVideoTagsMetaField.UpdateVideoTags.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .Produces<AdminUpdateVideoTagsResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status401Unauthorized)
             .ProducesProblem(statusCode: StatusCodes.Status403Forbidden)
