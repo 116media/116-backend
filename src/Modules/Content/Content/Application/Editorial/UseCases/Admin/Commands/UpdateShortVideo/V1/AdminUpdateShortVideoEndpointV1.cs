de03180df624cf163212fa5c49d4fc7347fbf435
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 12:00:15 +0200

    refactor(update-short-video): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateShortVideo/V1/AdminUpdateShortVideoEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateShortVideo/V1/AdminUpdateShortVideoEndpointV1.cs
@@ -72,7 +72,7 @@ public class AdminUpdateShortVideoEndpointV1 : ICarterModule
             .WithDescription(description: AdminUpdateShortVideoMetaField.UpdateShortVideo.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireAdminOrSuperAdmin)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .ProducesValidationProblem()
             .Produces<AdminUpdateShortVideoResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
