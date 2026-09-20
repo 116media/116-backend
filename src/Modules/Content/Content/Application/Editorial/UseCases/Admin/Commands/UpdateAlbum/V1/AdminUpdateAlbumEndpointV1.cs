cd15ba6cf10f840b9fc63db289ea16304cd9fcce
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-20 11:07:17 +0200

    refactor(update-album): use the admin write rate limit

--- src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateAlbum/V1/AdminUpdateAlbumEndpointV1.cs
+++ src/Modules/Content/Content/Application/Editorial/UseCases/Admin/Commands/UpdateAlbum/V1/AdminUpdateAlbumEndpointV1.cs
@@ -77,7 +77,7 @@ public class AdminUpdateAlbumEndpointV1 : ICarterModule
             .WithDescription(description: AdminUpdateAlbumMetaField.UpdateAlbum.Description)
             .WithAuthorization(AccountStatusPolicies.RequireActiveUser)
             .WithAuthorization(UserRolePolicies.RequireSuperAdminOnly)
-            .RequireRateLimiting(policyName: RateLimitPolicies.ContentBrowsing)
+            .RequireRateLimiting(policyName: RateLimitPolicies.ContentManagement)
             .ProducesValidationProblem()
             .Produces<AdminUpdateAlbumResponse>(statusCode: StatusCodes.Status200OK)
             .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
