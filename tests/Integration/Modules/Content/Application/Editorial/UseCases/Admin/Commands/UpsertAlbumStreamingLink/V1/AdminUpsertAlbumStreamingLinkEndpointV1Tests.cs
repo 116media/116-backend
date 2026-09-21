34a6911e39f4c8300108dccffb88b4ed73bc2a25
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-21 20:59:57 +0200

    test(upsert-album-streaming-link): cover the url length rule

--- tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UpsertAlbumStreamingLink/V1/AdminUpsertAlbumStreamingLinkEndpointV1Tests.cs
+++ tests/Integration/Modules/Content/Application/Editorial/UseCases/Admin/Commands/UpsertAlbumStreamingLink/V1/AdminUpsertAlbumStreamingLinkEndpointV1Tests.cs
@@ -1,5 +1,7 @@
 using _116.Content.Application.Editorial.Constants;
 using _116.Content.Application.Editorial.UseCases.Admin.Commands.UpsertAlbumStreamingLink.V1;
+using _116.Content.Application.Shared.Errors.Messages;
+using _116.Content.Domain.Constants;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Enums;
 using _116.Content.Infrastructure.Persistence;
@@ -192,5 +194,24 @@ public class AdminUpsertAlbumStreamingLinkEndpointV1Tests(PostgresFixture db) : 
         await using ContentDbContext ctx = CreateDbContext<ContentDbContext>();
         bool anyPersisted = await ctx.StreamingLinks.AnyAsync(link => link.AlbumId == album.Id);
         anyPersisted.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task UpsertStreamingLink_WithAnOverlongUrl_ReturnsTheLocalizedLengthError()
+    {
+        AlbumEntity album = await SeedAlbumAsync();
+        Client.AuthenticateAsAdmin();
+
+        var response = await Client.PutAsJsonAsync(
+            Url(album.Id, EnumStreamingPlatform.Spotify),
+            new AdminUpsertAlbumStreamingLinkRequest(
+                "https://open.spotify.com/" + new string('a', ContentConstants.MaxStreamingLinkUrlLength)
+            )
+        );
+
+        await response.ShouldBeValidationProblem(
+            "Url",
+            Localized<StreamingLinkErrorMessage>(m => m.UrlTooLong(ContentConstants.MaxStreamingLinkUrlLength))
+        );
     }
 }
