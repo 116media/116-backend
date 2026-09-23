18fa60b4d8862797fa608f4f9bcdcb9ad8d2c175
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-23 13:44:25 +0200

    feat(db): add the preferred locale migration

---
+++ src/Modules/Identity/Identity/Infrastructure/Persistence/Migrations/20260922235645_AddUserPreferredLocale.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _116.Identity.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferredLocale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "preferred_locale",
+                schema: "identity",
+                table: "users",
+                type: "character varying(5)",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "en");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "preferred_locale",
+                schema: "identity",
+                table: "users");
+        }
+    }
+}
