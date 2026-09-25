be82dda0734ee040f1dcbe4c6f611f15cccce789
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-25 22:08:19 +0200

    test(translation-revision-decided): point at the outbound email types

--- tests/Unit/Modules/Content/Application/Editorial/EventHandlers/TranslationRevisionDecidedNotificationsHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/EventHandlers/TranslationRevisionDecidedNotificationsHandlerTests.cs
@@ -1,11 +1,11 @@
 using _116.Content.Application.Editorial.EventHandlers;
-using _116.Content.Application.Shared.Messages;
+using _116.Content.Application.Shared.OutboundEmails;
 using _116.Content.Application.Shared.Repositories;
 using _116.Content.Domain.Entities;
 using _116.Content.Domain.Events;
 using _116.Identity.Contracts.Application.DTOs;
 using _116.Identity.Contracts.Application.Services;
-using _116.Mailer.Contracts.Application.Messages;
+using _116.Mailer.Contracts.Application.OutboundEmails;
 using _116.Mailer.Contracts.Application.Services;
 using _116.Mailer.Contracts.Domain.Enums;
 using _116.Tests.Fixtures.Factories.Content;
@@ -24,7 +24,7 @@ public class TranslationRevisionDecidedNotificationsHandlerTests
     private readonly Mock<IUserLookupService> _userLookupServiceMock = new();
     private readonly Mock<ITranslationRepository> _translationRepositoryMock;
     private readonly Mock<ILyricsRepository> _lyricsRepositoryMock;
-    private readonly Mock<IMessageDispatcher> _dispatcherMock = new();
+    private readonly Mock<IEmailDispatcher> _dispatcherMock = new();
     private readonly Mock<INotificationService> _notifierMock = new();
     private readonly TranslationRevisionDecidedNotificationsHandler _handler;
     private readonly LyricsEntity _lyrics;
@@ -70,8 +70,8 @@ public class TranslationRevisionDecidedNotificationsHandlerTests
         _dispatcherMock.Verify(
             x =>
                 x.DispatchAsync(
-                    It.Is<Message>(m =>
-                        m.TemplateName == ContentMessageTemplates.RevisionDecided
+                    It.Is<OutboundEmail>(m =>
+                        m.TemplateName == ContentEmailTemplates.RevisionDecided
                         && m.Recipients[0].Address == "proposer@test.com"
                         && m.Tokens["userName"] == "Fally"
                         && m.Tokens["songTitle"] == _lyrics.SongTitle
