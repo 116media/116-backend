e8e7494005d75f2a479da89b646a8e1e08906b36
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-25 20:03:31 +0200

    refactor(newsletter): rename it to NewsletterWelcomeEmail

--- src/Modules/Mailer/Mailer/Application/Newsletter/OutboundEmails/NewsletterWelcomeEmail.cs
+++ src/Modules/Mailer/Mailer/Application/Newsletter/OutboundEmails/NewsletterWelcomeEmail.cs
@@ -1,6 +1,6 @@
-using _116.Mailer.Contracts.Application.Messages;
+using _116.Mailer.Contracts.Application.OutboundEmails;
 
-namespace _116.Mailer.Application.Newsletter.Messages;
+namespace _116.Mailer.Application.Newsletter.OutboundEmails;
 
 /// <summary>
 /// The first mail a confirmed subscriber receives. Subscription-class, so it is suppressed
@@ -8,16 +8,16 @@ namespace _116.Mailer.Application.Newsletter.Messages;
 /// </summary>
 /// <param name="Subscriber">The address that confirmed.</param>
 /// <param name="UnsubscribeUrl">The one-click opt-out link.</param>
-public record NewsletterWelcomeMessage(MessageRecipient Subscriber, string UnsubscribeUrl) : Message
+public record NewsletterWelcomeEmail(EmailRecipient Subscriber, string UnsubscribeUrl) : OutboundEmail
 {
     /// <inheritdoc />
-    public override EnumMessageClass Class => EnumMessageClass.Subscription;
+    public override EnumEmailClass Class => EnumEmailClass.Subscription;
 
     /// <inheritdoc />
-    public override string TemplateName => NewsletterMessageTemplates.NewsletterWelcome;
+    public override string TemplateName => NewsletterEmailTemplates.NewsletterWelcome;
 
     /// <inheritdoc />
-    public override IReadOnlyList<MessageRecipient> Recipients => [Subscriber];
+    public override IReadOnlyList<EmailRecipient> Recipients => [Subscriber];
 
     /// <inheritdoc />
     public override IReadOnlyDictionary<string, string> Tokens =>
