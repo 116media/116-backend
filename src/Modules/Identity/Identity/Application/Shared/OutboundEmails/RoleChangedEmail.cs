b967477d4a1f2d893e5f6e2c826c512240b6da90
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-25 19:26:44 +0200

    refactor(identity): rename RoleChangedMessage to RoleChangedEmail

--- src/Modules/Identity/Identity/Application/Shared/OutboundEmails/RoleChangedEmail.cs
+++ src/Modules/Identity/Identity/Application/Shared/OutboundEmails/RoleChangedEmail.cs
@@ -1,6 +1,6 @@
-using _116.Mailer.Contracts.Application.Messages;
+using _116.Mailer.Contracts.Application.OutboundEmails;
 
-namespace _116.Identity.Application.Shared.Messages;
+namespace _116.Identity.Application.Shared.OutboundEmails;
 
 /// <summary>
 /// A role was granted to or revoked from an account; the action token carries which.
@@ -8,16 +8,16 @@ namespace _116.Identity.Application.Shared.Messages;
 /// <param name="User">The account whose roles changed.</param>
 /// <param name="RoleName">The role that was granted or revoked.</param>
 /// <param name="Action">Either "granted" or "revoked".</param>
-public record RoleChangedMessage(MessageRecipient User, string RoleName, string Action) : Message
+public record RoleChangedEmail(EmailRecipient User, string RoleName, string Action) : OutboundEmail
 {
     /// <inheritdoc />
-    public override EnumMessageClass Class => EnumMessageClass.Transactional;
+    public override EnumEmailClass Class => EnumEmailClass.Transactional;
 
     /// <inheritdoc />
-    public override string TemplateName => IdentityMessageTemplates.RoleChanged;
+    public override string TemplateName => IdentityEmailTemplates.RoleChanged;
 
     /// <inheritdoc />
-    public override IReadOnlyList<MessageRecipient> Recipients => [User];
+    public override IReadOnlyList<EmailRecipient> Recipients => [User];
 
     /// <inheritdoc />
     public override IReadOnlyDictionary<string, string> Tokens =>
