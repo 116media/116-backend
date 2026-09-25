e8f552ad73b4115e27719caa9be3048a34d487e5
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-25 16:57:41 +0200

    test(shared): expect the neutral resx culture by default

--- tests/Unit/Shared/Application/Extensions/LocalizationExtensionTests.cs
+++ tests/Unit/Shared/Application/Extensions/LocalizationExtensionTests.cs
@@ -51,7 +51,7 @@ public class LocalizationExtensionTests
     }
 
     [Fact]
-    public void AddAppLocalization_ShouldConfigureDefaultCultureToFrench()
+    public void AddAppLocalization_ShouldDefaultToTheCultureOfTheNeutralResources()
     {
         // Arrange
         var services = new ServiceCollection();
@@ -62,11 +62,11 @@ public class LocalizationExtensionTests
         var options = provider.GetRequiredService<IOptions<RequestLocalizationOptions>>();
 
         // Assert
-        options.Value.DefaultRequestCulture.Culture.Name.Should().Be("fr");
+        options.Value.DefaultRequestCulture.Culture.Name.Should().Be("en");
     }
 
     [Fact]
-    public void AddAppLocalization_ShouldConfigureDefaultUICultureToFrench()
+    public void AddAppLocalization_ShouldDefaultTheUICultureToTheNeutralResources()
     {
         // Arrange
         var services = new ServiceCollection();
@@ -77,7 +77,7 @@ public class LocalizationExtensionTests
         var options = provider.GetRequiredService<IOptions<RequestLocalizationOptions>>();
 
         // Assert
-        options.Value.DefaultRequestCulture.UICulture.Name.Should().Be("fr");
+        options.Value.DefaultRequestCulture.UICulture.Name.Should().Be("en");
     }
 
     [Fact]
@@ -156,11 +156,11 @@ public class LocalizationExtensionTests
         var options = provider.GetRequiredService<IOptions<RequestLocalizationOptions>>();
 
         // Assert
-        options.Value.RequestCultureProviders.Should().ContainSingle();
+        options.Value.RequestCultureProviders.Should().HaveCount(3);
     }
 
     [Fact]
-    public void AddAppLocalization_ShouldUseAcceptLanguageHeaderProvider()
+    public void AddAppLocalization_ShouldOfferQueryThenCookieThenHeader()
     {
         // Arrange
         var services = new ServiceCollection();
@@ -171,7 +171,14 @@ public class LocalizationExtensionTests
         var options = provider.GetRequiredService<IOptions<RequestLocalizationOptions>>();
 
         // Assert
-        options.Value.RequestCultureProviders.First().Should().BeOfType<AcceptLanguageHeaderRequestCultureProvider>();
+        options
+            .Value.RequestCultureProviders.Select(provider => provider.GetType())
+            .Should()
+            .Equal(
+                typeof(QueryStringRequestCultureProvider),
+                typeof(CookieRequestCultureProvider),
+                typeof(AcceptLanguageHeaderRequestCultureProvider)
+            );
     }
 
     [Fact]
