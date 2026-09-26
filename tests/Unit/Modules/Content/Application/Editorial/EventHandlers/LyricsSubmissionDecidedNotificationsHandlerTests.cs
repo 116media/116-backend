f6856d90ef698b76b4186dce406f74c8a7c9c972
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-26 05:41:27 +0200

    test(lyrics-submission-decided): pass the submitter's locale

--- tests/Unit/Modules/Content/Application/Editorial/EventHandlers/LyricsSubmissionDecidedNotificationsHandlerTests.cs
+++ tests/Unit/Modules/Content/Application/Editorial/EventHandlers/LyricsSubmissionDecidedNotificationsHandlerTests.cs
@@ -1,3 +1,4 @@
+using _116.BuildingBlocks.Constants;
 using _116.Content.Application.Editorial.EventHandlers;
 using _116.Content.Application.Shared.OutboundEmails;
 using _116.Content.Application.Shared.Repositories;
@@ -241,6 +242,6 @@ public class LyricsSubmissionDecidedNotificationsHandlerTests
     {
         _userLookupServiceMock
             .Setup(x => x.GetAuthorInfoByIdAsync(userId, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new AuthorDto("Fally", email, null, "Visitor"));
+            .ReturnsAsync(new AuthorDto("Fally", email, null, "Visitor", UserConstants.DefaultLocale));
     }
 }
