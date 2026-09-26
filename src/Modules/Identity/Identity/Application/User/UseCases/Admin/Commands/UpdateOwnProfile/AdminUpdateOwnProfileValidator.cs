a8e63922fb5cfeba9375e4e3a064ac45d158e1d4
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-26 20:44:30 +0200

    feat(admin-update-own-profile): validate the preferred locale

--- src/Modules/Identity/Identity/Application/User/UseCases/Admin/Commands/UpdateOwnProfile/AdminUpdateOwnProfileValidator.cs
+++ src/Modules/Identity/Identity/Application/User/UseCases/Admin/Commands/UpdateOwnProfile/AdminUpdateOwnProfileValidator.cs
@@ -30,5 +30,6 @@ public class AdminUpdateOwnProfileValidator : AbstractValidator<AdminUpdateOwnPr
         RuleFor(x => x.CountryIsoCode).ValidCountryIsoCode(i18n.User.Validation);
         RuleFor(x => x.CountryDialCode).ValidCountryDialCode(i18n.User.Validation);
         RuleFor(x => x.PartialPhoneNumber).ValidPartialPhoneNumber(i18n.User.Validation);
+        RuleFor(x => x.PreferredLocale).ValidPreferredLocale(i18n.User.Validation);
     }
 }
