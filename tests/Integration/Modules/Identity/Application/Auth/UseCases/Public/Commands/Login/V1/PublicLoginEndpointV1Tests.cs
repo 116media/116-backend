1ac7e3f7e63f615b04df870f71e8184cb072151a
Mutombo Jean-vincent <sigmacool@gmail.com>
2026-09-26 03:23:18 +0200

    test(login): drop the now redundant french culture argument

--- tests/Integration/Modules/Identity/Application/Auth/UseCases/Public/Commands/Login/V1/PublicLoginEndpointV1Tests.cs
+++ tests/Integration/Modules/Identity/Application/Auth/UseCases/Public/Commands/Login/V1/PublicLoginEndpointV1Tests.cs
@@ -361,7 +361,7 @@ public class PublicLoginEndpointV1Tests(PostgresFixture db) : BaseApiTest(db)
 
         await response.ShouldBeProblem<AuthenticationException>(
             HttpStatusCode.Unauthorized,
-            Localized<AuthenticationErrorMessage>(m => m.InvalidCredentials(), culture: "fr")
+            Localized<AuthenticationErrorMessage>(m => m.InvalidCredentials())
         );
 
         ProblemDetails problem = await response.ReadAsAsync<ProblemDetails>();
